46a2cea8ee786661de4f93f514bc01b5c6a29e73
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-05 14:43:53 +0200

    DEV-2234: Adjust datum format in medication

--- actin-common/src/main/java/com/hartwig/actin/clinical/feed/medication/MedicationEntryCreator.java
+++ actin-common/src/main/java/com/hartwig/actin/clinical/feed/medication/MedicationEntryCreator.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class MedicationEntryCreator implements FeedEntryCreator<MedicationEntry> {
 
-    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("d-M-yyyy HH:mm");
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
     public MedicationEntryCreator() {
     }
@@ -38,15 +38,24 @@ public class MedicationEntryCreator implements FeedEntryCreator<MedicationEntry>
                         "dosageInstruction_period_between_dosages_value")]))
                 .dosageInstructionText(parts[fieldIndexMap.get("dosageInstruction_text")])
                 .status(parts[fieldIndexMap.get("status")])
-                .active(parts[fieldIndexMap.get("actief")])
+                .active(parts[fieldIndexMap.get("active")])
                 .dosageDoseValue(parts[fieldIndexMap.get("dosage_dose_value")])
                 .dosageRateQuantityUnit(parts[fieldIndexMap.get("dosage_rateQuantity_unit")])
                 .dosageDoseUnitDisplayOriginal(parts[fieldIndexMap.get("dosage_dose_unit_display_original")])
-                .periodOfUseValuePeriodStart(FeedUtil.parseDate(parts[fieldIndexMap.get("periodOfUse_valuePeriod_start")], FORMAT))
-                .periodOfUseValuePeriodEnd(FeedUtil.parseOptionalDate(parts[fieldIndexMap.get("periodOfUse_valuePeriod_end")], FORMAT))
+                .periodOfUseValuePeriodStart(FeedUtil.parseDate(transform(parts[fieldIndexMap.get("periodOfUse_valuePeriod_start")]),
+                        FORMAT))
+                .periodOfUseValuePeriodEnd(FeedUtil.parseOptionalDate(transform(parts[fieldIndexMap.get("periodOfUse_valuePeriod_end")]),
+                        FORMAT))
                 .stopTypeDisplay(parts[fieldIndexMap.get("stopType_display")])
                 .categoryMedicationRequestCategoryDisplay(parts[fieldIndexMap.get("category_medicationRequestCategory_display")])
                 .categoryMedicationRequestCategoryCodeOriginal(parts[fieldIndexMap.get("category_medicationRequestCategory_code_original")])
                 .build();
     }
+
+    @NotNull
+    private static String transform(@NotNull String date) {
+        // This date contains microseconds so need to remove before parsing.
+        return date.substring(0, date.length() - 4);
+    }
+
 }
