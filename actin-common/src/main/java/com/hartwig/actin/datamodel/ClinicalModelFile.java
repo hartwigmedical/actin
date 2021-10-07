5179fa8258358eec14ed66fce67bc765ea190386
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-07 12:06:41 +0200

    DEV-2234: Small start with deserialization of clinical model

--- actin-common/src/main/java/com/hartwig/actin/datamodel/ClinicalModelFile.java
+++ actin-common/src/main/java/com/hartwig/actin/datamodel/ClinicalModelFile.java
@@ -1,14 +1,21 @@
 package com.hartwig.actin.datamodel;
 
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.stream.JsonReader;
+import com.hartwig.actin.datamodel.clinical.ClinicalRecord;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +25,7 @@ public final class ClinicalModelFile {
     }
 
     public static void write(@NotNull ClinicalModel model, @NotNull String clinicalModelJson) throws IOException {
-        String json = new GsonBuilder().serializeNulls().serializeSpecialFloatingPointValues().create().toJson(model);
+        String json = new GsonBuilder().serializeNulls().create().toJson(model);
         BufferedWriter writer = new BufferedWriter(new FileWriter(clinicalModelJson));
 
         writer.write(json);
@@ -26,8 +33,20 @@ public final class ClinicalModelFile {
     }
 
     @NotNull
-    public static ClinicalModel read(@NotNull String clinicalModelJson) throws FileNotFoundException {
-        JsonReader reader = new JsonReader(new FileReader(clinicalModelJson));
-        return new Gson().fromJson(reader, ClinicalModel.class);
+    public static ClinicalModel read(@NotNull String clinicalModelJson) throws IOException {
+        Gson gson = new GsonBuilder().registerTypeAdapter(ClinicalRecord.class, new ClinicalModelCreator()).create();
+
+        return gson.fromJson(new JsonReader(new FileReader(clinicalModelJson)), ClinicalModel.class);
+    }
+
+    private static class ClinicalModelCreator implements JsonDeserializer<ClinicalModel> {
+
+        @Override
+        public ClinicalModel deserialize(@NotNull JsonElement jsonElement, @NotNull Type type,
+                @NotNull JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            List<ClinicalRecord> records = Lists.newArrayList();
+
+            return new ClinicalModel(records);
+        }
     }
 }
--- actin-common/src/test/java/com/hartwig/actin/datamodel/ClinicalModelFileTest.java
+++ actin-common/src/test/java/com/hartwig/actin/datamodel/ClinicalModelFileTest.java
@@ -2,11 +2,10 @@ package com.hartwig.actin.datamodel;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.google.common.io.Resources;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ClinicalModelFileTest {
@@ -14,9 +13,7 @@ public class ClinicalModelFileTest {
     private static final String CLINICAL_MODEL_JSON = Resources.getResource("clinical/clinical_model_example.json").getPath();
 
     @Test
-    @Ignore
-    public void canReadClinicalModelFile() throws FileNotFoundException {
+    public void canReadClinicalModelFile() throws IOException {
         assertNotNull(ClinicalModelFile.read(CLINICAL_MODEL_JSON));
     }
-
 }
