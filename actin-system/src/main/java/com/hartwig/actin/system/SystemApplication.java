0c4dd3e4f745729985cbf61b17f0df9700293335
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-08 13:56:20 +0200

    DEV-2234: Add runnable classes to ACTIN system application

--- actin-system/src/main/java/com/hartwig/actin/system/SystemApplication.java
+++ actin-system/src/main/java/com/hartwig/actin/system/SystemApplication.java
@@ -1,5 +1,10 @@
 package com.hartwig.actin.system;
 
+import com.hartwig.actin.algo.AlgoRunner;
+import com.hartwig.actin.clinical.ClinicalIngestionApplication;
+import com.hartwig.actin.database.DatabaseLoaderApplication;
+import com.hartwig.actin.report.ReportApplication;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +14,10 @@ public class SystemApplication {
     private static final Logger LOGGER = LogManager.getLogger(SystemApplication.class);
 
     public static void main(@NotNull String... args) {
-        LOGGER.info("Hello world");
+        LOGGER.info("The following applications are available through ACTIN");
+        LOGGER.info(" {}", ClinicalIngestionApplication.class);
+        LOGGER.info(" {}", AlgoRunner.class);
+        LOGGER.info(" {}", DatabaseLoaderApplication.class);
+        LOGGER.info(" {}", ReportApplication.class);
     }
 }
