3a997602f222b255698f9cc4d181058281302ccb
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-15 09:49:16 +0200

    DEV-2234: Add test for production writing

--- report/src/test/java/com/hartwig/actin/report/pdf/ReportWriterTest.java
+++ report/src/test/java/com/hartwig/actin/report/pdf/ReportWriterTest.java
@@ -1,5 +1,6 @@
 package com.hartwig.actin.report.pdf;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.hartwig.actin.datamodel.ActinAnalysis;
@@ -9,6 +10,9 @@ import org.junit.Test;
 
 public class ReportWriterTest {
 
+    private static final boolean RUN_PDF_WRITING_TEST = false;
+    private static final String OUTPUT_DIRECTORY = System.getProperty("user.home") + File.separator + "hmf" + File.separator + "tmp";
+
     @Test
     public void canGenerateInMemoryReport() throws IOException {
         ActinAnalysis analysis = TestDataFactory.createTestActinAnalysis();
@@ -16,5 +20,16 @@ public class ReportWriterTest {
         ReportWriter memoryWriter = ReportWriterFactory.createInMemoryReportWriter();
 
         memoryWriter.write(analysis);
+    }
+
+    @Test
+    public void generateProductionReport() throws IOException {
+        if (RUN_PDF_WRITING_TEST) {
+            ActinAnalysis analysis = TestDataFactory.createTestActinAnalysis();
+
+            ReportWriter writer = ReportWriterFactory.createProductionReportWriter(OUTPUT_DIRECTORY);
+
+            writer.write(analysis);
+        }
     }
 }
