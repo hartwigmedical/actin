d14344f3b706ee2c89d8f813176204da4c418e30
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-18 15:35:09 +0200

    DEV-2234: Handle blood pressures without value

--- clinical/src/main/java/com/hartwig/actin/clinical/feed/bloodpressure/BloodPressureEntryCreator.java
+++ clinical/src/main/java/com/hartwig/actin/clinical/feed/bloodpressure/BloodPressureEntryCreator.java
@@ -5,9 +5,13 @@ import java.util.Map;
 import com.hartwig.actin.clinical.feed.FeedEntryCreator;
 import com.hartwig.actin.clinical.feed.FeedUtil;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class BloodPressureEntryCreator implements FeedEntryCreator<BloodPressureEntry> {
+
+    private static final Logger LOGGER = LogManager.getLogger(BloodPressureEntryCreator.class);
 
     public BloodPressureEntryCreator() {
     }
@@ -15,6 +19,15 @@ public class BloodPressureEntryCreator implements FeedEntryCreator<BloodPressure
     @NotNull
     @Override
     public BloodPressureEntry fromParts(@NotNull Map<String, Integer> fieldIndexMap, @NotNull String[] parts) {
+        // TODO: In latest blood pressure data, there are entries with no value. Sort out how to deal with them properly!
+        String componentValueQuantityValue = parts[fieldIndexMap.get("component_valueQuantity_value")];
+        double value = 0D;
+        if (!componentValueQuantityValue.isEmpty()) {
+            value = FeedUtil.parseDouble(componentValueQuantityValue);
+        } else {
+            LOGGER.warn("Empty value found for blood pressure with code '{}'", parts[fieldIndexMap.get("component_code_display")]);
+        }
+
         return ImmutableBloodPressureEntry.builder()
                 .subject(parts[fieldIndexMap.get("subject")])
                 .effectiveDateTime(FeedUtil.parseDate(parts[fieldIndexMap.get("effectiveDateTime")]))
@@ -25,7 +38,7 @@ public class BloodPressureEntryCreator implements FeedEntryCreator<BloodPressure
                 .componentCodeCode(parts[fieldIndexMap.get("component_code_code")])
                 .componentCodeDisplay(parts[fieldIndexMap.get("component_code_display")])
                 .componentValueQuantityCode(parts[fieldIndexMap.get("component_valueQuantity_code")])
-                .componentValueQuantityValue(Double.parseDouble(parts[fieldIndexMap.get("component_valueQuantity_value")]))
+                .componentValueQuantityValue(value)
                 .build();
     }
 }
