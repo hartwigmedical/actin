cc07dd9bbee154bcab17642994296b9236acc953
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-19 10:25:55 +0200

    DEV-2234: Add initial lab table

--- report/src/main/java/com/hartwig/actin/report/pdf/tables/LaboratoryTableGenerator.java
+++ report/src/main/java/com/hartwig/actin/report/pdf/tables/LaboratoryTableGenerator.java
@@ -1,6 +1,7 @@
 package com.hartwig.actin.report.pdf.tables;
 
 import com.hartwig.actin.datamodel.clinical.ClinicalRecord;
+import com.hartwig.actin.report.pdf.util.Cells;
 import com.hartwig.actin.report.pdf.util.Tables;
 import com.itextpdf.layout.element.Table;
 
@@ -28,7 +29,54 @@ public class LaboratoryTableGenerator implements TableGenerator {
     @NotNull
     @Override
     public Table contents() {
-        Table table = Tables.createFixedWidthCols(new float[] { keyWidth, valueWidth });
+        float key1Width = keyWidth / 2;
+        float key2Width = keyWidth - key1Width;
+
+        Table table = Tables.createFixedWidthCols(new float[] { key1Width, key2Width, valueWidth });
+
+        table.addCell(Cells.createKey("Liver function"));
+        table.addCell(Cells.createKey("Total bilirubin (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createKey("ASAT (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createKey("ALAT (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createKey("ALP (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createKey("Albumin"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createKey("Kidney function"));
+        table.addCell(Cells.createKey("Creatinine (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createKey("CKD-EPI eGFR (ref range + unit)"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createKey("Hemoglobin"));
+        table.addCell(Cells.createKey("ref range + unit"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createKey("Thrombocytes"));
+        table.addCell(Cells.createKey("ref range + unit"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createKey("PT"));
+        table.addCell(Cells.createKey("ref range + unit"));
+        table.addCell(Cells.createValue("some value"));
+
+        table.addCell(Cells.createKey("INR"));
+        table.addCell(Cells.createEmpty());
+        table.addCell(Cells.createValue("some value"));
 
         return table;
     }
