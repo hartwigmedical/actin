3a21740d533547641e01d370a168be70fb237091
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-19 11:30:23 +0200

    DEV-2234: Add test for minimal json

--- common/src/test/java/com/hartwig/actin/serialization/MolecularRecordJsonTest.java
+++ common/src/test/java/com/hartwig/actin/serialization/MolecularRecordJsonTest.java
@@ -13,11 +13,12 @@ import org.junit.Test;
 
 public class MolecularRecordJsonTest {
 
-    private static final String MOLECULAR_JSON = Resources.getResource("molecular/sample.orange.json").getPath();
+    private static final String ORANGE_MOLECULAR_JSON = Resources.getResource("molecular/sample.orange.json").getPath();
+    private static final String MINIMAL_MOLECULAR_JSON = Resources.getResource("molecular/sample.minimal.json").getPath();
 
     @Test
-    public void canReadMolecularRecordJson() throws IOException {
-        MolecularRecord record = MolecularRecordJson.read(MOLECULAR_JSON);
+    public void canReadOrangeMolecularRecordJson() throws IOException {
+        MolecularRecord record = MolecularRecordJson.read(ORANGE_MOLECULAR_JSON);
 
         assertEquals(TestDataFactory.TEST_SAMPLE, record.sampleId());
         assertTrue(record.hasReliableQuality());
@@ -25,5 +26,16 @@ public class MolecularRecordJsonTest {
         assertEquals(25, record.genomicTreatmentEvidences().size());
 
         assertEquals(2, record.actionableGenomicEvents().size());
+    }
+
+    @Test
+    public void canReadMinimalMolecularRecordJson() throws IOException {
+        MolecularRecord record = MolecularRecordJson.read(MINIMAL_MOLECULAR_JSON);
+
+        assertEquals(TestDataFactory.TEST_SAMPLE, record.sampleId());
+        assertTrue(record.hasReliableQuality());
+        assertTrue(record.hasReliablePurity());
+        assertTrue(record.genomicTreatmentEvidences().isEmpty());
+        assertTrue(record.actionableGenomicEvents().isEmpty());
     }
 }
