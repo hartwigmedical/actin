70bb97a19007e999e911f163c03b7bdd4cd33d97
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-10-21 11:45:47 +0200

    DEV-2234: Remove code duplication

--- clinical/src/test/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireReaderTest.java
+++ clinical/src/test/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireReaderTest.java
@@ -3,35 +3,35 @@ package com.hartwig.actin.clinical.feed.questionnaire;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 public class QuestionnaireReaderTest {
 
     @Test
     public void canReadQuestionnaire() {
-        QuestionnaireEntry entry1 = ImmutableQuestionnaireEntry.builder()
-                .from(TestQuestionnaireFactory.createTestQuestionnaireEntry())
-                .itemAnswerValueValueString("this \n is \n a \n questionnaire")
-                .build();
+        QuestionnaireEntry entry1 = createWithQuestionnaire("this \n is \n a \n questionnaire");
 
         assertEquals(4, QuestionnaireReader.read(entry1).length);
 
-        QuestionnaireEntry entry2 = ImmutableQuestionnaireEntry.builder()
-                .from(TestQuestionnaireFactory.createTestQuestionnaireEntry())
-                .itemAnswerValueValueString(QuestionnaireReader.TERMS_TO_CLEAN.iterator().next() + "\n test")
-                .build();
+        QuestionnaireEntry entry2 = createWithQuestionnaire(QuestionnaireReader.TERMS_TO_CLEAN.iterator().next() + "\n test");
 
         String[] lines = QuestionnaireReader.read(entry2);
         assertEquals(2, lines.length);
         assertEquals(Strings.EMPTY, lines[0]);
 
-        QuestionnaireEntry entry3 = ImmutableQuestionnaireEntry.builder()
-                .from(TestQuestionnaireFactory.createTestQuestionnaireEntry())
-                .itemAnswerValueValueString("value1: x\nand y\nvalue2: z\n\nheader\nvalue3: 5\nvalue4: 6\n7")
-                .build();
+        QuestionnaireEntry entry3 = createWithQuestionnaire("value1: x\nand y\nvalue2: z\n\nheader\nvalue3: 5\nvalue4: 6\n7");
 
         lines = QuestionnaireReader.read(entry3);
         assertEquals(6, lines.length);
         assertEquals("value4: 6,7", lines[5]);
+    }
+
+    @NotNull
+    private static QuestionnaireEntry createWithQuestionnaire(@NotNull String questionnaire) {
+        return ImmutableQuestionnaireEntry.builder()
+                .from(TestQuestionnaireFactory.createTestQuestionnaireEntry())
+                .itemAnswerValueValueString(questionnaire)
+                .build();
     }
 }
