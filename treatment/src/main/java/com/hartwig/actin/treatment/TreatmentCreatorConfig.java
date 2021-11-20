602bb4d33f3d28c80300d984e351cd42f2ec5c6e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-11-20 17:24:11 +0100

    DEV-2234: Add option for enabling debug logging

--- treatment/src/main/java/com/hartwig/actin/treatment/TreatmentCreatorConfig.java
+++ treatment/src/main/java/com/hartwig/actin/treatment/TreatmentCreatorConfig.java
@@ -5,6 +5,10 @@ import com.hartwig.actin.util.ApplicationConfig;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,9 +17,13 @@ import org.jetbrains.annotations.Nullable;
 @Value.Style(passAnnotations = { NotNull.class, Nullable.class })
 public interface TreatmentCreatorConfig {
 
+    Logger LOGGER = LogManager.getLogger(TreatmentCreatorConfig.class);
+
     String TRIAL_CONFIG_DIRECTORY = "trial_config_directory";
 
     String OUTPUT_DIRECTORY = "output_directory";
+
+    String LOG_DEBUG = "log_debug";
 
     @NotNull
     static Options createOptions() {
@@ -24,6 +32,8 @@ public interface TreatmentCreatorConfig {
         options.addOption(TRIAL_CONFIG_DIRECTORY, true, "Directory containing the trial config files");
 
         options.addOption(OUTPUT_DIRECTORY, true, "Directory where treatment data will be written to");
+
+        options.addOption(LOG_DEBUG, false, "If set, debug logging gets enabled");
 
         return options;
     }
@@ -36,7 +46,13 @@ public interface TreatmentCreatorConfig {
 
     @NotNull
     static TreatmentCreatorConfig createConfig(@NotNull CommandLine cmd) throws ParseException {
-        return ImmutableTreatmentCreatorConfig.builder().trialConfigDirectory(ApplicationConfig.nonOptionalDir(cmd, TRIAL_CONFIG_DIRECTORY))
+        if (cmd.hasOption(LOG_DEBUG)) {
+            Configurator.setRootLevel(Level.DEBUG);
+            LOGGER.debug("Switched root level logging to DEBUG");
+        }
+
+        return ImmutableTreatmentCreatorConfig.builder()
+                .trialConfigDirectory(ApplicationConfig.nonOptionalDir(cmd, TRIAL_CONFIG_DIRECTORY))
                 .outputDirectory(ApplicationConfig.nonOptionalDir(cmd, OUTPUT_DIRECTORY))
                 .build();
     }
