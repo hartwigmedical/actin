7fb5e456f5e2171b08d512d02bde7c489161b4e9
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-11-23 10:50:01 +0100

    DEV-2234: Update OR logic

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/composite/Or.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/composite/Or.java
@@ -28,10 +28,10 @@ public class Or implements EvaluationFunction {
 
         if (evaluations.contains(Evaluation.PASS)) {
             return Evaluation.PASS;
-        } else if (evaluations.contains(Evaluation.IGNORED)) {
-            return Evaluation.IGNORED;
         } else if (evaluations.contains(Evaluation.PASS_BUT_WARN)) {
             return Evaluation.PASS_BUT_WARN;
+        } else if (evaluations.contains(Evaluation.IGNORED)) {
+            return Evaluation.IGNORED;
         } else if (evaluations.contains(Evaluation.NOT_IMPLEMENTED)) {
             return Evaluation.NOT_IMPLEMENTED;
         } else if (evaluations.contains(Evaluation.UNDETERMINED)) {
