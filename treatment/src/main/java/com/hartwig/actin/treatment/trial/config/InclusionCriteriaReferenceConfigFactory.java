27cca3c9b605952a2e0036f2a4892d402ef86fcb
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-01-25 10:16:48 +0100

    DEV-2234: Improve error message for invalid criteria reference configs

--- treatment/src/main/java/com/hartwig/actin/treatment/trial/config/InclusionCriteriaReferenceConfigFactory.java
+++ treatment/src/main/java/com/hartwig/actin/treatment/trial/config/InclusionCriteriaReferenceConfigFactory.java
@@ -1,6 +1,7 @@
 package com.hartwig.actin.treatment.trial.config;
 
 import java.util.Map;
+import java.util.StringJoiner;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -9,10 +10,24 @@ public class InclusionCriteriaReferenceConfigFactory implements TrialConfigFacto
     @NotNull
     @Override
     public InclusionCriteriaReferenceConfig create(@NotNull Map<String, Integer> fields, @NotNull String[] parts) {
+        if (parts.length != 3) {
+            throw new IllegalArgumentException(
+                    "Invalid criteria reference config provided. Possibly due to hard line breaks? ('" + toString(parts) + "')");
+        }
+
         return ImmutableInclusionCriteriaReferenceConfig.builder()
                 .trialId(parts[fields.get("trialId")])
                 .referenceId(parts[fields.get("referenceId")])
                 .referenceText(parts[fields.get("referenceText")])
                 .build();
+    }
+
+    @NotNull
+    private static String toString(@NotNull String[] parts) {
+        StringJoiner joiner = new StringJoiner(" ");
+        for (String part : parts) {
+            joiner.add(part);
+        }
+        return joiner.toString().trim();
     }
 }
