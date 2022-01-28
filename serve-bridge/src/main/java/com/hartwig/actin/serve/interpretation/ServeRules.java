58dbd49a3b27e17e9b797cb578b89b03ed04d945
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-01-28 15:00:27 +0100

    DEV-2234: Merge

--- serve-bridge/src/main/java/com/hartwig/actin/serve/interpretation/ServeRules.java
+++ serve-bridge/src/main/java/com/hartwig/actin/serve/interpretation/ServeRules.java
@@ -13,13 +13,19 @@ public final class ServeRules {
 
     static {
         MOLECULAR_RULES.add(EligibilityRule.ACTIVATION_OR_AMPLIFICATION_OF_GENE_X);
-        MOLECULAR_RULES.add(EligibilityRule.ACTIVATING_MUTATION_IN_GENE_X);
-        MOLECULAR_RULES.add(EligibilityRule.FUSION_IN_GENE_X);
         MOLECULAR_RULES.add(EligibilityRule.INACTIVATION_OF_GENE_X);
+        MOLECULAR_RULES.add(EligibilityRule.ACTIVATING_MUTATION_IN_GENE_X);
         MOLECULAR_RULES.add(EligibilityRule.MUTATION_IN_GENE_X_OF_TYPE_Y);
-        MOLECULAR_RULES.add(EligibilityRule.SPECIFIC_FUSION_OF_X_TO_Y);
         MOLECULAR_RULES.add(EligibilityRule.AMPLIFICATION_OF_GENE_X);
         MOLECULAR_RULES.add(EligibilityRule.DELETION_OF_GENE_X);
+        MOLECULAR_RULES.add(EligibilityRule.FUSION_IN_GENE_X);
+        MOLECULAR_RULES.add(EligibilityRule.SPECIFIC_FUSION_OF_X_TO_Y);
+        MOLECULAR_RULES.add(EligibilityRule.WILDTYPE_OF_GENE_X);
+        MOLECULAR_RULES.add(EligibilityRule.MSI_SIGNATURE);
+        MOLECULAR_RULES.add(EligibilityRule.HRD_SIGNATURE);
+        MOLECULAR_RULES.add(EligibilityRule.TMB_OF_AT_LEAST_X);
+        MOLECULAR_RULES.add(EligibilityRule.TML_OF_AT_LEAST_X);
+        MOLECULAR_RULES.add(EligibilityRule.TML_OF_AT_MOST_X);
     }
 
     private ServeRules() {
