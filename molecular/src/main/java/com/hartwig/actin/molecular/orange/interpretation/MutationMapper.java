8c366873aa326d7ff6cbeb5c2300c5c7498e6974
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-02-07 17:25:28 +0100

    DEV-2234: Add TODO

--- molecular/src/main/java/com/hartwig/actin/molecular/orange/interpretation/MutationMapper.java
+++ molecular/src/main/java/com/hartwig/actin/molecular/orange/interpretation/MutationMapper.java
@@ -6,6 +6,8 @@ import org.jetbrains.annotations.NotNull;
 
 public interface MutationMapper {
 
+    // TODO: Change to a set of strings and map everything.
+
     @NotNull
     String map(@NotNull TreatmentEvidence evidence);
 }
