3e9576e0f737395aa9b5c9621f1a9f49b94c43b2
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-03-08 17:47:00 +0100

    DEV-2234: Improve complication messages

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/complication/HasSpecificComplication.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/complication/HasSpecificComplication.java
@@ -1,5 +1,9 @@
 package com.hartwig.actin.algo.evaluation.complication;
 
+import java.util.Set;
+import java.util.StringJoiner;
+
+import com.google.common.collect.Sets;
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
@@ -21,18 +25,31 @@ public class HasSpecificComplication implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
+        Set<String> complications = Sets.newHashSet();
         for (Complication complication : record.clinical().complications()) {
             if (complication.name().toLowerCase().contains(termToFind.toLowerCase())) {
-                return ImmutableEvaluation.builder()
-                        .result(EvaluationResult.PASS)
-                        .addPassMessages("Patient has complication " + termToFind)
-                        .build();
+                complications.add(complication.name());
             }
+        }
+
+        if (!complications.isEmpty()) {
+            return ImmutableEvaluation.builder().result(EvaluationResult.PASS)
+                    .addPassMessages("Patient has complication " + concat(complications))
+                    .build();
         }
 
         return ImmutableEvaluation.builder()
                 .result(EvaluationResult.FAIL)
                 .addFailMessages("Patient does not have complication " + termToFind)
                 .build();
+    }
+
+    @NotNull
+    private static String concat(@NotNull Set<String> strings) {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (String string : strings) {
+            joiner.add(string);
+        }
+        return joiner.toString();
     }
 }
