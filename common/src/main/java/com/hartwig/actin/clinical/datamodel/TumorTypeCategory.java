8ef99a6f879ed8e1324614a2c1ea50181f2e90da
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-03-14 15:14:16 +0100

    DEV-2234: Formatting

--- common/src/main/java/com/hartwig/actin/clinical/datamodel/TumorTypeCategory.java
+++ common/src/main/java/com/hartwig/actin/clinical/datamodel/TumorTypeCategory.java
@@ -4,14 +4,14 @@ import com.hartwig.actin.clinical.interpretation.TumorTypeCategoryResolver;
 
 import org.jetbrains.annotations.NotNull;
 
-    public enum TumorTypeCategory {
-        CARCINOMA,
-        ADENOCARCINOMA,
-        SQUAMOUS_CELL_CARCINOMA,
-        MELANOMA;
+public enum TumorTypeCategory {
+    CARCINOMA,
+    ADENOCARCINOMA,
+    SQUAMOUS_CELL_CARCINOMA,
+    MELANOMA;
 
-        @NotNull
-        public String display() {
-            return TumorTypeCategoryResolver.toString(this).toLowerCase();
-        }
+    @NotNull
+    public String display() {
+        return TumorTypeCategoryResolver.toString(this).toLowerCase();
+    }
 }
