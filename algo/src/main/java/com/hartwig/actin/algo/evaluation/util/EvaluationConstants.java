85426d1b8c74fd0842fb2ce16fe7b94b3a4d3bdd
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-03-16 17:30:11 +0100

    DEV-2234: Add way to run in the past

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/util/EvaluationConstants.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/util/EvaluationConstants.java
@@ -7,6 +7,8 @@ public final class EvaluationConstants {
     private EvaluationConstants() {
     }
 
+    // Uncomment below to test running on a specific date.
+//    public static final LocalDate REFERENCE_DATE = LocalDate.of(2021, 11, 16);
     public static final LocalDate REFERENCE_DATE = LocalDate.now();
     public static final int REFERENCE_YEAR = REFERENCE_DATE.getYear();
     public static final int REFERENCE_MONTH = REFERENCE_DATE.getMonthValue();
