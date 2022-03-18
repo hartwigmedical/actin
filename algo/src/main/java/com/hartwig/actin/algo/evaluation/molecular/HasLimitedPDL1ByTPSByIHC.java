400986279bccc88bf1195c058b6c75db8b82cf1a
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-03-18 16:36:00 +0100

    DEV-2234: Fix issue

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/HasLimitedPDL1ByTPSByIHC.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/HasLimitedPDL1ByTPSByIHC.java
@@ -4,6 +4,7 @@ import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.datamodel.ImmutableEvaluation;
+import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
 
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +18,7 @@ public class HasLimitedPDL1ByTPSByIHC implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
-        return ImmutableEvaluation.builder()
+        return EvaluationFactory.unrecoverable()
                 .result(EvaluationResult.UNDETERMINED)
                 .addUndeterminedSpecificMessages("PDL1 by TPS is currently undetermined")
                 .build();
