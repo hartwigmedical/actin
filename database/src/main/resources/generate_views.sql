08a6c8c0ec72662cbd50a983c4b6fa1025ab7ebb
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-03-18 15:07:30 +0100

    DEV-2234: Formatting of views

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -6,9 +6,9 @@ SELECT  referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId
 	    passGeneralMessages, warnSpecificMessages, warnGeneralMessages, undeterminedSpecificMessages, undeterminedGeneralMessages,
 	    failSpecificMessages, failGeneralMessages
 FROM evaluation
-INNER JOIN trialMatch ON trialMatch.id=evaluation.trialMatchId
-INNER JOIN treatmentMatch ON treatmentMatch.id=trialMatch.treatmentMatchId
-LEFT JOIN cohortMatch ON trialMatch.id=cohortMatch.trialMatchId AND cohortMatch.Id=evaluation.cohortMatchId
+INNER JOIN trialMatch ON trialMatch.id = evaluation.trialMatchId
+INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
+LEFT JOIN cohortMatch ON trialMatch.id = cohortMatch.trialMatchId AND cohortMatch.Id = evaluation.cohortMatchId
 );
 
 CREATE OR REPLACE VIEW criteriaMapping
@@ -17,8 +17,8 @@ SELECT  trial.code AS trialId, acronym AS trialAcronym, cohort.code AS cohortId,
         reference.code AS criterionCode, text AS criterionText, display AS eligibilityRule
 FROM eligibilityReference
 INNER JOIN (SELECT DISTINCT id, trialId, cohortId, display FROM eligibility) AS a ON a.id = eligibilityReference.eligibilityId
-INNER JOIN reference ON eligibilityReference.referenceId=reference.Id
-INNER JOIN trial ON trial.id=a.trialId
-LEFT JOIN cohort ON cohort.id=a.cohortId
+INNER JOIN reference ON eligibilityReference.referenceId = reference.Id
+INNER JOIN trial ON trial.id = a.trialId
+LEFT JOIN cohort ON cohort.id = a.cohortId
 ORDER BY trialId
 );
