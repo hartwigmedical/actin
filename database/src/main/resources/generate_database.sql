87abf477720fa0e53af4b30039d3f81aa4c4f30f
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-03-23 10:40:41 +0100

    Revert change to sql

--- database/src/main/resources/generate_database.sql
+++ database/src/main/resources/generate_database.sql
@@ -3,6 +3,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 -- TODO Can be removed per 25th of March 2022
 DROP TABLE IF EXISTS eligibilityReferences;
+-- TODO Can be removed per April 1st 2022
+DROP TABLE IF EXISTS allergy;
 
 -- CLINICAL
 DROP TABLE IF EXISTS patient;
@@ -155,13 +157,14 @@ CREATE TABLE toxicity
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS allergy;
-CREATE TABLE allergy
+DROP TABLE IF EXISTS intolerance;
+CREATE TABLE intolerance
 (   id int NOT NULL AUTO_INCREMENT,
     sampleId varchar(50) NOT NULL,
     name varchar(50) NOT NULL,
     doids varchar(50) NOT NULL,
     category varchar(50) NOT NULL,
+    type varchar(50) NOT NULL,
     clinicalStatus varchar(50) NOT NULL,
     verificationStatus varchar(50) NOT NULL,
     criticality varchar(50) NOT NULL,
