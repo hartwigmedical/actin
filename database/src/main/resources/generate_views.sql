1b4f8a2e2c7e4bc819293ca10e90d7c266965aef
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-03-30 15:49:00 +0200

    DEV-2234: Update view

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -1,13 +1,14 @@
 CREATE OR REPLACE VIEW trialEvaluation
 AS (
-SELECT  referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId, acronym AS trialAcronym,
-        trialMatch.isEligible AS isEligibleTrial, cohortMatch.code AS cohortId, description AS cohortDescription,
-	    open AS cohortOpen, cohortMatch.isEligible AS isEligibleCohort, eligibility AS eligibilityRule, result, passSpecificMessages,
-	    passGeneralMessages, warnSpecificMessages, warnGeneralMessages, undeterminedSpecificMessages, undeterminedGeneralMessages,
-	    failSpecificMessages, failGeneralMessages
+SELECT  referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId, trial.acronym AS trialAcronym,
+        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial, cohortMatch.code AS cohortId,
+        description AS cohortDescription, open AS cohortOpen, blacklist AS cohortBlacklist, cohortMatch.isEligible AS isEligibleCohort,
+        eligibility AS eligibilityRule, result, passSpecificMessages, passGeneralMessages, warnSpecificMessages, warnGeneralMessages,
+        undeterminedSpecificMessages, undeterminedGeneralMessages, failSpecificMessages, failGeneralMessages
 FROM evaluation
 INNER JOIN trialMatch ON trialMatch.id = evaluation.trialMatchId
 INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
+INNER JOIN trial ON trial.code = trialMatch.code
 LEFT JOIN cohortMatch ON trialMatch.id = cohortMatch.trialMatchId AND cohortMatch.Id = evaluation.cohortMatchId
 ORDER BY sampleId
 );
