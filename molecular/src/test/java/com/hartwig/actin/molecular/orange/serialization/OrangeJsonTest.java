29af05e5ad4f4f81cce59ad0722c3e1866b2d2ac
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-04-12 15:23:51 +0200

    Extend testing of ORANGE datamodel reading

--- molecular/src/test/java/com/hartwig/actin/molecular/orange/serialization/OrangeJsonTest.java
+++ molecular/src/test/java/com/hartwig/actin/molecular/orange/serialization/OrangeJsonTest.java
@@ -1,7 +1,9 @@
 package com.hartwig.actin.molecular.orange.serialization;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -12,11 +14,26 @@ import com.hartwig.actin.TestDataFactory;
 import com.hartwig.actin.molecular.orange.datamodel.OrangeRecord;
 import com.hartwig.actin.molecular.orange.datamodel.chord.ChordRecord;
 import com.hartwig.actin.molecular.orange.datamodel.cuppa.CuppaRecord;
+import com.hartwig.actin.molecular.orange.datamodel.linx.FusionLikelihood;
+import com.hartwig.actin.molecular.orange.datamodel.linx.FusionType;
 import com.hartwig.actin.molecular.orange.datamodel.linx.LinxRecord;
+import com.hartwig.actin.molecular.orange.datamodel.linx.ReportableDisruption;
+import com.hartwig.actin.molecular.orange.datamodel.linx.ReportableFusion;
+import com.hartwig.actin.molecular.orange.datamodel.peach.PeachEntry;
 import com.hartwig.actin.molecular.orange.datamodel.peach.PeachRecord;
+import com.hartwig.actin.molecular.orange.datamodel.protect.EvidenceDirection;
+import com.hartwig.actin.molecular.orange.datamodel.protect.EvidenceLevel;
+import com.hartwig.actin.molecular.orange.datamodel.protect.EvidenceType;
+import com.hartwig.actin.molecular.orange.datamodel.protect.ProtectEvidence;
 import com.hartwig.actin.molecular.orange.datamodel.protect.ProtectRecord;
+import com.hartwig.actin.molecular.orange.datamodel.purple.GainLossInterpretation;
 import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleRecord;
+import com.hartwig.actin.molecular.orange.datamodel.purple.ReportableGainLoss;
+import com.hartwig.actin.molecular.orange.datamodel.purple.ReportableVariant;
+import com.hartwig.actin.molecular.orange.datamodel.purple.VariantHotspot;
+import com.hartwig.actin.molecular.orange.datamodel.virus.VirusInterpreterEntry;
 import com.hartwig.actin.molecular.orange.datamodel.virus.VirusInterpreterRecord;
+import com.hartwig.actin.molecular.orange.datamodel.virus.VirusLikelihood;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
@@ -62,18 +79,70 @@ public class OrangeJsonTest {
         assertEquals(185, purple.tumorMutationalLoad());
 
         assertEquals(2, purple.variants().size());
+        ReportableVariant variant1 = findByGene(purple.variants(), "SF3B1");
+        assertEquals("p.Pro718Leu", variant1.hgvsProteinImpact());
+        assertEquals("c.2153C>T", variant1.hgvsCodingImpact());
+        assertEquals("missense_variant", variant1.effect());
+        assertEquals(2.03, variant1.alleleCopyNumber(), EPSILON);
+        assertEquals(3.02, variant1.totalCopyNumber(), EPSILON);
+        assertEquals(VariantHotspot.NON_HOTSPOT, variant1.hotspot());
+        assertFalse(variant1.biallelic());
+        assertEquals(0.15, variant1.driverLikelihood(), EPSILON);
+        assertEquals(1.0, variant1.clonalLikelihood(), EPSILON);
+
+        ReportableVariant variant2 = findByGene(purple.variants(), "BRCA1");
+        assertEquals("p.?", variant2.hgvsProteinImpact());
+        assertEquals("c.5340+1G>A", variant2.hgvsCodingImpact());
+        assertEquals("splice_donor_variant&intron_variant", variant2.effect());
+        assertEquals(1.0, variant2.alleleCopyNumber(), EPSILON);
+        assertEquals(2.0, variant2.totalCopyNumber(), EPSILON);
+        assertEquals(VariantHotspot.HOTSPOT, variant2.hotspot());
+        assertFalse(variant2.biallelic());
+        assertEquals(1.0, variant2.driverLikelihood(), EPSILON);
+        assertEquals(1.0, variant2.clonalLikelihood(), EPSILON);
 
         assertEquals(1, purple.gainsLosses().size());
+        ReportableGainLoss gainLoss = purple.gainsLosses().iterator().next();
+        assertEquals("SMAD4", gainLoss.gene());
+        assertEquals(GainLossInterpretation.FULL_LOSS, gainLoss.interpretation());
+        assertEquals(0, gainLoss.minCopies());
+    }
+
+    @NotNull
+    private static ReportableVariant findByGene(@NotNull Iterable<ReportableVariant> variants, @NotNull String geneToFind) {
+        for (ReportableVariant variant : variants) {
+            if (variant.gene().equals(geneToFind)) {
+                return variant;
+            }
+        }
+
+        throw new IllegalStateException("Could not find variant for gene: " + geneToFind);
     }
 
     private static void assertLinx(@NotNull LinxRecord linx) {
         assertEquals(1, linx.fusions().size());
+        ReportableFusion fusion = linx.fusions().iterator().next();
+        assertEquals(FusionType.KNOWN_PAIR, fusion.type());
+        assertEquals("TMPRSS2", fusion.geneStart());
+        assertEquals("Exon 1", fusion.geneContextStart());
+        assertEquals("ETV4", fusion.geneEnd());
+        assertEquals("Promoter Region", fusion.geneContextEnd());
+        assertEquals(FusionLikelihood.HIGH, fusion.likelihood());
+
         assertEquals(1, linx.homozygousDisruptedGenes().size());
+        assertEquals("NF1", linx.homozygousDisruptedGenes().iterator().next());
+
         assertEquals(1, linx.disruptions().size());
+        ReportableDisruption disruption = linx.disruptions().iterator().next();
+        assertEquals("NF1", disruption.gene());
+        assertEquals("Intron 5 -> Intron 8", disruption.range());
     }
 
     private static void assertPeach(@NotNull PeachRecord peach) {
         assertEquals(1, peach.entries().size());
+        PeachEntry entry = peach.entries().iterator().next();
+        assertEquals("DPYD", entry.gene());
+        assertEquals("*1_HOM", entry.haplotype());
     }
 
     private static void assertCuppa(@NotNull CuppaRecord cuppa) {
@@ -83,6 +152,10 @@ public class OrangeJsonTest {
 
     private static void assertVirusInterpreter(@NotNull VirusInterpreterRecord virusInterpreter) {
         assertEquals(1, virusInterpreter.entries().size());
+        VirusInterpreterEntry entry = virusInterpreter.entries().iterator().next();
+        assertEquals("Human betaherpesvirus 6B", entry.name());
+        assertEquals(1, entry.integrations());
+        assertEquals(VirusLikelihood.HIGH, entry.likelihood());
     }
 
     private static void assertChord(@NotNull ChordRecord chord) {
@@ -91,5 +164,16 @@ public class OrangeJsonTest {
 
     private static void assertProtect(@NotNull ProtectRecord protect) {
         assertEquals(1, protect.evidences().size());
+        ProtectEvidence evidence = protect.evidences().iterator().next();
+        assertTrue(evidence.reported());
+        assertEquals("BRAF", evidence.gene());
+        assertEquals("p.Val600Glu", evidence.event());
+        assertNull(evidence.rangeRank());
+        assertEquals("Cobimetinib + Vemurafenib", evidence.treatment());
+        assertTrue(evidence.onLabel());
+        assertEquals(EvidenceType.HOTSPOT_MUTATION, evidence.type());
+        assertEquals(EvidenceLevel.A, evidence.level());
+        assertEquals(EvidenceDirection.RESPONSIVE, evidence.direction());
+        assertTrue(evidence.sources().contains("VICC_CGI"));
     }
 }
