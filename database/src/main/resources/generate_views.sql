00e57987f624fb5245c4748feb3b85c215c6e3cf
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-04-29 15:16:01 +0200

    Update view

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -1,17 +1,29 @@
 CREATE OR REPLACE VIEW trialEvaluation
 AS (
-SELECT  referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId, trial.acronym AS trialAcronym,
+SELECT * FROM (SELECT  referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
         IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
-        cohortMatch.code AS cohortId, description AS cohortDescription, cohortMatch.open AS cohortOpen, blacklist AS cohortBlacklist,
+        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen, cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist,
         cohortMatch.isEligible AS isEligibleCohort,
-        eligibility AS eligibilityRule, result, passSpecificMessages, passGeneralMessages, warnSpecificMessages, warnGeneralMessages,
+        eligibility AS eligibilityRule, result, recoverable, passSpecificMessages, passGeneralMessages, warnSpecificMessages, warnGeneralMessages,
         undeterminedSpecificMessages, undeterminedGeneralMessages, failSpecificMessages, failGeneralMessages
 FROM evaluation
 INNER JOIN trialMatch ON trialMatch.id = evaluation.trialMatchId
 INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
 INNER JOIN trial ON trial.code = trialMatch.code
 LEFT JOIN cohortMatch ON trialMatch.id = cohortMatch.trialMatchId AND cohortMatch.Id = evaluation.cohortMatchId
-ORDER BY sampleId
+    UNION
+SELECT DISTINCT referenceDate, referenceDateIsLive, sampleId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
+        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
+        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen, cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist,
+        cohortMatch.isEligible AS isEligibleCohort,
+        NULL AS eligibilityRule, NULL AS result, NULL as recoverable, NULL AS passSpecificMessages, NULL AS passGeneralMessages, NULL AS warnSpecificMessages, NULL AS warnGeneralMessages,
+        NULL AS undeterminedSpecificMessages, NULL AS undeterminedGeneralMessages, NULL AS failSpecificMessages, NULL AS failGeneralMessages
+FROM cohortMatch
+INNER JOIN trialMatch ON trialMatch.id = cohortMatch.trialMatchId
+INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
+INNER JOIN trial ON trial.code = trialMatch.code
+WHERE cohortMatch.id NOT IN (SELECT DISTINCT cohortMatchId FROM evaluation WHERE NOT isnull(cohortMatchId))
+ORDER BY sampleId, cohortId) AS a
 );
 
 CREATE OR REPLACE VIEW criteriaMapping
