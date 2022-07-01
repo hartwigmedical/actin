e969b0b62a1f2a61af2ddcb611600f0fd221f088
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-07-01 09:30:59 +0200

    Start of implementation of MUGA or TTE

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/cardiacfunction/HasNormalCardiacFunctionByMUGAOrTTE.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/cardiacfunction/HasNormalCardiacFunctionByMUGAOrTTE.java
@@ -17,10 +17,17 @@ public class HasNormalCardiacFunctionByMUGAOrTTE implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
+        Double lvef = record.clinical().clinicalStatus().lvef();
+        if (lvef != null && lvef < 0.5) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.WARN)
+                    .addWarnSpecificMessages("LVEF of " + lvef + " exceeds 50%")
+                    .build();
+        }
+
         return EvaluationFactory.unrecoverable()
                 .result(EvaluationResult.UNDETERMINED)
                 .addUndeterminedSpecificMessages("Currently normal cardiac function by MUGA or TTE cannot be determined")
                 .build();
     }
-
 }
