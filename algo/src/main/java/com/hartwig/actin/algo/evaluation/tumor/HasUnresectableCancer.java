6949b79987e7b1af074467b0ff8bc3749b2bab81
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-07-03 14:19:03 +0200

    Implement unresectable cancer

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/tumor/HasUnresectableCancer.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/tumor/HasUnresectableCancer.java
@@ -3,12 +3,13 @@ package com.hartwig.actin.algo.evaluation.tumor;
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
+import com.hartwig.actin.algo.datamodel.ImmutableEvaluation;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
+import com.hartwig.actin.clinical.datamodel.TumorStage;
 
 import org.jetbrains.annotations.NotNull;
 
-//TODO: Implement according to README
 public class HasUnresectableCancer implements EvaluationFunction {
 
     HasUnresectableCancer() {
@@ -17,9 +18,37 @@ public class HasUnresectableCancer implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
-        return EvaluationFactory.unrecoverable()
-                .result(EvaluationResult.UNDETERMINED)
-                .addUndeterminedSpecificMessages("Currently it cannot be determined if cancer is unresectable")
-                .build();
+        TumorStage stage = record.clinical().tumor().stage();
+
+        if (stage == null) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.UNDETERMINED)
+                    .addUndeterminedSpecificMessages("Tumor stage details are missing")
+                    .addUndeterminedGeneralMessages("Missing tumor stage details")
+                    .build();
+        }
+
+        EvaluationResult result;
+        if (stage == TumorStage.IV) {
+            result = EvaluationResult.PASS;
+        } else if (stage == TumorStage.III || stage.category() == TumorStage.III) {
+            result = EvaluationResult.UNDETERMINED;
+        } else {
+            result = EvaluationResult.FAIL;
+        }
+
+        ImmutableEvaluation.Builder builder = EvaluationFactory.unrecoverable().result(result);
+        if (result == EvaluationResult.FAIL) {
+            builder.addFailSpecificMessages("Patient has no unresectable cancer");
+            builder.addFailGeneralMessages("No unresectable cancer");
+        } else if (result == EvaluationResult.UNDETERMINED) {
+            builder.addUndeterminedSpecificMessages("Unclear if patient has unresectable cancer");
+            builder.addUndeterminedGeneralMessages("Unclear resectability of cancer");
+        } else if (result == EvaluationResult.PASS) {
+            builder.addPassSpecificMessages("Patient has unresectable cancer");
+            builder.addPassGeneralMessages("Unresectable cancer");
+        }
+
+        return builder.build();
     }
 }
