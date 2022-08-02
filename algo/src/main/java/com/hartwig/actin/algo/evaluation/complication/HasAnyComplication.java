f6a3ca67472b6d641a721a84e758dfe8d7b3d2f0
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-08-02 13:46:32 +0200

    Implement new rule for patient having any complication

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/complication/HasAnyComplication.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/complication/HasAnyComplication.java
@@ -1,14 +1,16 @@
 package com.hartwig.actin.algo.evaluation.complication;
 
+import java.util.List;
+
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
+import com.hartwig.actin.clinical.datamodel.Complication;
 
 import org.jetbrains.annotations.NotNull;
 
-//TODO: Update according to README
 public class HasAnyComplication implements EvaluationFunction {
 
     HasAnyComplication() {
@@ -17,11 +19,27 @@ public class HasAnyComplication implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
+        List<Complication> complications = record.clinical().complications();
+        if (complications == null) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.UNDETERMINED)
+                    .addUndeterminedSpecificMessages("Unclear whether patient has any complications")
+                    .addUndeterminedGeneralMessages("Undetermined complication status")
+                    .build();
+        }
+
+        if (!complications.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.PASS)
+                    .addPassSpecificMessages("Patient has at least one complication")
+                    .addPassGeneralMessages("Present complication")
+                    .build();
+        }
+
         return EvaluationFactory.unrecoverable()
-                .result(EvaluationResult.UNDETERMINED)
-                .addUndeterminedSpecificMessages("Any complications currently cannot be determined")
-                .addUndeterminedGeneralMessages("Undetermined complication status")
+                .result(EvaluationResult.FAIL)
+                .addFailSpecificMessages("Patient has no complications")
+                .addFailGeneralMessages("No complications")
                 .build();
     }
-
 }
---
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/complication/HasAnyComplicationTest.java
@@ -0,0 +1,21 @@
+package com.hartwig.actin.algo.evaluation.complication;
+
+import static com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation;
+
+import com.google.common.collect.Lists;
+import com.hartwig.actin.algo.datamodel.EvaluationResult;
+
+import org.junit.Test;
+
+public class HasAnyComplicationTest {
+
+    @Test
+    public void canEvaluate() {
+        HasAnyComplication function = new HasAnyComplication();
+
+        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(ComplicationTestFactory.withComplications(null)));
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(ComplicationTestFactory.withComplications(Lists.newArrayList())));
+        assertEvaluation(EvaluationResult.PASS,
+                function.evaluate(ComplicationTestFactory.withComplication(ComplicationTestFactory.builder().build())));
+    }
+}
