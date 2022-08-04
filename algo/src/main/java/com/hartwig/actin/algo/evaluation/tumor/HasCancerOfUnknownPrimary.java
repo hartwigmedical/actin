d9ff376540b263b66cf28c119217b12baca59c6f
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-08-04 09:52:00 +0200

    Add TODO

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/tumor/HasCancerOfUnknownPrimary.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/tumor/HasCancerOfUnknownPrimary.java
@@ -14,6 +14,7 @@ import com.hartwig.actin.treatment.input.datamodel.TumorTypeInput;
 
 import org.jetbrains.annotations.NotNull;
 
+//TODO: Update according to README
 public class HasCancerOfUnknownPrimary implements EvaluationFunction {
 
     static final String CANCER_DOID = "162";
