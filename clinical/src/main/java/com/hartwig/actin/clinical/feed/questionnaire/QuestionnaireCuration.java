786ab416dad76bb8046e83719aee91d32670c7b5
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-08-17 13:34:36 +0200

    Add option

--- clinical/src/main/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireCuration.java
+++ clinical/src/main/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireCuration.java
@@ -61,6 +61,7 @@ final class QuestionnaireCuration {
         STAGE_MAPPING.put("IV", TumorStage.IV);
         STAGE_MAPPING.put("4", TumorStage.IV);
         STAGE_MAPPING.put("cT3N2M1", TumorStage.IV);
+        STAGE_MAPPING.put("onknown", null);
     }
 
     private QuestionnaireCuration() {
