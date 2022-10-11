8a3ffa42c8665025606649626ed752b8e2cb741e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-10-11 10:04:30 +0200

    Concat every prior condition that belongs to the DOID requested

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/othercondition/HasHadPriorConditionWithDoid.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/othercondition/HasHadPriorConditionWithDoid.java
@@ -1,10 +1,14 @@
 package com.hartwig.actin.algo.evaluation.othercondition;
 
+import java.util.Set;
+
+import com.google.common.collect.Sets;
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
+import com.hartwig.actin.algo.evaluation.util.Format;
 import com.hartwig.actin.algo.othercondition.OtherConditionSelector;
 import com.hartwig.actin.clinical.datamodel.PriorOtherCondition;
 import com.hartwig.actin.doid.DoidModel;
@@ -28,14 +32,19 @@ public class HasHadPriorConditionWithDoid implements EvaluationFunction {
     public Evaluation evaluate(@NotNull PatientRecord record) {
         String doidTerm = doidModel.resolveTermForDoid(doidToFind);
 
+        Set<String> conditions = Sets.newHashSet();
         for (PriorOtherCondition condition : OtherConditionSelector.selectClinicallyRelevant(record.clinical().priorOtherConditions())) {
             if (conditionHasDoid(condition, doidToFind)) {
-                return EvaluationFactory.unrecoverable()
-                        .result(EvaluationResult.PASS)
-                        .addPassSpecificMessages("Patient has " + condition.name() + ", which belongs to category " + doidTerm)
-                        .addPassGeneralMessages("Present " + condition.name())
-                        .build();
+                conditions.add(condition.name());
             }
+        }
+
+        if (!conditions.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.PASS)
+                    .addPassSpecificMessages("Patient has " + Format.concat(conditions) + ", which belong(s) to category " + doidTerm)
+                    .addPassGeneralMessages("Present " + Format.concat(conditions))
+                    .build();
         }
 
         return EvaluationFactory.unrecoverable()
