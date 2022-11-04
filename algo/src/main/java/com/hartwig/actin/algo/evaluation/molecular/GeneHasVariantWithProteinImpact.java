34975a81aa91a2b214d84e54d9d88a8a627972ac
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-11-04 14:32:20 +0100

    Implement rule

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantWithProteinImpact.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantWithProteinImpact.java
@@ -1,16 +1,21 @@
 package com.hartwig.actin.algo.evaluation.molecular;
 
 import java.util.List;
+import java.util.Set;
 
+import com.google.common.collect.Sets;
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
 
+import com.hartwig.actin.algo.evaluation.util.Format;
+import com.hartwig.actin.molecular.datamodel.driver.TranscriptImpact;
+import com.hartwig.actin.molecular.datamodel.driver.Variant;
+
 import org.jetbrains.annotations.NotNull;
 
-//TODO: Implement
 public class GeneHasVariantWithProteinImpact implements EvaluationFunction {
 
     @NotNull
@@ -26,10 +31,51 @@ public class GeneHasVariantWithProteinImpact implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
+        Set<String> proteinImpactsCanonicalFound = Sets.newHashSet();
+        Set<String> proteinImpactsOtherFound = Sets.newHashSet();
+
+        for (Variant variant : record.molecular().drivers().variants()) {
+            if (variant.gene().equals(gene)) {
+                for (String allowedProteinImpact : allowedProteinImpacts) {
+                    boolean proteinImpactOtherMatch = false;
+                    for (TranscriptImpact otherImpact : variant.otherImpacts()) {
+                        if (otherImpact.proteinImpact().equals(allowedProteinImpact)) {
+                            proteinImpactOtherMatch = true;
+                        }
+                    }
+
+                    if (variant.canonicalImpact().proteinImpact().equals(allowedProteinImpact)) {
+                        proteinImpactsCanonicalFound.add(allowedProteinImpact);
+                    }
+
+                    if (proteinImpactOtherMatch) {
+                        proteinImpactsOtherFound.add(allowedProteinImpact);
+                    }
+                }
+            }
+        }
+
+        if (!proteinImpactsCanonicalFound.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.PASS)
+                    .addPassSpecificMessages("Variant(s) " + Format.concat(proteinImpactsCanonicalFound) + " in gene " + gene
+                            + " detected in canonical transcript")
+                    .addPassGeneralMessages(Format.concat(proteinImpactsCanonicalFound) + " found in " + gene)
+                    .build();
+        } else if (!proteinImpactsOtherFound.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.WARN)
+                    .addWarnSpecificMessages("Variant(s) " + Format.concat(proteinImpactsCanonicalFound) + " in " + gene
+                            + " detected, but in non-canonical transcript")
+                    .addWarnGeneralMessages(
+                            Format.concat(proteinImpactsCanonicalFound) + " found in non-canonical transcript of gene " + gene)
+                    .build();
+        }
+
         return EvaluationFactory.unrecoverable()
                 .result(EvaluationResult.FAIL)
-                .addFailSpecificMessages("No variant with specific protein impact detected in gene " + gene)
-                .addFailGeneralMessages("Molecular requirements")
+                .addFailSpecificMessages("None of " + Format.concat(allowedProteinImpacts) + " detected in gene " + gene)
+                .addFailGeneralMessages("No specific variants in " + gene + " detected")
                 .build();
     }
 }
--- algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantWithProteinImpactTest.java
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantWithProteinImpactTest.java
@@ -6,6 +6,8 @@ import com.google.common.collect.Lists;
 import com.hartwig.actin.TestDataFactory;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 
+import com.hartwig.actin.molecular.datamodel.driver.TestTranscriptImpactFactory;
+import com.hartwig.actin.molecular.datamodel.driver.TestVariantFactory;
 import org.junit.Test;
 
 public class GeneHasVariantWithProteinImpactTest {
@@ -14,6 +16,42 @@ public class GeneHasVariantWithProteinImpactTest {
     public void canEvaluate() {
         GeneHasVariantWithProteinImpact function = new GeneHasVariantWithProteinImpact("gene A", Lists.newArrayList("V600E", "V600K"));
 
+        // gene not present
         assertEvaluation(EvaluationResult.FAIL, function.evaluate(TestDataFactory.createMinimalTestPatientRecord()));
+
+        // correct gene, no protein impacts configured
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder().gene("gene A").build())));
+
+        // correct gene, only wrong protein impacts configured
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().proteinImpact("V600P").build())
+                        .addOtherImpacts(TestTranscriptImpactFactory.builder().proteinImpact("V600P").build())
+                        .build())));
+
+        // correct gene, protein impact detected in canonical
+        assertEvaluation(EvaluationResult.PASS,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().proteinImpact("V600E").build())
+                        .build())));
+
+        // incorrect gene, protein impact detected in canonical
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene B")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().proteinImpact("V600E").build())
+                        .build())));
+
+        // correct gene, protein impact detected in non-canonical only
+        assertEvaluation(EvaluationResult.WARN,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().proteinImpact("V600P").build())
+                        .addOtherImpacts(TestTranscriptImpactFactory.builder().proteinImpact("V600P").build())
+                        .addOtherImpacts(TestTranscriptImpactFactory.builder().proteinImpact("V600E").build())
+                        .build())));
     }
 }
