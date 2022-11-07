57fc43f3d007adfdff384a60e8d8af24e9cac25f
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-11-07 11:22:29 +0100

    Initial implementation

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantInExonRangeOfType.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantInExonRangeOfType.java
@@ -7,11 +7,13 @@ import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
+import com.hartwig.actin.molecular.datamodel.driver.TranscriptImpact;
+import com.hartwig.actin.molecular.datamodel.driver.Variant;
+import com.hartwig.actin.molecular.datamodel.driver.VariantType;
 import com.hartwig.actin.treatment.input.datamodel.VariantTypeInput;
 
 import org.jetbrains.annotations.NotNull;
 
-//TODO: Implement
 public class GeneHasVariantInExonRangeOfType implements EvaluationFunction {
 
     @NotNull
@@ -32,10 +34,57 @@ public class GeneHasVariantInExonRangeOfType implements EvaluationFunction {
     @NotNull
     @Override
     public Evaluation evaluate(@NotNull PatientRecord record) {
-        return EvaluationFactory.unrecoverable()
-                .result(EvaluationResult.FAIL)
-                .addFailSpecificMessages("No variant in required exon range detected in gene " + gene)
-                .addFailGeneralMessages("Molecular requirements")
-                .build();
+        for (Variant variant : record.molecular().drivers().variants()) {
+            if (variant.gene().equals(gene)) {
+
+                boolean exonRangeCanonicalMatch = false;
+                if (variant.canonicalImpact().affectedExon() != null) {
+                    if (variant.canonicalImpact().affectedExon() >= minExon && variant.canonicalImpact().affectedExon() <= maxExon) {
+                        exonRangeCanonicalMatch = true;
+                    }
+                }
+
+                boolean exonRangeOtherMatch = false;
+                for (TranscriptImpact otherImpact : variant.otherImpacts()) {
+                    if (otherImpact.affectedExon() != null && otherImpact.affectedExon() >= minExon
+                            && otherImpact.affectedExon() <= maxExon) {
+                        exonRangeOtherMatch = true;
+                    }
+                }
+
+                //TODO: Fix
+                boolean requiredVariantTypeMatch = false;
+                if (requiredVariantType == null) {
+                    requiredVariantTypeMatch = true;
+                } else if (requiredVariantType.equals(variant.type())) {
+                    requiredVariantTypeMatch = true;
+                } else if (requiredVariantType.equals(VariantTypeInput.INDEL) && (variant.type().equals(VariantType.INSERT)
+                        || variant.type().equals(VariantType.DELETE))) {
+                    requiredVariantTypeMatch = true;
+                }
+
+                if (exonRangeCanonicalMatch && requiredVariantTypeMatch) {
+                    return EvaluationFactory.unrecoverable()
+                            .result(EvaluationResult.PASS)
+                            .addPassSpecificMessages("Variant(s) in exon range " + minExon + " - " + maxExon + " in gene " + gene
+                                    + " of adequate type detected in canonical transcript")
+                            .addPassGeneralMessages("Adequate variant(s) found in " + gene)
+                            .build();
+                } else if (exonRangeOtherMatch && requiredVariantTypeMatch) {
+                    return EvaluationFactory.unrecoverable()
+                            .result(EvaluationResult.WARN)
+                            .addWarnSpecificMessages("Variant(s) in exon range " + minExon + " - " + maxExon + " in gene " + gene
+                                    + " of adequate type detected, but in non-canonical transcript")
+                            .addWarnGeneralMessages("Adequate variant(s) found in non-canonical transcript of gene " + gene)
+                            .build();
+                }
+            }
+        }
+
+        return EvaluationFactory.unrecoverable().
+                result(EvaluationResult.FAIL).
+                addFailSpecificMessages("None adequate variant in exon range " + minExon + " - " + maxExon + " detected in gene " + gene).
+                addFailGeneralMessages("No specific variants in " + gene + " detected").
+                build();
     }
 }
--- algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantInExonRangeOfTypeTest.java
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/GeneHasVariantInExonRangeOfTypeTest.java
@@ -1,5 +1,12 @@
 package com.hartwig.actin.algo.evaluation.molecular;
 
+import static com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation;
+
+import com.hartwig.actin.TestDataFactory;
+import com.hartwig.actin.algo.datamodel.EvaluationResult;
+import com.hartwig.actin.molecular.datamodel.driver.TestTranscriptImpactFactory;
+import com.hartwig.actin.molecular.datamodel.driver.TestVariantFactory;
+import com.hartwig.actin.molecular.datamodel.driver.VariantType;
 import com.hartwig.actin.treatment.input.datamodel.VariantTypeInput;
 
 import org.junit.Test;
@@ -9,6 +16,52 @@ public class GeneHasVariantInExonRangeOfTypeTest {
     @Test
     public void canEvaluate() {
         GeneHasVariantInExonRangeOfType function = new GeneHasVariantInExonRangeOfType("gene A", 1, 2, VariantTypeInput.INSERT);
+        GeneHasVariantInExonRangeOfType function2 = new GeneHasVariantInExonRangeOfType("gene A", 1, 2, null);
 
+        // gene not present
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(TestDataFactory.createMinimalTestPatientRecord()));
+
+        // no exons configured
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder().gene("gene A").build())));
+
+        // no variant type configured
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().affectedExon(1).build())
+                        .build())));
+
+        // no variant type configured when input = null
+        assertEvaluation(EvaluationResult.PASS,
+                function2.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().affectedExon(1).build())
+                        .build())));
+
+        // wrong exon range
+        assertEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().affectedExon(6).build())
+                        .build())));
+
+        // correct gene, correct exon, correct variant type, canonical
+        assertEvaluation(EvaluationResult.PASS,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .type(VariantType.INSERT)
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().affectedExon(1).build())
+                        .build())));
+
+        // correct gene, protein impact detected in non-canonical only
+        assertEvaluation(EvaluationResult.WARN,
+                function.evaluate(MolecularTestFactory.withVariant(TestVariantFactory.builder()
+                        .gene("gene A")
+                        .type(VariantType.INSERT)
+                        .canonicalImpact(TestTranscriptImpactFactory.builder().affectedExon(6).build())
+                        .addOtherImpacts(TestTranscriptImpactFactory.builder().affectedExon(6).build())
+                        .addOtherImpacts(TestTranscriptImpactFactory.builder().affectedExon(1).build())
+                        .build())));
     }
 }
