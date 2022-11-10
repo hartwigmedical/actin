559fbdd385c7b17c05be8dcc77992ec5b4ce0ae2
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-11-10 11:14:50 +0100

    Implement new rule for fusion evaluation

--- algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/HasFusionInGene.java
+++ algo/src/main/java/com/hartwig/actin/algo/evaluation/molecular/HasFusionInGene.java
@@ -1,14 +1,21 @@
 package com.hartwig.actin.algo.evaluation.molecular;
 
+import java.util.Set;
+
+import com.google.common.collect.Sets;
 import com.hartwig.actin.PatientRecord;
 import com.hartwig.actin.algo.datamodel.Evaluation;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
 import com.hartwig.actin.algo.evaluation.EvaluationFactory;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
+import com.hartwig.actin.algo.evaluation.util.Format;
+import com.hartwig.actin.molecular.datamodel.driver.DriverLikelihood;
+import com.hartwig.actin.molecular.datamodel.driver.Fusion;
+import com.hartwig.actin.molecular.datamodel.driver.ProteinEffect;
+import com.hartwig.actin.molecular.util.MolecularEventFactory;
 
 import org.jetbrains.annotations.NotNull;
 
-// TODO Implement
 public class HasFusionInGene implements EvaluationFunction {
 
     @NotNull
@@ -20,7 +27,75 @@ public class HasFusionInGene implements EvaluationFunction {
 
     @NotNull
     @Override
-    public Evaluation evaluate(@NotNull PatientRecord record)  {
+    public Evaluation evaluate(@NotNull PatientRecord record) {
+        Set<String> matchingFusions = Sets.newHashSet();
+        Set<String> fusionsWithNoEffect = Sets.newHashSet();
+        Set<String> fusionsWithNoHighDriverLikelihood = Sets.newHashSet();
+        Set<String> unreportableFusionsWithGainOfFunction = Sets.newHashSet();
+
+        for (Fusion fusion : record.molecular().drivers().fusions()) {
+            if (fusion.geneStart().equals(gene) || fusion.geneEnd().equals(gene)) {
+                String fusionEvent = MolecularEventFactory.fusionEvent(fusion);
+                if (fusion.isReportable()) {
+                    boolean hasNoEffect = fusion.proteinEffect() == ProteinEffect.NO_EFFECT
+                            || fusion.proteinEffect() == ProteinEffect.NO_EFFECT_PREDICTED;
+                    if (fusion.driverLikelihood() != DriverLikelihood.HIGH) {
+                        fusionsWithNoHighDriverLikelihood.add(fusionEvent);
+                    } else if (hasNoEffect) {
+                        fusionsWithNoEffect.add(fusionEvent);
+                    } else {
+                        matchingFusions.add(fusionEvent);
+                    }
+                } else {
+                    boolean isGainOfFunction = fusion.proteinEffect() == ProteinEffect.GAIN_OF_FUNCTION
+                            || fusion.proteinEffect() == ProteinEffect.GAIN_OF_FUNCTION_PREDICTED;
+                    if (isGainOfFunction) {
+                        unreportableFusionsWithGainOfFunction.add(fusionEvent);
+                    }
+                }
+            }
+        }
+
+        if (!matchingFusions.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.PASS)
+                    .addAllInclusionMolecularEvents(matchingFusions)
+                    .addPassSpecificMessages("Fusion(s) " + Format.concat(matchingFusions) + " detected in gene " + gene)
+                    .addPassGeneralMessages("Fusion(s) detected in gene " + gene)
+                    .build();
+        }
+
+        if (!fusionsWithNoEffect.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.WARN)
+                    .addAllInclusionMolecularEvents(fusionsWithNoEffect)
+                    .addWarnSpecificMessages("Fusion(s) " + Format.concat(fusionsWithNoEffect) + " detected in gene " + gene
+                            + " but annotated as having no effect")
+                    .addWarnGeneralMessages("Potential fusion(s) detected in gene " + gene)
+                    .build();
+        }
+
+        if (!fusionsWithNoHighDriverLikelihood.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.WARN)
+                    .addAllInclusionMolecularEvents(fusionsWithNoHighDriverLikelihood)
+                    .addWarnSpecificMessages("Fusion(s) " + Format.concat(fusionsWithNoHighDriverLikelihood) + " detected in gene " + gene
+                            + " but annotated as having no high driver likelihood")
+                    .addWarnGeneralMessages("Potential fusion(s) detected in gene " + gene)
+                    .build();
+        }
+
+        if (!unreportableFusionsWithGainOfFunction.isEmpty()) {
+            return EvaluationFactory.unrecoverable()
+                    .result(EvaluationResult.WARN)
+                    .addAllInclusionMolecularEvents(unreportableFusionsWithGainOfFunction)
+                    .addWarnSpecificMessages(
+                            "Fusion(s) " + Format.concat(unreportableFusionsWithGainOfFunction) + " detected in gene " + gene
+                                    + " but not considered reportable yet having gain-of-function")
+                    .addWarnGeneralMessages("Potential fusion(s) detected in gene " + gene)
+                    .build();
+        }
+
         return EvaluationFactory.unrecoverable()
                 .result(EvaluationResult.FAIL)
                 .addFailSpecificMessages("No fusion detected with gene " + gene)
--- algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/HasFusionInGeneTest.java
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/HasFusionInGeneTest.java
@@ -1,9 +1,13 @@
 package com.hartwig.actin.algo.evaluation.molecular;
 
-import static com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation;
+import static com.hartwig.actin.algo.evaluation.EvaluationAssert.assertMolecularEvaluation;
 
 import com.hartwig.actin.TestDataFactory;
 import com.hartwig.actin.algo.datamodel.EvaluationResult;
+import com.hartwig.actin.molecular.datamodel.driver.DriverLikelihood;
+import com.hartwig.actin.molecular.datamodel.driver.Fusion;
+import com.hartwig.actin.molecular.datamodel.driver.ProteinEffect;
+import com.hartwig.actin.molecular.datamodel.driver.TestFusionFactory;
 
 import org.junit.Test;
 
@@ -13,6 +17,47 @@ public class HasFusionInGeneTest {
     public void canEvaluate() {
         HasFusionInGene function = new HasFusionInGene("gene A");
 
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(TestDataFactory.createMinimalTestPatientRecord()));
+        assertMolecularEvaluation(EvaluationResult.FAIL, function.evaluate(TestDataFactory.createMinimalTestPatientRecord()));
+
+        Fusion matchingFusion = TestFusionFactory.builder()
+                .geneStart("gene A")
+                .isReportable(true)
+                .driverLikelihood(DriverLikelihood.HIGH)
+                .proteinEffect(ProteinEffect.GAIN_OF_FUNCTION)
+                .build();
+
+        assertMolecularEvaluation(EvaluationResult.PASS, function.evaluate(MolecularTestFactory.withFusion(matchingFusion)));
+
+        assertMolecularEvaluation(EvaluationResult.PASS,
+                function.evaluate(MolecularTestFactory.withFusion(TestFusionFactory.builder()
+                        .from(matchingFusion)
+                        .geneStart("gene B")
+                        .geneEnd("gene A")
+                        .build())));
+
+        assertMolecularEvaluation(EvaluationResult.WARN,
+                function.evaluate(MolecularTestFactory.withFusion(TestFusionFactory.builder()
+                        .from(matchingFusion)
+                        .isReportable(false)
+                        .build())));
+
+        assertMolecularEvaluation(EvaluationResult.FAIL,
+                function.evaluate(MolecularTestFactory.withFusion(TestFusionFactory.builder()
+                        .from(matchingFusion)
+                        .isReportable(false)
+                        .proteinEffect(ProteinEffect.NO_EFFECT)
+                        .build())));
+
+        assertMolecularEvaluation(EvaluationResult.WARN,
+                function.evaluate(MolecularTestFactory.withFusion(TestFusionFactory.builder()
+                        .from(matchingFusion)
+                        .driverLikelihood(DriverLikelihood.LOW)
+                        .build())));
+
+        assertMolecularEvaluation(EvaluationResult.WARN,
+                function.evaluate(MolecularTestFactory.withFusion(TestFusionFactory.builder()
+                        .from(matchingFusion)
+                        .proteinEffect(ProteinEffect.NO_EFFECT)
+                        .build())));
     }
 }
