9254f129badedb84c23ed585db7dbb6b4abdf798
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-11-21 14:43:51 +0100

    Add evidence SQL view

--- database/src/main/resources/generate_views.sql
+++ database/src/main/resources/generate_views.sql
@@ -10,42 +10,38 @@ SELECT  trial.code AS trialId, acronym AS trialAcronym, cohort.code AS cohortId,
     ORDER BY trialId
 );
 
-CREATE OR REPLACE VIEW trialEvaluation
-AS (
-SELECT * FROM (
-SELECT  referenceDate, referenceDateIsLive, patientId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
-        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
-        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen,
-        cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist, cohortMatch.isEligible AS isEligibleCohort,
-        eligibility AS eligibilityRule, result, recoverable, passSpecificMessages, passGeneralMessages, warnSpecificMessages, warnGeneralMessages,
-        undeterminedSpecificMessages, undeterminedGeneralMessages, failSpecificMessages, failGeneralMessages
-    FROM evaluation
-    INNER JOIN trialMatch ON trialMatch.id = evaluation.trialMatchId
-    INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
-    LEFT JOIN trial ON trial.code = trialMatch.code
-    LEFT JOIN cohortMatch ON trialMatch.id = cohortMatch.trialMatchId AND cohortMatch.Id = evaluation.cohortMatchId
-UNION
-SELECT  DISTINCT referenceDate, referenceDateIsLive, patientId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
-        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
-        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen,
-        cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist, cohortMatch.isEligible AS isEligibleCohort,
-        NULL AS eligibilityRule, NULL AS result, NULL as recoverable, NULL AS passSpecificMessages, NULL AS passGeneralMessages,
-        NULL AS warnSpecificMessages, NULL AS warnGeneralMessages, NULL AS undeterminedSpecificMessages,
-        NULL AS undeterminedGeneralMessages, NULL AS failSpecificMessages, NULL AS failGeneralMessages
-    FROM cohortMatch
-    INNER JOIN trialMatch ON trialMatch.id = cohortMatch.trialMatchId
-    INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
-    LEFT JOIN trial ON trial.code = trialMatch.code
-    WHERE cohortMatch.id NOT IN (SELECT DISTINCT cohortMatchId FROM evaluation WHERE NOT isnull(cohortMatchId))
-    ORDER BY patientId, cohortId)
-AS a
-);
-
 CREATE OR REPLACE VIEW eligibleCohorts
 AS (
 SELECT DISTINCT patientId, trialId, trialAcronym, cohortDescription
     FROM trialEvaluation
     WHERE ((isEligibleTrial AND NOT trialHasCohorts AND trialOpen) OR (isEligibleTrial AND isEligibleCohort AND cohortOpen AND NOT cohortBlacklist))
+);
+
+CREATE OR REPLACE VIEW evidence
+AS (
+SELECT * FROM (
+	SELECT molecular.sampleId, treatment, type, IF(isMicrosatelliteUnstable,"MSI","MSS") AS event, NULL AS driverLikelihood, NULL AS isReportableDriver FROM microsatelliteEvidence INNER JOIN molecular ON microsatelliteEvidence.sampleId=molecular.sampleId
+		UNION
+	SELECT molecular.sampleId, treatment, type, IF(isHomologousRepairDeficient,"HRD","HRP") AS event, NULL AS driverLikelihood, NULL AS isReportableDriver FROM homologousRepairEvidence INNER JOIN molecular ON homologousRepairEvidence.sampleId=molecular.sampleId
+		UNION
+	SELECT molecular.sampleId, treatment, type, concat("TMB ", round(tumorMutationalBurden,1)) AS event, NULL AS driverLikelihood, NULL AS isReportableDriver FROM tumorMutationalBurdenEvidence INNER JOIN molecular ON tumorMutationalBurdenEvidence.sampleId=molecular.sampleId
+		UNION
+	SELECT molecular.sampleId, treatment, type, concat("TML ", tumorMutationalLoad) AS event, NULL AS driverLikelihood, NULL AS isReportableDriver FROM tumorMutationalLoadEvidence INNER JOIN molecular ON tumorMutationalLoadEvidence.sampleId=molecular.sampleId
+		UNION
+	SELECT variant.sampleId, treatment, variantEvidence.type, event, driverLikelihood, isReportable AS isReportableDriver FROM variantEvidence INNER JOIN variant ON variantEvidence.variantId = variant.Id
+		UNION
+	SELECT amplification.sampleId, treatment, type, event, driverLikelihood, isReportable AS isReportableDriver FROM amplificationEvidence INNER JOIN amplification ON amplificationEvidence.amplificationId = amplification.Id
+		UNION
+	SELECT loss.sampleId, treatment, type, event, driverLikelihood, isReportable AS isReportableDriver FROM lossEvidence INNER JOIN loss ON lossEvidence.lossId = loss.Id
+		UNION
+	SELECT homozygousDisruption.sampleId, treatment, type, event, driverLikelihood, isReportable AS isReportableDriver FROM homozygousDisruptionEvidence INNER JOIN homozygousDisruption ON homozygousDisruptionEvidence.homozygousDisruptionId = homozygousDisruption.Id
+		UNION
+	SELECT disruption.sampleId, treatment, disruptionEvidence.type, event, driverLikelihood, isReportable AS isReportableDriver FROM disruptionEvidence INNER JOIN disruption ON disruptionEvidence.disruptionId = disruption.Id
+		UNION
+	SELECT fusion.sampleId, treatment, type, event, driverLikelihood, isReportable AS isReportableDriver FROM fusionEvidence INNER JOIN fusion ON fusionEvidence.fusionId = fusion.Id
+		UNION
+	SELECT virus.sampleId, treatment, virusEvidence.type, event, driverLikelihood, isReportable AS isReportableDriver FROM virusEvidence INNER JOIN virus ON virusEvidence.virusId = virus.Id)
+    AS a
 );
 
 CREATE OR REPLACE VIEW molecularDetails
@@ -135,5 +131,36 @@ SELECT  x.sampleId,
     LEFT JOIN (SELECT * FROM treatmentEvidence WHERE NOT isResponsive) AS y ON x.sampleId=y.sampleId AND x.event=y.event
 	GROUP BY 1,3
 ORDER BY 1,2,3)
+AS a
+);
+
+CREATE OR REPLACE VIEW trialEvaluation
+AS (
+SELECT * FROM (
+SELECT  referenceDate, referenceDateIsLive, patientId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
+        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
+        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen,
+        cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist, cohortMatch.isEligible AS isEligibleCohort,
+        eligibility AS eligibilityRule, result, recoverable, passSpecificMessages, passGeneralMessages, warnSpecificMessages, warnGeneralMessages,
+        undeterminedSpecificMessages, undeterminedGeneralMessages, failSpecificMessages, failGeneralMessages
+    FROM evaluation
+    INNER JOIN trialMatch ON trialMatch.id = evaluation.trialMatchId
+    INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
+    LEFT JOIN trial ON trial.code = trialMatch.code
+    LEFT JOIN cohortMatch ON trialMatch.id = cohortMatch.trialMatchId AND cohortMatch.Id = evaluation.cohortMatchId
+UNION
+SELECT  DISTINCT referenceDate, referenceDateIsLive, patientId, trialMatch.code AS trialId, trial.acronym AS trialAcronym, trial.open AS trialOpen,
+        IF(trial.id IN (SELECT trialId FROM cohort),1,0) AS trialHasCohorts, trialMatch.isEligible AS isEligibleTrial,
+        cohortMatch.code AS cohortId, cohortMatch.description AS cohortDescription, cohortMatch.open AS cohortOpen,
+        cohortMatch.slotsAvailable AS cohortSlotsAvailable, cohortMatch.blacklist AS cohortBlacklist, cohortMatch.isEligible AS isEligibleCohort,
+        NULL AS eligibilityRule, NULL AS result, NULL as recoverable, NULL AS passSpecificMessages, NULL AS passGeneralMessages,
+        NULL AS warnSpecificMessages, NULL AS warnGeneralMessages, NULL AS undeterminedSpecificMessages,
+        NULL AS undeterminedGeneralMessages, NULL AS failSpecificMessages, NULL AS failGeneralMessages
+    FROM cohortMatch
+    INNER JOIN trialMatch ON trialMatch.id = cohortMatch.trialMatchId
+    INNER JOIN treatmentMatch ON treatmentMatch.id = trialMatch.treatmentMatchId
+    LEFT JOIN trial ON trial.code = trialMatch.code
+    WHERE cohortMatch.id NOT IN (SELECT DISTINCT cohortMatchId FROM evaluation WHERE NOT isnull(cohortMatchId))
+    ORDER BY patientId, cohortId)
 AS a
 );
