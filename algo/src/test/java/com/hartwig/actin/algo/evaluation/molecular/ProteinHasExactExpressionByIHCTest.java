718862a69469840b5de93040aa2fedf6ab068fa8
jbartletthmf <125090035+jbartletthmf@users.noreply.github.com>
2023-03-27 09:50:59 -0400

    ACTIN-48: Refactor PriorMolecularTestFunctions and add constants
    Rename "gene" references to "protein" in IHC evaluation functions.

--- algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/ProteinHasExactExpressionByIHCTest.java
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/molecular/ProteinHasExactExpressionByIHCTest.java
@@ -16,40 +16,40 @@ public class ProteinHasExactExpressionByIHCTest {
 
     @Test
     public void canEvaluate() {
-        String gene = "gene 1";
-        ProteinHasExactExpressionByIHC exact = new ProteinHasExactExpressionByIHC(gene, 2);
+        String protein = "protein 1";
+        ProteinHasExactExpressionByIHC exact = new ProteinHasExactExpressionByIHC(protein, 2);
 
         // No prior tests
         List<PriorMolecularTest> priorTests = Lists.newArrayList();
         assertEvaluation(EvaluationResult.UNDETERMINED, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with no result
-        priorTests.add(ihcBuilder(gene).build());
+        priorTests.add(ihcBuilder(protein).build());
         assertEvaluation(EvaluationResult.FAIL, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with too low result
-        priorTests.add(ihcBuilder(gene).scoreValue(1D).build());
+        priorTests.add(ihcBuilder(protein).scoreValue(1D).build());
         assertEvaluation(EvaluationResult.FAIL, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with too high result
-        priorTests.add(ihcBuilder(gene).scoreValue(3D).build());
+        priorTests.add(ihcBuilder(protein).scoreValue(3D).build());
         assertEvaluation(EvaluationResult.FAIL, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with exact result but with prefix
-        priorTests.add(ihcBuilder(gene).scoreValuePrefix(">").scoreValue(2D).build());
+        priorTests.add(ihcBuilder(protein).scoreValuePrefix(">").scoreValue(2D).build());
         assertEvaluation(EvaluationResult.FAIL, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with 'positive' result
-        priorTests.add(ihcBuilder(gene).scoreText("Positive").build());
+        priorTests.add(ihcBuilder(protein).scoreText("Positive").build());
         assertEvaluation(EvaluationResult.UNDETERMINED, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
 
         // Add test with exact result
-        priorTests.add(ihcBuilder(gene).scoreValue(2D).build());
+        priorTests.add(ihcBuilder(protein).scoreValue(2D).build());
         assertEvaluation(EvaluationResult.PASS, exact.evaluate(MolecularTestFactory.withPriorTests(priorTests)));
     }
 
     @NotNull
-    private static ImmutablePriorMolecularTest.Builder ihcBuilder(@NotNull String gene) {
-        return MolecularTestFactory.priorBuilder().test("IHC").item(gene);
+    private static ImmutablePriorMolecularTest.Builder ihcBuilder(@NotNull String protein) {
+        return MolecularTestFactory.priorBuilder().test("IHC").item(protein);
     }
 }
