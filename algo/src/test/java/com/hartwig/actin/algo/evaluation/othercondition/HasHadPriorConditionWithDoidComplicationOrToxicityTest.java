16a47ac836e49b3de69b2e3d52715ad72fbb38a1
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2023-03-30 14:22:28 -0400

    ACTIN-14 WIP

--- algo/src/test/java/com/hartwig/actin/algo/evaluation/othercondition/HasHadPriorConditionWithDoidComplicationOrToxicityTest.java
+++ algo/src/test/java/com/hartwig/actin/algo/evaluation/othercondition/HasHadPriorConditionWithDoidComplicationOrToxicityTest.java
@@ -1,7 +1,11 @@
 package com.hartwig.actin.algo.evaluation.othercondition;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.hartwig.actin.TestDataFactory;
 import com.hartwig.actin.algo.datamodel.Evaluation;
+import com.hartwig.actin.algo.datamodel.EvaluationResult;
+import com.hartwig.actin.algo.evaluation.EvaluationAssert;
 import com.hartwig.actin.algo.evaluation.EvaluationFunction;
 import com.hartwig.actin.doid.TestDoidModelFactory;
 
@@ -16,8 +20,10 @@ public class HasHadPriorConditionWithDoidComplicationOrToxicityTest {
                         "1234",
                         "complication",
                         "toxicity");
-        Evaluation evaluate = victim.evaluate(TestDataFactory.createMinimalTestPatientRecord());
-        assertThat
+        Evaluation evaluation = victim.evaluate(TestDataFactory.createMinimalTestPatientRecord());
+        EvaluationAssert.assertEvaluation(EvaluationResult.FAIL, evaluation);
+        assertThat(evaluation.failSpecificMessages()).containsOnly("test");
+        assertThat(evaluation.failGeneralMessages()).containsOnly("test");
     }
 
 }
