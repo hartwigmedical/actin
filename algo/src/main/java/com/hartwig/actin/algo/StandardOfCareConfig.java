dfcd4886a64d9447e6f409022e11c51a4ec2233a
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-04-03 15:51:24 -0400

    ACTIN-31: Remove unused output directory parameter

--- algo/src/main/java/com/hartwig/actin/algo/StandardOfCareConfig.java
+++ algo/src/main/java/com/hartwig/actin/algo/StandardOfCareConfig.java
@@ -24,8 +24,6 @@ public interface StandardOfCareConfig {
 
     String DOID_JSON = "doid_json";
 
-    String OUTPUT_DIRECTORY = "output_directory";
-
     String RUN_HISTORICALLY = "run_historically";
     String LOG_DEBUG = "log_debug";
 
@@ -37,8 +35,6 @@ public interface StandardOfCareConfig {
         options.addOption(MOLECULAR_JSON, true, "File containing the most recent molecular record of the patient");
 
         options.addOption(DOID_JSON, true, "Path to JSON file containing the full DOID tree.");
-
-        options.addOption(OUTPUT_DIRECTORY, true, "Directory where the matcher output will be written to");
 
         options.addOption(RUN_HISTORICALLY, false, "If set, runs the algo with a date just after the original patient registration date");
         options.addOption(LOG_DEBUG, false, "If set, debug logging gets enabled");
@@ -54,9 +50,6 @@ public interface StandardOfCareConfig {
 
     @NotNull
     String doidJson();
-
-    @NotNull
-    String outputDirectory();
 
     boolean runHistorically();
 
@@ -76,7 +69,6 @@ public interface StandardOfCareConfig {
                 .clinicalJson(ApplicationConfig.nonOptionalFile(cmd, CLINICAL_JSON))
                 .molecularJson(ApplicationConfig.nonOptionalFile(cmd, MOLECULAR_JSON))
                 .doidJson(ApplicationConfig.nonOptionalFile(cmd, DOID_JSON))
-                .outputDirectory(ApplicationConfig.nonOptionalDir(cmd, OUTPUT_DIRECTORY))
                 .runHistorically(runHistorically)
                 .build();
     }
