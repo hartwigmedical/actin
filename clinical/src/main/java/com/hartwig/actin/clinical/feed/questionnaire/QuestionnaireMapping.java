74da53afc153ca8a2344b684ba7cd42c3ec8e813
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-05-02 10:17:06 +0200

    ACTIN-91: Add minimal support for ACTIN questionnaire v1.6

--- clinical/src/main/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireMapping.java
+++ clinical/src/main/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireMapping.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 
 final class QuestionnaireMapping {
 
+    static final Map<QuestionnaireKey, String> KEYS_V1_6 = Maps.newHashMap();
     static final Map<QuestionnaireKey, String> KEYS_V1_5 = Maps.newHashMap();
     static final Map<QuestionnaireKey, String> KEYS_V1_4 = Maps.newHashMap();
     static final Map<QuestionnaireKey, String> KEYS_V1_3 = Maps.newHashMap();
@@ -18,6 +19,28 @@ final class QuestionnaireMapping {
     static final Map<QuestionnaireKey, String> KEYS_V0_1 = Maps.newHashMap();
 
     static {
+        KEYS_V1_6.put(QuestionnaireKey.TREATMENT_HISTORY_CURRENT_TUMOR, "Treatment history current tumor");
+        KEYS_V1_6.put(QuestionnaireKey.OTHER_ONCOLOGICAL_HISTORY, "Other oncological history");
+        KEYS_V1_6.put(QuestionnaireKey.SECONDARY_PRIMARY, "Secondary primary");
+        KEYS_V1_6.put(QuestionnaireKey.NON_ONCOLOGICAL_HISTORY, "Non-oncological history");
+        KEYS_V1_6.put(QuestionnaireKey.PRIMARY_TUMOR_LOCATION, "Primary tumor location");
+        KEYS_V1_6.put(QuestionnaireKey.PRIMARY_TUMOR_TYPE, "Primary tumor type");
+        KEYS_V1_6.put(QuestionnaireKey.BIOPSY_LOCATION, "Biopsy location");
+        KEYS_V1_6.put(QuestionnaireKey.STAGE, "Stage");
+        KEYS_V1_6.put(QuestionnaireKey.HAS_MEASURABLE_DISEASE, "Measurable disease");
+        KEYS_V1_6.put(QuestionnaireKey.HAS_CNS_LESIONS, "CNS lesions");
+        KEYS_V1_6.put(QuestionnaireKey.HAS_BRAIN_LESIONS, "Brain lesions");
+        KEYS_V1_6.put(QuestionnaireKey.HAS_BONE_LESIONS, "Bone lesions");
+        KEYS_V1_6.put(QuestionnaireKey.HAS_LIVER_LESIONS, "Liver lesions");
+        KEYS_V1_6.put(QuestionnaireKey.OTHER_LESIONS, "Other lesions (e.g. lymph node, pulmonal)");
+        KEYS_V1_6.put(QuestionnaireKey.IHC_TEST_RESULTS, "IHC test results");
+        KEYS_V1_6.put(QuestionnaireKey.PDL1_TEST_RESULTS, "PD L1 test results");
+        KEYS_V1_6.put(QuestionnaireKey.WHO_STATUS, "WHO status");
+        KEYS_V1_6.put(QuestionnaireKey.UNRESOLVED_TOXICITIES, "Unresolved toxicities grade => 2");
+        KEYS_V1_6.put(QuestionnaireKey.SIGNIFICANT_CURRENT_INFECTION, "Significant current infection");
+        KEYS_V1_6.put(QuestionnaireKey.SIGNIFICANT_ABERRATION_LATEST_ECG, "Significant aberration on latest ECG");
+        KEYS_V1_6.put(QuestionnaireKey.COMPLICATIONS, "Cancer-related complications (e.g. pleural effusion)");
+
         KEYS_V1_5.put(QuestionnaireKey.TREATMENT_HISTORY_CURRENT_TUMOR, "Treatment history current tumor");
         KEYS_V1_5.put(QuestionnaireKey.OTHER_ONCOLOGICAL_HISTORY, "Other oncological history");
         KEYS_V1_5.put(QuestionnaireKey.SECONDARY_PRIMARY, "Secondary primary");
@@ -199,6 +222,8 @@ final class QuestionnaireMapping {
     public static Map<QuestionnaireKey, String> mapping(@NotNull QuestionnaireEntry entry) {
         QuestionnaireVersion version = QuestionnaireVersion.version(entry);
         switch (version) {
+            case V1_6:
+                return KEYS_V1_6;
             case V1_5:
                 return KEYS_V1_5;
             case V1_4:
--- clinical/src/test/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireVersionTest.java
+++ clinical/src/test/java/com/hartwig/actin/clinical/feed/questionnaire/QuestionnaireVersionTest.java
@@ -9,6 +9,7 @@ public class QuestionnaireVersionTest {
 
     @Test
     public void canResolveAllVersions() {
+        assertVersion(QuestionnaireVersion.V1_6, TestQuestionnaireFactory.createTestQuestionnaireValueV1_6());
         assertVersion(QuestionnaireVersion.V1_5, TestQuestionnaireFactory.createTestQuestionnaireValueV1_5());
         assertVersion(QuestionnaireVersion.V1_4, TestQuestionnaireFactory.createTestQuestionnaireValueV1_4());
         assertVersion(QuestionnaireVersion.V1_3, TestQuestionnaireFactory.createTestQuestionnaireValueV1_3());
