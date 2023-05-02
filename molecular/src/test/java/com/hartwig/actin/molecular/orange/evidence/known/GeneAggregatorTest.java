64f409a941803e05e9fc822c5ac1d8b8b9ae9636
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2023-05-02 10:14:02 -0400

    ACTIN-84 Add one more test for gene aggregation
    More complex precedence logic.

--- molecular/src/test/java/com/hartwig/actin/molecular/orange/evidence/known/GeneAggregatorTest.java
+++ molecular/src/test/java/com/hartwig/actin/molecular/orange/evidence/known/GeneAggregatorTest.java
@@ -14,10 +14,16 @@ public class GeneAggregatorTest {
 
     private static final String GENE = "gene";
     private static final KnownGene ONCO_GENE = gene(GeneRole.ONCO);
+    private static final KnownGene BOTH_GENE = gene(GeneRole.BOTH);
     private static final KnownGene UNKNOWN_GENE = gene(GeneRole.UNKNOWN);
 
     @Test
-    public void shouldUseKnownGeneRolesWhenMultipleRoles() {
+    public void shouldUseBothGeneRolesWhenOncoBothAndUnknownInSet() {
+        assertThat(GeneAggregator.aggregate(Set.of(ONCO_GENE, BOTH_GENE, UNKNOWN_GENE))).containsOnly(BOTH_GENE);
+    }
+
+    @Test
+    public void shouldUseOncoGeneRolesWhenOncoAndUnknownInSet() {
         assertThat(GeneAggregator.aggregate(Set.of(ONCO_GENE, UNKNOWN_GENE))).containsOnly(ONCO_GENE);
     }
 
