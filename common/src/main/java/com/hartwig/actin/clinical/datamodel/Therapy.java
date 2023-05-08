c281dae75f0eb2cc33dda925a146262752d79460
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-05-08 17:06:27 -0400

    ACTIN-26: Make Therapy and TherapyHistoryEntry implement treatment interfaces

--- common/src/main/java/com/hartwig/actin/clinical/datamodel/Therapy.java
+++ common/src/main/java/com/hartwig/actin/clinical/datamodel/Therapy.java
@@ -1,6 +1,9 @@
 package com.hartwig.actin.clinical.datamodel;
 
 import java.util.List;
+import java.util.Set;
+
+import com.hartwig.actin.treatment.datamodel.EligibilityFunction;
 
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
@@ -8,7 +11,34 @@ import org.jetbrains.annotations.Nullable;
 
 @Value.Immutable
 @Value.Style(passAnnotations = { NotNull.class, Nullable.class })
-public abstract class Therapy {
+public abstract class Therapy implements Treatment {
+
+    @NotNull
+    public abstract String name();
+
+    @Nullable
+    public abstract Therapy therapy();
+
+    public abstract boolean isSystemic();
+
+    public abstract boolean isOptional();
+
+    public abstract int score();
+
+    @NotNull
+    public abstract Set<TreatmentTypes> types();
+
+    @NotNull
+    public abstract Set<TreatmentCategory> categories();
+
+    @NotNull
+    public abstract Set<EligibilityFunction> eligibilityFunctions();
+
+    @NotNull
+    public abstract Set<Integer> lines();
+
+    @NotNull
+    public abstract Set<String> synonyms();
 
     @NotNull
     public abstract List<Drug> drugs();
