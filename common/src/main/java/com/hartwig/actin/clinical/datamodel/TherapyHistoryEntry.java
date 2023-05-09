ceea38f99f161a35a588cbcd681cf03afb600813
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-05-09 10:15:26 -0400

    ACTIN-26: Make Therapy and TherapyHistoryEntry implement treatment interfaces

---
+++ common/src/main/java/com/hartwig/actin/clinical/datamodel/TherapyHistoryEntry.java
@@ -0,0 +1,50 @@
+package com.hartwig.actin.clinical.datamodel;
+
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.Set;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public abstract class TherapyHistoryEntry implements TreatmentHistoryEntry {
+
+    @NotNull
+    public abstract Set<Treatment> treatments();
+
+    @Nullable
+    public abstract Year startYear();
+
+    @Nullable
+    public abstract YearMonth startMonth();
+
+    @Nullable
+    public abstract Year endYear();
+
+    @Nullable
+    public abstract YearMonth endMonth();
+
+    @Nullable
+    public abstract Boolean ongoing();
+
+    @Nullable
+    public abstract Integer chemoCycles();
+
+    @Nullable
+    public abstract TreatmentResponse bestResponse();
+
+    @Nullable
+    public abstract StopReason stopReason();
+
+    @Nullable
+    public abstract String stopReasonDetail();
+
+    @Nullable
+    public abstract Set<Toxicity> toxicities();
+
+    @Nullable
+    public abstract Intent intent();
+}
--- common/src/main/java/com/hartwig/actin/clinical/datamodel/TreatmentHistoryEntry.java
+++ common/src/main/java/com/hartwig/actin/clinical/datamodel/TreatmentHistoryEntry.java
@@ -4,47 +4,20 @@ import java.time.Year;
 import java.time.YearMonth;
 import java.util.Set;
 
-import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Value.Immutable
-@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
-public abstract class TreatmentHistoryEntry {
+public interface TreatmentHistoryEntry {
 
     @NotNull
-    public abstract Set<Treatment> treatments();
+    Set<Treatment> treatments();
 
     @Nullable
-    public abstract Year startYear();
+    Year startYear();
 
     @Nullable
-    public abstract YearMonth startMonth();
+    YearMonth startMonth();
 
     @Nullable
-    public abstract Year endYear();
-
-    @Nullable
-    public abstract YearMonth endMonth();
-
-    @Nullable
-    public abstract Boolean ongoing();
-
-    @Nullable
-    public abstract Integer chemoCycles();
-
-    @Nullable
-    public abstract TreatmentResponse bestResponse();
-
-    @Nullable
-    public abstract StopReason stopReason();
-
-    @Nullable
-    public abstract String stopReasonDetail();
-
-    @Nullable
-    public abstract Set<Toxicity> toxicities();
-
-    @Nullable
-    public abstract Intent intent();
+    Intent intent();
 }
