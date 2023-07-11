f309f4ea44024a43d6fa067b6dbe1e70aaafe9b2
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2023-07-11 10:33:51 +0200

    ACTIN-178: Evaluate QT prolongating rule

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/medication/CurrentlyGetsQTProlongatingMedication.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/medication/CurrentlyGetsQTProlongatingMedication.kt
@@ -5,11 +5,20 @@ import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 
-class CurrentlyGetsQTProlongatingMedication internal constructor() : EvaluationFunction {
+class CurrentlyGetsQTProlongatingMedication internal constructor(private val selector: MedicationSelector) :
+    EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
-        return EvaluationFactory.undetermined(
-            "Currently not determined if patient gets QT prolongating medication",
-            "Potential QT prolongating medication undetermined"
-        )
+        val hasReceivedQT = selector.activeWithQTProlongatingMedication(record.clinical().medications()).isNotEmpty()
+        return if (hasReceivedQT) {
+            EvaluationFactory.pass(
+                "Patient currently gets known QT prolongating medication",
+                "QT prolongating medication use "
+            )
+        } else {
+            EvaluationFactory.recoverableFail(
+                "Patient currently does not get known QT prolongating medication ",
+                "No QT prolongating medication use "
+            )
+        }
     }
 }
--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/medication/MedicationSelector.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/medication/MedicationSelector.kt
@@ -4,6 +4,7 @@ import com.hartwig.actin.algo.evaluation.util.ValueComparison.stringCaseInsensit
 import com.hartwig.actin.algo.medication.MedicationStatusInterpretation
 import com.hartwig.actin.algo.medication.MedicationStatusInterpreter
 import com.hartwig.actin.clinical.datamodel.Medication
+import com.hartwig.actin.clinical.datamodel.QTProlongatingRisk
 import java.time.LocalDate
 
 internal class MedicationSelector(private val interpreter: MedicationStatusInterpreter) {
@@ -29,8 +30,16 @@ internal class MedicationSelector(private val interpreter: MedicationStatusInter
     fun activeOrRecentlyStoppedWithCategory(
         medications: List<Medication>, categoryToFind: String, minStopDate: LocalDate
     ): List<Medication> {
-        return medications.filter { medication -> medication.categories().any { it.equals(categoryToFind, ignoreCase = true) } }
+        return medications.filter { medication ->
+            medication.categories().any { it.equals(categoryToFind, ignoreCase = true) }
+        }
             .filter { isActive(it) || isRecentlyStopped(it, minStopDate) }
+    }
+
+    fun activeWithQTProlongatingMedication(medications: List<Medication>): List<Medication> {
+        return active(medications).filter { medication ->
+            medication.qtProlongatingRisk().equals(QTProlongatingRisk.KNOWN)
+        }
     }
 
     private fun isRecentlyStopped(medication: Medication, minStopDate: LocalDate): Boolean {
