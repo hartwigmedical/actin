fc62e7012dd8dc9343a10d9038ea7ee1d6ce0fcc
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-07-12 16:06:51 +0200

    ACTIN-115: Fix other test

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/cardiacfunction/HasNormalCardiacFunctionByMUGAOrTTETest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/cardiacfunction/HasNormalCardiacFunctionByMUGAOrTTETest.kt
@@ -5,11 +5,12 @@ import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
 import org.junit.Test
 
 class HasNormalCardiacFunctionByMUGAOrTTETest {
+
     @Test
     fun canEvaluate() {
         val function = HasNormalCardiacFunctionByMUGAOrTTE()
         assertEvaluation(EvaluationResult.NOT_EVALUATED, function.evaluate(CardiacFunctionTestFactory.withLVEF(null)))
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(CardiacFunctionTestFactory.withLVEF(0.8)))
+        assertEvaluation(EvaluationResult.NOT_EVALUATED, function.evaluate(CardiacFunctionTestFactory.withLVEF(0.8)))
         assertEvaluation(EvaluationResult.WARN, function.evaluate(CardiacFunctionTestFactory.withLVEF(0.3)))
     }
 }
