e33bf1dd03b814719050e4be5612cf0873b59fff
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2023-08-15 16:09:41 -0400

    ACTIN-262 Log when we see an ATC code not of 8 chars

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/WhoAtcModel.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/WhoAtcModel.kt
@@ -1,9 +1,11 @@
 package com.hartwig.actin.clinical
 
+import com.hartwig.actin.clinical.curation.CurationDatabaseReader
 import com.hartwig.actin.clinical.datamodel.AtcClassification
 import com.hartwig.actin.clinical.datamodel.ImmutableAtcClassification
 import com.hartwig.actin.clinical.datamodel.ImmutableAtcLevel
 import com.hartwig.actin.util.TabularFile
+import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.nio.file.Files
 
@@ -15,12 +17,17 @@ class WhoAtcModel(private val atcMap: Map<String, String>) : AtcModel {
 
     override fun resolve(rawAtcCode: String): AtcClassification? {
         return if (rawAtcCode.trim().isNotEmpty() && rawAtcCode[0].lowercaseChar() in 'a'..'z') {
-            return ImmutableAtcClassification.builder()
-                .anatomicalMainGroup(atcLevel(rawAtcCode.substring(0, 1)))
-                .therapeuticSubGroup(atcLevel(rawAtcCode.substring(0, 3)))
-                .pharmacologicalSubGroup(atcLevel(rawAtcCode.substring(0, 4)))
-                .chemicalSubGroup(atcLevel(rawAtcCode.substring(0, 5)))
-                .chemicalSubstance(atcLevel(rawAtcCode.substring(0, 7))).build()
+            if (rawAtcCode.length == 8) {
+                ImmutableAtcClassification.builder()
+                    .anatomicalMainGroup(atcLevel(rawAtcCode.substring(0, 1)))
+                    .therapeuticSubGroup(atcLevel(rawAtcCode.substring(0, 3)))
+                    .pharmacologicalSubGroup(atcLevel(rawAtcCode.substring(0, 4)))
+                    .chemicalSubGroup(atcLevel(rawAtcCode.substring(0, 5)))
+                    .chemicalSubstance(atcLevel(rawAtcCode.substring(0, 7))).build()
+            }else{
+                LOGGER.warn("ATC code with incorrect length [{}]", rawAtcCode)
+                null
+            }
         } else {
             null
         }
@@ -33,6 +40,7 @@ class WhoAtcModel(private val atcMap: Map<String, String>) : AtcModel {
         atcMap[level] ?: throw IllegalArgumentException("ATC code [$level] not found in tree")
 
     companion object {
+        private val LOGGER = LogManager.getLogger(CurationDatabaseReader::class.java)
         fun createFromFile(tsvPath: String): WhoAtcModel {
             val lines = Files.readAllLines(File(tsvPath).toPath())
             val fields = TabularFile.createFields(lines[0].split(TabularFile.DELIMITER).dropLastWhile { it.isEmpty() }
