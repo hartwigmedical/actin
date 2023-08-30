2aa3a4bccb7f43a6ba21b56a7c575be08cd51c32
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-08-30 12:06:06 -0400

    ACTIN-287: Allow overriding display string for treatments

--- common/src/main/java/com/hartwig/actin/clinical/datamodel/treatment/Treatment.java
+++ common/src/main/java/com/hartwig/actin/clinical/datamodel/treatment/Treatment.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import com.hartwig.actin.Displayable;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface Treatment extends Displayable {
 
@@ -24,11 +25,17 @@ public interface Treatment extends Displayable {
 
     boolean isSystemic();
 
+    @Nullable
+    String displayOverride();
+
     @NotNull
     @Override
     default String display() {
-        return Arrays.stream(name().replace("_", " ").split("\\+"))
-                .map(name -> (name.length() < 2) ? name : name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase())
-                .collect(Collectors.joining("+"));
+        String alternateDisplay = displayOverride();
+        return (alternateDisplay != null)
+                ? alternateDisplay
+                : Arrays.stream(name().replace("_", " ").split("\\+"))
+                        .map(name -> (name.length() < 2) ? name : name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase())
+                        .collect(Collectors.joining("+"));
     }
 }
