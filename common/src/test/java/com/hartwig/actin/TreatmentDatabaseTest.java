77f133c37371e169687f95decdd2af82c6265084
jbartletthmf <125090035+jbartletthmf@users.noreply.github.com>
2023-08-31 13:06:35 -0400

    ACTIN-289: Convert spaces to underscores for treatment lookups (#161)

--- common/src/test/java/com/hartwig/actin/TreatmentDatabaseTest.java
+++ common/src/test/java/com/hartwig/actin/TreatmentDatabaseTest.java
@@ -4,11 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.io.Resources;
 import com.hartwig.actin.clinical.datamodel.treatment.Drug;
 import com.hartwig.actin.clinical.datamodel.treatment.DrugType;
+import com.hartwig.actin.clinical.datamodel.treatment.ImmutableDrug;
+import com.hartwig.actin.clinical.datamodel.treatment.ImmutableOtherTreatment;
 import com.hartwig.actin.clinical.datamodel.treatment.Therapy;
 import com.hartwig.actin.clinical.datamodel.treatment.Treatment;
 import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory;
@@ -17,6 +21,7 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 public class TreatmentDatabaseTest {
+
     @Test
     public void shouldFindExistingTreatmentByName() throws IOException {
         assertThat(treatmentDatabase().findTreatmentByName("nonexistent")).isNull();
@@ -31,12 +36,27 @@ public class TreatmentDatabaseTest {
     }
 
     @Test
+    public void shouldEquateSpacesAndUnderscoresInTreatmentLookups() {
+        Treatment treatment = ImmutableOtherTreatment.builder().isSystemic(false).name("MULTIWORD_NAME").build();
+        TreatmentDatabase treatmentDatabase =
+                new TreatmentDatabase(Collections.emptyMap(), Map.of(treatment.name().toLowerCase(), treatment));
+        assertThat(treatmentDatabase.findTreatmentByName("Multiword name")).isEqualTo(treatment);
+    }
+
+    @Test
     public void shouldFindExistingDrugByName() throws IOException {
         assertThat(treatmentDatabase().findDrugByName("nonexistent")).isNull();
         Drug drug = treatmentDatabase().findDrugByName("Capecitabine");
         assertThat(drug).isNotNull();
         assertThat(drug.name()).isEqualTo("CAPECITABINE");
         assertThat(drug.drugTypes()).containsExactly(DrugType.ANTIMETABOLITE);
+    }
+
+    @Test
+    public void shouldEquateSpacesAndUnderscoresInDrugLookups() {
+        Drug drug = ImmutableDrug.builder().name("MULTIWORD_NAME").category(TreatmentCategory.CHEMOTHERAPY).build();
+        TreatmentDatabase treatmentDatabase = new TreatmentDatabase(Map.of(drug.name().toLowerCase(), drug), Collections.emptyMap());
+        assertThat(treatmentDatabase.findDrugByName("Multiword name")).isEqualTo(drug);
     }
 
     @NotNull
