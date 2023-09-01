01338999c3991f60cae3155ca746f5318bcf0f1b
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2023-09-01 15:48:28 +0200

    ACTIN-291: rewrote test files and added recoverability

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMeasurableDiseaseRecistTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMeasurableDiseaseRecistTest.kt
@@ -4,24 +4,50 @@ import com.hartwig.actin.algo.datamodel.EvaluationResult
 import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
 import com.hartwig.actin.doid.TestDoidModelFactory
 import org.junit.Test
+import org.junit.Assert.assertTrue
 
 class HasMeasurableDiseaseRecistTest {
     @Test
-    fun canEvaluate() {
-        val doidModel = TestDoidModelFactory.createWithOneParentChild("100", "200")
-        val function = HasMeasurableDiseaseRecist(doidModel)
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(TumorTestFactory.withMeasurableDisease(null)))
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(TumorTestFactory.withMeasurableDisease(true)))
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(TumorTestFactory.withMeasurableDisease(false)))
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(TumorTestFactory.withMeasurableDiseaseAndDoid(true, "random")))
-        assertEvaluation(
-            EvaluationResult.WARN,
-            function.evaluate(
-                TumorTestFactory.withMeasurableDiseaseAndDoid(
-                    true,
-                    HasMeasurableDiseaseRecist.NON_RECIST_TUMOR_DOIDS.iterator().next()
-                )
+    fun shouldPassWhenHasMeasurableDiseaseIsTrue() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(true))
+        assertEvaluation(EvaluationResult.PASS, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun shouldPassWhenHasMeasurableDiseaseIsTrueAndRandomDoid() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDiseaseAndDoid(true, "random"))
+        assertEvaluation(EvaluationResult.PASS, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun shouldFailWhenHasMeasurableDiseaseIsFalse() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(false))
+        assertEvaluation(EvaluationResult.FAIL, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun shouldBeUndeterminedWhenHasMeasurableDiseaseIsUndetermined() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(null))
+        assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun shouldWarnWhenUncertainIfEvaluatedAgainstRecist() {
+        val evaluation = FUNCTION.evaluate(
+            TumorTestFactory.withMeasurableDiseaseAndDoid(
+                true,
+                HasMeasurableDiseaseRecist.NON_RECIST_TUMOR_DOIDS.iterator().next()
             )
         )
+        assertEvaluation(EvaluationResult.WARN, evaluation)
+    }
+
+    companion object {
+        private val DOID_MODEL = TestDoidModelFactory.createWithOneParentChild("100", "200")
+        private val FUNCTION = HasMeasurableDiseaseRecist(DOID_MODEL)
     }
 }
