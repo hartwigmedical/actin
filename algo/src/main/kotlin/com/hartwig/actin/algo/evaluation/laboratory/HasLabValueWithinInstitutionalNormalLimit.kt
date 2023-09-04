afafe603fe2a236410d919bd995b639dba09d09e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-09-04 14:38:21 +0200

    ACTIN-256: Formatting

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/laboratory/HasLabValueWithinInstitutionalNormalLimit.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/laboratory/HasLabValueWithinInstitutionalNormalLimit.kt
@@ -6,16 +6,24 @@ import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.clinical.datamodel.LabValue
 
 class HasLabValueWithinInstitutionalNormalLimit internal constructor() : LabEvaluationFunction {
+
     override fun evaluate(record: PatientRecord, labValue: LabValue): Evaluation {
         val isOutsideRef = labValue.isOutsideRef
             ?: return EvaluationFactory.recoverableUndetermined(
                 "Could not determine whether ${labValue.code()} is within institutional normal limits",
                 "Undetermined if ${labValue.code()} is within institutional normal limits"
             )
+
         return if (isOutsideRef) {
-            EvaluationFactory.recoverableFail("${labValue.code()} exceeds institutional normal limits", "${labValue.code()} exceeds normal limits")
+            EvaluationFactory.recoverableFail(
+                "${labValue.code()} exceeds institutional normal limits",
+                "${labValue.code()} exceeds normal limits"
+            )
         } else {
-            EvaluationFactory.recoverablePass("${labValue.code()} within institutional normal limits", "${labValue.code()} within normal limits")
+            EvaluationFactory.recoverablePass(
+                "${labValue.code()} within institutional normal limits",
+                "${labValue.code()} within normal limits"
+            )
         }
     }
 }
