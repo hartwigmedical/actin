094991fbbd39322786ecf00fff90d3f0cac7d05c
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-09-07 13:43:49 -0400

    ACTIN-275: Delete unused builder

--- report/src/test/kotlin/com/hartwig/actin/report/interpretation/EvaluatedCohortTestFactory.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/interpretation/EvaluatedCohortTestFactory.kt
@@ -1,7 +1,5 @@
 package com.hartwig.actin.report.interpretation
 
-import org.apache.logging.log4j.util.Strings
-
 object EvaluatedCohortTestFactory {
 
     fun evaluatedCohort(
@@ -24,14 +22,5 @@ object EvaluatedCohortTestFactory {
             warnings = emptySet(),
             fails = emptySet()
         )
-    }
-
-    fun builder(): ImmutableEvaluatedCohort.Builder {
-        return ImmutableEvaluatedCohort.builder()
-            .trialId(Strings.EMPTY)
-            .acronym(Strings.EMPTY)
-            .isPotentiallyEligible(false)
-            .isOpen(false)
-            .hasSlotsAvailable(false)
     }
 }
