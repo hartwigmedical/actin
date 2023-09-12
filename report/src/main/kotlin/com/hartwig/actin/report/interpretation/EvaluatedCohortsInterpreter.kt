ff6afc050c21383d1371fb49d86a715edb7183d4
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2023-09-12 14:35:27 +0200

    ACTIN-308: Test

--- report/src/main/kotlin/com/hartwig/actin/report/interpretation/EvaluatedCohortsInterpreter.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/interpretation/EvaluatedCohortsInterpreter.kt
@@ -4,10 +4,20 @@ import com.hartwig.actin.molecular.datamodel.driver.Driver
 
 class EvaluatedCohortsInterpreter(evaluatedCohorts: List<EvaluatedCohort>) {
     private val eligibleTrialsByInclusionEvent: Map<String, List<String>>
+    private val nonBlacklistedOpenTrialsByInclusionEvent: Map<String, List<String>>
 
     init {
         eligibleTrialsByInclusionEvent = evaluatedCohorts
             .filter(EvaluatedCohort::isPotentiallyEligible)
+            .filter(EvaluatedCohort::isOpen)
+            .flatMap { cohort -> cohort.molecularEvents.map { it to cohort.acronym } }
+            .groupBy({ it.first }, { it.second })
+            .mapValues { (_, acronym) -> acronym.sorted() }
+    }
+
+    init {
+        nonBlacklistedOpenTrialsByInclusionEvent = evaluatedCohorts
+            .filterNot(EvaluatedCohort::isBlacklisted)
             .filter(EvaluatedCohort::isOpen)
             .flatMap { cohort -> cohort.molecularEvents.map { it to cohort.acronym } }
             .groupBy({ it.first }, { it.second })
@@ -19,7 +29,7 @@ class EvaluatedCohortsInterpreter(evaluatedCohorts: List<EvaluatedCohort>) {
     }
 
     fun driverIsActionable(driver: Driver): Boolean {
-        return (driver.evidence().externalEligibleTrials().isNotEmpty() || eligibleTrialsByInclusionEvent.containsKey(driver.event())
+        return (driver.evidence().externalEligibleTrials().isNotEmpty() || nonBlacklistedOpenTrialsByInclusionEvent.containsKey(driver.event())
                 || driver.evidence().approvedTreatments().isNotEmpty())
     }
 }
