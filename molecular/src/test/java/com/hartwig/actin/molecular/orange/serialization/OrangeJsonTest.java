abc51c9734a9598a999f2a2e8ac61ffe32ade8ee
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-09-22 15:00:28 -0400

    ACTIN-86: update OrangejsonTest to run on top of upstream datamodel
    Turns out this won't work due to the
    sample orange json files missing
    attributes that upstream requires.

--- molecular/src/test/java/com/hartwig/actin/molecular/orange/serialization/OrangeJsonTest.java
+++ molecular/src/test/java/com/hartwig/actin/molecular/orange/serialization/OrangeJsonTest.java
@@ -11,46 +11,51 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.hartwig.hmftools.datamodel.OrangeJson;
 
 import com.google.common.io.Resources;
 import com.hartwig.actin.TestDataFactory;
-import com.hartwig.actin.molecular.datamodel.ExperimentType;
-import com.hartwig.actin.molecular.datamodel.characteristics.CuppaPrediction;
-import com.hartwig.actin.molecular.orange.datamodel.OrangeRecord;
-import com.hartwig.actin.molecular.orange.datamodel.OrangeRefGenomeVersion;
-import com.hartwig.actin.molecular.orange.datamodel.chord.ChordRecord;
-import com.hartwig.actin.molecular.orange.datamodel.chord.ChordStatus;
-import com.hartwig.actin.molecular.orange.datamodel.cuppa.CuppaRecord;
-import com.hartwig.actin.molecular.orange.datamodel.lilac.LilacHlaAllele;
-import com.hartwig.actin.molecular.orange.datamodel.lilac.LilacRecord;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxBreakend;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxBreakendType;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxCodingType;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxFusion;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxFusionDriverLikelihood;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxFusionType;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxRecord;
-import com.hartwig.actin.molecular.orange.datamodel.linx.LinxRegionType;
-import com.hartwig.actin.molecular.orange.datamodel.peach.PeachEntry;
-import com.hartwig.actin.molecular.orange.datamodel.peach.PeachRecord;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleCodingEffect;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleDriver;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleDriverType;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleGainLoss;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleGainLossInterpretation;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleHotspotType;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleMicrosatelliteStatus;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleQCStatus;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleRecord;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleTumorMutationalStatus;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleVariant;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleVariantEffect;
-import com.hartwig.actin.molecular.orange.datamodel.purple.PurpleVariantType;
-import com.hartwig.actin.molecular.orange.datamodel.virus.VirusDriverLikelihood;
-import com.hartwig.actin.molecular.orange.datamodel.virus.VirusInterpretation;
-import com.hartwig.actin.molecular.orange.datamodel.virus.VirusInterpreterEntry;
-import com.hartwig.actin.molecular.orange.datamodel.virus.VirusInterpreterRecord;
-import com.hartwig.actin.molecular.orange.datamodel.virus.VirusQCStatus;
+import com.hartwig.hmftools.datamodel.orange.ExperimentType;
+import com.hartwig.hmftools.datamodel.cuppa.CuppaPrediction;
+import com.hartwig.hmftools.datamodel.orange.OrangeRecord;
+import com.hartwig.hmftools.datamodel.orange.OrangeRefGenomeVersion;
+import com.hartwig.hmftools.datamodel.chord.ChordRecord;
+import com.hartwig.hmftools.datamodel.chord.ChordStatus;
+import com.hartwig.hmftools.datamodel.cuppa.CuppaData;
+import com.hartwig.hmftools.datamodel.hla.LilacAllele;
+import com.hartwig.hmftools.datamodel.hla.LilacRecord;
+import com.hartwig.hmftools.datamodel.linx.LinxBreakend;
+import com.hartwig.hmftools.datamodel.linx.LinxBreakendType;
+import com.hartwig.hmftools.datamodel.gene.TranscriptCodingType;
+import com.hartwig.hmftools.datamodel.linx.LinxFusion;
+import com.hartwig.hmftools.datamodel.linx.FusionLikelihoodType;
+import com.hartwig.hmftools.datamodel.linx.LinxFusionType;
+import com.hartwig.hmftools.datamodel.linx.LinxRecord;
+import com.hartwig.hmftools.datamodel.gene.TranscriptRegionType;
+import com.hartwig.hmftools.datamodel.peach.PeachGenotype;
+import com.hartwig.hmftools.datamodel.purple.PurpleCodingEffect;
+import com.hartwig.hmftools.datamodel.purple.PurpleDriver;
+import com.hartwig.hmftools.datamodel.purple.PurpleDriverType;
+import com.hartwig.hmftools.datamodel.purple.PurpleGainLoss;
+import com.hartwig.hmftools.datamodel.purple.CopyNumberInterpretation;
+import com.hartwig.hmftools.datamodel.purple.Hotspot;
+import com.hartwig.hmftools.datamodel.purple.PurpleMicrosatelliteStatus;
+import com.hartwig.hmftools.datamodel.purple.PurpleQCStatus;
+import com.hartwig.hmftools.datamodel.purple.PurpleRecord;
+import com.hartwig.hmftools.datamodel.purple.PurpleTumorMutationalStatus;
+import com.hartwig.hmftools.datamodel.purple.PurpleVariant;
+import com.hartwig.hmftools.datamodel.purple.PurpleVariantEffect;
+import com.hartwig.hmftools.datamodel.purple.PurpleVariantType;
+import com.hartwig.hmftools.datamodel.virus.AnnotatedVirus;
+import com.hartwig.hmftools.datamodel.virus.VirusLikelihoodType;
+import com.hartwig.hmftools.datamodel.virus.VirusInterpretation;
+import com.hartwig.hmftools.datamodel.virus.VirusInterpreterData;
+import com.hartwig.hmftools.datamodel.virus.VirusBreakendQCStatus;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -69,36 +74,36 @@ public class OrangeJsonTest {
 
     @Test
     public void canReadMinimallyEmptyOrangeRecordJson() throws IOException {
-        OrangeRecord x = OrangeJson.read(MINIMALLY_EMPTY_ORANGE_JSON);
-        assertNotNull(OrangeJson.read(MINIMALLY_EMPTY_ORANGE_JSON));
+        OrangeRecord x = OrangeJson.getInstance().read(MINIMALLY_EMPTY_ORANGE_JSON);
+        assertNotNull(OrangeJson.getInstance().read(MINIMALLY_EMPTY_ORANGE_JSON));
     }
 
     @Test
     public void canReadRealOrangeRecordJson() throws IOException {
-        assertNotNull(OrangeJson.read(REAL_ORANGE_JSON));
+        assertNotNull(OrangeJson.getInstance().read(REAL_ORANGE_JSON));
     }
 
     @Test
     public void canReadMinimallyPopulatedOrangeRecordJson() throws IOException {
-        OrangeRecord record = OrangeJson.read(MINIMALLY_POPULATED_ORANGE_JSON);
+        OrangeRecord record = OrangeJson.getInstance().read(MINIMALLY_POPULATED_ORANGE_JSON);
 
         assertEquals(TestDataFactory.TEST_SAMPLE, record.sampleId());
         assertEquals(LocalDate.of(2022, 1, 20), record.experimentDate());
-        assertEquals(ExperimentType.WGS, record.experimentType());
+        assertEquals(ExperimentType.WHOLE_GENOME, record.experimentType());
         assertEquals(OrangeRefGenomeVersion.V37, record.refGenomeVersion());
 
         assertPurple(record.purple());
         assertLinx(record.linx());
-        assertPeach(record.peach().orElseThrow());
-        assertCuppa(record.cuppa().orElseThrow());
-        assertVirusInterpreter(record.virusInterpreter().orElseThrow());
+        assertPeach(record.peach());
+        assertCuppa(record.cuppa());
+        assertVirusInterpreter(record.virusInterpreter());
         assertLilac(record.lilac());
-        assertChord(record.chord().orElseThrow());
+        assertChord(record.chord());
     }
 
     @Test
     public void shouldThrowWhenOrangeRecordJsonContainsLinxGermlineFeatures() {
-        assertThrows(RuntimeException.class, () -> OrangeJson.read(MINIMALLY_POPULATED_ORANGE_JSON_WITH_GERMLINE));
+        assertThrows(RuntimeException.class, () -> OrangeJson.getInstance().read(MINIMALLY_POPULATED_ORANGE_JSON_WITH_GERMLINE));
     }
 
     private static void assertPurple(@NotNull PurpleRecord purple) {
@@ -106,8 +111,8 @@ public class OrangeJsonTest {
         assertFalse(purple.fit().containsTumorCells());
         assertEquals(0.12, purple.fit().purity(), EPSILON);
         assertEquals(3.1, purple.fit().ploidy(), EPSILON);
-        assertEquals(1, purple.fit().qcStatuses().size());
-        assertTrue(purple.fit().qcStatuses().contains(PurpleQCStatus.PASS));
+        assertEquals(1, purple.fit().qc().status().size());
+        assertTrue(purple.fit().qc().status().contains(PurpleQCStatus.PASS));
 
         assertEquals(PurpleMicrosatelliteStatus.MSS, purple.characteristics().microsatelliteStatus());
         assertEquals(13.71, purple.characteristics().tumorMutationalBurdenPerMb(), EPSILON);
@@ -115,24 +120,27 @@ public class OrangeJsonTest {
         assertEquals(185, purple.characteristics().tumorMutationalLoad());
         assertEquals(PurpleTumorMutationalStatus.HIGH, purple.characteristics().tumorMutationalLoadStatus());
 
-        assertEquals(3, purple.drivers().size());
-        PurpleDriver driver1 = findDriverByGene(purple.drivers(), "SF3B1");
+        List<PurpleDriver> drivers = Stream.concat(purple.somaticDrivers().stream(), purple.germlineDrivers().stream()).collect(Collectors.toList());
+        assertEquals(3, drivers.size());
+        PurpleDriver driver1 = findDriverByGene(drivers, "SF3B1");
         assertEquals("ENST00000335508", driver1.transcript());
-        assertEquals(PurpleDriverType.MUTATION, driver1.type());
+        assertEquals(PurpleDriverType.MUTATION, driver1.driver());
         assertEquals(0.2, driver1.driverLikelihood(), EPSILON);
 
-        PurpleDriver driver2 = findDriverByGene(purple.drivers(), "SMAD4");
+        PurpleDriver driver2 = findDriverByGene(drivers, "SMAD4");
         assertEquals("ENST00000342988", driver2.transcript());
-        assertEquals(PurpleDriverType.DEL, driver2.type());
+        assertEquals(PurpleDriverType.DEL, driver2.driver());
         assertEquals(1.0, driver2.driverLikelihood(), EPSILON);
 
-        PurpleDriver driver3 = findDriverByGene(purple.drivers(), "BRCA1");
+        PurpleDriver driver3 = findDriverByGene(drivers, "BRCA1");
         assertEquals("ENST00000471181", driver3.transcript());
-        assertEquals(PurpleDriverType.GERMLINE_MUTATION, driver3.type());
+        assertEquals(PurpleDriverType.GERMLINE_MUTATION, driver3.driver());
         assertEquals(0.8, driver3.driverLikelihood(), EPSILON);
 
-        assertEquals(2, purple.variants().size());
-        PurpleVariant variant1 = findVariantByGene(purple.variants(), "SF3B1");
+        List<PurpleVariant> variants = Stream.concat(purple.allSomaticVariants().stream(),
+                purple.reportableGermlineVariants().stream()).collect(Collectors.toList());
+        assertEquals(2, variants.size());
+        PurpleVariant variant1 = findVariantByGene(variants, "SF3B1");
         assertTrue(variant1.reported());
         assertEquals(PurpleVariantType.SNP, variant1.type());
         assertEquals("2", variant1.chromosome());
@@ -141,7 +149,7 @@ public class OrangeJsonTest {
         assertEquals("A", variant1.alt());
         assertEquals(2.03, variant1.variantCopyNumber(), EPSILON);
         assertEquals(3.02, variant1.adjustedCopyNumber(), EPSILON);
-        assertEquals(PurpleHotspotType.NON_HOTSPOT, variant1.hotspot());
+        assertEquals(Hotspot.NON_HOTSPOT, variant1.hotspot());
         assertEquals(0.0, variant1.subclonalLikelihood(), EPSILON);
         assertFalse(variant1.biallelic());
         assertNull(variant1.localPhaseSets());
@@ -156,7 +164,9 @@ public class OrangeJsonTest {
         assertEquals(PurpleCodingEffect.MISSENSE, variant1.canonicalImpact().codingEffect());
         assertTrue(variant1.otherImpacts().isEmpty());
 
-        PurpleVariant variant2 = findVariantByGene(purple.variants(), "BRCA1");
+        List<PurpleVariant> purpleVariants = Stream.concat(purple.allSomaticVariants().stream(),
+                purple.reportableGermlineVariants().stream()).collect(Collectors.toList());
+        PurpleVariant variant2 = findVariantByGene(purpleVariants, "BRCA1");
         assertTrue(variant2.reported());
         assertEquals(PurpleVariantType.SNP, variant2.type());
         assertEquals("17", variant2.chromosome());
@@ -165,7 +175,7 @@ public class OrangeJsonTest {
         assertEquals("T", variant2.alt());
         assertEquals(1.0, variant2.variantCopyNumber(), EPSILON);
         assertEquals(2.0, variant2.adjustedCopyNumber(), EPSILON);
-        assertEquals(PurpleHotspotType.HOTSPOT, variant2.hotspot());
+        assertEquals(Hotspot.HOTSPOT, variant2.hotspot());
         assertEquals(0.2, variant2.subclonalLikelihood(), EPSILON);
         assertFalse(variant2.biallelic());
         assertEquals(2, variant2.localPhaseSets().size());
@@ -183,10 +193,10 @@ public class OrangeJsonTest {
         assertEquals(PurpleCodingEffect.SPLICE, variant2.canonicalImpact().codingEffect());
         assertTrue(variant2.otherImpacts().isEmpty());
 
-        assertEquals(1, purple.gainsLosses().size());
-        PurpleGainLoss gainLoss = purple.gainsLosses().iterator().next();
+        assertEquals(1, purple.allSomaticGainsLosses().size());
+        PurpleGainLoss gainLoss = purple.allSomaticGainsLosses().iterator().next();
         assertEquals("SMAD4", gainLoss.gene());
-        assertEquals(PurpleGainLossInterpretation.FULL_LOSS, gainLoss.interpretation());
+        assertEquals(CopyNumberInterpretation.FULL_LOSS, gainLoss.interpretation());
         assertEquals(0, gainLoss.minCopies());
         assertEquals(1, gainLoss.maxCopies());
     }
@@ -214,49 +224,49 @@ public class OrangeJsonTest {
     }
 
     private static void assertLinx(@NotNull LinxRecord linx) {
-        assertThat(linx.structuralVariants()).hasSize(1)
+        assertThat(linx.allSomaticStructuralVariants()).hasSize(1)
                 .extracting("svId", "clusterId")
                 .contains(tuple(1, 2));
 
-        assertThat(linx.homozygousDisruptions()).hasSize(1)
+        assertThat(linx.somaticHomozygousDisruptions()).hasSize(1)
                 .extracting("gene")
                 .contains("NF1");
-        assertThat(linx.breakends()).hasSize(1)
+        assertThat(linx.allSomaticBreakends()).hasSize(1)
                 .extracting("svId", "gene", "junctionCopyNumber", "undisruptedCopyNumber")
                 .contains(tuple(2, "NF1", 1.1, 1.0));
 
-        LinxBreakend breakend = linx.breakends().stream().filter(b -> b.svId() == 2).findAny().orElseThrow();
-        assertFalse(breakend.reported());
+        LinxBreakend breakend = linx.allSomaticBreakends().stream().filter(b -> b.svId() == 2).findAny().orElseThrow();
+        assertFalse(breakend.reportedDisruption());
         assertEquals(2, breakend.svId());
         assertEquals("NF1", breakend.gene());
         assertEquals(LinxBreakendType.DUP, breakend.type());
         assertEquals(1.1, breakend.junctionCopyNumber(), EPSILON);
         assertEquals(1.0, breakend.undisruptedCopyNumber(), EPSILON);
-        assertEquals(LinxRegionType.EXONIC, breakend.regionType());
-        assertEquals(LinxCodingType.UTR_3P, breakend.codingType());
+        assertEquals(TranscriptRegionType.EXONIC, breakend.regionType());
+        assertEquals(TranscriptCodingType.UTR_3P, breakend.codingType());
 
-        assertEquals(1, linx.fusions().size());
-        LinxFusion fusion = linx.fusions().iterator().next();
+        assertEquals(1, linx.allSomaticFusions().size());
+        LinxFusion fusion = linx.allSomaticFusions().iterator().next();
         assertTrue(fusion.reported());
-        assertEquals(LinxFusionType.KNOWN_PAIR, fusion.type());
+        assertEquals(LinxFusionType.KNOWN_PAIR, fusion.reportedType());
         assertEquals("TMPRSS2", fusion.geneStart());
         assertEquals("ENST00000332149", fusion.geneTranscriptStart());
         assertEquals(1, fusion.fusedExonUp());
         assertEquals("ETV4", fusion.geneEnd());
         assertEquals("ENST00000319349", fusion.geneTranscriptEnd());
         assertEquals(2, fusion.fusedExonDown());
-        assertEquals(LinxFusionDriverLikelihood.HIGH, fusion.driverLikelihood());
+        assertEquals(FusionLikelihoodType.HIGH, fusion.likelihood());
     }
 
-    private static void assertPeach(@NotNull PeachRecord peach) {
-        assertEquals(1, peach.entries().size());
-        PeachEntry entry = peach.entries().iterator().next();
+    private static void assertPeach(@NotNull Set<PeachGenotype> peachGenotypes) {
+        assertEquals(1, peachGenotypes.size());
+        PeachGenotype entry = peachGenotypes.iterator().next();
         assertEquals("DPYD", entry.gene());
         assertEquals("*1_HOM", entry.haplotype());
         assertEquals("Normal Function", entry.function());
     }
 
-    private static void assertCuppa(@NotNull CuppaRecord cuppa) {
+    private static void assertCuppa(@NotNull CuppaData cuppa) {
         assertEquals(1, cuppa.predictions().size());
         CuppaPrediction prediction = cuppa.predictions().iterator().next();
         assertEquals("Melanoma", prediction.cancerType());
@@ -271,26 +281,26 @@ public class OrangeJsonTest {
         assertEquals(expected, actual, EPSILON);
     }
 
-    private static void assertVirusInterpreter(@NotNull VirusInterpreterRecord virusInterpreter) {
-        assertEquals(2, virusInterpreter.entries().size());
-        VirusInterpreterEntry entry1 = findVirusByName(virusInterpreter.entries(), "Human papillomavirus 16");
+    private static void assertVirusInterpreter(@NotNull VirusInterpreterData virusInterpreter) {
+        assertEquals(2, virusInterpreter.allViruses().size());
+        AnnotatedVirus entry1 = findVirusByName(virusInterpreter.allViruses(), "Human papillomavirus 16");
         assertTrue(entry1.reported());
-        assertEquals(VirusQCStatus.NO_ABNORMALITIES, entry1.qcStatus());
+        assertEquals(VirusBreakendQCStatus.NO_ABNORMALITIES, entry1.qcStatus());
         assertEquals(VirusInterpretation.HPV, entry1.interpretation());
         assertEquals(1, entry1.integrations());
-        assertEquals(VirusDriverLikelihood.HIGH, entry1.driverLikelihood());
+        assertEquals(VirusLikelihoodType.HIGH, entry1.virusDriverLikelihoodType());
 
-        VirusInterpreterEntry entry2 = findVirusByName(virusInterpreter.entries(), "Human betaherpesvirus 6B");
+        AnnotatedVirus entry2 = findVirusByName(virusInterpreter.allViruses(), "Human betaherpesvirus 6B");
         assertFalse(entry2.reported());
-        assertEquals(VirusQCStatus.NO_ABNORMALITIES, entry2.qcStatus());
+        assertEquals(VirusBreakendQCStatus.NO_ABNORMALITIES, entry2.qcStatus());
         assertNull(entry2.interpretation());
         assertEquals(0, entry2.integrations());
-        assertEquals(VirusDriverLikelihood.LOW, entry2.driverLikelihood());
+        assertEquals(VirusLikelihoodType.LOW, entry2.virusDriverLikelihoodType());
     }
 
     @NotNull
-    private static VirusInterpreterEntry findVirusByName(@NotNull Iterable<VirusInterpreterEntry> entries, @NotNull String nameToFind) {
-        for (VirusInterpreterEntry entry : entries) {
+    private static AnnotatedVirus findVirusByName(@NotNull Iterable<AnnotatedVirus> entries, @NotNull String nameToFind) {
+        for (AnnotatedVirus entry : entries) {
             if (entry.name().equals(nameToFind)) {
                 return entry;
             }
@@ -303,7 +313,7 @@ public class OrangeJsonTest {
         assertEquals("PASS", lilac.qc());
 
         assertEquals(1, lilac.alleles().size());
-        LilacHlaAllele allele = lilac.alleles().iterator().next();
+        LilacAllele allele = lilac.alleles().iterator().next();
         assertEquals("A*01:01", allele.allele());
         assertEquals(6.1, allele.tumorCopyNumber(), EPSILON);
         assertEquals(5.0, allele.somaticMissense(), EPSILON);
