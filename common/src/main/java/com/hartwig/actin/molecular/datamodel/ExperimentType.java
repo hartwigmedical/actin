8b46d6fd31579ebebb14193dd9eaee9dfbb9c0a6
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-10-10 12:50:02 -0400

    ACTIN-338: update experiment-type display in report

--- common/src/main/java/com/hartwig/actin/molecular/datamodel/ExperimentType.java
+++ common/src/main/java/com/hartwig/actin/molecular/datamodel/ExperimentType.java
@@ -1,6 +1,16 @@
 package com.hartwig.actin.molecular.datamodel;
 
 public enum ExperimentType {
-    TARGETED,
-    WHOLE_GENOME
+    TARGETED {
+        @Override
+        public String toString() {
+            return "Panel analysis";
+        }
+    },
+    WHOLE_GENOME {
+        @Override
+        public String toString() {
+            return "WGS";
+        }
+    }
 }
