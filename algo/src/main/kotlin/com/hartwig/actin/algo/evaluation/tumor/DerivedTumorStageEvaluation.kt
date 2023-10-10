d7b37929ed7ede97fda4b2f5088db605a0919b14
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2023-10-09 22:40:04 -0400

    ACTIN-319: Undo minWith -> minWithOrNull change

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/DerivedTumorStageEvaluation.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/DerivedTumorStageEvaluation.kt
@@ -6,13 +6,13 @@ import com.hartwig.actin.clinical.datamodel.TumorStage
 internal object DerivedTumorStageEvaluation {
    
     fun create(derived: Map<TumorStage, Evaluation>, createEvaluation: (String, String) -> Evaluation): Evaluation {
-        val worstEvaluation = worstEvaluation(derived)!!
+        val worstEvaluation = worstEvaluation(derived)
         return createEvaluation(allSpecificMessagesFrom(derived, worstEvaluation), allGeneralMessagesFrom(worstEvaluation))
     }
 
-    private fun worstEvaluation(derived: Map<TumorStage, Evaluation>): Evaluation? {
+    private fun worstEvaluation(derived: Map<TumorStage, Evaluation>): Evaluation {
         return derived.values
-            .minWithOrNull { e1: Evaluation, e2: Evaluation -> if (e1 == e2) 0 else if (e1.result().isWorseThan(e2.result())) -1 else 1 }
+            .minWith { e1: Evaluation, e2: Evaluation -> if (e1 == e2) 0 else if (e1.result().isWorseThan(e2.result())) -1 else 1 }
     }
 
     private fun allSpecificMessagesFrom(derived: Map<TumorStage, Evaluation>, worstEvaluation: Evaluation): String {
