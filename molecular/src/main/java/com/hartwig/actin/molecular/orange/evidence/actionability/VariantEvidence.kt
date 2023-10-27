a5222a3864f99c7af452dfac739d906935fcecf9
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-10-27 13:23:28 -0400

    ACTIN-355: less stream() more good

--- molecular/src/main/java/com/hartwig/actin/molecular/orange/evidence/actionability/VariantEvidence.kt
+++ molecular/src/main/java/com/hartwig/actin/molecular/orange/evidence/actionability/VariantEvidence.kt
@@ -11,42 +11,36 @@ import com.hartwig.serve.datamodel.gene.GeneEvent
 import com.hartwig.serve.datamodel.hotspot.ActionableHotspot
 import com.hartwig.serve.datamodel.range.ActionableRange
 import java.util.function.BiPredicate
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 internal class VariantEvidence private constructor(private val actionableHotspots: List<ActionableHotspot>,
                                                    private val actionableRanges: List<ActionableRange>, private val applicableActionableGenes: List<ActionableGene>) : EvidenceMatcher<PurpleVariant> {
     override fun findMatches(variant: PurpleVariant): MutableList<ActionableEvent> {
-        return Stream.of(hotspotMatches(variant), rangeMatches(variant), geneMatches(variant))
-            .flatMap { obj: List<ActionableEvent> -> obj.stream() }
-            .collect(Collectors.toList())
+        return listOf(hotspotMatches(variant), rangeMatches(variant), geneMatches(variant)).flatten().toMutableList()
     }
 
     private fun hotspotMatches(variant: PurpleVariant): List<ActionableEvent> {
         return filterMatchingEvents(actionableHotspots, variant) { obj: ActionableHotspot, hotspot: PurpleVariant -> HotspotMatching.isMatch(obj, hotspot) }
     }
 
-    private fun rangeMatches(variant: PurpleVariant): MutableList<ActionableEvent> {
+    private fun rangeMatches(variant: PurpleVariant): List<ActionableEvent> {
         return filterMatchingEvents(actionableRanges, variant) { obj: ActionableRange, rangeAnnotation: PurpleVariant -> RangeMatching.isMatch(obj, rangeAnnotation) }
     }
 
-    private fun geneMatches(variant: PurpleVariant): MutableList<ActionableEvent> {
+    private fun geneMatches(variant: PurpleVariant): List<ActionableEvent> {
         return filterMatchingEvents(applicableActionableGenes, variant) { obj: ActionableGene, geneAnnotation: PurpleVariant -> GeneMatching.isMatch(obj, geneAnnotation) }
     }
 
     private fun <T : ActionableEvent> filterMatchingEvents(events: List<T>, variant: PurpleVariant,
-                                                           predicate: BiPredicate<T, PurpleVariant>): MutableList<ActionableEvent> {
-        return if (!variant.reported()) mutableListOf() else events.stream().filter { event: T -> predicate.test(event, variant) }.collect(Collectors.toList())
+                                                           predicate: BiPredicate<T, PurpleVariant>): List<ActionableEvent> {
+        return if (!variant.reported()) listOf() else events.filter { event: T -> predicate.test(event, variant) }
     }
 
     companion object {
         private val APPLICABLE_GENE_EVENTS = setOf(GeneEvent.ACTIVATION, GeneEvent.INACTIVATION, GeneEvent.ANY_MUTATION)
         fun create(actionableEvents: ActionableEvents): VariantEvidence {
             val applicableActionableGenes = actionableEvents.genes()
-                .stream()
                 .filter { actionableGene: ActionableGene -> APPLICABLE_GENE_EVENTS.contains(actionableGene.event()) }
-                .collect(Collectors.toList())
-            val ranges = Stream.of(actionableEvents.codons(), actionableEvents.exons()).flatMap { obj: MutableList<ActionableRange> -> obj.stream() }.collect(Collectors.toList())
+            val ranges = listOf(actionableEvents.codons(), actionableEvents.exons()).flatten()
             return VariantEvidence(actionableEvents.hotspots(), ranges, applicableActionableGenes)
         }
     }
