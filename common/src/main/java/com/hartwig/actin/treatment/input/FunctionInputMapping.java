25d7691d6f3cf10ae5ad43732994fadb30174f65
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2023-11-23 10:13:42 +0100

    ACTIN-425: Delete unused IS_TREATED_IN_HOSPITAL_X rule

--- common/src/main/java/com/hartwig/actin/treatment/input/FunctionInputMapping.java
+++ common/src/main/java/com/hartwig/actin/treatment/input/FunctionInputMapping.java
@@ -21,7 +21,6 @@ final class FunctionInputMapping {
         RULE_INPUT_MAP.put(EligibilityRule.CAN_GIVE_ADEQUATE_INFORMED_CONSENT, FunctionInput.NONE);
         RULE_INPUT_MAP.put(EligibilityRule.HAS_LIFE_EXPECTANCY_OF_AT_LEAST_X_WEEKS, FunctionInput.ONE_INTEGER);
         RULE_INPUT_MAP.put(EligibilityRule.HAS_LIFE_EXPECTANCY_OF_AT_LEAST_X_MONTHS, FunctionInput.ONE_INTEGER);
-        RULE_INPUT_MAP.put(EligibilityRule.IS_TREATED_IN_HOSPITAL_X, FunctionInput.ONE_STRING);
         RULE_INPUT_MAP.put(EligibilityRule.WILL_PARTICIPATE_IN_TRIAL_IN_COUNTRY_X, FunctionInput.ONE_STRING);
         RULE_INPUT_MAP.put(EligibilityRule.IS_LEGALLY_INSTITUTIONALIZED, FunctionInput.NONE);
         RULE_INPUT_MAP.put(EligibilityRule.IS_INVOLVED_IN_STUDY_PROCEDURES, FunctionInput.NONE);
