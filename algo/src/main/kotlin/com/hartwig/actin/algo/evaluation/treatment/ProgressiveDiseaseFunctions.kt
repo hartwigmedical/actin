46d55d8a8de30ccbee54f429b41da449c056edc5
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2023-11-24 14:01:11 +0100

    ACTIN-419: WIP

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/ProgressiveDiseaseFunctions.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/ProgressiveDiseaseFunctions.kt
@@ -1,5 +1,6 @@
 package com.hartwig.actin.algo.evaluation.treatment
 
+import com.hartwig.actin.algo.evaluation.util.DateComparison
 import com.hartwig.actin.clinical.datamodel.treatment.history.StopReason
 import com.hartwig.actin.clinical.datamodel.treatment.history.TreatmentHistoryEntry
 import com.hartwig.actin.clinical.datamodel.treatment.history.TreatmentResponse
@@ -9,8 +10,18 @@ object ProgressiveDiseaseFunctions {
     fun treatmentResultedInPDOption(treatment: TreatmentHistoryEntry): Boolean? {
         val bestResponse = treatment.treatmentHistoryDetails()?.bestResponse()
         val stopReason = treatment.treatmentHistoryDetails()?.stopReason()
+        val treatmentDuration: Long? = DateComparison.minWeeksBetweenDates(
+            treatment.startYear(),
+            treatment.startMonth(),
+            treatment.treatmentHistoryDetails()?.stopYear(),
+            treatment.treatmentHistoryDetails()?.stopMonth()
+        )
+        val minWeeksToAssumeStopDueToPD = 26 // half year
+
         return when {
             bestResponse == TreatmentResponse.PROGRESSIVE_DISEASE || stopReason == StopReason.PROGRESSIVE_DISEASE -> true
+
+            stopReason == null && treatmentDuration != null && treatmentDuration > minWeeksToAssumeStopDueToPD -> true
 
             stopReason != null -> false
 
--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/ProgressiveDiseaseFunctionsTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/ProgressiveDiseaseFunctionsTest.kt
@@ -10,13 +10,18 @@ import org.junit.Test
 
 class ProgressiveDiseaseFunctionsTest {
     @Test
-    fun shouldReturnTrueWhenStopReasonIsNullAndBestResponseIsPD() {
+    fun `Should return true when stop reason is null and best response is PD and duration null`() {
         assertEquals(true, treatmentResultedInPDOption(treatmentHistoryEntry(null, TreatmentResponse.PROGRESSIVE_DISEASE)))
     }
 
     @Test
-    fun shouldReturnTrueWhenStopReasonIsPDAndBestResponseIsNull() {
+    fun `Should return true when stop reason is PD and best response is null and duration null`() {
         assertEquals(true, treatmentResultedInPDOption(treatmentHistoryEntry(StopReason.PROGRESSIVE_DISEASE, null)))
+    }
+
+    @Test
+    fun `Should return true when stop reason is null and duration at least 26 weeks`() {
+        assertEquals(true, treatmentResultedInPDOption(treatmentHistoryEntryWithDates(null, null, 1999, 1, 2000, 1)))
     }
 
     @Test
@@ -26,7 +31,7 @@ class ProgressiveDiseaseFunctionsTest {
 
     @Test
     fun shouldBeNullWhenStopReasonIsNotPDAndBestResponseIsNull() {
-        assertNull(treatmentResultedInPDOption(treatmentHistoryEntry(StopReason.TOXICITY, null)))
+        assertEquals(false, treatmentResultedInPDOption(treatmentHistoryEntry(StopReason.TOXICITY, null)))
     }
 
     @Test
@@ -50,6 +55,25 @@ class ProgressiveDiseaseFunctionsTest {
                 setOf(TreatmentTestFactory.treatment("test treatment", true)),
                 stopReason = stopReason,
                 bestResponse = bestResponse
+            )
+        }
+
+        private fun treatmentHistoryEntryWithDates(
+            stopReason: StopReason?,
+            bestResponse: TreatmentResponse?,
+            startYear: Int? = null,
+            startMonth: Int? = null,
+            stopYear: Int?,
+            stopMonth: Int?
+        ): TreatmentHistoryEntry {
+            return TreatmentTestFactory.treatmentHistoryEntry(
+                setOf(TreatmentTestFactory.treatment("test treatment", true)),
+                stopReason = stopReason,
+                bestResponse = bestResponse,
+                startYear = startYear,
+                startMonth = startMonth,
+                stopYear = stopYear,
+                stopMonth = stopMonth
             )
         }
     }
