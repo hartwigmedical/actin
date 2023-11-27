47cad54fe76cc5bd735c5e770b209cff063c7a49
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2023-11-27 17:02:20 +0100

    ACTIN-400: PR - fixed error in private val names

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/vitalfunction/HasLimitedBodyWeight.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/vitalfunction/HasLimitedBodyWeight.kt
@@ -4,10 +4,10 @@ import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 
-class HasLimitedBodyWeight(private val minimalWeight: Double) : EvaluationFunction {
+class HasLimitedBodyWeight(private val maximumWeight: Double) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        return BodyWeightFunctions.evaluatePatientForMaximumBodyWeight(record, this.minimalWeight)
+        return BodyWeightFunctions.evaluatePatientForMaximumBodyWeight(record, this.maximumWeight)
     }
 }
 
--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/vitalfunction/HasSufficientBodyWeight.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/vitalfunction/HasSufficientBodyWeight.kt
@@ -4,9 +4,9 @@ import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 
-class HasSufficientBodyWeight(private val maximumWeight: Double) : EvaluationFunction {
+class HasSufficientBodyWeight(private val minimumWeight: Double) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        return BodyWeightFunctions.evaluatePatientForMinimumBodyWeight(record, this.maximumWeight)
+        return BodyWeightFunctions.evaluatePatientForMinimumBodyWeight(record, this.minimumWeight)
     }
 }
