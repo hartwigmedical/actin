114d4dfcf836309c2cf439afad5d26d267a6c637
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2023-11-30 12:05:00 +0100

    ACTIN-437: Improve hasEvaluableDisease logics and add test

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasEvaluableDiseaseTest.kt
@@ -0,0 +1,33 @@
+package com.hartwig.actin.algo.evaluation.tumor
+
+import com.hartwig.actin.algo.datamodel.EvaluationResult
+import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class HasEvaluableDiseaseTest {
+    @Test
+    fun `Should pass when has measurable disease is true`() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(true))
+        assertEvaluation(EvaluationResult.PASS, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun `Should be undetermined when has measurable disease is false`() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(false))
+        assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    @Test
+    fun `Should be undetermined when has measurable disease is unknown`() {
+        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(null))
+        assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
+        assertTrue(evaluation.recoverable())
+    }
+
+    companion object {
+        private val FUNCTION = HasEvaluableDisease()
+    }
+}
