16749def18881de930c5cc8f705952ee7967c130
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2023-12-05 12:18:06 +0100

    ACTIN-446: improved warning message for ECG abnormalities and potential heart disease (#256)

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/cardiacfunction/HasECGAberration.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/cardiacfunction/HasECGAberration.kt
@@ -9,21 +9,21 @@ class HasECGAberration internal constructor() : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
         val ecg = record.clinical().clinicalStatus().ecg()
             ?: return EvaluationFactory.fail(
-                "ECG details are missing, it is assumed there are no abnormalities",
+                "ECG details are missing - it is assumed there are no abnormalities",
                 "Assumed no ECG abnormalities"
             )
         return when {
             ecg.hasSigAberrationLatestECG() && ecg.aberrationDescription() != null -> {
                 EvaluationFactory.pass(
                     "Patient has known ECG abnormalities: " + ecg.aberrationDescription(),
-                    "Present ECG abnormalities: " + ecg.aberrationDescription()
+                    "ECG abnormalities: " + ecg.aberrationDescription()
                 )
             }
 
             ecg.hasSigAberrationLatestECG() -> {
                 EvaluationFactory.pass(
-                    "Patient has present ECG abnormalities, but aberration details unknown",
-                    "Present ECG abnormalities (details unknown)"
+                    "Patient has ECG abnormalities (details unknown)",
+                    "ECG abnormalities present (details unknown)"
                 )
             }
 
