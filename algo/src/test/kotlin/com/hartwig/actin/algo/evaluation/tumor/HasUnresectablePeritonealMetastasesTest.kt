6f4ef90080283b87a47a6d0b19ac91165e3f5af7
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-01-16 11:51:08 +0100

    ACTIN-503: Improve test function

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasUnresectablePeritonealMetastasesTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasUnresectablePeritonealMetastasesTest.kt
@@ -7,19 +7,34 @@ import org.junit.Test
 class HasUnresectablePeritonealMetastasesTest {
 
     @Test
-    fun canEvaluate() {
-        val function = HasUnresectablePeritonealMetastases()
-        EvaluationAssert.assertEvaluation(
-            EvaluationResult.WARN,
-            function.evaluate(TumorTestFactory.withOtherLesions(listOf("Abdominal lesion located in peritoneum")))
-        )
-        EvaluationAssert.assertEvaluation(
-            EvaluationResult.WARN,
-            function.evaluate(TumorTestFactory.withOtherLesions(listOf("Multiple depositions abdominal and peritoneal")))
-        )
+    fun `Should fail if patient has no peritoneal metastases`() {
         EvaluationAssert.assertEvaluation(
             EvaluationResult.FAIL,
-            function.evaluate(TumorTestFactory.withOtherLesions(listOf("Lymph node")))
+            FUNCTION.evaluate(TumorTestFactory.withOtherLesions(listOf("Lymph node")))
         )
+    }
+
+    @Test
+    fun `Should fail if patient has no metastases`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL,
+            FUNCTION.evaluate(TumorTestFactory.withOtherLesions(emptyList()))
+        )
+    }
+
+    @Test
+    fun `Should warn if patient has peritoneal metastases`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.WARN,
+            FUNCTION.evaluate(TumorTestFactory.withOtherLesions(listOf("Abdominal lesion located in peritoneum")))
+        )
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.WARN,
+            FUNCTION.evaluate(TumorTestFactory.withOtherLesions(listOf("Multiple depositions abdominal and peritoneal")))
+        )
+    }
+
+    companion object {
+        val FUNCTION = HasUnresectablePeritonealMetastases()
     }
 }
