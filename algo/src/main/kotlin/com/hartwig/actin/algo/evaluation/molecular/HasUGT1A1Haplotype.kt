4e4dd1d4ea443f77ef2aac37faab3fc4ac8ae224
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-01-16 17:58:17 +0100

    ACTIN-505: PR feedback

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1Haplotype.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1Haplotype.kt
@@ -7,41 +7,35 @@ import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.molecular.datamodel.pharmaco.PharmacoEntry
 
-class HasUGT1A1Haplotype internal constructor(private val haplotypeToFind: String) : EvaluationFunction {
+class HasUGT1A1Haplotype(private val haplotypeToFind: String) : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
         val pharmaco = record.molecular().pharmaco()
 
-        if (pharmaco.find { it.gene() == "UGT1A1" } == null) {
+        if (pharmaco.none { it.gene() == "UGT1A1" }) {
             return EvaluationFactory.recoverableUndetermined("UGT1A1 haplotype undetermined", "UGT1A1 haplotype undetermined")
         }
 
-        val hasUGT1A1Type = hasUGT1A1Type(pharmaco, haplotypeToFind)
-
-        if (hasUGT1A1Type) {
-            return EvaluationFactory.unrecoverable()
+        return if (hasUGT1A1Type(pharmaco, haplotypeToFind)) {
+            EvaluationFactory.unrecoverable()
                 .result(EvaluationResult.PASS)
                 .addInclusionMolecularEvents(haplotypeToFind)
                 .addPassSpecificMessages("Patient has required UGT1A1 type $haplotypeToFind")
                 .addPassGeneralMessages("Patient has required UGT1A1 type")
                 .build()
+        } else {
+            EvaluationFactory.unrecoverable()
+                .result(EvaluationResult.FAIL)
+                .addFailSpecificMessages("Patient does not have required HLA type $haplotypeToFind")
+                .addFailGeneralMessages("Patient does not have required HLA type")
+                .build()
         }
-
-        return EvaluationFactory.unrecoverable()
-            .result(EvaluationResult.FAIL)
-            .addFailSpecificMessages("Patient does not have required HLA type $haplotypeToFind")
-            .addFailGeneralMessages("Patient does not have required HLA type")
-            .build()
     }
 
 
     private fun hasUGT1A1Type(pharmaco: Set<PharmacoEntry>, hapolotypeToFind: String): Boolean {
         for (pharmacoEntry in pharmaco) {
-            if (pharmacoEntry.gene() == "UGT1A1") {
-                for (haplotype in pharmacoEntry.haplotypes()) {
-                    if (haplotype.name() == hapolotypeToFind) {
-                        return true
-                    }
-                }
+            if (pharmacoEntry.gene() == "UGT1A1" && pharmacoEntry.haplotypes().any { it.name() == hapolotypeToFind }) {
+                return true
             }
         }
         return false
--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1HaplotypeTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1HaplotypeTest.kt
@@ -10,8 +10,6 @@ class HasUGT1A1HaplotypeTest {
 
     @Test
     fun `Should pass if patient has at least one UGT1A1 allel with required haplotype`() {
-        val function = HasUGT1A1Haplotype("*1_HET")
-
         EvaluationAssert.assertMolecularEvaluation(
             EvaluationResult.PASS,
             function.evaluate(
@@ -28,14 +26,13 @@ class HasUGT1A1HaplotypeTest {
 
     @Test
     fun `Should fail if patient does not have required UGT1A1 haplotype`() {
-        val function = HasUGT1A1Haplotype("*1_HOM")
         EvaluationAssert.assertMolecularEvaluation(
             EvaluationResult.FAIL,
             function.evaluate(
                 MolecularTestFactory.withUGT1A1(
                     ImmutablePharmacoEntry.builder()
                         .gene("UGT1A1")
-                        .addHaplotypes(TestPharmacoFactory.builder().name("*1_HET").function("Normal function").build())
+                        .addHaplotypes(TestPharmacoFactory.builder().name("*17_HET").function("Normal function").build())
                         .addHaplotypes(TestPharmacoFactory.builder().name("*18_HET").function("Normal function").build())
                         .build()
                 )
@@ -45,7 +42,6 @@ class HasUGT1A1HaplotypeTest {
 
     @Test
     fun `Should return undetermined if patient has no UGT1A1 haplotype information`() {
-        val function = HasUGT1A1Haplotype("*1_HOM")
         EvaluationAssert.assertMolecularEvaluation(
             EvaluationResult.UNDETERMINED,
             function.evaluate(
@@ -58,5 +54,7 @@ class HasUGT1A1HaplotypeTest {
             )
         )
     }
+
+    private val function = HasUGT1A1Haplotype("*1_HET")
 
 }
