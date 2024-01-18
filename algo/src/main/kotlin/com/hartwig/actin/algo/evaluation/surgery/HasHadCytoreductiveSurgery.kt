865cc0773fb8a2f2b9b0c4557aa57e7e340212cc
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-01-18 14:17:34 +0100

    ACTIN-506: Add rule HAS_HAD_CYTOREDUCTIVE_SURGERY

---
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/surgery/HasHadCytoreductiveSurgery.kt
@@ -0,0 +1,42 @@
+package com.hartwig.actin.algo.evaluation.surgery
+
+import com.hartwig.actin.PatientRecord
+import com.hartwig.actin.algo.datamodel.Evaluation
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory
+
+class HasHadCytoreductiveSurgery : EvaluationFunction {
+
+    override fun evaluate(record: PatientRecord): Evaluation {
+        // TODO: once we curate surgery names from the surgeries tsv file evaluate these as well
+        val undeterminedSurgery = record.clinical().oncologicalHistory()
+            .any { it.categories().contains(TreatmentCategory.SURGERY) && it.treatmentName().equals("Surgery", true) }
+
+        if (undeterminedSurgery) {
+            return EvaluationFactory.undetermined(
+                "Undetermined if the surgery the patient received was cytoreductive",
+                "Undetermined if surgery patient received was cytoreductive"
+            )
+        }
+
+        val hasHadCytoreductiveSurgery = record.clinical().oncologicalHistory()
+            .any {
+                (it.categories().contains(TreatmentCategory.SURGERY) && it.treatmentName()
+                    .contains("cytoreduct", true)) || it.treatmentName().contains("HIPEC", true)
+            }
+
+        return if (hasHadCytoreductiveSurgery) {
+            EvaluationFactory.pass(
+                "Patient has had cytoreductive surgery ",
+                "Has had cytoreductive surgery "
+            )
+        } else {
+            EvaluationFactory.fail(
+                "Patient has not received cytoreductive surgery ",
+                "Has not received cytoreductive surgery "
+            )
+        }
+
+    }
+}
---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/surgery/HasHadCytoreductiveSurgeryTest.kt
@@ -0,0 +1,74 @@
+package com.hartwig.actin.algo.evaluation.surgery
+
+import com.hartwig.actin.algo.datamodel.EvaluationResult
+import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.algo.evaluation.surgery.SurgeryTestFactory.withOncologicalHistory
+import com.hartwig.actin.clinical.datamodel.treatment.ImmutableOtherTreatment
+import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory
+import com.hartwig.actin.clinical.datamodel.treatment.history.ImmutableTreatmentHistoryEntry
+import com.hartwig.actin.clinical.datamodel.treatment.history.TreatmentHistoryEntry
+import org.junit.Test
+
+
+class HasHadCytoreductiveSurgeryTest {
+
+    private val function = HasHadCytoreductiveSurgery()
+
+    private fun treatmentHistoryEntry(
+        categories: Set<TreatmentCategory>, name: String
+    ): TreatmentHistoryEntry {
+        return ImmutableTreatmentHistoryEntry.builder()
+            .addTreatments(ImmutableOtherTreatment.builder().name(name).isSystemic(false).categories(categories).build())
+            .build()
+    }
+
+    @Test
+    fun `Should fail with no surgeries`() {
+        assertEvaluation(
+            EvaluationResult.FAIL,
+            function.evaluate(withOncologicalHistory(listOf(treatmentHistoryEntry(setOf(TreatmentCategory.RADIOTHERAPY), "Brachytherapy"))))
+        )
+    }
+
+    @Test
+    fun `Should fail with non cytoreductive surgery`() {
+        assertEvaluation(
+            EvaluationResult.FAIL,
+            function.evaluate(withOncologicalHistory(listOf(treatmentHistoryEntry(setOf(TreatmentCategory.SURGERY), "Nephrectomy"))))
+        )
+    }
+
+    @Test
+    fun `Should pass with history of HIPEC`() {
+        assertEvaluation(
+            EvaluationResult.PASS,
+            function.evaluate(withOncologicalHistory(listOf(treatmentHistoryEntry(setOf(TreatmentCategory.CHEMOTHERAPY), "HIPEC"))))
+        )
+    }
+
+    @Test
+    fun `Should pass with history of cytoreductive surgery`() {
+        assertEvaluation(
+            EvaluationResult.PASS,
+            function.evaluate(
+                withOncologicalHistory(
+                    listOf(
+                        treatmentHistoryEntry(
+                            setOf(TreatmentCategory.SURGERY),
+                            "Cytoreductive surgery"
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `Should return undetermined if surgery name not specified`() {
+        assertEvaluation(
+            EvaluationResult.UNDETERMINED,
+            function.evaluate(withOncologicalHistory(listOf(treatmentHistoryEntry(setOf(TreatmentCategory.SURGERY), "Surgery"))))
+        )
+    }
+
+}
