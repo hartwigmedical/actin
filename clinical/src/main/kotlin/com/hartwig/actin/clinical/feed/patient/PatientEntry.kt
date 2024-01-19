87ac6cff969d4a34637e4764f8dad9bc30d2e46d
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-01-19 12:11:56 -0500

    ACTIN-498 Replace our own TSV parser with Jackson (#283)
    Removes use of our own tsv parsing in favour of the jackson library for the deserialization of clinical feed. 
    The parsing relies on the columns being snake case, and in cases they are not, uses jackons annotations.
    
    Validation moves from is the isValid method to separate validator classes. Invalid entries are still filtered but 
    messages (like from vital functions) can now be propagated up.
    
    Preprocessing is required to handle quotes within quotes. Newlines are now handled by the parsing library and
    will be present in the string as the characters \ and n rather than being a real newline.

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/patient/PatientEntry.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/patient/PatientEntry.kt
@@ -2,8 +2,10 @@ package com.hartwig.actin.clinical.feed.patient
 
 import com.hartwig.actin.clinical.datamodel.Gender
 import com.hartwig.actin.clinical.feed.FeedEntry
+import com.hartwig.actin.clinical.feed.TsvRow
 import java.time.LocalDate
 
+@TsvRow
 data class PatientEntry(
     override val subject: String,
     val birthYear: Int,
