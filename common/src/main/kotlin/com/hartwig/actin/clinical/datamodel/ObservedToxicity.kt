3b5bf6d84af304f22417e777d949d4b70202dab1
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-01-24 10:24:11 -0500

    ACTIN-467: Make ObservedToxicity comparable

--- common/src/main/kotlin/com/hartwig/actin/clinical/datamodel/ObservedToxicity.kt
+++ common/src/main/kotlin/com/hartwig/actin/clinical/datamodel/ObservedToxicity.kt
@@ -4,4 +4,9 @@ data class ObservedToxicity(
     val name: String,
     val categories: Set<String>,
     val grade: Int?
-)
+) : Comparable<ObservedToxicity> {
+   
+    override fun compareTo(other: ObservedToxicity): Int {
+        return name.compareTo(other.name)
+    }
+}
