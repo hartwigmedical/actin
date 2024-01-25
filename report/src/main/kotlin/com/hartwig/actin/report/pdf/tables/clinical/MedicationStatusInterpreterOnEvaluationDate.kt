a54f619cd9b5038e4d6a36a7979dbb1f0a331362
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-01-25 16:16:08 +0100

    ACTIN-15: Correctly interpretate status of medication

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/MedicationStatusInterpreterOnEvaluationDate.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/MedicationStatusInterpreterOnEvaluationDate.kt
@@ -6,10 +6,14 @@ import java.time.LocalDate
 
 // TODO (CB): move this class and the copy in algo and the test files to common module once common is converted to kotlin
 class MedicationStatusInterpreterOnEvaluationDate(private val evaluationDate: LocalDate) : MedicationStatusInterpreter {
+
     override fun interpret(medication: Medication): MedicationStatusInterpretation {
         val status = medication.status()
         if (status == MedicationStatus.CANCELLED) {
             return MedicationStatusInterpretation.CANCELLED
+        }
+        if (status == MedicationStatus.ON_HOLD) {
+            return MedicationStatusInterpretation.STOPPED
         }
         val startDate = medication.startDate() ?: return MedicationStatusInterpretation.UNKNOWN
         val startIsBeforeEvaluation = startDate.isBefore(evaluationDate)
@@ -18,19 +22,7 @@ class MedicationStatusInterpreterOnEvaluationDate(private val evaluationDate: Lo
         } else {
             val stopDate = medication.stopDate()
             if (stopDate == null) {
-                when (status) {
-                    MedicationStatus.ON_HOLD -> {
-                        MedicationStatusInterpretation.STOPPED
-                    }
-
-                    MedicationStatus.UNKNOWN -> {
-                        MedicationStatusInterpretation.UNKNOWN
-                    }
-
-                    else -> {
-                        MedicationStatusInterpretation.ACTIVE
-                    }
-                }
+                if (status == MedicationStatus.UNKNOWN) MedicationStatusInterpretation.UNKNOWN else MedicationStatusInterpretation.ACTIVE
             } else {
                 val stopIsBeforeEvaluation = stopDate.isBefore(evaluationDate)
                 if (stopIsBeforeEvaluation) MedicationStatusInterpretation.STOPPED else MedicationStatusInterpretation.ACTIVE
