af8061d90f39077678085a41cf2c23b0bd4cb7ee
Khalid Zuberi <144375926+kzuberihmf@users.noreply.github.com>
2024-01-29 15:13:44 -0500

    ACTIN-10: special handling for PD-L1 molecular test input (#299)
    The curation sheet 'molecular_test.tsv' will be replaced with 'molecular_test_ihc.tsv' and 'molecular_test_pdl1.tsv' to go with this change. The two tests are now curated independently, preventing the possibility of inadvertent mis-curation due to the same input occurring for both tests.

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/extraction/PriorMolecularTestsExtractorTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/extraction/PriorMolecularTestsExtractorTest.kt
@@ -13,7 +13,9 @@ private const val CANNOT_CURATE = "cannot curate"
 
 private const val MOLECULAR_TEST_INPUT = "Molecular test input"
 
-private const val MOLECULAR_TEST_INTERPRETATION = "Molecular test interpretation"
+private const val MOLECULAR_TEST_INTERPRETATION_IHC = "Molecular test interpretation IHC"
+private const val MOLECULAR_TEST_INTERPRETATION_PDL1 = "Molecular test interpretation PD-L1"
+
 
 class PriorMolecularTestsExtractorTest {
 
@@ -23,7 +25,16 @@ class PriorMolecularTestsExtractorTest {
                 input = MOLECULAR_TEST_INPUT,
                 ignore = false,
                 curated = PriorMolecularTest(
-                    impliesPotentialIndeterminateStatus = false, test = MOLECULAR_TEST_INTERPRETATION, item = "item"
+                    impliesPotentialIndeterminateStatus = false, test = MOLECULAR_TEST_INTERPRETATION_IHC, item = "item"
+                )
+            )
+        ),
+        TestCurationFactory.curationDatabase(
+            MolecularTestConfig(
+                input = MOLECULAR_TEST_INPUT,
+                ignore = false,
+                curated = PriorMolecularTest(
+                    impliesPotentialIndeterminateStatus = false, test = MOLECULAR_TEST_INTERPRETATION_PDL1, item = "item"
                 )
             )
         )
@@ -31,20 +42,29 @@ class PriorMolecularTestsExtractorTest {
 
     @Test
     fun `Should curate prior molecular tests`() {
-        val inputs = listOf(MOLECULAR_TEST_INPUT, CANNOT_CURATE)
-        val questionnaire = TestCurationFactory.emptyQuestionnaire().copy(ihcTestResults = inputs)
-        val (priorMolecularTests, evaluation) = extractor.extract(PATIENT_ID, questionnaire)
-        assertThat(priorMolecularTests).hasSize(1)
-        assertThat(priorMolecularTests[0].test).isEqualTo(MOLECULAR_TEST_INTERPRETATION)
+        val ihcInputs = listOf(MOLECULAR_TEST_INPUT, CANNOT_CURATE)
+        val pdl1Inputs = listOf(MOLECULAR_TEST_INPUT, CANNOT_CURATE)
 
-        assertThat(evaluation.warnings).containsOnly(
+        val questionnaire = TestCurationFactory.emptyQuestionnaire().copy(ihcTestResults = ihcInputs, pdl1TestResults = pdl1Inputs)
+        val (priorMolecularTests, evaluation) = extractor.extract(PATIENT_ID, questionnaire)
+        assertThat(priorMolecularTests).hasSize(2)
+        assertThat(priorMolecularTests[0].test).isEqualTo(MOLECULAR_TEST_INTERPRETATION_IHC)
+        assertThat(priorMolecularTests[1].test).isEqualTo(MOLECULAR_TEST_INTERPRETATION_PDL1)
+
+        assertThat(evaluation.warnings).containsExactly(
             CurationWarning(
                 PATIENT_ID,
                 CurationCategory.MOLECULAR_TEST,
                 CANNOT_CURATE,
                 "Could not find IHC molecular test config for input '$CANNOT_CURATE'"
+            ),
+            CurationWarning(
+                PATIENT_ID,
+                CurationCategory.MOLECULAR_TEST,
+                CANNOT_CURATE,
+                "Could not find PD-L1 molecular test config for input '$CANNOT_CURATE'"
             )
         )
-        assertThat(evaluation.molecularTestEvaluatedInputs).isEqualTo(inputs.map(String::lowercase).toSet())
+        assertThat(evaluation.molecularTestEvaluatedInputs).isEqualTo((ihcInputs + pdl1Inputs).map(String::lowercase).toSet())
     }
 }
