9c20f655c9e54b190b1a78f4b17983d98d0638ee
Khalid Zuberi <144375926+kzuberihmf@users.noreply.github.com>
2024-01-30 15:44:05 -0500

    ACTIN-552: split MOLECULAR_TEST curation category (#308)
    * ACTIN-552: split MOLECULAR_TEST curation category
    
    Now two categories, MOLECULAR_TEST_IHC and
    MOLECULAR_TEST_PDL1
    
    * ACTIN-552: test for curation category enum consistency vs test tsv
    
    Related fixes:
    * adds enum QUESTIONNAIRE_MAPPING
    * changes CYP_INTERACTION -> CYP_INTERACTIONS
    * changes QT_PROLONGATION -> QT_PROLONGATING

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/ClinicalIngestionTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/ClinicalIngestionTest.kt
@@ -81,8 +81,8 @@ class ClinicalIngestionTest {
         )
 
         assertThat(ingestionResult.unusedConfigs).containsExactly(
-            UnusedCurationConfig(categoryName = "Molecular Test", input = "ihc erbb2 3+"),
-            UnusedCurationConfig(categoryName = "Molecular Test", input = "cps pd l1 > 20"),
+            UnusedCurationConfig(categoryName = "Molecular Test IHC", input = "ihc erbb2 3+"),
+            UnusedCurationConfig(categoryName = "Molecular Test PDL1", input = "cps pd l1 > 20"),
             UnusedCurationConfig(categoryName = "Dosage Unit Translation", input = "stuk")
         )
 
