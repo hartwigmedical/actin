983b6b7287b89dc6891032bd239303c32925ef2d
Nina Jacobs <67696681+ninajacobs@users.noreply.github.com>
2024-02-05 16:57:29 +0100

    ACTIN-568: Add implementation for CURRENTLY_GETS_HERBAL_MEDICATION (#319)
    * ACTIN-568: Add implementation for CURRENTLY_GETS_HERBAL_MEDICINE_MEDICATION
    
    * ACTIN-568: Prefer undetermined over warn
    
    * ACTIN-568: Cleanup and use consistent naming
    
    * ACTIN-568: Use when
    
    * ACTIN-568: PR message feedback

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/medication/CurrentlyGetsHerbalMedicationTest.kt
@@ -0,0 +1,58 @@
+package com.hartwig.actin.algo.evaluation.medication
+
+import com.hartwig.actin.algo.datamodel.EvaluationResult
+import com.hartwig.actin.algo.evaluation.EvaluationAssert
+import org.junit.Test
+
+class CurrentlyGetsHerbalMedicationTest {
+    private val alwaysActiveFunction = CurrentlyGetsHerbalMedication(MedicationTestFactory.alwaysActive())
+    private val alwaysPlannedFunction = CurrentlyGetsHerbalMedication(MedicationTestFactory.alwaysPlanned())
+    private val alwaysInactiveFunction = CurrentlyGetsHerbalMedication(MedicationTestFactory.alwaysInactive())
+
+    @Test
+    fun `Should fail when patient uses no medications`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL,
+            alwaysActiveFunction.evaluate(MedicationTestFactory.withMedications(emptyList()))
+        )
+    }
+
+    @Test
+    fun `Should be fail when no self care medication`() {
+        val medications = listOf(MedicationTestFactory.medication())
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL,
+            alwaysActiveFunction.evaluate(
+                MedicationTestFactory.withMedications(medications)
+            )
+        )
+    }
+
+    @Test
+    fun `Should be undetermined when medication is self care and active or planned`() {
+        val medications = listOf(MedicationTestFactory.medication(isSelfCare = true))
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.UNDETERMINED,
+            alwaysActiveFunction.evaluate(
+                MedicationTestFactory.withMedications(medications)
+            )
+        )
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.UNDETERMINED,
+            alwaysPlannedFunction.evaluate(
+                MedicationTestFactory.withMedications(medications)
+            )
+        )
+    }
+
+    @Test
+    fun `Should be fail when medication is self care but not active or planned`() {
+        val medications = listOf(MedicationTestFactory.medication(isSelfCare = true))
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL,
+            alwaysInactiveFunction.evaluate(
+                MedicationTestFactory.withMedications(medications)
+            )
+        )
+    }
+}
