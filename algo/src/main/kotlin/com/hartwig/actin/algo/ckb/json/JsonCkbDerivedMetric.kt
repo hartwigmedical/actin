924566b7bc9f2088d907f1508edf8610869af08c
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-02-12 15:43:17 +0100

    ACTIN-536: Fix typo

--- algo/src/main/kotlin/com/hartwig/actin/algo/ckb/json/JsonCkbDerivedMetric.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/ckb/json/JsonCkbDerivedMetric.kt
@@ -6,6 +6,6 @@ data class JsonCkbDerivedMetric(
     @SerializedName("relative_metric_id") val relativeMetricId: Int,
     @SerializedName("comparator_statistic") val comparatorStatistic: String?,
     @SerializedName("comparator_statistic_type") val comparatorStatisticType: String?,
-    @SerializedName("confidence_interval_95_cs") val confidenceInterval95Cs: String?,
+    @SerializedName("confidence_interval95_cs") val confidenceInterval95Cs: String?,
     @SerializedName("p_value") val pValue: String
 )
