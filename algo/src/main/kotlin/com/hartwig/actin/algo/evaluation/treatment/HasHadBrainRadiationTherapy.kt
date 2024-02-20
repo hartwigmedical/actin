4438bd5a8f85c3c56f6be17907675a5a2ed9b08c
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2024-02-20 17:53:18 +0100

    ACTIN-587: Implement functionality for HAS_HAD_BRAIN_RADIATION_THERAPY

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadBrainRadiationTherapy.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadBrainRadiationTherapy.kt
@@ -4,14 +4,39 @@ import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.clinical.datamodel.BodyLocationCategory
+import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory
 
 class HasHadBrainRadiationTherapy : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        return EvaluationFactory.undetermined(
-            "Undetermined if patient received prior brain radiation therapy",
-            "Undetermined prior brain radiation therapy"
-        )
-    }
+        val hasBrainMetastases = record.clinical.tumor.hasBrainLesions == true || record.clinical.tumor.hasActiveBrainLesions == true
+        val priorRadiotherapy = record.clinical.oncologicalHistory
+            .filter { it.categories().contains(TreatmentCategory.RADIOTHERAPY) }
+        val brainRadiotherapy =
+            priorRadiotherapy.any { it.treatmentHistoryDetails?.bodyLocationCategories?.contains(BodyLocationCategory.BRAIN) ?: false }
 
+        return when {
+            brainRadiotherapy -> {
+                EvaluationFactory.pass(
+                    "Patient has had prior brain radiation therapy",
+                    "Has had brain radiation therapy"
+                )
+            }
+
+            hasBrainMetastases && priorRadiotherapy.isNotEmpty() -> {
+                EvaluationFactory.undetermined(
+                    "Patient has brain metastases and has received radiotherapy but undetermined if brain radiation therapy",
+                    "Undetermined prior brain radiation therapy"
+                )
+            }
+
+            else -> {
+                EvaluationFactory.fail(
+                    "Patient has not received prior brain radiation therapy",
+                    "Has not received prior brain radiation therapy"
+                )
+            }
+        }
+    }
 }
