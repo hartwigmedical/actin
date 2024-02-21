4594d8c40cbe0e5aad2c6a05d55fa634af36e0fc
Ned Leitch <ned.leitch@gmail.com>
2024-02-20 20:25:45 -0500

    ACTIN-593 Make treatment type optional

--- common/src/main/kotlin/com/hartwig/actin/trial/input/single/OneSpecificTreatmentOneTreatmentCategoryManyTypes.kt
+++ common/src/main/kotlin/com/hartwig/actin/trial/input/single/OneSpecificTreatmentOneTreatmentCategoryManyTypes.kt
@@ -7,5 +7,5 @@ import com.hartwig.actin.clinical.datamodel.treatment.TreatmentType
 data class OneSpecificTreatmentOneTreatmentCategoryManyTypes(
     val treatment: Treatment,
     val category: TreatmentCategory,
-    val types: Set<TreatmentType>
+    val types: Set<TreatmentType>?
 )
