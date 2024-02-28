1bfe0acb500d727f88981cfa3e9ed3a2d07d6cac
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2024-02-28 12:59:29 +0100

    ACTIN-601: PR - moved fun evaluateTreatmentHistory to ProgressiveDiseaseFunctions

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasAcquiredResistanceToSomeDrug.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasAcquiredResistanceToSomeDrug.kt
@@ -4,18 +4,15 @@ import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
-import com.hartwig.actin.algo.evaluation.treatment.ProgressiveDiseaseFunctions
-import com.hartwig.actin.algo.evaluation.treatment.TrialFunctions
+import com.hartwig.actin.algo.evaluation.treatment.ProgressiveDiseaseFunctions.evaluateTreatmentHistory
 import com.hartwig.actin.algo.evaluation.util.Format
 import com.hartwig.actin.clinical.datamodel.treatment.Drug
-import com.hartwig.actin.clinical.datamodel.treatment.DrugTreatment
-import com.hartwig.actin.clinical.datamodel.treatment.history.StopReason
 
 class HasAcquiredResistanceToSomeDrug(private val drugsToMatch: Set<Drug>) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
 
-        val treatmentEvaluation = evaluateTreatmentHistory(record)
+        val treatmentEvaluation = evaluateTreatmentHistory(record, drugsToMatch)
         val toxicityMessage =
             if (treatmentEvaluation.matchesWithToxicity) "(stop reason toxicity) " else ""
 
@@ -43,8 +40,8 @@ class HasAcquiredResistanceToSomeDrug(private val drugsToMatch: Set<Drug>) : Eva
                 )
             }
 
-            (treatmentEvaluation.matchingTreatment.isNotEmpty()) -> {
-                EvaluationFactory.fail("Has received drugs ${Format.concatItemsWithAnd(treatmentEvaluation.matchingTreatment)} but " +
+            (treatmentEvaluation.matchingDrugs.isNotEmpty()) -> {
+                EvaluationFactory.fail("Has received drugs ${Format.concatItemsWithAnd(treatmentEvaluation.matchingDrugs)} but " +
                         "no progressive disease")
             }
 
@@ -57,48 +54,4 @@ class HasAcquiredResistanceToSomeDrug(private val drugsToMatch: Set<Drug>) : Eva
             }
         }
     }
-
-    private fun evaluateTreatmentHistory(record: PatientRecord): TreatmentHistoryEvaluation {
-        val treatmentHistory = record.clinical.oncologicalHistory
-
-        val trialCategoriesToMatch = drugsToMatch.map(Drug::category).filter(TrialFunctions::categoryAllowsTrialMatches).toSet()
-
-        return treatmentHistory.map { entry ->
-            val isPD = ProgressiveDiseaseFunctions.treatmentResultedInPD(entry)
-            val matchingDrugs = entry.allTreatments().flatMap {
-                (it as? DrugTreatment)?.drugs?.intersect(drugsToMatch) ?: emptyList()
-            }.toSet()
-            val possibleTrialMatch =
-                entry.isTrial && (entry.categories().isEmpty() || entry.categories().intersect(trialCategoriesToMatch).isNotEmpty())
-            val matchesWithToxicity = entry.treatmentHistoryDetails?.stopReason == StopReason.TOXICITY
-            if (matchingDrugs.isNotEmpty()) {
-                TreatmentHistoryEvaluation(
-                    matchingDrugsWithPD = if (isPD == true) matchingDrugs else emptySet(),
-                    matchingTreatment = matchingDrugs,
-                    matchesWithUnclearPD = isPD == null,
-                    possibleTrialMatch = possibleTrialMatch,
-                    matchesWithToxicity = matchesWithToxicity
-                )
-            } else {
-                TreatmentHistoryEvaluation(possibleTrialMatch = possibleTrialMatch)
-            }
-        }.fold(TreatmentHistoryEvaluation()) { acc, result ->
-            TreatmentHistoryEvaluation(
-                matchingDrugsWithPD = acc.matchingDrugsWithPD + result.matchingDrugsWithPD,
-                matchingTreatment = acc.matchingTreatment + result.matchingTreatment,
-                matchesWithUnclearPD = acc.matchesWithUnclearPD || result.matchesWithUnclearPD,
-                possibleTrialMatch = acc.possibleTrialMatch || result.possibleTrialMatch,
-                matchesWithToxicity = acc.matchesWithToxicity || result.matchesWithToxicity
-            )
-        }
-    }
-
-    private data class TreatmentHistoryEvaluation(
-        val matchingDrugsWithPD: Set<Drug> = emptySet(),
-        val matchingTreatment: Set<Drug> = emptySet(),
-        val matchesWithUnclearPD: Boolean = false,
-        val possibleTrialMatch: Boolean = false,
-        val matchesWithToxicity: Boolean = false
-    )
-
 }
