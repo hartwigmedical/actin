2e7363c78d738d5fa1715214ecbf1d11de3ddc1f
Daphne van Beek <d.vanbeek@hartwigmedicalfoundation.nl>
2024-03-01 16:46:48 +0100

    ACTIN-667: Added panel support for MOLECULAR_TEST_MUST_HAVE_BEEN_DONE_FOR_GENE_X rule

--- common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/ExperimentType.kt
+++ common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/ExperimentType.kt
@@ -4,7 +4,8 @@ import com.hartwig.actin.Displayable
 
 enum class ExperimentType(private val display: String) : Displayable {
     TARGETED("Panel analysis"),
-    WHOLE_GENOME("WGS");
+    WHOLE_GENOME("WGS"),
+    AMPLISEQ("AmpliSeq panel");
 
     override fun display(): String {
         return display
