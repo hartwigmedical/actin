a86522943e0b52a33889813a4c121a914b07ffc9
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-03-07 10:43:37 -0500

    ACTIN-697: Clean up composite and prior condition functions

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/composite/And.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/composite/And.kt
@@ -2,30 +2,19 @@ package com.hartwig.actin.algo.evaluation.composite
 
 import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.Evaluation
-import com.hartwig.actin.algo.datamodel.EvaluationResult
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 
-class And(functions: List<EvaluationFunction>) : EvaluationFunction {
-    private val functions: List<EvaluationFunction>
-
-    init {
-        this.functions = functions
-    }
+class And(private val functions: List<EvaluationFunction>) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        val evaluations = functions.map { it.evaluate(record) }.toSet()
-        var worst: EvaluationResult? = null
-        var recoverable: Boolean? = null
-        for (eval in evaluations) {
-            if (worst == null || eval.result.isWorseThan(worst)) {
-                worst = eval.result
-                recoverable = eval.recoverable
-            } else if (worst == eval.result) {
-                recoverable = eval.recoverable && recoverable!!
-            }
-        }
-        check(worst != null && recoverable != null) { "Could not determine AND result for functions: $functions" }
-        return evaluations.filter { it.result == worst && it.recoverable == recoverable }
-            .fold(Evaluation(worst, recoverable)) { acc, eval -> acc.addMessagesAndEvents(eval) }
+        val evaluationsByResult = functions.map { it.evaluate(record) }.distinct().groupBy(Evaluation::result)
+        val worst = evaluationsByResult.keys.minOrNull()
+            ?: throw IllegalStateException("Could not determine AND result for functions: $functions")
+
+        val (recoverableEvaluations, unrecoverableEvaluations) = evaluationsByResult[worst]!!.partition(Evaluation::recoverable)
+        val recoverable = unrecoverableEvaluations.isEmpty()
+        val evaluations = if (recoverable) recoverableEvaluations else unrecoverableEvaluations
+
+        return evaluations.fold(Evaluation(worst, recoverable), Evaluation::addMessagesAndEvents)
     }
 }
