60541f1022aa33ca1025d68d7769e9b1e90c5721
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2024-03-08 12:41:24 +0100

    ACTIN-695: add nctId property and filter CKB-trials based on nctIds of local trials

--- common/src/main/kotlin/com/hartwig/actin/trial/datamodel/TrialIdentification.kt
+++ common/src/main/kotlin/com/hartwig/actin/trial/datamodel/TrialIdentification.kt
@@ -2,7 +2,8 @@ package com.hartwig.actin.trial.datamodel
 
 data class TrialIdentification(
     val trialId: String,
+    val nctId: String?,
     val open: Boolean,
     val acronym: String,
-    val title: String
+    val title: String,
 )
