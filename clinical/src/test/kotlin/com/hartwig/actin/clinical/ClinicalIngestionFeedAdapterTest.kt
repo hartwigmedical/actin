af642d2380f232fa7214afe25338458601fb7932
Ned Leitch <ned.leitch@gmail.com>
2024-03-13 15:48:46 -0400

    ACTIN-713 Add parallel questionnaire v1.3 feed
    Still WIP, the new test doesn't pass with my made-up values. Want to
    get early validation on the approach (keeping both versions around).

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/ClinicalIngestionFeedAdapterTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/ClinicalIngestionFeedAdapterTest.kt
@@ -8,6 +8,7 @@ import com.hartwig.actin.clinical.curation.CurationDoidValidator
 import com.hartwig.actin.clinical.curation.TestAtcFactory
 import com.hartwig.actin.clinical.feed.emc.EmcClinicalFeedIngestor
 import com.hartwig.actin.clinical.feed.emc.FEED_DIRECTORY
+import com.hartwig.actin.clinical.feed.emc.FEED_DIRECTORY_1_3
 import com.hartwig.actin.clinical.feed.emc.FeedValidationWarning
 import com.hartwig.actin.clinical.feed.emc.questionnaire.QuestionnaireCurationError
 import com.hartwig.actin.clinical.serialization.ClinicalRecordJson
@@ -113,6 +114,97 @@ class ClinicalIngestionFeedAdapterTest {
                     patientIngestionResult.feedValidationWarnings
                 )
             )
+    }
 
+    @Test
+    fun `Should run ingestion from proper curation and v1-3 feed files, read from filesystem`() {
+        val curationDatabase = CurationDatabaseContext.create(
+            CURATION_DIRECTORY,
+            CurationDoidValidator(
+                TestDoidModelFactory.createWithDoidManualConfig(
+                    DoidManualConfig(
+                        emptySet(),
+                        emptySet(),
+                        mapOf(
+                            "2513" to CurationDoidValidator.DISEASE_OF_CELLULAR_PROLIFERATION_DOID,
+                            "299" to CurationDoidValidator.DISEASE_OF_CELLULAR_PROLIFERATION_DOID,
+                            "3908" to CurationDoidValidator.DISEASE_OF_CELLULAR_PROLIFERATION_DOID,
+                            "5082" to CurationDoidValidator.DISEASE_DOID,
+                            "11335" to CurationDoidValidator.DISEASE_DOID,
+                            "0060500" to CurationDoidValidator.DISEASE_DOID
+                        )
+                    )
+                )
+            ),
+            TestTreatmentDatabaseFactory.createProper()
+        )
+        val ingestion = ClinicalIngestionFeedAdapter(
+            EmcClinicalFeedIngestor.create(
+                FEED_DIRECTORY_1_3,
+                CURATION_DIRECTORY,
+                curationDatabase,
+                TestAtcFactory.createProperAtcModel()
+            ), curationDatabase
+        )
+
+        val validationErrors = curationDatabase.validate()
+        assertThat(validationErrors).isEmpty()
+
+        val ingestionResult = ingestion.run()
+        assertThat(ingestionResult).isNotNull
+        val patientResults = ingestionResult.patientResults
+        assertThat(patientResults[0].status).isEqualTo(PatientIngestionStatus.PASS)
+        assertThat(patientResults).hasSize(1)
+        assertThat(patientResults[0].patientId).isEqualTo(PATIENT)
+        assertThat(patientResults[0].curationResults).isEmpty()
+        assertThat(patientResults[0].clinicalRecord.oncologicalHistory).isNotEmpty()
+        assertThat(patientResults[0].clinicalRecord).isEqualTo(ClinicalRecordJson.read(EXPECTED_CLINICAL_RECORD))
+        assertThat(patientResults[0].questionnaireCurationErrors)
+            .containsExactly(QuestionnaireCurationError(PATIENT, "Unrecognized questionnaire option: 'Probably'"))
+        assertThat(patientResults[0].feedValidationWarnings).containsExactly(
+            FeedValidationWarning(
+                PATIENT,
+                "Empty vital function value"
+            )
+        )
+
+        assertThat(ingestionResult.unusedConfigs).containsExactlyInAnyOrder(
+            UnusedCurationConfig(categoryName = "Oncological History", input = "capecitabine and oxi"),
+            UnusedCurationConfig(categoryName = "Primary Tumor", input = "long | metastase adenocarcinoom"),
+            UnusedCurationConfig(categoryName = "Non Oncological History", input = "pijn bij maligne neoplasma van longen"),
+            UnusedCurationConfig(categoryName = "Complication", input = "overige"),
+            UnusedCurationConfig(categoryName = "Lesion Location", input = "brain"),
+            UnusedCurationConfig(categoryName = "Toxicity", input = "dysphagia"),
+            UnusedCurationConfig(categoryName = "Molecular Test IHC", input = "immunohistochemie erbb2 3+"),
+            UnusedCurationConfig(categoryName = "Molecular Test PDL1", input = "cps pd l1 > 20"),
+            UnusedCurationConfig(categoryName = "Dosage Unit Translation", input = "stuk")
+        )
+
+        val gson = GsonSerializer.create()
+        val serialized = gson.toJson(ingestionResult).toByteArray()
+
+        val deserialized = gson.fromJson(serialized.decodeToString(), IngestionResult::class.java)
+        // The clinical record is not serialized, so we need to compare the patient results separately:
+        assertThat(deserialized.copy(patientResults = emptyList())).isEqualTo(ingestionResult.copy(patientResults = emptyList()))
+
+        val patientIngestionResult = ingestionResult.patientResults.first()
+        assertThat(deserialized.patientResults).extracting(
+            PatientIngestionResult::patientId,
+            PatientIngestionResult::status,
+            PatientIngestionResult::clinicalRecord,
+            PatientIngestionResult::curationResults,
+            PatientIngestionResult::questionnaireCurationErrors,
+            PatientIngestionResult::feedValidationWarnings
+        )
+            .containsExactly(
+                tuple(
+                    patientIngestionResult.patientId,
+                    patientIngestionResult.status,
+                    null,
+                    patientIngestionResult.curationResults,
+                    patientIngestionResult.questionnaireCurationErrors,
+                    patientIngestionResult.feedValidationWarnings
+                )
+            )
     }
 }
