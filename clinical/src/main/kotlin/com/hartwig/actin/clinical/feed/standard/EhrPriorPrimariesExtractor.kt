f6b33f67a507e2f5e23e36ddc932c98898fa75ec
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-03-19 16:37:29 -0400

    ACTIN-750 Add curation for prior primary

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/EhrPriorPrimariesExtractor.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/EhrPriorPrimariesExtractor.kt
@@ -1,24 +1,34 @@
 package com.hartwig.actin.clinical.feed.standard
 
 import com.hartwig.actin.clinical.ExtractionResult
+import com.hartwig.actin.clinical.curation.CurationCategory
+import com.hartwig.actin.clinical.curation.CurationDatabase
+import com.hartwig.actin.clinical.curation.CurationResponse
+import com.hartwig.actin.clinical.curation.config.SecondPrimaryConfig
 import com.hartwig.actin.clinical.curation.extraction.CurationExtractionEvaluation
 import com.hartwig.actin.clinical.datamodel.PriorSecondPrimary
-import com.hartwig.actin.clinical.datamodel.TumorStatus
 
-class EhrPriorPrimariesExtractor :
+class EhrPriorPrimariesExtractor(private val priorPrimaryCuration: CurationDatabase<SecondPrimaryConfig>) :
     EhrExtractor<List<PriorSecondPrimary>> {
     override fun extract(ehrPatientRecord: EhrPatientRecord): ExtractionResult<List<PriorSecondPrimary>> {
-        return ExtractionResult(ehrPatientRecord.priorPrimaries.map {
-            PriorSecondPrimary(
-                tumorLocation = it.tumorLocation,
-                tumorType = it.tumorType,
-                status = it.status?.let { status -> TumorStatus.valueOf(status) } ?: TumorStatus.UNKNOWN,
-                diagnosedYear = it.diagnosisDate?.year,
-                diagnosedMonth = it.diagnosisDate?.monthValue,
-                tumorSubLocation = "",
-                tumorSubType = "",
-                treatmentHistory = ""
+        return ehrPatientRecord.priorPrimaries.map {
+            val input = "${it.tumorLocation} | ${it.tumorType}"
+            val curatedPriorPrimary = CurationResponse.createFromConfigs(
+                priorPrimaryCuration.find(input),
+                ehrPatientRecord.patientDetails.hashedId,
+                CurationCategory.SECOND_PRIMARY,
+                input,
+                "prior primary"
             )
-        }, CurationExtractionEvaluation())
+            ExtractionResult(listOfNotNull(curatedPriorPrimary.config()?.let { secondPrimaryConfig ->
+                if (!secondPrimaryConfig.ignore) {
+                    null
+                } else {
+                    secondPrimaryConfig.curated
+                }
+            }), curatedPriorPrimary.extractionEvaluation)
+        }.fold(ExtractionResult(emptyList(), CurationExtractionEvaluation())) { acc, extractionResult ->
+            ExtractionResult(acc.extracted + extractionResult.extracted, acc.evaluation + extractionResult.evaluation)
+        }
     }
 }
--- clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrPriorPrimariesExtractorTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrPriorPrimariesExtractorTest.kt
@@ -1,60 +1,73 @@
 package com.hartwig.actin.clinical.feed.standard
 
+import com.hartwig.actin.clinical.curation.CurationDatabase
+import com.hartwig.actin.clinical.curation.config.SecondPrimaryConfig
 import com.hartwig.actin.clinical.curation.extraction.CurationExtractionEvaluation
 import com.hartwig.actin.clinical.datamodel.PriorSecondPrimary
 import com.hartwig.actin.clinical.datamodel.TumorStatus
 import io.mockk.every
 import io.mockk.mockk
+import java.time.LocalDate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.time.LocalDate
+
+private const val LOCATION = "location"
+private const val TYPE = "type"
+
+private val PRIOR_SECOND_PRIMARY = PriorSecondPrimary(
+    tumorLocation = LOCATION,
+    tumorType = TYPE,
+    status = TumorStatus.ACTIVE,
+    diagnosedYear = 2024,
+    diagnosedMonth = 2,
+    tumorSubLocation = "",
+    tumorSubType = "",
+    treatmentHistory = ""
+)
+
+private val EHR_PRIOR_PRIMARY = EhrPriorPrimary(
+    tumorLocation = LOCATION,
+    tumorType = TYPE,
+    status = "ACTIVE",
+    diagnosisDate = LocalDate.of(2024, 2, 23),
+    statusDate = LocalDate.of(2024, 2, 23)
+)
 
 class EhrPriorPrimariesExtractorTest {
 
+    private val secondPrimaryConfigCurationDatabase = mockk<CurationDatabase<SecondPrimaryConfig>>()
+    private val extractor = EhrPriorPrimariesExtractor(secondPrimaryConfigCurationDatabase)
+
     @Test
-    fun `Should extract prior primary`() {
-        val extractor = EhrPriorPrimariesExtractor()
+    fun `Should extract and curate prior primary`() {
         val ehrPatientRecord = mockk<EhrPatientRecord>()
+        val input = "location | type"
+        every { secondPrimaryConfigCurationDatabase.find(input) } returns setOf(
+            SecondPrimaryConfig(
+                ignore = false,
+                input = input,
+                curated = PRIOR_SECOND_PRIMARY
+            )
+        )
         every { ehrPatientRecord.priorPrimaries } returns listOf(
-            ehrPriorPrimary()
+            EHR_PRIOR_PRIMARY
         )
         val result = extractor.extract(ehrPatientRecord)
-        assertThat(result.extracted).containsExactly(
-            priorSecondPrimary()
-        )
+        assertThat(result.extracted).containsExactly(PRIOR_SECOND_PRIMARY)
         assertThat(result.evaluation).isEqualTo(CurationExtractionEvaluation())
     }
 
     @Test
     fun `Should default tumor status to UNKNOWN when null`() {
-        val extractor = EhrPriorPrimariesExtractor()
         val ehrPatientRecord = mockk<EhrPatientRecord>()
         every { ehrPatientRecord.priorPrimaries } returns listOf(
-            ehrPriorPrimary().copy(status = null)
+            EHR_PRIOR_PRIMARY.copy(status = null)
         )
         val result = extractor.extract(ehrPatientRecord)
         assertThat(result.extracted).containsExactly(
-            priorSecondPrimary().copy(status = TumorStatus.UNKNOWN)
+            PRIOR_SECOND_PRIMARY.copy(status = TumorStatus.UNKNOWN)
         )
         assertThat(result.evaluation).isEqualTo(CurationExtractionEvaluation())
     }
 
-    private fun ehrPriorPrimary() = EhrPriorPrimary(
-        tumorLocation = "location",
-        tumorType = "type",
-        status = "ACTIVE",
-        diagnosisDate = LocalDate.of(2024, 2, 23),
-        statusDate = LocalDate.of(2024, 2, 23)
-    )
-
-    private fun priorSecondPrimary() = PriorSecondPrimary(
-        tumorLocation = "location",
-        tumorType = "type",
-        status = TumorStatus.ACTIVE,
-        diagnosedYear = 2024,
-        diagnosedMonth = 2,
-        tumorSubLocation = "",
-        tumorSubType = "",
-        treatmentHistory = ""
-    )
 }
