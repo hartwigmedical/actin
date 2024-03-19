a3847a5adff85cd073729febff00254387dc0cca
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-03-19 14:04:36 -0400

    ACTIN-722 Review comment

--- trial/src/main/kotlin/com/hartwig/actin/trial/status/ctc/CTCStatusResolver.kt
+++ trial/src/main/kotlin/com/hartwig/actin/trial/status/ctc/CTCStatusResolver.kt
@@ -3,9 +3,9 @@ package com.hartwig.actin.trial.status.ctc
 import com.hartwig.actin.trial.status.TrialStatus
 
 object CTCStatusResolver {
-    private val OPEN_STATES = setOf("Open").map { it.lowercase() }
+    private val OPEN_STATES = listOf("Open").map { it.lowercase() }
     private val CLOSED_STATES =
-        setOf("Gesloten", "Nog niet geopend", "Gesloten voor inclusie", "Onbekend", "Tijdelijk gesloten", "Closed").map { it.lowercase() }
+        listOf("Gesloten", "Nog niet geopend", "Gesloten voor inclusie", "Onbekend", "Tijdelijk gesloten", "Closed").map { it.lowercase() }
 
     fun resolve(string: String): TrialStatus {
         return when (string.lowercase()) {
