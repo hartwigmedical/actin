8b7b0aa2e4d6ecfaed91186653756c5cbd70b45a
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-03-25 15:25:30 -0400

    ACTIN-752: Correct trial matching and refactor for clarity

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSpecificDrugCombinedWithCategoryAndOptionallyTypes.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSpecificDrugCombinedWithCategoryAndOptionallyTypes.kt
@@ -22,34 +22,34 @@ class HasHadSpecificDrugCombinedWithCategoryAndOptionallyTypes(
             history.allTreatments().any { (it as? DrugTreatment)?.drugs?.contains(drugToFind) == true }
         }
 
-        val possiblyRelevantHistory = record.oncologicalHistory.filter { history ->
-            (history.allTreatments().any { (it as? DrugTreatment)?.drugs?.any {
-                    drug -> drug.category == drugToFind.category
-            } == true } || history.allTreatments().isEmpty()) && history.isTrial
-        }
+        val hadSpecificCombination = relevantHistory.any(::historyEntryWithoutDrugMatchesCategoryAndType)
+        val hadCombinationWithTrialWithUnknownType = relevantHistory.any { TrialFunctions.treatmentMayMatchAsTrial(it, category) }
+        val hadTrialWithUnspecifiedTreatment = record.oncologicalHistory.any { it.isTrial && it.allTreatments().isEmpty() }
 
-        val treatmentDesc =
-            "combined therapy with $drugToFind and ${types?.let { concatItemsWithAnd(types) } ?: ""} ${category.display()}"
+        val treatmentDesc = "combined therapy with $drugToFind and ${types?.let { concatItemsWithAnd(types) } ?: ""} ${category.display()}"
 
-        return if (historyMatchesCategoryAndTypes(relevantHistory)) {
-            EvaluationFactory.pass("Patient has received $treatmentDesc", "Has received $treatmentDesc")
-        } else if (historyMatchesCategoryAndTypes(possiblyRelevantHistory)) {
-            EvaluationFactory.undetermined(
-                "Undetermined if patient may have received $treatmentDesc",
-                "Undetermined if received $treatmentDesc"
-            )
-        } else {
-            EvaluationFactory.fail("Patient has not received $treatmentDesc", "Has not received $treatmentDesc")
-        }
-    }
+        return when {
+            hadSpecificCombination -> {
+                EvaluationFactory.pass("Patient has received $treatmentDesc", "Has received $treatmentDesc")
+            }
 
-    private fun historyMatchesCategoryAndTypes(treatmentHistory: List<TreatmentHistoryEntry>): Boolean {
-        return treatmentHistory.any { treatmentLine ->
-            treatmentLine.allTreatments().any { pastTreatment ->
-                val treatmentWithoutDrug = (pastTreatment as? DrugTreatment)?.let { it.copy(drugs = it.drugs - drugToFind) } ?: pastTreatment
-                treatmentWithoutDrug.categories().contains(category) && treatmentWithoutDrug.types()
-                    .containsAll(types ?: emptySet())
+            hadCombinationWithTrialWithUnknownType || hadTrialWithUnspecifiedTreatment -> {
+                EvaluationFactory.undetermined(
+                    "Undetermined if patient may have received $treatmentDesc",
+                    "Undetermined if received $treatmentDesc"
+                )
+            }
+
+            else -> {
+                EvaluationFactory.fail("Patient has not received $treatmentDesc", "Has not received $treatmentDesc")
             }
         }
     }
+
+    private fun historyEntryWithoutDrugMatchesCategoryAndType(treatmentLine: TreatmentHistoryEntry) =
+        treatmentLine.allTreatments().any { pastTreatment ->
+            val treatmentWithoutDrug = (pastTreatment as? DrugTreatment)?.let { it.copy(drugs = it.drugs - drugToFind) }
+                ?: pastTreatment
+            treatmentWithoutDrug.categories().contains(category) && treatmentWithoutDrug.types().containsAll(types ?: emptySet())
+        }
 }
