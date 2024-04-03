502ba96cbac734a67b7a881565fa9843c2dc83e3
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-04-03 14:27:10 +0200

    ACTIN-803: Use case-insensitive matching & catch unexpected haplotype functions in the future

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasHomozygousDPYDDeficiency.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasHomozygousDPYDDeficiency.kt
@@ -9,27 +9,53 @@ class HasHomozygousDPYDDeficiency internal constructor() : MolecularEvaluationFu
 
     override fun evaluate(molecular: MolecularRecord): Evaluation {
         val pharmaco = molecular.pharmaco
+        val geneDPYD = "DPYD"
 
-        if (pharmaco.none { it.gene == "DPYD" }) {
+        if (pharmaco.none { it.gene == geneDPYD }) {
             return EvaluationFactory.recoverableUndetermined("DPYD haplotype is undetermined", "DPYD haplotype undetermined")
         }
 
-        val isHomozygousDeficient = isHomozygousDeficient(pharmaco)
+        val containsUnexpectedHaplotypeFunction = containsUnexpectedHaplotypeFunction(pharmaco, geneDPYD)
+        val isHomozygousDeficient = isHomozygousDeficient(pharmaco, geneDPYD)
 
-        return if (isHomozygousDeficient) {
-            EvaluationFactory.pass("Patient is homozygous DPYD deficient", inclusionEvents = setOf("DPYD deficient"))
-        } else
-            EvaluationFactory.fail("Patient is not homozygous DPYD deficient")
+        return when {
+            (containsUnexpectedHaplotypeFunction) -> {
+                EvaluationFactory.recoverableUndetermined(
+                    "DPYD haplotype function cannot be determined due to unexpected haplotype function",
+                    "DPYD haplotype function undetermined"
+                )
+            }
+
+            (isHomozygousDeficient) -> {
+                EvaluationFactory.pass("Patient is homozygous DPYD deficient", inclusionEvents = setOf("DPYD deficient"))
+            }
+
+            else -> {
+                EvaluationFactory.fail("Patient is not homozygous DPYD deficient")
+            }
+        }
     }
 
-    private fun isHomozygousDeficient(pharmaco: Set<PharmacoEntry>): Boolean {
+    private fun containsUnexpectedHaplotypeFunction(pharmaco: Set<PharmacoEntry>, gene: String): Boolean {
         for (pharmacoEntry in pharmaco) {
-            if (pharmacoEntry.gene == "DPYD" && pharmacoEntry.haplotypes.any { it.function == "Normal function" }) {
+            if (pharmacoEntry.gene == gene && pharmacoEntry.haplotypes.any { it.function.lowercase() !in expectedHaplotypeFunctions}) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    private fun isHomozygousDeficient(pharmaco: Set<PharmacoEntry>, gene: String): Boolean {
+        for (pharmacoEntry in pharmaco) {
+            if (pharmacoEntry.gene == gene && pharmacoEntry.haplotypes.any { it.function.lowercase() == "normal function" }) {
                 return false
             }
         }
         return true
     }
+
+    private val expectedHaplotypeFunctions = setOf("normal function", "reduced function", "no function")
 }
 
 
