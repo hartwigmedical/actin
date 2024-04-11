5129629bac912bc1341c669215e2d50bdd0a9a61
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-04-11 15:06:47 -0400

    ACTIN-837: missing files

---
+++ common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/archer/Fusion.kt
@@ -0,0 +1,6 @@
+package com.hartwig.actin.molecular.datamodel.archer
+
+data class Fusion(
+    val geneStart: String,
+    val geneEnd: String,
+)
---
+++ common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/archer/Variant.kt
@@ -0,0 +1,6 @@
+package com.hartwig.actin.molecular.datamodel.archer
+
+class Variant(
+    val gene: String,
+    val hgvsCodingImpact: String
+)
