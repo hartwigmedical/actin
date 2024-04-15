ae2f81bbf242970f6dcc881c4308e88968a7bc07
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-04-15 14:09:04 +0200

    ACTIN-863: Add "RANKL targeting agents" as medication category + sorting

--- common/src/main/kotlin/com/hartwig/actin/medication/MedicationCategories.kt
+++ common/src/main/kotlin/com/hartwig/actin/medication/MedicationCategories.kt
@@ -13,21 +13,28 @@ class MedicationCategories(private val knownCategories: Map<String, Set<AtcLevel
         fun create(atcTree: AtcTree): MedicationCategories {
             return MedicationCategories(
                 mapOf(
-                    "Bone resorptive" to convertToAtcLevel(setOf("H05", "M05B"), atcTree),
+                    "Anticancer" to convertToAtcLevel(setOf("L01", "L02", "L04", "H01CC", "H01CA", "G03XA", "L02AE"), atcTree),
                     "Anticoagulants" to convertToAtcLevel(setOf("B01AA", "B01AB", "B01AC", "B01AD", "B01AE", "B01AF", "B01AX"), atcTree),
+                    "Antiepileptics" to convertToAtcLevel(setOf("N03"), atcTree),
                     "Antiinflammatory and antirheumatic products" to convertToAtcLevel(setOf("M01"), atcTree),
                     "Antineoplastic agents" to convertToAtcLevel(setOf("L01"), atcTree),
-                    "Platelet aggregation inhibitors" to convertToAtcLevel(setOf("B01AC"), atcTree),
                     "Azole" to convertToAtcLevel(setOf("A07AC", "D01AC", "G01AF", "G01BF", "G01AG", "J02AC", "J02AB"), atcTree),
-                    "Coumarin derivative" to convertToAtcLevel(setOf("B01AA"), atcTree),
-                    "Gonadorelin" to convertToAtcLevel(setOf("H01CC", "H01CA", "G03XA", "L02AE"), atcTree),
-                    "Anticancer" to convertToAtcLevel(setOf("L01", "L02", "L04", "H01CC", "H01CA", "G03XA", "L02AE"), atcTree),
-                    "Immunotherapy" to convertToAtcLevel(setOf("L01FF", "L01FX04"), atcTree),
-                    "Hypomethylating agents" to convertToAtcLevel(setOf("L01BC07", "L01BC08"), atcTree),
+                    "Bisphosphonates" to convertToAtcLevel(setOf("M05BA", "M05BB"), atcTree),
+                    "Bone resorptive" to convertToAtcLevel(setOf("H05", "M05B"), atcTree),
                     "Chemotherapy" to convertToAtcLevel(setOf("L01XA", "L01BC", "L01CD", "L01A"), atcTree),
-                    "PARP inhibitors" to convertToAtcLevel(setOf("L01XK"), atcTree),
-                    "Immunosuppressants" to convertToAtcLevel(setOf("L04"), atcTree),
+                    "Colony stimulating factors" to convertToAtcLevel(setOf("L03AA"), atcTree),
+                    "Coumarin derivative" to convertToAtcLevel(setOf("B01AA"), atcTree),
+                    "Endocrine therapy" to convertToAtcLevel(setOf("L02"), atcTree),
+                    "Gonadorelin" to convertToAtcLevel(setOf("H01CC", "H01CA", "G03XA", "L02AE"), atcTree),
+                    "Hypomethylating agents" to convertToAtcLevel(setOf("L01BC07", "L01BC08"), atcTree),
                     "Immunostimulants" to convertToAtcLevel(setOf("L03"), atcTree),
+                    "Immunosuppressants" to convertToAtcLevel(setOf("L04"), atcTree),
+                    "Immunotherapy" to convertToAtcLevel(setOf("L01FF", "L01FX04"), atcTree),
+                    "Monoclonal antibodies and antibody drug conjugates" to convertToAtcLevel(setOf("L01F"), atcTree),
+                    "Other antianemic preparations" to convertToAtcLevel(setOf("B03X"), atcTree),
+                    "PARP inhibitors" to convertToAtcLevel(setOf("L01XK"), atcTree),
+                    "Platelet aggregation inhibitors" to convertToAtcLevel(setOf("B01AC"), atcTree),
+                    "RANKL targeting agents" to convertToAtcLevel(setOf("M05BX04"), atcTree),
                     "Systemic antibiotics" to convertToAtcLevel(
                         setOf(
                             "A07A",
@@ -39,12 +46,6 @@ class MedicationCategories(private val knownCategories: Map<String, Set<AtcLevel
                         ), atcTree
                     ),
                     "Systemic corticosteroids" to convertToAtcLevel(setOf("H02", "M01BA"), atcTree),
-                    "Antiepileptics" to convertToAtcLevel(setOf("N03"), atcTree),
-                    "Monoclonal antibodies and antibody drug conjugates" to convertToAtcLevel(setOf("L01F"), atcTree),
-                    "Endocrine therapy" to convertToAtcLevel(setOf("L02"), atcTree),
-                    "Other antianemic preparations" to convertToAtcLevel(setOf("B03X"), atcTree),
-                    "Colony stimulating factors" to convertToAtcLevel(setOf("L03AA"), atcTree),
-                    "Bisphosphonates" to convertToAtcLevel(setOf("M05BA", "M05BB"), atcTree)
                 ),
                 atcTree
             )
