42fa3bfc37c67041bf26af7ec1988c05ca72b3d9
John Bartlett <125090035+jbartletthmf@users.noreply.github.com>
2024-04-17 16:57:48 -0400

    ACTIN-879: Make second iteration of SOC treatments optional for exhaustion evaluation (#515)
    Add additionalCriteriaForRequirement to TreatmentCandidate so the criteria for requirement can be stricter than the eligibility criteria.
    
    A treatment may be eligible if it or some of its components were previously administered over 6 months ago, but it will only be required if the excluded components don't appear in the patient's oncological history at all.

--- common/src/main/kotlin/com/hartwig/actin/algo/datamodel/TreatmentCandidate.kt
+++ common/src/main/kotlin/com/hartwig/actin/algo/datamodel/TreatmentCandidate.kt
@@ -6,13 +6,24 @@ import com.hartwig.actin.trial.datamodel.EligibilityFunction
 data class TreatmentCandidate(
     val treatment: Treatment,
     val optional: Boolean,
-    val eligibilityFunctions: Set<EligibilityFunction>
+    val eligibilityFunctions: Set<EligibilityFunction>,
+    val additionalCriteriaForRequirement: Set<EligibilityFunction> = emptySet()
 ) {
 
     override fun toString(): String {
+        val requiredFunctionsString = if (additionalCriteriaForRequirement.isEmpty()) "" else {
+            " when:\n${functionString(additionalCriteriaForRequirement)}"
+        }
         return "${treatment.display()} eligible if:\n${functionString(eligibilityFunctions)}" +
-                if (!optional) "\n   Treatment is required" else ""
+                if (!optional) "\n   Treatment is required$requiredFunctionsString" else ""
     }
 
     private fun functionString(functions: Set<EligibilityFunction>) = functions.joinToString("\n") { "    $it" }
+
+    fun eligibilityFunctionsForRequirement(): Set<EligibilityFunction> {
+        if (optional) {
+            throw IllegalStateException("Requirement criteria undefined for optional treatment")
+        }
+        return eligibilityFunctions union additionalCriteriaForRequirement
+    }
 }
