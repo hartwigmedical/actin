12f50cf1113499943c7d771e7dca62cb18aa6e7e
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-04-22 10:54:34 -0400

    ACTIN-878 Add report display for non-WGS non-IHC test types (#516)
    Refactors the interpretation to be more general. Each "intepretation" represents various results for a given test type.
    Within the results we have a grouping and details. The grouping can be either a common result (eg. Negative) or a
    common gene (eg. EGFR) depending on the test type.
    
    Then for display all types get one line per grouping, with comma delimited details. 
    
    Adds a visitor to avoid a chain of instance-ofs, this can be used for other functionality involving aggregating results
    of various types of molecular tests.

--- report/src/main/kotlin/com/hartwig/actin/report/interpretation/PriorMolecularTestInterpretation.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/interpretation/PriorMolecularTestInterpretation.kt
@@ -1,8 +1,24 @@
 package com.hartwig.actin.report.interpretation
 
-import com.hartwig.actin.clinical.datamodel.PriorMolecularTest
-
 data class PriorMolecularTestInterpretation(
-    val textBasedPriorTests: Map<PriorMolecularTestKey, List<PriorMolecularTest>>,
-    val valueBasedPriorTests: Set<PriorMolecularTest>
+    val type: String,
+    val results: List<PriorMolecularTestResultInterpretation>
 )
+
+data class PriorMolecularTestResultInterpretation(val grouping: String, val details: String, val sortPrecedence: Int = 0)
+
+class PriorMolecularTestInterpretationBuilder {
+    private val results = mutableMapOf<Triple<String, String, String>, Int>()
+
+    fun addInterpretation(type: String, grouping: String, details: String, sortPrecedence: Int = 0) {
+        results[Triple(type, grouping, details)] = sortPrecedence
+    }
+
+    fun build(): List<PriorMolecularTestInterpretation> {
+        return results.entries.groupBy { it.key.first }.map { (type, results) ->
+            PriorMolecularTestInterpretation(
+                type,
+                results.map { PriorMolecularTestResultInterpretation(it.key.second, it.key.third, it.value) })
+        }
+    }
+}
