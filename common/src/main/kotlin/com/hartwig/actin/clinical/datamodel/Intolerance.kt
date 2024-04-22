cc57148d523bdabcffeafb8f56cec7e318076972
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-04-22 16:55:13 -0400

    ACTIN-883 PR Feedback

--- common/src/main/kotlin/com/hartwig/actin/clinical/datamodel/Intolerance.kt
+++ common/src/main/kotlin/com/hartwig/actin/clinical/datamodel/Intolerance.kt
@@ -6,10 +6,10 @@ data class Intolerance(
     val name: String,
     val doids: Set<String>,
     val category: String? = null,
-    val subcategories: Set<String>,
+    val subcategories: Set<String>? = null,
     val type: String? = null,
     val clinicalStatus: String? = null,
     val verificationStatus: String? = null,
     val criticality: String? = null,
-    val treatmentCategories: Set<TreatmentCategory>
+    val treatmentCategories: Set<TreatmentCategory>? = null
 )
