edd44543ee2369d444edb1727037937883d19eaf
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-04-24 08:18:09 -0400

    ACTIN-895: fix tests and also add KRAS to generic panels

--- report/src/test/kotlin/com/hartwig/actin/report/interpretation/PriorMolecularTestInterpreterTest.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/interpretation/PriorMolecularTestInterpreterTest.kt
@@ -53,6 +53,8 @@ class PriorMolecularTestInterpreterTest {
                     PriorMolecularTestResultInterpretation(grouping = "Negative", details = "RET"),
                     PriorMolecularTestResultInterpretation(grouping = "Negative", details = "MET"),
                     PriorMolecularTestResultInterpretation(grouping = "Negative", details = "NTRK1"),
+                    PriorMolecularTestResultInterpretation(grouping = "Negative", details = "NTRK2"),
+                    PriorMolecularTestResultInterpretation(grouping = "Negative", details = "NTRK3"),
                     PriorMolecularTestResultInterpretation(grouping = "Negative", details = "NRG1")
                 )
             )
