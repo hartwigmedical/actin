dae9a31559f355ee665de33d5285cf8687003b04
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2024-04-25 10:48:21 +0200

    ACTIN-889: Added tests for invalid value and IllegalArgumentException

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrBodyHeightExtractorTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrBodyHeightExtractorTest.kt
@@ -2,6 +2,7 @@ package com.hartwig.actin.clinical.feed.standard
 
 import com.hartwig.actin.clinical.datamodel.BodyHeight
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.time.LocalDateTime
 
@@ -29,12 +30,34 @@ class EhrBodyHeightExtractorTest {
     @Test
     fun `Should extract body height from EHR`() {
         val results = extractor.extract(EHR_PATIENT_RECORD)
-        Assertions.assertThat(results.extracted).containsExactly(
+        assertThat(results.extracted).containsExactly(
             BodyHeight(
                 date = DATE,
                 value = VALUE,
                 unit = UNIT,
                 valid = true
+            )
+        )
+    }
+
+    @Test
+    fun `Should throw IllegalArgumentException when the unit is not centimeters`() {
+        val record = EHR_PATIENT_RECORD.copy(measurements = EHR_PATIENT_RECORD.measurements.map { it.copy(unit = "wrong") })
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { extractor.extract(record) }
+            .withMessage("Unit of body height is not centimeters")
+    }
+
+    @Test
+    fun `Should set valid property to false if value is outside of allowed range`() {
+        val record = EHR_PATIENT_RECORD.copy(measurements = EHR_PATIENT_RECORD.measurements.map { it.copy(value = 300.0) })
+        val results = extractor.extract(record)
+        assertThat(results.extracted).containsExactly(
+            BodyHeight(
+                date = DATE,
+                value = 300.0,
+                unit = UNIT,
+                valid = false
             )
         )
     }
--- clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrBodyWeightExtractorTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/EhrBodyWeightExtractorTest.kt
@@ -9,7 +9,7 @@ private val CATEGORY = EhrMeasurementCategory.BODY_WEIGHT.name
 private val DATE = LocalDateTime.of(2024, 4, 24, 0, 0, 0)
 private val SUB_CATEGORY = null
 private const val VALUE = 80.0
-private const val UNIT = "kilograms"
+private const val UNIT = "Kilograms"
 
 private val EHR_PATIENT_RECORD = EhrTestData.createEhrPatientRecord().copy(
     measurements = listOf(
@@ -34,8 +34,30 @@ class EhrBodyWeightExtractorTest {
             BodyWeight(
                 date = DATE,
                 value = VALUE,
-                unit = "Kilograms",
+                unit = UNIT,
                 valid = true
+            )
+        )
+    }
+
+    @Test
+    fun `Should throw IllegalArgumentException when the unit is not centimeters`() {
+        val record = EHR_PATIENT_RECORD.copy(measurements = EHR_PATIENT_RECORD.measurements.map { it.copy(unit = "wrong") })
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { extractor.extract(record) }
+            .withMessage("Unit of body weight is not Kilograms")
+    }
+
+    @Test
+    fun `Should set valid property to false if value is outside of allowed range`() {
+        val record = EHR_PATIENT_RECORD.copy(measurements = EHR_PATIENT_RECORD.measurements.map { it.copy(value = 300.0) })
+        val results = extractor.extract(record)
+        Assertions.assertThat(results.extracted).containsExactly(
+            BodyWeight(
+                date = DATE,
+                value = 300.0,
+                unit = UNIT,
+                valid = false
             )
         )
     }
