a305e4da61691e163248d000f79a2c09f0988c2c
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-04-25 11:06:09 -0400

    ACTIN-893: tests

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/MolecularEvaluationFunctionTest.kt
@@ -0,0 +1,48 @@
+package com.hartwig.actin.algo.evaluation.molecular
+
+import com.hartwig.actin.TestPatientFactory
+import com.hartwig.actin.algo.datamodel.Evaluation
+import com.hartwig.actin.algo.datamodel.EvaluationResult
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.molecular.datamodel.MolecularHistory
+import com.hartwig.actin.molecular.datamodel.MolecularRecord
+import com.hartwig.actin.molecular.datamodel.TestMolecularFactory.archerPriorMolecularNoFusionsFoundRecord
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class MolecularEvaluationFunctionTest {
+    private val function = object : MolecularEvaluationFunction {
+        override fun evaluate(molecular: MolecularRecord): Evaluation {
+            return EvaluationFactory.fail("Fail specific message", "Fail general message")
+        }
+    }
+
+    @Test
+    fun `Should return no molecular data message when no Orange nor other molecular data`() {
+
+        val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
+        val evaluation = function.evaluate(patient)
+        assertThat(evaluation.result).isEqualTo(EvaluationResult.UNDETERMINED)
+        assertThat(evaluation.undeterminedSpecificMessages).containsExactly("No molecular data")
+        assertThat(evaluation.undeterminedGeneralMessages).containsExactly("No molecular data")
+    }
+
+    @Test
+    fun `Should return insufficient molecular data when no Orange but other molecular data`() {
+        val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
+            .copy(molecularHistory = MolecularHistory.fromInputs(emptyList(), listOf(archerPriorMolecularNoFusionsFoundRecord())))
+        val evaluation = function.evaluate(patient)
+        assertThat(evaluation.result).isEqualTo(EvaluationResult.UNDETERMINED)
+        assertThat(evaluation.undeterminedSpecificMessages).containsExactly("Insufficient molecular data")
+        assertThat(evaluation.undeterminedGeneralMessages).containsExactly("Insufficient molecular data")
+    }
+
+    @Test
+    fun `Should execute rule when Orange molecular data`() {
+        val patient = TestPatientFactory.createMinimalTestWGSPatientRecord()
+        val evaluation = function.evaluate(patient)
+        assertThat(evaluation.result).isEqualTo(EvaluationResult.FAIL)
+        assertThat(evaluation.failSpecificMessages).containsExactly("Fail specific message")
+        assertThat(evaluation.failGeneralMessages).containsExactly("Fail general message")
+    }
+}
