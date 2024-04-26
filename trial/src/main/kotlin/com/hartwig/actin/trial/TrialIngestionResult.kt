6c97cdbc9ad67e4e87e4bdff3232e28a56300d20
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-04-26 17:14:47 -0400

    ACTIN-910 Serialize the warning message from the validation errors (#539)
    This makes life much easier for a client wishing to simply write out warning messages and not
    further downstream process the trial validation error using the config.

--- trial/src/main/kotlin/com/hartwig/actin/trial/TrialIngestionResult.kt
+++ trial/src/main/kotlin/com/hartwig/actin/trial/TrialIngestionResult.kt
@@ -1,9 +1,15 @@
 package com.hartwig.actin.trial
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import com.hartwig.actin.trial.config.TrialConfig
 import com.hartwig.actin.trial.config.TrialDatabaseValidation
 import com.hartwig.actin.trial.datamodel.Trial
 import com.hartwig.actin.trial.status.TrialStatusDatabaseValidation
+import java.lang.reflect.Type
 
 enum class TrialIngestionStatus {
     PASS,
@@ -24,6 +30,8 @@ interface ValidationError<T> : Comparable<ValidationError<T>> {
     val message: String
 
     fun configFormat(config: T): String
+
+
     fun warningMessage(): String {
         return "${this::class.java.simpleName} ${configFormat(config)}: $message"
     }
@@ -42,4 +50,18 @@ data class TrialIngestionResult(
     val trialValidationResult: TrialDatabaseValidation,
     val unusedRules: Set<String>,
     @Transient val trials: List<Trial>
-)
+) {
+    fun serialize(): String {
+        return GsonBuilder().registerTypeHierarchyAdapter(ValidationError::class.java, ValidationErrorSerializer())
+            .create()
+            .toJson(this)
+    }
+}
+
+class ValidationErrorSerializer : JsonSerializer<ValidationError<*>> {
+    override fun serialize(src: ValidationError<*>, p1: Type, context: JsonSerializationContext): JsonElement {
+        val serialized = Gson().toJsonTree(src)
+        serialized.asJsonObject.addProperty("warningMessage", src.warningMessage())
+        return serialized
+    }
+}
