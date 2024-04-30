550e1ce55f46d2a8fe77c8a139a4e1f7ca4706f8
John Bartlett <125090035+jbartletthmf@users.noreply.github.com>
2024-04-30 12:35:22 -0400

    ACTIN-917: Show SOC warnings and improve MSI feedback (#540)
    Show warnings for SOC matches in report.
    Improve MSI feedback when molecular not available and refactor for immutability.
    Drop personalized prediction column while these are unavailable.

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/MolecularEvaluationFunctionTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/MolecularEvaluationFunctionTest.kt
@@ -1,5 +1,6 @@
 package com.hartwig.actin.algo.evaluation.molecular
 
+import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.TestPatientFactory
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.datamodel.EvaluationResult
@@ -11,6 +12,8 @@ import com.hartwig.actin.molecular.datamodel.TestMolecularFactory.archerPriorMol
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
+private const val OVERRIDE_MESSAGE = "Override message"
+
 class MolecularEvaluationFunctionTest {
     private val function = object : MolecularEvaluationFunction {
         override fun evaluate(molecular: MolecularRecord): Evaluation {
@@ -18,8 +21,16 @@ class MolecularEvaluationFunctionTest {
         }
     }
 
+    private val functionWithOverride = object : MolecularEvaluationFunction {
+        override fun evaluate(molecular: MolecularRecord): Evaluation {
+            return EvaluationFactory.pass("OK")
+        }
+
+        override fun noMolecularRecordEvaluation() = EvaluationFactory.fail(OVERRIDE_MESSAGE)
+    }
+
     @Test
-    fun `Should return no molecular data message when no Orange nor other molecular data`() {
+    fun `Should return no molecular data message when no ORANGE nor other molecular data`() {
         val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
         val evaluation = function.evaluate(patient)
         assertMolecularEvaluation(EvaluationResult.UNDETERMINED, evaluation)
@@ -29,7 +40,7 @@ class MolecularEvaluationFunctionTest {
     }
 
     @Test
-    fun `Should return insufficient molecular data when no Orange but other molecular data`() {
+    fun `Should return insufficient molecular data when no ORANGE but other molecular data`() {
         val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
             .copy(molecularHistory = MolecularHistory.fromInputs(emptyList(), listOf(archerPriorMolecularNoFusionsFoundRecord())))
         val evaluation = function.evaluate(patient)
@@ -39,11 +50,31 @@ class MolecularEvaluationFunctionTest {
     }
 
     @Test
-    fun `Should execute rule when Orange molecular data`() {
+    fun `Should execute rule when ORANGE molecular data`() {
         val patient = TestPatientFactory.createMinimalTestWGSPatientRecord()
         val evaluation = function.evaluate(patient)
         assertMolecularEvaluation(EvaluationResult.FAIL, evaluation)
         assertThat(evaluation.failSpecificMessages).containsExactly("Fail specific message")
         assertThat(evaluation.failGeneralMessages).containsExactly("Fail general message")
+    }
+
+    @Test
+    fun `Should use override message when provided for patient with no molecular data`() {
+        val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
+        assertOverrideEvaluation(patient)
+    }
+
+    @Test
+    fun `Should use override message when provided for patient with no ORANGE record but other data`() {
+        val patient = TestPatientFactory.createEmptyMolecularTestPatientRecord()
+            .copy(molecularHistory = MolecularHistory.fromInputs(emptyList(), listOf(archerPriorMolecularNoFusionsFoundRecord())))
+        assertOverrideEvaluation(patient)
+    }
+
+    private fun assertOverrideEvaluation(patient: PatientRecord) {
+        val evaluation = functionWithOverride.evaluate(patient)
+        assertMolecularEvaluation(EvaluationResult.FAIL, evaluation)
+        assertThat(evaluation.failSpecificMessages).containsExactly(OVERRIDE_MESSAGE)
+        assertThat(evaluation.failGeneralMessages).containsExactly(OVERRIDE_MESSAGE)
     }
 }
