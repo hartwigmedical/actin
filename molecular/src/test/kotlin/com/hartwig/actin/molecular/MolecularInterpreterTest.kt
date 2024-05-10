f1a902085b0c2343317c6e5e06773811ec99498b
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-05-10 16:04:28 -0400

    ACTIN-945 Adding tests and cleanups

---
+++ molecular/src/test/kotlin/com/hartwig/actin/molecular/MolecularInterpreterTest.kt
@@ -0,0 +1,41 @@
+package com.hartwig.actin.molecular
+
+import com.hartwig.actin.clinical.datamodel.PriorMolecularTest
+import com.hartwig.actin.molecular.datamodel.panel.archer.ArcherPanel
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class MolecularInterpreterTest {
+    private val archerPanel = ArcherPanel()
+    private val extractor = object : MolecularExtractor<PriorMolecularTest, ArcherPanel> {
+        override fun extract(input: List<PriorMolecularTest>): List<ArcherPanel> {
+            return input.map { archerPanel }
+        }
+    }
+    private val annotator = object : MolecularAnnotator<ArcherPanel> {
+        override fun annotate(input: ArcherPanel): ArcherPanel {
+            return input
+        }
+    }
+
+    @Test
+    fun `Should extract and annotate inputs`() {
+        val result = MolecularInterpreter(
+            extractor = extractor,
+            annotator = annotator,
+            inputPredicate = { true }
+        ).run(listOf(PriorMolecularTest(test = "test", impliesPotentialIndeterminateStatus = false)))
+        assertThat(result).containsExactly(archerPanel)
+    }
+
+    @Test
+    fun `Should filter inputs with predicate`() {
+        val result = MolecularInterpreter(
+            extractor = extractor,
+            annotator = annotator,
+            inputPredicate = { false }
+        ).run(listOf(PriorMolecularTest(test = "test", impliesPotentialIndeterminateStatus = false)))
+        assertThat(result).isEmpty()
+    }
+
+}
