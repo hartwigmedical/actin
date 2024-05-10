299e2f07e6df82e9943d9820e7447ec0988db039
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-05-10 16:16:05 -0400

    ACTIN-945 Adding tests and cleanups

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/priormoleculartest/ArcherAnnotator.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/priormoleculartest/ArcherAnnotator.kt
@@ -1,11 +1,9 @@
 package com.hartwig.actin.molecular.priormoleculartest
 
 import com.hartwig.actin.molecular.MolecularAnnotator
-import com.hartwig.actin.molecular.datamodel.driver.FusionDriverType
 import com.hartwig.actin.molecular.datamodel.panel.archer.ArcherPanel
 import com.hartwig.actin.molecular.datamodel.panel.archer.ArcherVariantAnnotation
 import com.hartwig.actin.molecular.evidence.EvidenceDatabase
-import com.hartwig.actin.molecular.evidence.matching.FusionMatchCriteria
 import com.hartwig.actin.molecular.evidence.matching.VariantMatchCriteria
 import com.hartwig.actin.molecular.orange.interpretation.ActionableEvidenceFactory
 import com.hartwig.actin.molecular.orange.interpretation.GeneAlterationFactory
@@ -42,17 +40,6 @@ class ArcherAnnotator(private val evidenceDatabase: EvidenceDatabase) : Molecula
             )
         }
 
-        val annotatedFusions = input.fusions.map {
-            val criteria =
-                FusionMatchCriteria(geneStart = it.gene, geneEnd = it.gene, driverType = FusionDriverType.NONE, isReportable = true)
-            val evidence = ActionableEvidenceFactory.create(
-                evidenceDatabase.evidenceForFusion(
-                    criteria
-                )
-            )
-            it.copy(evidence = evidence)
-        }
-
-        return input.copy(variants = annotatedVariants, fusions = annotatedFusions)
+        return input.copy(variants = annotatedVariants)
     }
 }
