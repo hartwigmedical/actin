8ec02301a857c3831111e20f76c171bd2970915c
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-06-03 13:46:06 +0200

    ACTIN-924: WIP

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/IsHomologousRepairDeficientWithMutationOrWithVUSMutation.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/IsHomologousRepairDeficientWithMutationOrWithVUSMutation.kt
@@ -2,6 +2,7 @@ package com.hartwig.actin.algo.evaluation.molecular
 
 import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.util.Format
 import com.hartwig.actin.molecular.datamodel.MolecularRecord
 import com.hartwig.actin.molecular.datamodel.driver.CopyNumberType
 import com.hartwig.actin.molecular.datamodel.driver.DriverLikelihood
@@ -9,49 +10,53 @@ import com.hartwig.actin.molecular.datamodel.driver.DriverLikelihood
 class IsHomologousRepairDeficientWithMutationOrWithVUSMutation : MolecularEvaluationFunction {
 
     override fun evaluate(molecular: MolecularRecord): Evaluation {
-        val brcaWithBiallelicDriver: MutableSet<String> = mutableSetOf()
-        val brcaWithNonBiallelicDriver: MutableSet<String> = mutableSetOf()
+        val brcaNonHotspotBiallelicDriver: MutableSet<String> = mutableSetOf()
+        val brcaNonHotspotNonBiallelicDriver: MutableSet<String> = mutableSetOf()
         val brcaHotspot: MutableSet<String> = mutableSetOf()
         val brcaLowDriver: MutableSet<String> = mutableSetOf()
-        for (gene in BRCA_GENES) {
+        val brcaDeletionOrHomozygousDisruption: MutableSet<String> = mutableSetOf()
+        val brcaNonHomozygousDisruption: MutableSet<String> = mutableSetOf()
+        for (gene in MolecularConstants.HRD_GENES) {
             for (variant in molecular.drivers.variants) {
                 if (variant.gene == gene && variant.isReportable) {
                     if (variant.isHotspot) {
                         brcaHotspot.add(gene)
                     } else if (variant.isBiallelic && variant.driverLikelihood == DriverLikelihood.HIGH) {
-                        brcaWithBiallelicDriver.add(gene)
+                        brcaNonHotspotBiallelicDriver.add(gene)
                     } else if (!variant.isBiallelic && variant.driverLikelihood == DriverLikelihood.HIGH) {
-                        brcaWithNonBiallelicDriver.add(gene)
+                        brcaNonHotspotNonBiallelicDriver.add(gene)
                     } else { brcaLowDriver.add(gene)}
                 }
             }
             for (copyNumber in molecular.drivers.copyNumbers) {
                 if (copyNumber.type == CopyNumberType.LOSS && copyNumber.gene == gene && copyNumber.driverLikelihood == DriverLikelihood.HIGH) {
-                    brcaWithBiallelicDriver.add(gene)
+                    brcaDeletionOrHomozygousDisruption.add(gene)
                 }
             }
             for (homozygousDisruption in molecular.drivers.homozygousDisruptions) {
                 if (homozygousDisruption.gene == gene && homozygousDisruption.driverLikelihood == DriverLikelihood.HIGH) {
-                    brcaWithBiallelicDriver.add(gene)
+                    brcaDeletionOrHomozygousDisruption.add(gene)
                 }
             }
             for (disruption in molecular.drivers.disruptions) {
                 if (disruption.gene == gene && disruption.isReportable && disruption.driverLikelihood == DriverLikelihood.HIGH) {
-                    brcaWithNonBiallelicDriver.add(gene)
+                    brcaNonHomozygousDisruption.add(gene)
                 }
             }
         }
         return when (molecular.characteristics.isHomologousRepairDeficient) {
             null -> {
-                if (brcaWithBiallelicDriver.isNotEmpty()) {
+                if (brcaHotspot.isNotEmpty() || brcaDeletionOrHomozygousDisruption.isNotEmpty() || brcaNonHotspotBiallelicDriver.isNotEmpty()) {
                     EvaluationFactory.undetermined(
-                        "Unknown homologous repair deficiency (HRD) status, but biallelic drivers in BRCA1 or 2 are detected; an HRD test may be recommended",
-                        "Unknown HRD status but biallelic high drivers in BRCA1 or 2"
+                        "Unknown homologous repair deficiency (HRD) status, but biallelic drivers in HR genes: "
+                                + Format.concat(brcaHotspot intersect brcaDeletionOrHomozygousDisruption intersect brcaNonHotspotBiallelicDriver) + " are detected; an HRD test may be recommended",
+                        "Unknown HRD status but biallelic drivers in HR genes"
                     )
-                } else if (brcaWithNonBiallelicDriver.isNotEmpty()) {
+                } else if (brcaNonHotspotNonBiallelicDriver.isNotEmpty()) {
                     EvaluationFactory.undetermined(
-                        "Unknown homologous repair deficiency (HRD) status, but non-biallelic drivers in BRCA are detected; an HRD test may be recommended",
-                        "Unknown HRD status but non-biallelic high drivers in BRCA"
+                        "Unknown homologous repair deficiency (HRD) status, but non-biallelic drivers in HR genes: "
+                                + Format.concat(brcaNonHotspotNonBiallelicDriver) + " are detected; an HRD test may be recommended",
+                        "Unknown HRD status but non-biallelic drivers in HR genes"
                     )
                 } else {
                     EvaluationFactory.fail("Unknown homologous repair deficiency (HRD) status", "Unknown HRD status")
@@ -59,23 +64,36 @@ class IsHomologousRepairDeficientWithMutationOrWithVUSMutation : MolecularEvalua
             }
 
             true -> {
-                if (brcaHotspot.isNotEmpty()) {
+                if (brcaHotspot.any { MolecularConstants.BRCA_GENES.contains(it) }) {
                     EvaluationFactory.fail("Homologous repair deficiency (HRD) detected, with BRCA hotspot", "Tumor is HRD with BRCA hotspot")
                 }
-                else if (brcaWithBiallelicDriver.isNotEmpty()) {
-                    EvaluationFactory.pass(
-                        "Homologous repair deficiency (HRD) detected, together with biallelic drivers in BRCA",
-                        "Tumor is HRD and biallelic high drivers in BRCA1 or 2 detected",
-                    )
-                } else if (brcaWithNonBiallelicDriver.isNotEmpty()) {
+                else if (brcaDeletionOrHomozygousDisruption.any { MolecularConstants.BRCA_GENES.contains(it)}) {
+                    EvaluationFactory.fail("Homologous repair deficiency (HRD) detected, with deletion or homozygous disruption in BRCA", "Tumor is HRD with BRCA deletion or homozygous disruption")
+                }
+                else if (brcaNonHotspotBiallelicDriver.any { MolecularConstants.BRCA_GENES.contains(it)}) {
                     EvaluationFactory.warn(
-                        "Homologous repair deficiency (HRD) detected, together with non-biallelic drivers in BRCA were detected",
-                        "Tumor is HRD (but with only non-biallelic high drivers in BRCA)",
+                        "Homologous repair deficiency (HRD) detected, together with non-hotspot biallelic driver(s) in BRCA were detected",
+                        "Tumor is HRD with non-hotspot biallelic high driver(s) in BRCA",
                     )
+                } else if (brcaNonHotspotNonBiallelicDriver.any { MolecularConstants.BRCA_GENES.contains(it)}) {
+                    EvaluationFactory.warn(
+                        "Homologous repair deficiency (HRD) detected, together with non-hotspot non-biallelic driver(s) in BRCA were detected",
+                        "Tumor is HRD with non-hotspot non-biallelic high drivers in BRCA)",
+                    )
+                } else if (brcaLowDriver.any { MolecularConstants.BRCA_GENES.contains(it)}) {
+                    EvaluationFactory.pass(
+                        "Homologous repair deficiency (HRD) detected, together with low driver in BRCA",
+                        "Tumor is HRD with low driver in BRCA",
+                    )
+                } else if (brcaNonHomozygousDisruption.any { MolecularConstants.BRCA_GENES.contains(it)}){
+                        EvaluationFactory.pass(
+                            "Homologous repair deficiency (HRD) detected, with non-homozygous disruption in BRCA",
+                            "Tumor is HRD with non-homozygous disruption in BRCA",
+                        )
                 } else {
-                    EvaluationFactory.fail(
-                        "Homologous repair deficiency (HRD) detected, without high drivers in BRCA detected",
-                        "Tumor is HRD (but without detected high drivers in BRCA)",
+                    EvaluationFactory.pass(
+                        "Homologous repair deficiency (HRD) detected, without BRCA variants",
+                        "Tumor is HRD without any BRCA variants",
                     )
                 }
             }
@@ -84,9 +102,5 @@ class IsHomologousRepairDeficientWithMutationOrWithVUSMutation : MolecularEvalua
                 EvaluationFactory.fail("No homologous repair deficiency (HRD) detected", "Tumor is not HRD")
             }
         }
-    }
-
-    companion object {
-        private val BRCA_GENES = setOf("BRCA1", "BRCA2")
     }
 }
