2bb09f98e95935903ea625be16a65c39f3f67cca
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-06-07 12:55:57 -0400

    ACTIN-1006 Trim each line from radiology conclusions

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardTumorDetailsExtractor.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardTumorDetailsExtractor.kt
@@ -104,8 +104,7 @@ class StandardTumorDetailsExtractor(
         return radiologyReport?.substringAfter(CONCLUSIE_)?.split(CONCLUSIE_)?.flatMap { section ->
             section.substringBefore("\r\n\n\n").split("\n")
                 .filter { it.isNotBlank() }
-                .map { it.trim() }
-                .map { line -> line.substringBeforeLast(".") }
+                .map { line -> line.trim().substringBeforeLast(".") }
                 .map { line ->
                     lesionCurationResponse(patientId, line)
                 }
