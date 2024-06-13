006244b4684aaab04e4e39096973243677b10ce8
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2024-06-13 16:30:22 +0200

    ACTIN-1023: Create new rule HAS_MEASURABLE_DISEASE_RANO (#596)
    * ACTIN-1023: Add new rule HAS_MEASURABLE_DISEASE_RANO
    
    * ACTIN-1023: Change README
    
    * ACTIN-1023: Fix test
    
    * ACTIN-1023: Move constants to top of test class
    
    * ACTIN-1023: PR feedback

---
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMeasurableDiseaseRano.kt
@@ -0,0 +1,38 @@
+package com.hartwig.actin.algo.evaluation.tumor
+
+import com.hartwig.actin.PatientRecord
+import com.hartwig.actin.algo.datamodel.Evaluation
+import com.hartwig.actin.algo.doid.DoidConstants
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.doid.DoidModel
+
+class HasMeasurableDiseaseRano(private val doidModel: DoidModel) : EvaluationFunction {
+
+    override fun evaluate(record: PatientRecord): Evaluation {
+        val hasMeasurableDisease = record.tumor.hasMeasurableDisease ?: return EvaluationFactory.recoverableUndetermined(
+            "Data regarding measurable disease is missing, unknown if measurable by RANO", "Undetermined RANO measurable disease"
+        )
+
+        return when {
+            (hasMeasurableDisease && DoidEvaluationFunctions.isOfDoidType(
+                doidModel,
+                record.tumor.doids,
+                DoidConstants.CNS_CANCER_DOID
+            )) -> {
+                EvaluationFactory.recoverablePass("Patient has measurable disease", "Has measurable disease")
+            }
+
+            hasMeasurableDisease -> {
+                EvaluationFactory.warn(
+                    "Patient has measurable disease, but given the patient's tumor type uncertain if this has been evaluated against RANO",
+                    "Measurable disease by RANO unknown"
+                )
+            }
+
+            else -> {
+                EvaluationFactory.recoverableFail("Patient has no measurable disease", "No measurable disease")
+            }
+        }
+    }
+}
