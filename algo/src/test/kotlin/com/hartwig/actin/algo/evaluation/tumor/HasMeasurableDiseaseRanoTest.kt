3604d44ac0caf9d3ae47c5ab949e5adb22372dae
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-06-13 12:09:51 +0200

    ACTIN-1023: Move constants to top of test class

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMeasurableDiseaseRanoTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMeasurableDiseaseRanoTest.kt
@@ -8,40 +8,39 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class HasMeasurableDiseaseRanoTest {
+
+    private val doidModel = TestDoidModelFactory.createWithOneParentChild(DoidConstants.CNS_CANCER_DOID, DoidConstants.BRAIN_CANCER_DOID)
+    private val function = HasMeasurableDiseaseRano(doidModel)
+
     @Test
     fun `Should pass when has measurable disease is true and brain cancer`() {
-        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDiseaseAndDoid(true, DoidConstants.BRAIN_CANCER_DOID))
+        val evaluation = function.evaluate(TumorTestFactory.withMeasurableDiseaseAndDoid(true, DoidConstants.BRAIN_CANCER_DOID))
         assertEvaluation(EvaluationResult.PASS, evaluation)
         assertTrue(evaluation.recoverable)
     }
 
     @Test
     fun `Should fail when has measurable disease is false`() {
-        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(false))
+        val evaluation = function.evaluate(TumorTestFactory.withMeasurableDisease(false))
         assertEvaluation(EvaluationResult.FAIL, evaluation)
         assertTrue(evaluation.recoverable)
     }
 
     @Test
     fun `Should be undetermined when has measurable disease is undetermined`() {
-        val evaluation = FUNCTION.evaluate(TumorTestFactory.withMeasurableDisease(null))
+        val evaluation = function.evaluate(TumorTestFactory.withMeasurableDisease(null))
         assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
         assertTrue(evaluation.recoverable)
     }
 
     @Test
     fun `Should warn when has measurable disease is true but has colorectal cancer`() {
-        val evaluation = FUNCTION.evaluate(
+        val evaluation = function.evaluate(
             TumorTestFactory.withMeasurableDiseaseAndDoid(
                 true,
                 DoidConstants.COLORECTAL_CANCER_DOID
             )
         )
         assertEvaluation(EvaluationResult.WARN, evaluation)
-    }
-
-    companion object {
-        private val DOID_MODEL = TestDoidModelFactory.createWithOneParentChild(DoidConstants.CNS_CANCER_DOID, DoidConstants.BRAIN_CANCER_DOID)
-        private val FUNCTION = HasMeasurableDiseaseRano(DOID_MODEL)
     }
 }
