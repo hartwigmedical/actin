41255378cc8fff90d3dfade8ef2d4e67a62f01c4
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-06-19 12:42:52 -0400

    ACTIN-1050: move patient record json serialization out of molecular/datamodel/orange

--- algo/src/main/kotlin/com/hartwig/actin/algo/soc/StandardOfCareApplication.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/soc/StandardOfCareApplication.kt
@@ -1,6 +1,7 @@
 package com.hartwig.actin.algo.soc
 
 import com.hartwig.actin.PatientPrinter
+import com.hartwig.actin.PatientRecordJson
 import com.hartwig.actin.TreatmentDatabaseFactory
 import com.hartwig.actin.algo.calendar.ReferenceDateProviderFactory
 import com.hartwig.actin.algo.evaluation.RuleMappingResources
@@ -11,17 +12,16 @@ import com.hartwig.actin.doid.datamodel.DoidEntry
 import com.hartwig.actin.doid.serialization.DoidJson
 import com.hartwig.actin.medication.AtcTree
 import com.hartwig.actin.medication.MedicationCategories
-import com.hartwig.actin.molecular.datamodel.orange.PatientRecordJson
 import com.hartwig.actin.molecular.interpretation.MolecularInputChecker
 import com.hartwig.actin.trial.input.FunctionInputResolver
-import java.io.IOException
-import kotlin.system.exitProcess
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.HelpFormatter
 import org.apache.commons.cli.Options
 import org.apache.commons.cli.ParseException
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import java.io.IOException
+import kotlin.system.exitProcess
 
 class StandardOfCareApplication(private val config: StandardOfCareConfig) {
 
