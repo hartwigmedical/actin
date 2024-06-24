14e40f77019a3ab2a4d9baa30cec23ee79d70049
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-06-24 14:13:18 +0200

    ACTIN-1065: PR feedback

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/infection/HasReceivedLiveVaccineWithinMonths.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/infection/HasReceivedLiveVaccineWithinMonths.kt
@@ -5,7 +5,7 @@ import com.hartwig.actin.algo.datamodel.Evaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 
-class HasReceivedLiveVaccineWithinMonths internal constructor() : EvaluationFunction {
+class HasReceivedLiveVaccineWithinMonths : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
         return EvaluationFactory.undetermined(
             "Undetermined if patient has received a live vaccine within nr of months",
