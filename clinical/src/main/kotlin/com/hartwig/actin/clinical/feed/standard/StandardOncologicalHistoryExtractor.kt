0c989345acafe18e362a871a7d9e539094ac72c6
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-06-27 14:40:43 -0400

    ACTIN-1077 Don't push curation warnings to both treatment categories
    Previously the warnings would end up in both sheets (onco and non-onco history), which was
    confusing.

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardOncologicalHistoryExtractor.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardOncologicalHistoryExtractor.kt
@@ -27,7 +27,7 @@ class StandardOncologicalHistoryExtractor(
 
         return ExtractionResult(
             oncologicalTreatmentHistory.extracted + oncologicalPreviousConditions.extracted,
-            oncologicalTreatmentHistory.evaluation + oncologicalPreviousConditions.evaluation
+            oncologicalTreatmentHistory.evaluation
         )
     }
 
@@ -52,7 +52,8 @@ class StandardOncologicalHistoryExtractor(
                                 intents = curatedTreatment.intents,
                                 treatmentHistoryDetails = TreatmentHistoryDetails(
                                     stopYear = curatedTreatment.treatmentHistoryDetails?.stopYear ?: ehrPreviousCondition.endDate?.year,
-                                    stopMonth = curatedTreatment.treatmentHistoryDetails?.stopMonth?: ehrPreviousCondition.endDate?.monthValue,
+                                    stopMonth = curatedTreatment.treatmentHistoryDetails?.stopMonth
+                                        ?: ehrPreviousCondition.endDate?.monthValue,
                                     stopReason = curatedTreatment.treatmentHistoryDetails?.stopReason,
                                     bestResponse = curatedTreatment.treatmentHistoryDetails?.bestResponse,
                                     switchToTreatments = curatedTreatment.treatmentHistoryDetails?.switchToTreatments,
