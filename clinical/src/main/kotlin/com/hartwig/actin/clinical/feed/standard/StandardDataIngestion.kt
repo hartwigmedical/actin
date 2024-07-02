03e3655cdd980a02ed07965ee89714a309b79c29
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-07-02 14:49:33 -0400

    ACTIN-1088 Use camel case for reading standard feed json

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardDataIngestion.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/StandardDataIngestion.kt
@@ -2,7 +2,6 @@ package com.hartwig.actin.clinical.feed.standard
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.hartwig.actin.clinical.AtcModel
@@ -43,7 +42,6 @@ class StandardDataIngestion(
 ) : ClinicalFeedIngestion {
     private val mapper = ObjectMapper().apply {
         disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
         registerModule(JavaTimeModule())
         registerModule(KotlinModule.Builder().build())
     }
