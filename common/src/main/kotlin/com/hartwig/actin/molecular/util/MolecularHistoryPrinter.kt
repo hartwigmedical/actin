2c0fd767580d302423891d8e73b4b73423bc883a
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-07-05 15:01:34 +0200

    ACTIN-1095: Add one more logging line

--- common/src/main/kotlin/com/hartwig/actin/molecular/util/MolecularHistoryPrinter.kt
+++ common/src/main/kotlin/com/hartwig/actin/molecular/util/MolecularHistoryPrinter.kt
@@ -1,11 +1,16 @@
 package com.hartwig.actin.molecular.util
 
+import com.hartwig.actin.TreatmentDatabaseFactory
 import com.hartwig.actin.molecular.datamodel.MolecularHistory
+import org.apache.logging.log4j.LogManager
 
 class MolecularHistoryPrinter() {
 
+    private val LOGGER = LogManager.getLogger(TreatmentDatabaseFactory::class.java)
+
     fun print(molecularHistory: MolecularHistory) {
         // TODO (kz): print all records in history
+        LOGGER.info("Printing molecular history")
         molecularHistory.latestOrangeMolecularRecord()?.let(MolecularRecordPrinter::printRecord)
     }
 
