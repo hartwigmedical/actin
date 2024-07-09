f935e23e00928d65aed668eb51db11a6be12cd4b
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-07-09 16:07:33 -0400

    ACTIN-1097: remove header metadata example

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/paver/PaveImpact.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/paver/PaveImpact.kt
@@ -2,8 +2,6 @@ package com.hartwig.actin.molecular.paver
 
 import com.hartwig.actin.molecular.datamodel.CodingEffect
 
-//##INFO=<ID=IMPACT,Number=10,Type=String,Description="Variant Impact [Gene, Transcript, CanonicalEffect, CanonicalCodingEffect, SpliceRegion, HgvsCodingImpact, HgvsProteinImpact, OtherReportableEffects, WorstCodingEffect, GenesAffected]">
-
 data class PaveImpact(
     val gene: String,
     val transcript: String,
--- molecular/src/main/kotlin/com/hartwig/actin/molecular/paver/PaveTranscriptImpact.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/paver/PaveTranscriptImpact.kt
@@ -2,8 +2,6 @@ package com.hartwig.actin.molecular.paver
 
 import com.hartwig.actin.molecular.datamodel.VariantEffect
 
-//##INFO=<ID=PAVE_TI,Number=.,Type=String,Description="Transcript impact [Gene|GeneName|Transcript|Effects|SpliceRegion|HGVS.c|HGVS.p]">
-
 data class PaveTranscriptImpact(
     val gene: String,
     val geneName: String,
