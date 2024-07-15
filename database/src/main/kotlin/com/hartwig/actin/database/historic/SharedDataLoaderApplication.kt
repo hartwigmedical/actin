ae1bbf03a9b49b13f1975f6d57f2446ee3e2e255
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-07-15 11:45:34 +0200

    ACTIN-1106: Add logging of various jsons

--- database/src/main/kotlin/com/hartwig/actin/database/historic/SharedDataLoaderApplication.kt
+++ database/src/main/kotlin/com/hartwig/actin/database/historic/SharedDataLoaderApplication.kt
@@ -16,9 +16,26 @@ class SharedDataLoaderApplication(private val config: SharedDataLoaderConfig) {
     fun run() {
         LOGGER.info("Running {} v{}", APPLICATION, VERSION)
 
-        LOGGER.info("Loading shared data records from {}", config.sharedDataDirectory)
-        for (directory in File(config.sharedDataDirectory).list()!!) {
-            LOGGER.info(" Processing {}", directory)
+        LOGGER.info("Converting shared data records from {} to patient records", config.sharedDataDirectory)
+        val patients = File(config.sharedDataDirectory).list()!!.map {
+            LOGGER.info(" Processing {}", it)
+
+            val clinical = findClinicalJson(it)
+            val molecular = findMolecularJson(it)
+            val treatmentMatch = findTreatmentMatchJson(it)
+
+            if (!clinical.exists()) {
+                LOGGER.warn("Clinical file does not exist: {}", clinical)
+            }
+
+            if (!molecular.exists()) {
+                LOGGER.warn("Molecular file does not exist: {}", molecular)
+            }
+
+            if (!treatmentMatch.exists()) {
+                LOGGER.warn("Treatment match file does not exist: {}", treatmentMatch)
+            }
+            "1"
         }
 
         val access: DatabaseAccess = DatabaseAccess.fromCredentials(config.dbUser, config.dbPass, config.dbUrl)
@@ -26,9 +43,27 @@ class SharedDataLoaderApplication(private val config: SharedDataLoaderConfig) {
         LOGGER.info("Done!")
     }
 
+    private fun findClinicalJson(directory: String): File {
+        return File(sharedPath(directory) + directory + "T.clinical.json")
+    }
+
+    private fun findMolecularJson(directory: String): File {
+        return File(sharedPath(directory) + directory + "T.molecular.json")
+    }
+
+    private fun findTreatmentMatchJson(directory: String): File {
+        return File(sharedPath(directory) + directory + "T.treatment_match.json")
+    }
+
+    private fun sharedPath(directory: String): String {
+        return config.sharedDataDirectory + File.pathSeparator + directory + File.pathSeparator + BASE_PATH + File.pathSeparator
+    }
+
     companion object {
         val LOGGER: Logger = LogManager.getLogger(MolecularLoaderApplication::class.java)
         const val APPLICATION = "ACTIN Shared Data Loader"
+
+        private val BASE_PATH = "actin" + File.pathSeparator + "1"
         private val VERSION = MolecularLoaderApplication::class.java.getPackage().implementationVersion
     }
 }
