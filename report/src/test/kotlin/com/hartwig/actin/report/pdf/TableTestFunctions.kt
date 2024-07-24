3f2276b35a2701335f6f015ccd6dd94b4bc510b7
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-07-24 13:55:23 -0400

    ACTIN-1124 PR comments

--- report/src/test/kotlin/com/hartwig/actin/report/pdf/TableTestFunctions.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/pdf/TableTestFunctions.kt
@@ -1,12 +1,12 @@
 package com.hartwig.actin.report.pdf
 
-import com.hartwig.actin.report.pdf.tables.molecular.LongitudinalMolecularHistoryGenerator
+import com.hartwig.actin.report.pdf.tables.TableGenerator
 import com.itextpdf.layout.element.Paragraph
 import com.itextpdf.layout.element.Table
 import com.itextpdf.layout.element.Text
 import org.assertj.core.api.Assertions
 
-fun getWrappedTable(result: LongitudinalMolecularHistoryGenerator) = result.contents().getCell(0, 0).children[0] as Table
+fun getWrappedTable(result: TableGenerator) = result.contents().getCell(0, 0).children[0] as Table
 
 fun getCellContents(table: Table, row: Int, column: Int): String =
     ((table.getCell(row, column).children[0] as Paragraph).children[0] as Text).text
