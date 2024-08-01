0c85c8f7bfbc8b8ed237ce23e467c15712c9d887
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-08-01 13:48:24 +0200

    ACTIN-1147: Improve testing

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/IsEligibleForFirstLinePalliativeChemotherapy.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/IsEligibleForFirstLinePalliativeChemotherapy.kt
@@ -10,15 +10,15 @@ import com.hartwig.actin.algo.evaluation.util.Format
 import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory
 import com.hartwig.actin.clinical.datamodel.treatment.history.Intent
 
-class IsEligibleForFirstLinePalliativeChemotherapy(private val hasMetastaticCancerFunction: HasMetastaticCancer) : EvaluationFunction {
+class IsEligibleForFirstLinePalliativeChemotherapy(private val hasMetastaticCancer: HasMetastaticCancer) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
         val palliativeTreatments = record.oncologicalHistory.filter { it.intents?.any { intent -> intent == Intent.PALLIATIVE } == true }
         val categoriesList = Format.concatItemsWithAnd(palliativeTreatments.flatMap { it.categories() }.toSet())
-        val hasMetastaticCancer = hasMetastaticCancerFunction.evaluate(record).result
+        val hasMetastaticCancerResult = hasMetastaticCancer.evaluate(record).result
 
         return when {
-            hasMetastaticCancer == EvaluationResult.FAIL -> {
+            hasMetastaticCancerResult == EvaluationResult.FAIL -> {
                 EvaluationFactory.fail(
                     "Patient has no metastatic cancer and is hence not eligible for first line palliative chemotherapy",
                     "No metastatic cancer and hence no eligibility for first line palliative chemotherapy"
@@ -32,14 +32,14 @@ class IsEligibleForFirstLinePalliativeChemotherapy(private val hasMetastaticCanc
                 )
             }
 
-            palliativeTreatments.isNotEmpty() && hasMetastaticCancer == EvaluationResult.PASS -> {
+            palliativeTreatments.isNotEmpty() && hasMetastaticCancerResult == EvaluationResult.PASS -> {
                 EvaluationFactory.undetermined(
                     "Patient has had palliative $categoriesList and hence may not be eligible for first line palliative chemotherapy",
                     "Patient had palliative $categoriesList (hence may not be eligible for first line palliative chemotherapy)"
                 )
             }
 
-            hasMetastaticCancer == EvaluationResult.PASS -> {
+            hasMetastaticCancerResult == EvaluationResult.PASS -> {
                 EvaluationFactory.undetermined(
                     "Undetermined if patient with metastatic disease is eligible for first line palliative chemotherapy",
                     "Undetermined eligibility for first line palliative chemotherapy"
--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/IsEligibleForFirstLinePalliativeChemotherapyTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/IsEligibleForFirstLinePalliativeChemotherapyTest.kt
@@ -1,23 +1,31 @@
 package com.hartwig.actin.algo.evaluation.treatment
 
+import com.hartwig.actin.PatientRecord
 import com.hartwig.actin.algo.datamodel.EvaluationResult
 import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.tumor.HasMetastaticCancer
 import com.hartwig.actin.algo.evaluation.tumor.TumorTestFactory
 import com.hartwig.actin.clinical.datamodel.TreatmentTestFactory
+import com.hartwig.actin.clinical.datamodel.TumorStage
 import com.hartwig.actin.clinical.datamodel.treatment.TreatmentCategory
 import com.hartwig.actin.clinical.datamodel.treatment.history.Intent
 import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class IsEligibleForFirstLinePalliativeChemotherapyTest {
 
-    private val patientRecord = TumorTestFactory.withTumorStage(null)
     private val alwaysPassMetastaticCancerEvaluation = mockk<HasMetastaticCancer> {
         every { evaluate(any()) } returns EvaluationFactory.pass("metastatic cancer")
     }
+    private val alwaysUndeterminedMetastaticCancerEvaluation = mockk<HasMetastaticCancer> {
+        every { evaluate(any()) } returns EvaluationFactory.undetermined("tumor stage unknown")
+    }
+    private val functionMetastaticCancer = IsEligibleForFirstLinePalliativeChemotherapy(alwaysPassMetastaticCancerEvaluation)
+    private val functionUndeterminedMetastaticCancer =
+        IsEligibleForFirstLinePalliativeChemotherapy(alwaysUndeterminedMetastaticCancerEvaluation)
 
     @Test
     fun `Should fail when no metastatic cancer`() {
@@ -25,58 +33,58 @@ class IsEligibleForFirstLinePalliativeChemotherapyTest {
             every { evaluate(any()) } returns EvaluationFactory.fail("no metastatic cancer")
         }
         val function = IsEligibleForFirstLinePalliativeChemotherapy(alwaysFailsMetastaticCancerEvaluation)
+        val patientRecord = TumorTestFactory.withTumorStage(TumorStage.I)
         assertEvaluation(EvaluationResult.FAIL, function.evaluate(patientRecord))
     }
 
     @Test
-    fun `Should fail when previous palliative chemotherapy`() {
-        val function = IsEligibleForFirstLinePalliativeChemotherapy(alwaysPassMetastaticCancerEvaluation)
-        val treatment = TreatmentTestFactory.drugTreatment("drug therapy", TreatmentCategory.CHEMOTHERAPY)
-        val patientRecord =
-            TreatmentTestFactory.withTreatmentHistoryEntry(
-                TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = setOf(Intent.PALLIATIVE)
-                )
-            )
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(patientRecord))
+    fun `Should fail when metastatic cancer and previous palliative chemotherapy`() {
+        val patientRecord = patientRecordWithTreatmentWithCategoryAndIntent(TreatmentCategory.CHEMOTHERAPY, Intent.PALLIATIVE)
+        assertEvaluation(EvaluationResult.FAIL, functionMetastaticCancer.evaluate(patientRecord))
     }
 
     @Test
     fun `Should be undetermined when patient has metastatic cancer and previous palliative targeted therapy`() {
-        val treatment = TreatmentTestFactory.drugTreatment("drug therapy", TreatmentCategory.TARGETED_THERAPY)
-        val function = IsEligibleForFirstLinePalliativeChemotherapy(alwaysPassMetastaticCancerEvaluation)
-        val patientRecord =
-            TreatmentTestFactory.withTreatmentHistoryEntry(
-                TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = setOf(Intent.PALLIATIVE)
-                )
-            )
-
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(patientRecord))
+        val patientRecord = patientRecordWithTreatmentWithCategoryAndIntent(TreatmentCategory.TARGETED_THERAPY, Intent.PALLIATIVE)
+        val result = functionMetastaticCancer.evaluate(patientRecord)
+        assertEvaluation(EvaluationResult.UNDETERMINED, result)
+        assertThat(result.undeterminedGeneralMessages).containsExactly("Patient had palliative targeted therapy (hence may not be eligible for first line palliative chemotherapy)")
     }
 
     @Test
     fun `Should be undetermined when patient has metastatic cancer and no previous palliative therapy`() {
-        val function = IsEligibleForFirstLinePalliativeChemotherapy(alwaysPassMetastaticCancerEvaluation)
-        val treatment = TreatmentTestFactory.drugTreatment("drug therapy", TreatmentCategory.CHEMOTHERAPY)
-        val patientRecord =
-            TreatmentTestFactory.withTreatmentHistoryEntry(
-                TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = setOf(Intent.CONSOLIDATION)
-                )
-            )
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(patientRecord))
+        val patientRecord = patientRecordWithTreatmentWithCategoryAndIntent(TreatmentCategory.CHEMOTHERAPY, Intent.CONSOLIDATION)
+        val result = functionMetastaticCancer.evaluate(patientRecord)
+        assertEvaluation(EvaluationResult.UNDETERMINED, result)
+        assertThat(result.undeterminedGeneralMessages).containsExactly("Undetermined eligibility for first line palliative chemotherapy")
     }
 
     @Test
-    fun `Should be undetermined when undeterminted metastatic cancer`() {
-        val alwaysUndeterminedMetastaticCancerEvaluation = mockk<HasMetastaticCancer> {
-            every { evaluate(any()) } returns EvaluationFactory.undetermined("tumor stage unknown")
-        }
-        val function = IsEligibleForFirstLinePalliativeChemotherapy(alwaysUndeterminedMetastaticCancerEvaluation)
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(patientRecord))
+    fun `Should be undetermined when undetermined if patient has metastatic cancer and no previous palliative therapy`() {
+        val patientRecord = patientRecordWithTreatmentWithCategoryAndIntent(TreatmentCategory.CHEMOTHERAPY, Intent.CONSOLIDATION)
+        val result = functionUndeterminedMetastaticCancer.evaluate(patientRecord)
+        assertEvaluation(EvaluationResult.UNDETERMINED, result)
+        assertThat(result.undeterminedGeneralMessages).containsExactly("Undetermined if metastatic cancer (hence may not be eligible for first line palliative chemotherapy)")
+    }
+
+    @Test
+    fun `Should be undetermined when undetermined if patient has metastatic cancer and previous palliative targeted therapy`() {
+        val patientRecord = patientRecordWithTreatmentWithCategoryAndIntent(TreatmentCategory.TARGETED_THERAPY, Intent.PALLIATIVE)
+        val result = functionUndeterminedMetastaticCancer.evaluate(patientRecord)
+        assertEvaluation(EvaluationResult.UNDETERMINED, result)
+        assertThat(result.undeterminedGeneralMessages).containsExactly("Undetermined if metastatic cancer (hence may not be eligible for first line palliative chemotherapy)")
+    }
+
+    private fun patientRecordWithTreatmentWithCategoryAndIntent(category: TreatmentCategory, intent: Intent): PatientRecord {
+        return TreatmentTestFactory.withTreatmentHistoryEntry(
+            TreatmentTestFactory.treatmentHistoryEntry(
+                setOf(
+                    TreatmentTestFactory.drugTreatment(
+                        "therapy",
+                        category
+                    )
+                ), intents = setOf(intent)
+            )
+        )
     }
 }
