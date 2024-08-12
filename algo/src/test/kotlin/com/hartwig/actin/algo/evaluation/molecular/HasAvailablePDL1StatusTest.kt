16086377094b405d83fbdb4ff36c432e6e10ff28
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2024-08-12 16:04:23 +0200

    ACTIN-1187: Implement functionality for HasAvailablePDL1Status (#702)
    * ACTIN-1187: Implement functionality for HasAvailablePDL1Status
    
    * ACTIN-1187: Added test

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasAvailablePDL1StatusTest.kt
@@ -0,0 +1,24 @@
+package com.hartwig.actin.algo.evaluation.molecular
+
+import com.hartwig.actin.algo.datamodel.EvaluationResult
+import com.hartwig.actin.algo.evaluation.EvaluationAssert
+import org.junit.Test
+
+class HasAvailablePDL1StatusTest {
+
+    @Test
+    fun `Should pass if record contains PD-L1 IHC test`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.PASS, HasAvailablePDL1Status().evaluate(
+                MolecularTestFactory.withIHCTests(listOf(MolecularTestFactory.priorMolecularTest(test = "IHC", item = "PD-L1")))
+            )
+        )
+    }
+
+    @Test
+    fun `Should fail if record does not contain PD-L1 IHC test`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL, HasAvailablePDL1Status().evaluate(MolecularTestFactory.withIHCTests(emptyList()))
+        )
+    }
+}
