e8c8575db6d9427c2bff3bd3a5c162deb892136a
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-08-12 09:29:37 -0400

    ACTIN-1177 Fix issue deserializing prior sequencing panel records (#700)

---
+++ common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/panel/PanelExtractionAdapter.kt
@@ -0,0 +1,24 @@
+package com.hartwig.actin.molecular.datamodel.panel
+
+import com.hartwig.actin.clinical.datamodel.PriorSequencingTest
+
+data class PanelExtractionAdapter(val priorSequencingTest: PriorSequencingTest) : PanelExtraction {
+    override val panelType = priorSequencingTest.test
+    override val tumorMutationalBurden = priorSequencingTest.tumorMutationalBurden
+    override val isMicrosatelliteUnstable = priorSequencingTest.isMicrosatelliteUnstable
+    override val variants = priorSequencingTest.variants.map {
+        PanelVariantExtraction(
+            it.gene,
+            it.hgvsCodingImpact ?: it.hgvsProteinImpact ?: throw IllegalStateException()
+        )
+    }
+    override val amplifications = priorSequencingTest.amplifications.map {
+        PanelAmplificationExtraction(it.gene)
+    }
+    override val date = priorSequencingTest.date
+    override val extractionClass: String = PanelExtractionAdapter::class.java.simpleName
+
+    override fun testedGenes() = priorSequencingTest.testedGenes ?: emptySet()
+
+    override fun events() = emptySet<PanelEvent>()
+}
