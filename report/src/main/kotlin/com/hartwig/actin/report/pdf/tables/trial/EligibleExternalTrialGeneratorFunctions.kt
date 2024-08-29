a6d275296a4cd394dbcdd1c02ede18309f3c4710
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-08-29 11:55:42 +0200

    ACTIN-1233: Reformatting

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/trial/EligibleExternalTrialGeneratorFunctions.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/trial/EligibleExternalTrialGeneratorFunctions.kt
@@ -38,18 +38,25 @@ object EligibleExternalTrialGeneratorFunctions {
 
     fun hospitalsInHomeCountry(externalTrial: ExternalTrial, homeCountryName: CountryName): List<String> {
         val homeCountry = externalTrial.countries.filter { it.countryName == homeCountryName }
-        return if (homeCountry.size > 1) { throw IllegalStateException("Home country ${homeCountryName.display()} found multiple times") } else {
+        return if (homeCountry.size > 1) {
+            throw IllegalStateException("Home country ${homeCountryName.display()} found multiple times")
+        } else {
             val hospitals = homeCountry.first().hospitalsPerCity.flatMap { it.value }
             if (hospitals.size > 10) {
-                (listOf("Many (please check link for full list)"))
+                (listOf("Many (please check clinicaltrials.gov link)"))
             } else hospitals
         }
     }
 
     fun countryNamesAndCities(externalTrial: ExternalTrial): String {
         return externalTrial.countries.joinToString { country ->
-            val cities = if (country.hospitalsPerCity.keys.size > 8) { "Many cities"} else {country.hospitalsPerCity.keys.joinToString ( ", " )}
-            "${country.countryName.display()} ($cities)"}
+            val cities = if (country.hospitalsPerCity.keys.size > 8) {
+                "Many (please check clinicaltrials.gov link)"
+            } else {
+                country.hospitalsPerCity.keys.joinToString(", ")
+            }
+            "${country.countryName.display()} ($cities)"
+        }
     }
 
     private fun filterMapOfExternalTrials(
