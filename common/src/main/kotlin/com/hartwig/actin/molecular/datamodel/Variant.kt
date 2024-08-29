bdbc1693fd784008c5cb5338cb1ca40492391779
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-08-29 09:15:26 +0200

    ACTIN-1106: Take over Variant class

--- common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/Variant.kt
+++ common/src/main/kotlin/com/hartwig/actin/molecular/datamodel/Variant.kt
@@ -12,6 +12,7 @@ data class Variant(
     val type: VariantType,
     val variantAlleleFrequency: Double? = null,
     val canonicalImpact: TranscriptImpact,
+    val otherImpacts: Set<TranscriptImpact>,
     val extendedVariantDetails: ExtendedVariantDetails? = null,
     val isHotspot: Boolean,
     override val isReportable: Boolean,
@@ -27,42 +28,5 @@ data class Variant(
     override fun compareTo(other: Variant): Int {
         return VariantComparator().compare(this, other)
     }
-
-    constructor(
-        chromosome: String,
-        position: Int,
-        ref: String,
-        alt: String,
-        type: VariantType,
-        canonicalImpact: TranscriptImpact,
-        otherImpacts: Set<TranscriptImpact> = emptySet(),
-        extendedVariantDetails: ExtendedVariantDetails? = null,
-        isHotspot: Boolean,
-        isReportable: Boolean,
-        event: String,
-        driverLikelihood: DriverLikelihood?,
-        evidence: ClinicalEvidence,
-        gene: String,
-        geneRole: GeneRole,
-        proteinEffect: ProteinEffect,
-        isAssociatedWithDrugResistance: Boolean?,
-    ) : this(
-        chromosome,
-        position,
-        ref,
-        alt,
-        type,
-        canonicalImpact,
-        extendedVariantDetails?.copy(otherImpacts = otherImpacts),
-        isHotspot,
-        isReportable,
-        event,
-        driverLikelihood,
-        evidence,
-        gene,
-        geneRole,
-        proteinEffect,
-        isAssociatedWithDrugResistance
-    )
 }
 
