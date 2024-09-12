8b8b9f844c2676f2c44f0db7b1dea9b76f79997e
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-09-12 09:40:25 +0200

    ACTIN-1283: PR feedback

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasLesionsCloseToOrInvolvingAirway.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasLesionsCloseToOrInvolvingAirway.kt
@@ -5,18 +5,34 @@ import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
-import com.hartwig.actin.datamodel.algo.EvaluationResult
 import com.hartwig.actin.doid.DoidModel
 
 class HasLesionsCloseToOrInvolvingAirway(private val doidModel: DoidModel) : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
-        val hasRespiratorySystemCancer =
-            DoidEvaluationFunctions.isOfDoidType(doidModel, record.tumor.doids, DoidConstants.RESPIRATORY_SYSTEM_CANCER)
-        val hasLungMetastases = TumorMetastasisEvaluator.evaluate(record.tumor.hasLungLesions, "lung").result == EvaluationResult.PASS
+        val expandedDoidSet = DoidEvaluationFunctions.createFullExpandedDoidTree(doidModel, record.tumor.doids)
+        val isMajorAirwayCancer = MAJOR_AIRWAYS_CANCER.any { it in expandedDoidSet }
+        val isLungCancer = DoidEvaluationFunctions.isOfDoidType(doidModel, record.tumor.doids, DoidConstants.LUNG_CANCER_DOID)
+        val hasLungLesions = record.tumor.hasLungLesions
+        val hasOtherLesions = record.tumor.otherLesions?.isNotEmpty()
+        val noLesionsCloseToAirway = !isMajorAirwayCancer && !(hasOtherLesions ?: true) && !(hasLungLesions ?: true)
 
         return when {
-            hasLungMetastases || hasRespiratorySystemCancer -> {
+            isMajorAirwayCancer -> {
                 EvaluationFactory.pass("Patient has lesions close to or involving airway", "Lesions close to or involving airway")
+            }
+
+            hasLungLesions == true || isLungCancer -> {
+                EvaluationFactory.warn(
+                    "Patient has lung lesions which may be close to or involving airways",
+                    "Lung lesions which may be close to or involving airway"
+                )
+            }
+
+            noLesionsCloseToAirway -> {
+                EvaluationFactory.fail(
+                    "Patient does not have lesions close to or involving airway",
+                    "Lesions not close to or involving airway"
+                )
             }
 
             else -> {
@@ -26,5 +42,14 @@ class HasLesionsCloseToOrInvolvingAirway(private val doidModel: DoidModel) : Eva
                 )
             }
         }
+    }
+
+    companion object {
+        val MAJOR_AIRWAYS_CANCER = setOf(
+            DoidConstants.BRONCHUS_CANCER,
+            DoidConstants.MAIN_BRONCHUS_CANCER,
+            DoidConstants.LUNG_HILUM_CANCER,
+            DoidConstants.TRACHEAL_CANCER
+        )
     }
 }
--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasLesionsCloseToOrInvolvingAirwayTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasLesionsCloseToOrInvolvingAirwayTest.kt
@@ -3,41 +3,55 @@ package com.hartwig.actin.algo.evaluation.tumor
 import com.hartwig.actin.algo.doid.DoidConstants
 import com.hartwig.actin.algo.evaluation.EvaluationAssert
 import com.hartwig.actin.datamodel.algo.EvaluationResult
+import com.hartwig.actin.datamodel.clinical.TumorDetails
 import com.hartwig.actin.doid.TestDoidModelFactory
 import org.junit.Test
-
-private const val NASAL_CAVITY_CANCER = "10911"
 
 class HasLesionsCloseToOrInvolvingAirwayTest {
 
     private val function = HasLesionsCloseToOrInvolvingAirway(
-        TestDoidModelFactory.createWithOneParentChild(
-            DoidConstants.RESPIRATORY_SYSTEM_CANCER,
-            NASAL_CAVITY_CANCER
-        )
+        TestDoidModelFactory.createWithOneDoidAndTerm(DoidConstants.TRACHEAL_CANCER, "tracheal cancer")
     )
+
+    @Test
+    fun `Should fail if patient has no respiratory system cancer and only bone lesions`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.FAIL,
+            function.evaluate(
+                TumorTestFactory.withDoidsAndLiverLesions(setOf(DoidConstants.LIVER_CANCER_DOID), true)
+                    .copy(tumor = TumorDetails().copy(hasLungLesions = false, otherLesions = emptyList()))
+            )
+        )
+    }
 
     @Test
     fun `Should be undetermined if patient has no lung metastases or respiratory system cancer`() {
         EvaluationAssert.assertEvaluation(
             EvaluationResult.UNDETERMINED,
-            function.evaluate(TumorTestFactory.withDoids(DoidConstants.LIVER_CANCER_DOID))
+            function.evaluate(
+                TumorTestFactory.withDoids(DoidConstants.LIVER_CANCER_DOID)
+                    .copy(tumor = TumorDetails().copy(otherLesions = listOf("Peritoneal")))
+            )
         )
     }
 
     @Test
-    fun `Should pass if patient has lung metastases`() {
+    fun `Should pass if patient has tracheal cancer`() {
         EvaluationAssert.assertEvaluation(
             EvaluationResult.PASS,
+            function.evaluate(TumorTestFactory.withDoids(DoidConstants.TRACHEAL_CANCER))
+        )
+    }
+
+    @Test
+    fun `Should warn if patient has lung cancer or lung lesions`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.WARN,
+            function.evaluate(TumorTestFactory.withDoids(DoidConstants.LUNG_CANCER_DOID))
+        )
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.WARN,
             function.evaluate(TumorTestFactory.withLungLesions(true))
-        )
-    }
-
-    @Test
-    fun `Should pass if patient has nasal cavity cancer`() {
-        EvaluationAssert.assertEvaluation(
-            EvaluationResult.PASS,
-            function.evaluate(TumorTestFactory.withDoids(NASAL_CAVITY_CANCER))
         )
     }
 }
