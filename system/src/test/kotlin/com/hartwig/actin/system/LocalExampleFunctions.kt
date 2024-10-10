4212d646c74c0558306caf978858d0e99764a3b8
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-10-10 13:48:04 +0200

    ACTIN-1326: Move code into class where it is used

--- system/src/test/kotlin/com/hartwig/actin/system/LocalExampleFunctions.kt
+++ system/src/test/kotlin/com/hartwig/actin/system/LocalExampleFunctions.kt
@@ -1,33 +1,13 @@
 package com.hartwig.actin.system
 
-import com.hartwig.actin.TreatmentDatabase
-import com.hartwig.actin.TreatmentDatabaseFactory
-import com.hartwig.actin.algo.calendar.ReferenceDateProvider
-import com.hartwig.actin.algo.evaluation.RuleMappingResources
-import com.hartwig.actin.algo.soc.ResistanceEvidenceMatcher
 import com.hartwig.actin.configuration.AlgoConfiguration
 import com.hartwig.actin.configuration.EnvironmentConfiguration
 import com.hartwig.actin.configuration.ReportConfiguration
-import com.hartwig.actin.datamodel.molecular.MolecularHistory
-import com.hartwig.actin.doid.DoidModel
-import com.hartwig.actin.doid.DoidModelFactory
-import com.hartwig.actin.doid.config.DoidManualConfig
-import com.hartwig.actin.doid.serialization.DoidJson
-import com.hartwig.actin.medication.AtcTree
-import com.hartwig.actin.medication.MedicationCategories
-import com.hartwig.actin.molecular.interpretation.MolecularInputChecker
-import com.hartwig.actin.trial.input.FunctionInputResolver
-import com.hartwig.serve.datamodel.ImmutableActionableEvents
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 import java.io.File
-import java.time.Period
 
 object LocalExampleFunctions {
 
     private const val TRIAL_SOURCE = "Example"
-
-    private val LOGGER: Logger = LogManager.getLogger(LocalExampleFunctions::class.java)
 
     fun createExampleEnvironmentConfiguration(): EnvironmentConfiguration {
         val base = EnvironmentConfiguration.create(null)
@@ -39,66 +19,6 @@ object LocalExampleFunctions {
                 includeMolecularDetailsChapter = false,
                 includeClinicalDetailsChapter = false
             )
-        )
-    }
-
-    fun createExampleRuleMappingResources(referenceDateProvider: ReferenceDateProvider): RuleMappingResources {
-        val resourceDirectory =
-            listOf(System.getProperty("user.home"), "hmf", "repos", "actin-resources-private").joinToString(File.separator)
-
-        val doidJson = listOf(resourceDirectory, "disease_ontology", "doid.json").joinToString(File.separator)
-        val atcTreeTsv = listOf(resourceDirectory, "atc_config", "atc_tree.tsv").joinToString(File.separator)
-        val treatmentDatabaseDir = listOf(resourceDirectory, "treatment_db").joinToString(File.separator)
-
-        LOGGER.info("Loading DOID tree from {}", doidJson)
-        val doidEntry = DoidJson.readDoidOwlEntry(doidJson)
-        LOGGER.info(" Loaded {} nodes", doidEntry.nodes.size)
-        val doidModel = DoidModelFactory.createFromDoidEntry(doidEntry)
-
-        LOGGER.info("Creating ATC tree from file {}", atcTreeTsv)
-        val atcTree = AtcTree.createFromFile(atcTreeTsv)
-
-        val treatmentDatabase = TreatmentDatabaseFactory.createFromPath(treatmentDatabaseDir)
-
-        val functionInputResolver =
-            FunctionInputResolver(
-                doidModel = doidModel,
-                molecularInputChecker = MolecularInputChecker.createAnyGeneValid(),
-                treatmentDatabase = treatmentDatabase,
-                medicationCategories = MedicationCategories.create(atcTree)
-            )
-
-        val environmentConfiguration = createExampleEnvironmentConfiguration()
-
-        return RuleMappingResources(
-            referenceDateProvider = referenceDateProvider,
-            doidModel = doidModel,
-            functionInputResolver = functionInputResolver,
-            atcTree = atcTree,
-            treatmentDatabase = treatmentDatabase,
-            personalizationDataPath = null,
-            algoConfiguration = environmentConfiguration.algo,
-            maxMolecularTestAge = environmentConfiguration.algo.maxMolecularTestAgeInDays?.let {
-                referenceDateProvider.date().minus(Period.ofDays(it))
-            }
-        )
-    }
-
-    fun createEmptyResistanceEvidenceMatcher(): ResistanceEvidenceMatcher {
-        return ResistanceEvidenceMatcher(
-            doidModel = DoidModel(
-                childToParentsMap = emptyMap(),
-                termForDoidMap = emptyMap(),
-                doidForLowerCaseTermMap = emptyMap(),
-                doidManualConfig = DoidManualConfig.create()
-            ),
-            applicableDoids = emptySet(),
-            actionableEvents = ImmutableActionableEvents.builder().build(),
-            treatmentDatabase = TreatmentDatabase(
-                drugsByName = emptyMap(),
-                treatmentsByName = emptyMap()
-            ),
-            molecularHistory = MolecularHistory(molecularTests = emptyList())
         )
     }
 
--- system/src/test/kotlin/com/hartwig/actin/system/LocalExampleTreatmentMatchApplication.kt
+++ system/src/test/kotlin/com/hartwig/actin/system/LocalExampleTreatmentMatchApplication.kt
@@ -1,16 +1,32 @@
 package com.hartwig.actin.system
 
 import com.hartwig.actin.PatientRecordJson
+import com.hartwig.actin.TreatmentDatabase
+import com.hartwig.actin.TreatmentDatabaseFactory
 import com.hartwig.actin.algo.TreatmentMatcher
+import com.hartwig.actin.algo.calendar.ReferenceDateProvider
 import com.hartwig.actin.algo.calendar.ReferenceDateProviderFactory
+import com.hartwig.actin.algo.evaluation.RuleMappingResources
 import com.hartwig.actin.algo.serialization.TreatmentMatchJson
+import com.hartwig.actin.algo.soc.ResistanceEvidenceMatcher
 import com.hartwig.actin.algo.util.TreatmentMatchPrinter
+import com.hartwig.actin.datamodel.molecular.MolecularHistory
+import com.hartwig.actin.doid.DoidModel
+import com.hartwig.actin.doid.DoidModelFactory
+import com.hartwig.actin.doid.config.DoidManualConfig
+import com.hartwig.actin.doid.serialization.DoidJson
+import com.hartwig.actin.medication.AtcTree
+import com.hartwig.actin.medication.MedicationCategories
+import com.hartwig.actin.molecular.interpretation.MolecularInputChecker
 import com.hartwig.actin.testutil.ResourceLocator
+import com.hartwig.actin.trial.input.FunctionInputResolver
 import com.hartwig.actin.trial.serialization.TrialJson
+import com.hartwig.serve.datamodel.ImmutableActionableEvents
 import org.apache.commons.cli.ParseException
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.io.File
+import java.time.Period
 import kotlin.system.exitProcess
 
 class LocalExampleTreatmentMatchApplication {
@@ -23,7 +39,7 @@ class LocalExampleTreatmentMatchApplication {
         val trials = TrialJson.readFromDir(exampleTrialDatabaseDir)
 
         val referenceDateProvider = ReferenceDateProviderFactory.create(patient, runHistorically = false)
-        val resources = LocalExampleFunctions.createExampleRuleMappingResources(referenceDateProvider)
+        val resources = createExampleRuleMappingResources(referenceDateProvider)
 
         LOGGER.info("Matching patient ${patient.patientId} to available trials")
 
@@ -32,7 +48,7 @@ class LocalExampleTreatmentMatchApplication {
                 resources = resources,
                 trials = trials,
                 efficacyEvidence = emptyList(),
-                resistanceEvidenceMatcher = LocalExampleFunctions.createEmptyResistanceEvidenceMatcher()
+                resistanceEvidenceMatcher = createEmptyResistanceEvidenceMatcher()
             )
             .evaluateAndAnnotateMatchesForPatient(patient)
 
@@ -40,6 +56,66 @@ class LocalExampleTreatmentMatchApplication {
         TreatmentMatchJson.write(match, outputDirectory)
 
         LOGGER.info("Done!")
+    }
+
+    private fun createExampleRuleMappingResources(referenceDateProvider: ReferenceDateProvider): RuleMappingResources {
+        val resourceDirectory =
+            listOf(System.getProperty("user.home"), "hmf", "repos", "actin-resources-private").joinToString(File.separator)
+
+        val doidJson = listOf(resourceDirectory, "disease_ontology", "doid.json").joinToString(File.separator)
+        val atcTreeTsv = listOf(resourceDirectory, "atc_config", "atc_tree.tsv").joinToString(File.separator)
+        val treatmentDatabaseDir = listOf(resourceDirectory, "treatment_db").joinToString(File.separator)
+
+        LOGGER.info("Loading DOID tree from {}", doidJson)
+        val doidEntry = DoidJson.readDoidOwlEntry(doidJson)
+        LOGGER.info(" Loaded {} nodes", doidEntry.nodes.size)
+        val doidModel = DoidModelFactory.createFromDoidEntry(doidEntry)
+
+        LOGGER.info("Creating ATC tree from file {}", atcTreeTsv)
+        val atcTree = AtcTree.createFromFile(atcTreeTsv)
+
+        val treatmentDatabase = TreatmentDatabaseFactory.createFromPath(treatmentDatabaseDir)
+
+        val functionInputResolver =
+            FunctionInputResolver(
+                doidModel = doidModel,
+                molecularInputChecker = MolecularInputChecker.createAnyGeneValid(),
+                treatmentDatabase = treatmentDatabase,
+                medicationCategories = MedicationCategories.create(atcTree)
+            )
+
+        val environmentConfiguration = LocalExampleFunctions.createExampleEnvironmentConfiguration()
+
+        return RuleMappingResources(
+            referenceDateProvider = referenceDateProvider,
+            doidModel = doidModel,
+            functionInputResolver = functionInputResolver,
+            atcTree = atcTree,
+            treatmentDatabase = treatmentDatabase,
+            personalizationDataPath = null,
+            algoConfiguration = environmentConfiguration.algo,
+            maxMolecularTestAge = environmentConfiguration.algo.maxMolecularTestAgeInDays?.let {
+                referenceDateProvider.date().minus(Period.ofDays(it))
+            }
+        )
+    }
+
+    private fun createEmptyResistanceEvidenceMatcher(): ResistanceEvidenceMatcher {
+        return ResistanceEvidenceMatcher(
+            doidModel = DoidModel(
+                childToParentsMap = emptyMap(),
+                termForDoidMap = emptyMap(),
+                doidForLowerCaseTermMap = emptyMap(),
+                doidManualConfig = DoidManualConfig.create()
+            ),
+            applicableDoids = emptySet(),
+            actionableEvents = ImmutableActionableEvents.builder().build(),
+            treatmentDatabase = TreatmentDatabase(
+                drugsByName = emptyMap(),
+                treatmentsByName = emptyMap()
+            ),
+            molecularHistory = MolecularHistory(molecularTests = emptyList())
+        )
     }
 
     companion object {
