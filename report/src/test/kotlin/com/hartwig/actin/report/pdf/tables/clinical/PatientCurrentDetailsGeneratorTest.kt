86c3ddbb1a315c1c1abf576c87026de024ba9ef9
John Bartlett <125090035+jbartletthmf@users.noreply.github.com>
2024-10-11 14:13:35 -0400

    ACTIN-1342: Ignore toxicities after end date (#781)
    Extract end date from ProvidedToxicity.
    Filter toxicities with earlier end date from algo consideration and reports.

--- report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/clinical/PatientCurrentDetailsGeneratorTest.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/clinical/PatientCurrentDetailsGeneratorTest.kt
@@ -5,10 +5,12 @@ import com.hartwig.actin.datamodel.clinical.Gender
 import com.hartwig.actin.datamodel.clinical.PatientDetails
 import com.hartwig.actin.datamodel.clinical.Surgery
 import com.hartwig.actin.datamodel.clinical.SurgeryStatus
+import com.hartwig.actin.datamodel.clinical.Toxicity
+import com.hartwig.actin.datamodel.clinical.ToxicitySource
 import com.hartwig.actin.report.pdf.tables.clinical.CellTestUtil.extractTextFromCell
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.time.LocalDateTime
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 private const val KEY_WIDTH = 100f
@@ -17,11 +19,11 @@ private val DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MMM-yyyy")
 
 class PatientCurrentDetailsGeneratorTest {
     private val minimalPatientRecord = TestPatientFactory.createMinimalTestWGSPatientRecord()
+    private val referenceDate = LocalDate.of(2024, 10, 1)
 
     @Test
     fun `Should return title with questionnaire date`() {
-
-        val questionnaireDate = LocalDateTime.of(2024, 9, 19, 1, 1).toLocalDate()
+        val questionnaireDate = LocalDate.of(2024, 9, 19)
 
         val patientRecord = minimalPatientRecord.copy(
             patient = PatientDetails(
@@ -33,19 +35,19 @@ class PatientCurrentDetailsGeneratorTest {
             )
         )
 
-        val patientCurrentDetailsGenerator = PatientCurrentDetailsGenerator(patientRecord, KEY_WIDTH, VALUE_WIDTH)
-        assertThat(patientCurrentDetailsGenerator.title()).isEqualTo("Patient current details (%s)", DATE_FORMAT.format(questionnaireDate))
+        val patientCurrentDetailsGenerator =
+            PatientCurrentDetailsGenerator(patientRecord, KEY_WIDTH, VALUE_WIDTH, referenceDate)
+        assertThat(patientCurrentDetailsGenerator.title())
+            .isEqualTo("Patient current details (%s)", DATE_FORMAT.format(questionnaireDate))
     }
 
     @Test
     fun `Should return content table with surgeries including surgery name`() {
-
-        val endDate = LocalDateTime.of(2024, 9, 19, 1, 1).toLocalDate()
+        val endDate = LocalDate.of(2024, 9, 19)
         val endDateMinus6 = endDate.minusDays(6)
         val endDateMinus4 = endDate.minusDays(4)
 
         val patientRecord = minimalPatientRecord.copy(
-
             surgeries = listOf(
                 Surgery(name = "Surgery 2", endDateMinus6, status = SurgeryStatus.FINISHED),
                 Surgery(name = "Surgery 1", endDate, status = SurgeryStatus.FINISHED),
@@ -53,23 +55,40 @@ class PatientCurrentDetailsGeneratorTest {
             )
         )
 
-        val patientCurrentDetailsGenerator = PatientCurrentDetailsGenerator(patientRecord, KEY_WIDTH, VALUE_WIDTH)
+        val patientCurrentDetailsGenerator =
+            PatientCurrentDetailsGenerator(patientRecord, KEY_WIDTH, VALUE_WIDTH, referenceDate)
         val table = patientCurrentDetailsGenerator.contents()
 
         assertThat(table.numberOfRows).isEqualTo(4)
         assertThat(extractTextFromCell(table.getCell(3, 0))).isEqualTo("Recent surgeries")
-        assertThat(
-            extractTextFromCell(
-                table.getCell(
-                    3,
-                    1
-                )
-            )
-        ).isEqualTo(
+        assertThat(extractTextFromCell(table.getCell(3, 1))).isEqualTo(
             "%s Surgery 1, %s, %s Surgery 2",
             DATE_FORMAT.format(endDate),
             DATE_FORMAT.format(endDateMinus4),
             DATE_FORMAT.format(endDateMinus6)
         )
     }
+
+    @Test
+    fun `Should include toxicities with known sufficient grade that were unresolved as of reference date`() {
+        val patientRecord = minimalPatientRecord.copy(
+            toxicities = listOf(
+                toxicity("Toxicity 1", null, 3),
+                toxicity("Toxicity 2", referenceDate.plusMonths(1), 2),
+                toxicity("Toxicity 3", referenceDate.minusDays(5), 2),
+                toxicity("Toxicity 4", referenceDate.plusMonths(1), null),
+                toxicity("Toxicity 5", referenceDate.plusMonths(1), 1)
+            )
+        )
+        val patientCurrentDetailsGenerator =
+            PatientCurrentDetailsGenerator(patientRecord, KEY_WIDTH, VALUE_WIDTH, referenceDate)
+        val table = patientCurrentDetailsGenerator.contents()
+
+        assertThat(table.numberOfRows).isEqualTo(3)
+        assertThat(extractTextFromCell(table.getCell(0, 0))).isEqualTo("Unresolved toxicities grade => 2")
+        assertThat(extractTextFromCell(table.getCell(0, 1))).isEqualTo("From EHR: Toxicity 1 (3), Toxicity 2 (2)")
+    }
+
+    private fun toxicity(name: String, endDate: LocalDate?, grade: Int?) =
+        Toxicity(name, emptySet(), referenceDate.minusMonths(1), ToxicitySource.EHR, grade, endDate)
 }
