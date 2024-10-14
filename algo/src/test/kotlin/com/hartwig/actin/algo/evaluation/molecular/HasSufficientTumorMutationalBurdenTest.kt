31c04710f0d7fb761ed81bdbfe256b61c39bc840
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-10-14 08:47:43 -0400

    ACTIN-1352 Override molecular test method rather than molecular record (#785)
    And adjust rule to make undermined on unknown TMB

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasSufficientTumorMutationalBurdenTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasSufficientTumorMutationalBurdenTest.kt
@@ -9,7 +9,7 @@ class HasSufficientTumorMutationalBurdenTest {
     @Test
     fun canEvaluate() {
         val function = HasSufficientTumorMutationalBurden(10.0)
-        assertMolecularEvaluation(EvaluationResult.FAIL, function.evaluate(MolecularTestFactory.withTumorMutationalBurden(null)))
+        assertMolecularEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(MolecularTestFactory.withTumorMutationalBurden(null)))
         assertMolecularEvaluation(EvaluationResult.PASS, function.evaluate(MolecularTestFactory.withTumorMutationalBurden(20.0)))
         assertMolecularEvaluation(EvaluationResult.PASS, function.evaluate(MolecularTestFactory.withTumorMutationalBurden(10.0)))
         assertMolecularEvaluation(EvaluationResult.FAIL, function.evaluate(MolecularTestFactory.withTumorMutationalBurden(1.0)))
