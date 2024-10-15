121c3da267680a64b8aeac1537f977f98b80bb45
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2024-10-15 13:28:22 +0200

    ACTIN-1356: Added rules: CURRENTLY_GETS_MEDICATION_INDUCING_PGP, and … (#790)
    * ACTIN-1356: Added rules: CURRENTLY_GETS_MEDICATION_INDUCING_PGP, and CURRENTLY_GETS_MEDICATION_INDUCING_BRCP
    
    * ACTIN-1356: PR changes
    
    * ACTIN-1356: PR changes

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/medication/CurrentlyGetsPGPInducingMedicationTest.kt
@@ -0,0 +1,23 @@
+package com.hartwig.actin.algo.evaluation.medication
+
+import com.hartwig.actin.algo.evaluation.EvaluationAssert
+import com.hartwig.actin.datamodel.algo.EvaluationResult
+import org.junit.Test
+
+class CurrentlyGetsPGPInducingMedicationTest {
+
+    private val function = CurrentlyGetsBCRPInducingMedication()
+
+    @Test
+    fun `Should evaluate to fail when patient uses no medication`() {
+        EvaluationAssert.assertEvaluation(EvaluationResult.FAIL, function.evaluate(MedicationTestFactory.withMedications(emptyList())))
+    }
+
+    @Test
+    fun `Should evaluate to undetermined when patient uses medication`() {
+        EvaluationAssert.assertEvaluation(
+            EvaluationResult.UNDETERMINED,
+            function.evaluate(MedicationTestFactory.withMedications(listOf(MedicationTestFactory.medication())))
+        )
+    }
+}
