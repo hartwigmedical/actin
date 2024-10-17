7462bc5ed054cf0234bf1a406cbdd369b629fb20
Andreia Esteves <a.esteves@hartwigmedicalfoundation.nl>
2024-10-17 16:53:19 +0200

    ACTIN-1364 Added meaningful headers.

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/MolecularCharacteristicsGenerator.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/MolecularCharacteristicsGenerator.kt
@@ -25,7 +25,7 @@ class MolecularCharacteristicsGenerator(private val molecular: MolecularTest, pr
         val colWidth = width / 10
         val table = Tables.createFixedWidthCols(colWidth, colWidth, colWidth, colWidth, colWidth, colWidth * 2, colWidth * 2)
 
-        listOf("Purity", "TML Status", "TMB Status", "MS Stability", "HR Status", "", "").forEach(
+        listOf("Purity", "TML Status", "TMB Status", "MS Stability", "HR Status", "DPYD", "UGT1A1").forEach(
             Consumer { title: String -> table.addHeaderCell(Cells.createHeader(title)) })
         listOfNotNull(
             createPurityCell(molecular.characteristics.purity),
