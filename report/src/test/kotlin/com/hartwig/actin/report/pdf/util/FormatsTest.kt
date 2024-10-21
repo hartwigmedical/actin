c6ddee466c50a0984f0200a9397523eadaa8bea7
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-10-21 17:15:47 +0200

    ACTIN-1372: Rename test to adhere to standards

--- report/src/test/kotlin/com/hartwig/actin/report/pdf/util/FormatsTest.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/pdf/util/FormatsTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class FormatsTest {
 
     @Test
-    fun `Can correctly format numbers to two digits`() {
+    fun `Should correctly format numbers to two digits`() {
         assertThat(Formats.twoDigitNumber(2.123)).isEqualTo("2.12")
         assertThat(Formats.twoDigitNumber(2.12)).isEqualTo("2.12")
         assertThat(Formats.twoDigitNumber(2.1)).isEqualTo("2.1")
@@ -14,7 +14,7 @@ class FormatsTest {
     }
 
     @Test
-    fun `Can insert spaces around plus signs`() {
+    fun `Should insert spaces around plus signs`() {
         assertThat(Formats.insertSpacesAroundPlus("nothing to be  done here")).isEqualTo("nothing to be  done here")
 
         assertThat(Formats.insertSpacesAroundPlus(" + ")).isEqualTo(" + ")
