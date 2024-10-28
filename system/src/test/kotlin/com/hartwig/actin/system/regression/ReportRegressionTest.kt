fc43d65e9675532e27ae3703ce16178546cd6e19
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-10-28 15:24:38 -0400

    ACTIN-1373 Add an assertion against WARNs or ERRORs in the log

--- system/src/test/kotlin/com/hartwig/actin/system/regression/ReportRegressionTest.kt
+++ system/src/test/kotlin/com/hartwig/actin/system/regression/ReportRegressionTest.kt
@@ -4,6 +4,10 @@ import com.hartwig.actin.system.example.ExampleFunctions
 import com.hartwig.actin.system.example.LUNG_01_EXAMPLE
 import com.hartwig.actin.system.example.LocalExampleReportApplication
 import java.time.LocalDate
+import org.apache.logging.log4j.Level
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -12,10 +16,22 @@ import org.junit.runners.Parameterized.Parameters
 @RunWith(Parameterized::class)
 class ReportRegressionTest(private val exampleName: String) {
 
+    private val logLevelRecorder = LogLevelRecorder()
+
     companion object {
         @Parameters
         @JvmStatic
         fun examples() = listOf(LUNG_01_EXAMPLE)
+    }
+
+    @Before
+    fun setUp() {
+        logLevelRecorder.start()
+    }
+
+    @After
+    fun tearDown() {
+        logLevelRecorder.stop()
     }
 
     @Test
@@ -27,6 +43,8 @@ class ReportRegressionTest(private val exampleName: String) {
             ExampleFunctions.resolveExampleTreatmentMatchJson(exampleName),
             outputDirectory
         )
+        assertThat(logLevelRecorder.levelRecorded(Level.WARN) || logLevelRecorder.levelRecorded(Level.ERROR)).withFailMessage("There are errors or warnings in the logs")
+            .isFalse()
         assertThatPdf("$outputDirectory/EXAMPLE-$exampleName.actin.pdf").isEqualToTextually("src/test/resources/example_reports/EXAMPLE-$exampleName.actin.pdf")
         assertThatPdf("$outputDirectory/EXAMPLE-$exampleName.actin.pdf").isEqualToVisually("src/test/resources/example_reports/EXAMPLE-$exampleName.actin.pdf")
     }
