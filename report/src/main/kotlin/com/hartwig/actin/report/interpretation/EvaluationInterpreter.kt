d8ab94923615ac7bede6191ec8642ac60c610237
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-10-29 14:02:56 +0100

    ACTIN-1372: Refactoring to use streams and other improvements that may impress John

--- report/src/main/kotlin/com/hartwig/actin/report/interpretation/EvaluationInterpreter.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/interpretation/EvaluationInterpreter.kt
@@ -4,9 +4,8 @@ import com.hartwig.actin.datamodel.algo.Evaluation
 import com.hartwig.actin.datamodel.algo.EvaluationResult
 import com.hartwig.actin.datamodel.trial.CriterionReference
 import com.hartwig.actin.trial.sort.CriterionReferenceComparator
-import java.util.stream.Collectors
 
-private val SURROUND_PLUS_WITH_SPACES_REGEX = "(\\S)\\+(\\S)".toRegex()
+private val PLUS_WITHOUT_SURROUNDING_SPACES_REGEX = "(\\S)\\+(\\S)".toRegex()
 
 object EvaluationInterpreter {
 
@@ -14,74 +13,71 @@ object EvaluationInterpreter {
         evaluations: Map<CriterionReference, Evaluation>,
         interpretFailOnly: Boolean
     ): List<EvaluationInterpretation> {
-        val references = evaluations.keys.sortedWith(CriterionReferenceComparator()).distinct()
         return if (interpretFailOnly) {
-            createInterpretationsOfType(references, evaluations, EvaluationResult.FAIL)
+            createInterpretationsOfType(evaluations, EvaluationResult.FAIL)
         } else {
-            createInterpretationsOfType(references, evaluations, EvaluationResult.FAIL) +
-                    createInterpretationsOfType(references, evaluations, EvaluationResult.WARN) +
-                    createInterpretationsOfType(references, evaluations, EvaluationResult.UNDETERMINED) +
-                    createInterpretationsOfType(references, evaluations, EvaluationResult.PASS) +
-                    createInterpretationsOfType(references, evaluations, EvaluationResult.NOT_EVALUATED)
+            listOf(
+                EvaluationResult.FAIL,
+                EvaluationResult.WARN,
+                EvaluationResult.UNDETERMINED,
+                EvaluationResult.PASS,
+                EvaluationResult.NOT_EVALUATED
+            ).flatMap { createInterpretationsOfType(evaluations, it) }
         }
     }
 
     private fun createInterpretationsOfType(
-        references: Iterable<CriterionReference>,
         evaluations: Map<CriterionReference, Evaluation>,
         resultToRender: EvaluationResult
     ): List<EvaluationInterpretation> {
-        val interpretations: MutableList<EvaluationInterpretation> = mutableListOf()
-        for (reference in references) {
-            val evaluation = evaluations[reference]!!
-            if (evaluation.result == resultToRender) {
-                val entriesPerResult: MutableMap<EvaluationResult, EvaluationEntry> = mutableMapOf()
-
-                when (evaluation.result) {
-                    EvaluationResult.PASS, EvaluationResult.NOT_EVALUATED -> {
-                        entriesPerResult[evaluation.result] = generateEntry(evaluation, evaluation.passSpecificMessages)
-                    }
-
-                    EvaluationResult.WARN -> {
-                        entriesPerResult[evaluation.result] = generateEntry(evaluation, evaluation.warnSpecificMessages)
-
-                        if (evaluation.undeterminedSpecificMessages.isNotEmpty()) {
-                            entriesPerResult[EvaluationResult.UNDETERMINED] =
-                                generateEntry(EvaluationResult.UNDETERMINED, evaluation.undeterminedSpecificMessages)
-
-                        }
-                    }
-
-                    EvaluationResult.UNDETERMINED -> {
-                        entriesPerResult[evaluation.result] = generateEntry(evaluation, evaluation.undeterminedSpecificMessages)
-                    }
-
-                    EvaluationResult.FAIL -> {
-                        entriesPerResult[evaluation.result] = generateEntry(evaluation, evaluation.failSpecificMessages)
-
-                        if (evaluation.recoverable) {
-                            if (evaluation.warnSpecificMessages.isNotEmpty()) {
-                                entriesPerResult[EvaluationResult.WARN] =
-                                    generateEntry(EvaluationResult.WARN, evaluation.warnSpecificMessages)
-                            }
-
-                            if (evaluation.undeterminedSpecificMessages.isNotEmpty()) {
-                                entriesPerResult[EvaluationResult.UNDETERMINED] =
-                                    generateEntry(EvaluationResult.UNDETERMINED, evaluation.undeterminedSpecificMessages)
-                            }
-                        }
-                    }
-                }
-                interpretations.add(
-                    EvaluationInterpretation(
-                        rule = reference.id,
-                        reference = reference.text,
-                        entriesPerResult = entriesPerResult
-                    )
+        return evaluations.keys.asSequence().sortedWith(CriterionReferenceComparator())
+            .mapNotNull { evaluations.entries.find { it.key.equals(it) } }
+            .filter { it.value.result == resultToRender }
+            .map {
+                EvaluationInterpretation(
+                    rule = it.key.id,
+                    reference = it.key.text,
+                    entriesPerResult = createEvaluationInterpretationMap(it.value)
                 )
+            }.toList()
+    }
+
+    private fun createEvaluationInterpretationMap(evaluation: Evaluation): Map<EvaluationResult, EvaluationEntry> {
+        return when (evaluation.result) {
+            EvaluationResult.PASS, EvaluationResult.NOT_EVALUATED -> {
+                mapOf(Pair(evaluation.result, generateEntry(evaluation, evaluation.passSpecificMessages)))
+            }
+
+            EvaluationResult.WARN -> {
+                mapOf(Pair(evaluation.result, generateEntry(evaluation, evaluation.warnSpecificMessages)),
+                    evaluation.undeterminedSpecificMessages.isNotEmpty().let {
+                        Pair(
+                            EvaluationResult.UNDETERMINED,
+                            generateEntry(EvaluationResult.UNDETERMINED, evaluation.undeterminedSpecificMessages)
+                        )
+                    })
+            }
+
+            EvaluationResult.UNDETERMINED -> {
+                mapOf(Pair(evaluation.result, generateEntry(evaluation, evaluation.undeterminedSpecificMessages)))
+            }
+
+            EvaluationResult.FAIL -> {
+                mapOf(Pair(evaluation.result, generateEntry(evaluation, evaluation.failSpecificMessages)),
+                    evaluation.warnSpecificMessages.isNotEmpty().let {
+                        Pair(
+                            EvaluationResult.WARN,
+                            generateEntry(EvaluationResult.WARN, evaluation.warnSpecificMessages)
+                        )
+                    },
+                    evaluation.undeterminedSpecificMessages.isNotEmpty().let {
+                        Pair(
+                            EvaluationResult.UNDETERMINED,
+                            generateEntry(EvaluationResult.UNDETERMINED, evaluation.undeterminedSpecificMessages)
+                        )
+                    })
             }
         }
-        return interpretations
     }
 
     private fun generateEntry(evaluation: Evaluation, messages: Set<String>): EvaluationEntry {
@@ -100,11 +96,11 @@ object EvaluationInterpreter {
     private fun createEntry(header: String, messages: Set<String>): EvaluationEntry {
         return EvaluationEntry(
             header = header,
-            messages = insertSpacesAroundPlus(messages)
+            messages = messages.map(::insertSpacesAroundPlus).toSet()
         )
     }
 
-    private fun insertSpacesAroundPlus(inputs: Set<String>): Set<String> {
-        return inputs.stream().map { s -> s.replace(SURROUND_PLUS_WITH_SPACES_REGEX, "$1 + $2") }.collect(Collectors.toSet())
+    private fun insertSpacesAroundPlus(input: String): String {
+        return input.replace(PLUS_WITHOUT_SURROUNDING_SPACES_REGEX, "$1 + $2")
     }
 }
