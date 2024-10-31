5553c4c08c472df1a324021c45f8fa258ad9b202
Khalid Zuberi <144375926+kzuberihmf@users.noreply.github.com>
2024-10-31 11:48:00 -0400

    ACTIN-1359: Build molecular tests for IHC fusions (#817)

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/ExperimentType.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/ExperimentType.kt
@@ -5,7 +5,8 @@ import com.hartwig.actin.datamodel.Displayable
 enum class ExperimentType(private val display: String) : Displayable {
     HARTWIG_WHOLE_GENOME("Hartwig WGS"),
     HARTWIG_TARGETED("Hartwig Panel"),
-    PANEL("NGS Panel");
+    PANEL("NGS Panel"),
+    IHC("IHC");
 
     override fun display(): String {
         return display
