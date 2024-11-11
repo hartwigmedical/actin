1252d0054d1bf84a58daf81cd3e1c148b0ff93ce
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2024-11-11 15:21:37 -0500

    ACTIN-1453 Add some additional tests and regression data
    Adds unit tests for the event grouping of drivers for molecular and inclusions of trials with no hospitals. An additional trial only offered
    internationally is added to the regression data.
    
    Turns our the regression test would have caught most issues, except I added the exhaustive config at the same time as changing the table. I thought I'd ran a regression with the full config before and afterthe table changes, but must have made some error in that process. Should have split it into two
    PRs.

--- report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/molecular/DriverTableFunctionsTest.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/molecular/DriverTableFunctionsTest.kt
@@ -1,19 +1,34 @@
 package com.hartwig.actin.report.pdf.tables.molecular
 
-import com.hartwig.actin.datamodel.molecular.evidence.TestClinicalEvidenceFactory
+import com.hartwig.actin.report.pdf.tables.trial.ExternalTrialSummary
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.entry
 import org.junit.Test
 
+private val BASE_EXTERNAL_TRIAL_SUMMARY = ExternalTrialSummary(
+    nctId = "nct",
+    title = "title",
+    url = "url",
+    actinMolecularEvents = sortedSetOf(),
+    sourceMolecularEvents = sortedSetOf(),
+    cancerTypes = sortedSetOf(),
+    countries = sortedSetOf(),
+    cities = sortedSetOf(),
+    hospitals = sortedSetOf()
+)
+
 class DriverTableFunctionsTest {
-    private val trial1 = TestClinicalEvidenceFactory.createExternalTrial("trial 1")
-    private val trial2 = TestClinicalEvidenceFactory.createExternalTrial("trial 2")
-    private val trial3 = TestClinicalEvidenceFactory.createExternalTrial("trial 3")
+    private val trial1 = BASE_EXTERNAL_TRIAL_SUMMARY.copy(title = "trial 1", actinMolecularEvents = sortedSetOf("PTEN del"))
+    private val trial2 = BASE_EXTERNAL_TRIAL_SUMMARY.copy(title = "trial 2", actinMolecularEvents = sortedSetOf("PTEN del", "MYC amp"))
+    private val trial3 = BASE_EXTERNAL_TRIAL_SUMMARY.copy(title = "trial 3", actinMolecularEvents = sortedSetOf("PTEN del", "MYC amp"))
 
     @Test
     fun `Should correctly group by single event`() {
-        val externalTrials = mutableMapOf("PTEN del" to setOf(trial1), "PTEN del, MYC amp" to setOf(trial2, trial3))
+        val externalTrials = setOf(trial1, trial2, trial3)
         val groupedByEvent = DriverTableFunctions.groupByEvent(externalTrials)
-        assertThat(groupedByEvent).isEqualTo(mapOf("PTEN del" to setOf(trial1, trial2, trial3), "MYC amp" to setOf(trial2, trial3)))
+        assertThat(groupedByEvent).containsOnly(
+            entry("MYC amp", setOf(trial2, trial3)),
+            entry("PTEN del", setOf(trial1, trial2, trial3))
+        )
     }
-
 }
