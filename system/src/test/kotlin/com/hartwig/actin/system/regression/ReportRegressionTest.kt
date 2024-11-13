bb407b1b9429c5b3cde55543901a0232bf7e9ca0
Paul Wolfe <paul.d.wolfe@gmail.com>
2024-11-13 09:44:28 -0500

    ACTIN-1460 Show all CKB trials in extended report (#858)
    Also add a test for the extended report to ensure this does not regress.

--- system/src/test/kotlin/com/hartwig/actin/system/regression/ReportRegressionTest.kt
+++ system/src/test/kotlin/com/hartwig/actin/system/regression/ReportRegressionTest.kt
@@ -3,6 +3,7 @@ package com.hartwig.actin.system.regression
 import com.hartwig.actin.system.example.ExampleFunctions
 import com.hartwig.actin.system.example.LUNG_01_EXAMPLE
 import com.hartwig.actin.system.example.LocalExampleReportApplication
+import java.time.LocalDate
 import org.apache.logging.log4j.Level
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
@@ -11,7 +12,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
-import java.time.LocalDate
 
 @RunWith(Parameterized::class)
 class ReportRegressionTest(private val exampleName: String) {
@@ -55,10 +55,14 @@ class ReportRegressionTest(private val exampleName: String) {
         assertThat(logLevelRecorder.levelRecorded(Level.WARN) || logLevelRecorder.levelRecorded(Level.ERROR))
             .withFailMessage("There are errors or warnings in the logs").isFalse()
 
-        val outputPdf = "$outputDirectory/EXAMPLE-$exampleName.actin.pdf"
-        val originalPdf = ExampleFunctions.resolveExampleReportPdf(exampleName)
+        val outputReportPdf = "$outputDirectory/EXAMPLE-$exampleName.actin.pdf"
+        val originalReportPdf = ExampleFunctions.resolveExampleReportPdf(exampleName)
+        assertThatPdf(outputReportPdf).isEqualToTextually(originalReportPdf)
+        assertThatPdf(outputReportPdf).isEqualToVisually(originalReportPdf)
 
-        assertThatPdf(outputPdf).isEqualToTextually(originalPdf)
-        assertThatPdf(outputPdf).isEqualToVisually(originalPdf)
+        val outputExtendedReportPdf = "$outputDirectory/EXAMPLE-$exampleName.actin.extended.pdf"
+        val originalExtendedReportPdf = ExampleFunctions.resolveExampleReportExtendedPdf(exampleName)
+        assertThatPdf(outputExtendedReportPdf).isEqualToTextually(outputExtendedReportPdf)
+        assertThatPdf(outputExtendedReportPdf).isEqualToVisually(originalExtendedReportPdf)
     }
 }
