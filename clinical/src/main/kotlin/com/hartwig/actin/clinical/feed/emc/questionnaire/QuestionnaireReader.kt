cf4ad41c948cac19d2ed7aa5a0c44e8f9cb2dde0
Andreia Esteves <a.esteves@hartwigmedicalfoundation.nl>
2024-11-13 15:21:44 +0100

    ACTIN-1459  Using sections headers when splitting merging the questionnaire lines.

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/emc/questionnaire/QuestionnaireReader.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/emc/questionnaire/QuestionnaireReader.kt
@@ -1,6 +1,7 @@
 package com.hartwig.actin.clinical.feed.emc.questionnaire
 
 import com.google.common.collect.Lists
+import org.apache.logging.log4j.util.Strings
 import java.util.StringJoiner
 
 internal object QuestionnaireReader {
@@ -8,11 +9,11 @@ internal object QuestionnaireReader {
     val TERMS_TO_CLEAN =
         setOf("{", "}", "\\tab", "\\li0", "\\ri0", "\\sa0", "\\sb0", "\\u000ci0", "\\ql", "\\par", "\\u000c2", "\\ltrch", "\\fi0", "\\f2")
 
-    fun read(entryText: String, validKeys: List<String>): Array<String> {
-        return merge(clean(entryText, validKeys).split("\\n").dropLastWhile { it.isEmpty() }.toTypedArray(), validKeys)
+    fun read(entryText: String, validKeys: List<String>, sectionHeaders: List<String>): Array<String> {
+        return merge(clean(entryText, validKeys).split("\\n").dropLastWhile { it.isEmpty() }.toTypedArray(), validKeys, sectionHeaders)
     }
 
-    private fun merge(lines: Array<String>, validKeys: List<String>): Array<String> {
+    private fun merge(lines: Array<String>, validKeys: List<String>, sectionHeaders: List<String>): Array<String> {
         val merged: MutableList<String> = Lists.newArrayList()
         var curLine = newValueStringJoiner()
         for (i in lines.indices) {
@@ -20,7 +21,9 @@ internal object QuestionnaireReader {
             if (lines[i].trim { it <= ' ' }.isEmpty() || i == lines.size - 1 || isField(
                     lines[i + 1],
                     validKeys
-                ) || lines[i + 1].trim { it <= ' ' }.isEmpty()
+                )
+                || sectionHeaders.any { lines[i + 1].contains(it) }
+                || lines[i + 1].trim { it <= ' ' }.isEmpty()
             ) {
                 merged.add(curLine.toString())
                 curLine = newValueStringJoiner()
@@ -39,7 +42,7 @@ internal object QuestionnaireReader {
 
     private fun clean(entryText: String, validKeys: List<String>): String {
         val cleanedText = TERMS_TO_CLEAN.fold(entryText) { acc, term ->
-            acc.replace(term, "")
+            acc.replace(term, Strings.EMPTY)
         }
         val pattern = "(${validKeys.joinToString("|")}):\\s*((\\\\n){2,})".toRegex()
         return pattern.replace(cleanedText) { match ->
--- clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/emc/questionnaire/QuestionnaireReaderTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/emc/questionnaire/QuestionnaireReaderTest.kt
@@ -5,21 +5,37 @@ import org.apache.logging.log4j.util.Strings
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
+
+val SECTION_HEADERS = QuestionnaireMapping.SECTION_HEADERS
+
 class QuestionnaireReaderTest {
 
     @Test
     fun `Should read simple questionnaire`() {
         val questionnaireText = """this: \n is: \n a: \n questionnaire:"""
         val lines = read(
-            questionnaireText, listOf(*questionnaireText.split(": \\n ").dropLastWhile { it.isEmpty() }.toTypedArray())
+            questionnaireText, listOf(*questionnaireText.split(": \\n ").dropLastWhile { it.isEmpty() }.toTypedArray()), SECTION_HEADERS
         )
         assertThat(lines.size).isEqualTo(4)
     }
 
     @Test
+    fun `Should read simple questionnaire with section headers`() {
+        val questionnaireText = """this: \n\nTumor details\nis: \n a: \n questionnaire:"""
+        val lines = read(questionnaireText, listOf("this", "is", "a", "questionnaire"), SECTION_HEADERS)
+        assertThat(lines.size).isEqualTo(5)
+        println(lines.toList().toString())
+        assertThat(lines[0]).isEqualTo("this:")
+        assertThat(lines[1]).isEqualTo("Tumor details")
+        assertThat(lines[2]).isEqualTo("is: ")
+        assertThat(lines[3]).isEqualTo(" a: ")
+        assertThat(lines[4]).isEqualTo(" questionnaire:")
+    }
+
+    @Test
     fun `Should clean terms from questionnaire`() {
         val lines = read(
-            """${QuestionnaireReader.TERMS_TO_CLEAN.joinToString("")}\ntest:""", listOf("test")
+            """${QuestionnaireReader.TERMS_TO_CLEAN.joinToString("")}\ntest:""", listOf("test"), SECTION_HEADERS
         )
         assertThat(lines.size).isEqualTo(2)
         assertThat(lines[0]).isEqualTo(Strings.EMPTY)
@@ -28,7 +44,7 @@ class QuestionnaireReaderTest {
 
     @Test
     fun `Should merge lines for multi line responses`() {
-        val lines = read("""value1: x\nand y\nand more\nvalue2: z\n\nheader\nvalue3: 5\nvalue4: 6\n7""", listOf("value"))
+        val lines = read("""value1: x\nand y\nand more\nvalue2: z\n\nheader\nvalue3: 5\nvalue4: 6\n7""", listOf("value"), SECTION_HEADERS)
         assertThat(lines.size).isEqualTo(6)
         assertThat(lines[0]).isEqualTo("value1: x,and y,and more")
         assertThat(lines[5]).isEqualTo("value4: 6,7")
@@ -37,7 +53,7 @@ class QuestionnaireReaderTest {
     @Test
     fun `Should keep one new line between key and value`() {
         val text = "- IHC test results: \\nERBB2 3+\\n- PD L1 test results: Positive"
-        val lines = read(text, listOf("IHC test results", "PD L1 test results"))
+        val lines = read(text, listOf("IHC test results", "PD L1 test results"), SECTION_HEADERS)
         assertThat(lines.size).isEqualTo(2)
         assertThat(lines[0]).isEqualTo("- IHC test results: ,ERBB2 3+")
         assertThat(lines[1]).isEqualTo("- PD L1 test results: Positive")
@@ -46,7 +62,7 @@ class QuestionnaireReaderTest {
     @Test
     fun `Should replace multiple empty lines with one line between key and value when value exists`() {
         val text = "- IHC test results: \\n\\nERBB2 3+\\n- PD L1 test results: Positive"
-        val lines = read(text, listOf("IHC test results", "PD L1 test results"))
+        val lines = read(text, listOf("IHC test results", "PD L1 test results"), SECTION_HEADERS)
         assertThat(lines.size).isEqualTo(2)
         assertThat(lines[0]).isEqualTo("- IHC test results:,ERBB2 3+")
         assertThat(lines[1]).isEqualTo("- PD L1 test results: Positive")
@@ -55,7 +71,7 @@ class QuestionnaireReaderTest {
     @Test
     fun `Should replace multiple empty lines with one line between key and value when value is missing`() {
         val text = "- IHC test results: \\n\\n- PD L1 test results: Positive"
-        val lines = read(text, listOf("IHC test results", "PD L1 test results"))
+        val lines = read(text, listOf("IHC test results", "PD L1 test results"), SECTION_HEADERS)
         assertThat(lines.size).isEqualTo(2)
         assertThat(lines[0]).isEqualTo("- IHC test results:")
         assertThat(lines[1]).isEqualTo("- PD L1 test results: Positive")
