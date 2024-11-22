89ba70ed0c8618dd3ad9e967faa365984d6fb9d4
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-11-22 17:18:45 +0100

    ACTIN-1503: Add attribution to CKB in footer

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/components/Footer.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/components/Footer.kt
@@ -17,7 +17,7 @@ class Footer {
     fun render(page: PdfPage) {
         val canvas = PdfCanvas(page.lastContentStream, page.resources, page.document)
         val pageNumber = page.document.getPageNumber(page)
-        val template = PdfFormXObject(Rectangle(0f, 0f, 450f, 20f))
+        val template = PdfFormXObject(Rectangle(0f, 0f, 450f, 25f))
         canvas.addXObjectAt(template, 58f, 20f)
         footerTemplates.add(FooterTemplate(pageNumber, template))
         canvas.release()
@@ -33,17 +33,24 @@ class Footer {
     private class FooterTemplate(private val pageNumber: Int, private val template: PdfFormXObject) {
 
         fun renderFooter(document: PdfDocument, totalPageCount: Int) {
-            val displayString = "$pageNumber/$totalPageCount"
             val canvas = Canvas(template, document)
-            val pageNumberParagraph = Paragraph().add(displayString).addStyle(Styles.pageNumberStyle())
+
+            val pageNumberString = "$pageNumber/$totalPageCount"
+            val pageNumberParagraph = Paragraph().add(pageNumberString).addStyle(Styles.pageNumberStyle())
             canvas.showTextAligned(pageNumberParagraph, 0f, 0f, TextAlignment.LEFT)
+
             val disclaimer =
                 "All results and data described in this report are for Research Use Only and have NOT been generated " +
                         "using a clinically validated and controlled procedure nor is it a validated medical device. " +
                         "The results should NOT be used for diagnostic or treatment purposes. " +
                         "No rights can be derived from the content of this report."
             val disclaimerParagraph = Paragraph(disclaimer).setMaxWidth(420f).addStyle(Styles.disclaimerStyle())
-            canvas.showTextAligned(disclaimerParagraph, 30f, 0f, TextAlignment.LEFT)
+            canvas.showTextAligned(disclaimerParagraph, 30f, 10f, TextAlignment.LEFT)
+
+            val attribution =
+                "Somatic gene and variant annotations and related content have been powered by Genomenon Clinical Knowledgebase (CKB)."
+            val attributionParagraph = Paragraph(attribution).setMaxWidth(420f).addStyle(Styles.disclaimerStyle())
+            canvas.showTextAligned(attributionParagraph, 30f, 0f, TextAlignment.LEFT)
         }
     }
 }
