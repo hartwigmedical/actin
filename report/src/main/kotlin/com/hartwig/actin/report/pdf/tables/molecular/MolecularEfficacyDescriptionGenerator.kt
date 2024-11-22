aeb40d766450ca2bd8bafddd2536da2c8c4ce1e3
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-11-22 16:40:41 -0500

    Revert "ACTIN-1487: Improve table splitting over two pages (#867)"
    This reverts commit 72c6a0a506981aa03bfe696f4ee47d322a3a74ac.

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/MolecularEfficacyDescriptionGenerator.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/MolecularEfficacyDescriptionGenerator.kt
@@ -8,7 +8,6 @@ import com.hartwig.actin.report.interpretation.TreatmentEvidenceFunctions.sortTr
 import com.hartwig.actin.report.pdf.tables.TableGenerator
 import com.hartwig.actin.report.pdf.util.Cells
 import com.hartwig.actin.report.pdf.util.Styles.PALETTE_RED
-import com.hartwig.actin.report.pdf.util.Tables.makeWrapping
 import com.itextpdf.layout.element.Cell
 import com.itextpdf.layout.element.Paragraph
 import com.itextpdf.layout.element.Table
@@ -31,7 +30,7 @@ class MolecularEfficacyDescriptionGenerator(val molecularHistory: MolecularHisto
 
         cells.forEach(table::addCell)
 
-        return makeWrapping(table)
+        return table
     }
 
     private fun createEventCells(event: String, evidences: List<TreatmentEvidence>): List<Cell> {
