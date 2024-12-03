c9d8931c8aa19311880bc3e2dfa6928c37c25226
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-12-03 10:02:06 +0100

    ACTIN-1510: Move category event function into own class

---
+++ common/src/main/kotlin/com/hartwig/actin/molecular/evidence/actionability/ActionableEventFunctions.kt
@@ -0,0 +1,33 @@
+package com.hartwig.actin.molecular.evidence.actionability
+
+import com.hartwig.serve.datamodel.molecular.ActionableEvent
+import com.hartwig.serve.datamodel.molecular.characteristic.ActionableCharacteristic
+import com.hartwig.serve.datamodel.molecular.fusion.ActionableFusion
+import com.hartwig.serve.datamodel.molecular.gene.ActionableGene
+import com.hartwig.serve.datamodel.molecular.gene.GeneEvent
+import com.hartwig.serve.datamodel.molecular.hotspot.ActionableHotspot
+import com.hartwig.serve.datamodel.molecular.immuno.ActionableHLA
+import com.hartwig.serve.datamodel.molecular.range.ActionableRange
+
+fun ActionableEvent.isCategoryEvent(): Boolean {
+    return when (this) {
+
+        is ActionableHotspot,
+        is ActionableHLA,
+        is ActionableCharacteristic,
+        is ActionableFusion -> false
+
+        is ActionableRange -> true
+
+        is ActionableGene -> this.event() !in setOf(
+            GeneEvent.AMPLIFICATION,
+            GeneEvent.DELETION,
+            GeneEvent.OVEREXPRESSION,
+            GeneEvent.UNDEREXPRESSION,
+            GeneEvent.PRESENCE_OF_PROTEIN,
+            GeneEvent.ABSENCE_OF_PROTEIN
+        )
+
+        else -> throw IllegalArgumentException()
+    }
+}
