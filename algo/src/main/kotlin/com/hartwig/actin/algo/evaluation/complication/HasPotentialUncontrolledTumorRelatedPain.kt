787dc6bb2732ecc2bbf0fd58037c75d90bc59428
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-12-05 13:30:39 +0100

    ACTIN-1543: Use ATC categories instead of string matching in HasPotentialUncontrolledTumorRelatedPain

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/HasPotentialUncontrolledTumorRelatedPain.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/HasPotentialUncontrolledTumorRelatedPain.kt
@@ -3,13 +3,15 @@ package com.hartwig.actin.algo.evaluation.complication
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.algo.evaluation.medication.MEDICATION_NOT_PROVIDED
+import com.hartwig.actin.algo.evaluation.medication.MedicationSelector
 import com.hartwig.actin.algo.evaluation.util.Format.concatLowercaseWithAnd
-import com.hartwig.actin.clinical.interpretation.MedicationStatusInterpretation
-import com.hartwig.actin.clinical.interpretation.MedicationStatusInterpreter
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
+import com.hartwig.actin.datamodel.clinical.AtcLevel
 
-class HasPotentialUncontrolledTumorRelatedPain(private val interpreter: MedicationStatusInterpreter) :
+private const val SEVERE_PAIN_COMPLICATION = "pain"
+
+class HasPotentialUncontrolledTumorRelatedPain(private val selector: MedicationSelector, private val severePainMedication: Set<AtcLevel>) :
     EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
@@ -21,25 +23,34 @@ class HasPotentialUncontrolledTumorRelatedPain(private val interpreter: Medicati
                 "Present " + concatLowercaseWithAnd(painComplications)
             )
         }
-        val activePainMedications = record.medications?.filter {
-            it.name.equals(SEVERE_PAIN_MEDICATION, ignoreCase = true) && interpreter.interpret(it) == MedicationStatusInterpretation.ACTIVE
-        }?.map { it.name } ?: return MEDICATION_NOT_PROVIDED
+        val medications = record.medications ?: return MEDICATION_NOT_PROVIDED
+        val (activePainMedications, plannedPainMedications) = selector.extractActiveAndPlannedWithCategory(
+            medications,
+            severePainMedication
+        )
 
-        return if (activePainMedications.isNotEmpty()) {
-            EvaluationFactory.pass(
-                "Patient receives pain medication: " + concatLowercaseWithAnd(activePainMedications) +
-                        ", potentially indicating uncontrolled tumor related pain",
-                "Receives " + concatLowercaseWithAnd(activePainMedications) + " indicating pain "
-            )
-        } else
-            EvaluationFactory.fail(
-                "Patient does not have uncontrolled tumor related pain",
-                "No potential uncontrolled tumor related pain"
-            )
-    }
+        return when {
+            activePainMedications.isNotEmpty() -> {
+                EvaluationFactory.pass(
+                    "Patient receives pain medication: " + concatLowercaseWithAnd(activePainMedications) +
+                            ", potentially indicating uncontrolled tumor related pain",
+                    "Receives " + concatLowercaseWithAnd(activePainMedications) + " indicating pain "
+                )
+            }
 
-    companion object {
-        const val SEVERE_PAIN_COMPLICATION = "pain"
-        const val SEVERE_PAIN_MEDICATION = "hydromorphone"
+            plannedPainMedications.isNotEmpty() -> {
+                EvaluationFactory.warn(
+                    "Patient plans to receive pain medication: " + concatLowercaseWithAnd(plannedPainMedications) +
+                            ", potentially indicating uncontrolled tumor related pain",
+                    "Plans to receive " + concatLowercaseWithAnd(plannedPainMedications) + " indicating pain"
+                )
+            }
+
+            else ->
+                EvaluationFactory.fail(
+                    "Patient does not have uncontrolled tumor related pain",
+                    "No potential uncontrolled tumor related pain"
+                )
+        }
     }
 }
