825b4c526ee1e8e6ec0775e2a330b8a463a9edd8
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-12-16 16:23:46 +0100

    ACTIN-1510: Add test for determining matching trials to indications

--- common/src/test/kotlin/com/hartwig/actin/molecular/evidence/actionability/PersonalizedActionabilityFactoryTest.kt
+++ common/src/test/kotlin/com/hartwig/actin/molecular/evidence/actionability/PersonalizedActionabilityFactoryTest.kt
@@ -1,8 +1,10 @@
 package com.hartwig.actin.molecular.evidence.actionability
 
+import com.hartwig.actin.datamodel.molecular.evidence.CancerType
 import com.hartwig.actin.doid.TestDoidModelFactory
 import com.hartwig.actin.molecular.evidence.TestServeEvidenceFactory
 import com.hartwig.actin.molecular.evidence.TestServeFactory
+import com.hartwig.actin.molecular.evidence.TestServeTrialFactory
 import com.hartwig.serve.datamodel.efficacy.EfficacyEvidence
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -10,7 +12,7 @@ import org.junit.Test
 class PersonalizedActionabilityFactoryTest {
 
     @Test
-    fun `Should be able to distinguish on-label and off-label`() {
+    fun `Should be able to distinguish on-label and off-label evidence`() {
         val doidModel = TestDoidModelFactory.createWithOneParentChild("parent", "child")
         val tumorDoids = setOf("child", "exclude")
         val factory = PersonalizedActionabilityFactory.create(doidModel, tumorDoids)
@@ -26,7 +28,7 @@ class PersonalizedActionabilityFactoryTest {
     }
 
     @Test
-    fun `Should qualify everything as off-label if tumor doids are unknown`() {
+    fun `Should qualify all evidence as off-label if tumor doids are unknown`() {
         val doidModel = TestDoidModelFactory.createMinimalTestDoidModel()
         val factory = PersonalizedActionabilityFactory.create(doidModel, emptySet())
 
@@ -34,10 +36,51 @@ class PersonalizedActionabilityFactoryTest {
         val evidence2 = create("doid 2")
         val evidence3 = create("doid 1", "exclude")
 
-        val match = factory.create(ActionabilityMatch(listOf(evidence1, evidence2, evidence3), emptyMap()))
+        val match = factory.create(
+            ActionabilityMatch(
+                evidenceMatches = listOf(evidence1, evidence2, evidence3),
+                matchingCriteriaPerTrialMatch = emptyMap()
+            )
+        )
 
         assertThat(match.treatmentEvidence.filter { it.isOnLabel }.size).isEqualTo(0)
         assertThat(match.treatmentEvidence.filter { !it.isOnLabel }.size).isEqualTo(3)
+    }
+
+    @Test
+    fun `Should determine matching indications for trials`() {
+        val doidModel = TestDoidModelFactory.createWithOneParentChild("parent", "child")
+        val tumorDoids = setOf("child", "exclude")
+        val factory = PersonalizedActionabilityFactory.create(doidModel, tumorDoids)
+
+        val onLabelTrial = TestServeTrialFactory.create(
+            nctId = "NCT00000001",
+            indications = setOf(
+                TestServeFactory.createIndicationWithDoidAndExcludedDoid("parent", "exclude"),
+                TestServeFactory.createIndicationWithDoidAndExcludedDoid("child", "not-exclude")
+            )
+        )
+
+        val offLabelTrial = TestServeTrialFactory.create(
+            nctId = "NCT00000002",
+            indications = setOf(
+                TestServeFactory.createIndicationWithDoidAndExcludedDoid("child", "exclude"),
+                TestServeFactory.createIndicationWithDoid("other")
+            )
+        )
+
+        val result = factory.create(
+            ActionabilityMatch(
+                evidenceMatches = emptyList(),
+                matchingCriteriaPerTrialMatch = mapOf(onLabelTrial to emptySet(), offLabelTrial to emptySet())
+            )
+        )
+
+        assertThat(result.eligibleTrials.size).isEqualTo(1)
+        val eligibleTrial = result.eligibleTrials.first()
+
+        assertThat(eligibleTrial.nctId).isEqualTo(onLabelTrial.nctId())
+        assertThat(eligibleTrial.applicableCancerTypes).containsExactly(CancerType("child", setOf("not-exclude")))
     }
 
     private fun create(doid: String): EfficacyEvidence {
