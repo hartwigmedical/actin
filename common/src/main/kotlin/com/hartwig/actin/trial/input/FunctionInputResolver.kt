0d11cb7326786b53c2864b0ab3d34318d00ab461
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-12-16 13:38:47 +0100

    ACTIN-175: Change and to or

--- common/src/main/kotlin/com/hartwig/actin/trial/input/FunctionInputResolver.kt
+++ common/src/main/kotlin/com/hartwig/actin/trial/input/FunctionInputResolver.kt
@@ -751,8 +751,8 @@ class FunctionInputResolver(
     }
 
     private fun throwExceptionIfAtcCategoryNotMapped(category: String) {
-        val hasMapping = (MedicationCategories.MEDICATION_CATEGORIES_TO_TREATMENT_CATEGORY.containsKey(category)
-                && MedicationCategories.MEDICATION_CATEGORIES_TO_DRUG_TYPES.containsKey(category))
+        val hasMapping = MedicationCategories.MEDICATION_CATEGORIES_TO_TREATMENT_CATEGORY.containsKey(category)
+                || MedicationCategories.MEDICATION_CATEGORIES_TO_DRUG_TYPES.containsKey(category)
         if (MedicationCategories.ANTI_CANCER_ATC_CODES.any { category.startsWith(it) } && !hasMapping) {
             throw IllegalStateException("No treatment category or drug type mapping for ATC code $category")
         }
