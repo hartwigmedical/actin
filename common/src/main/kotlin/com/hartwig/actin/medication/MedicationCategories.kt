21a606b0bf03723a33e32287abd2e03ebf4a9956
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-12-16 13:59:47 +0100

    ACTIN-175: Change O to 0

--- common/src/main/kotlin/com/hartwig/actin/medication/MedicationCategories.kt
+++ common/src/main/kotlin/com/hartwig/actin/medication/MedicationCategories.kt
@@ -49,7 +49,7 @@ class MedicationCategories(private val knownCategories: Map<String, Set<AtcLevel
             "L01CD" to setOf(DrugType.TAXANE),
             "L02BB" to setOf(DrugType.ANTI_ANDROGEN),
             "L01A" to setOf(DrugType.ALKYLATING_AGENT),
-            "LO1XL" to setOf(DrugType.ANTI_CLDN6_CAR_T, DrugType.HER2_CAR_T),
+            "L01XL" to setOf(DrugType.ANTI_CLDN6_CAR_T, DrugType.HER2_CAR_T),
             "L01E" to setOf(DrugType.TYROSINE_KINASE_INHIBITOR),
             "Nitrosoureas" to setOf(DrugType.NITROSOUREAS)
         )
