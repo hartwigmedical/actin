0d50108baf97b8c451f5431d754b81a2fb98d415
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-12-18 10:23:32 +0100

    ACTIN-1510: Fix test after merge

--- common/src/test/kotlin/com/hartwig/actin/algo/serialization/TreatmentMatchJsonTest.kt
+++ common/src/test/kotlin/com/hartwig/actin/algo/serialization/TreatmentMatchJsonTest.kt
@@ -51,7 +51,7 @@ class TreatmentMatchJsonTest {
             personalizedDataAnalysis = null
         )
 
-        val expectedJson = ("{\"patientId\":\"ACTN01029999\",\"sampleId\":\"ACTN01029999T\",\"trialSource\":\"Test hospital\","
+        val expectedJson = ("{\"patientId\":\"ACTN01029999\",\"sampleId\":\"ACTN01029999T\","
                 + "\"referenceDate\":{\"year\":2021,\"month\":8,\"day\":2},\"referenceDateIsLive\":true,\"trialMatches\":["
                 + "{\"identification\":{\"trialId\":\"Test Trial 1\",\"open\":true,\"acronym\":\"TEST-1\","
                 + "\"title\":\"Example test trial 1\",\"nctId\":\"NCT00000010\",\"phase\":\"PHASE_1\",\"source\":\"NKI\""
