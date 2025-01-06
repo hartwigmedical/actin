981dbf0a918bd3e84f97593937346f01a4403a99
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-01-06 13:02:27 -0500

    ACTIN-1596 WIP

--- report/src/main/kotlin/com/hartwig/actin/evidence/TreatmentRank.kt
+++ report/src/main/kotlin/com/hartwig/actin/evidence/TreatmentRank.kt
@@ -1,16 +1,35 @@
 package com.hartwig.actin.evidence
 
+import com.hartwig.actin.PatientRecordJson
 import com.hartwig.actin.datamodel.PatientRecord
+import java.nio.file.Files
+import java.nio.file.Path
 
 class TreatmentRank {
 
     fun rank(record: PatientRecord) {
         val scorer = TreatmentScorer()
-        val treatments =
-            record.molecularHistory.molecularTests.flatMap {
-                (it.drivers.fusions + it.drivers.variants).map { d -> d.evidence } +
-                        it.characteristics.microsatelliteEvidence + it.characteristics.homologousRepairEvidence + it.characteristics.tumorMutationalBurdenEvidence + it.characteristics.tumorMutationalLoadEvidence
-            }.filterNotNull().flatMap { it.treatmentEvidence }.associateBy { scorer.score(it) }
-
+        val treatments = record.molecularHistory.molecularTests.asSequence().flatMap {
+            (it.drivers.fusions + it.drivers.variants).map { d -> d.evidence } +
+                    it.characteristics.microsatelliteEvidence + it.characteristics.homologousRepairEvidence + it.characteristics.tumorMutationalBurdenEvidence + it.characteristics.tumorMutationalLoadEvidence
+        }.filterNotNull().flatMap { it.treatmentEvidence }.toSet()
+        val scoredTreatmentEntries = treatments.map { it to scorer.score(it) }
+        val scoredTreatments =
+            scoredTreatmentEntries.groupBy {
+                it.first.treatment to it.first.molecularMatch.sourceEvent
+            }.mapValues { entry ->
+                entry.value.map { it.second }.sumOf {
+                    it.score()
+                }
+            }
+                .map { it }.sortedBy { it.value }.reversed()
+        scoredTreatments.map { it }.forEach {
+            println("Score [${it.value}] for treatment [${it.key}]")
+        }
     }
+}
+
+fun main() {
+    val patientRecord = PatientRecordJson.fromJson(Files.readString(Path.of("/Users/pwolfe/Code/actin/case_976.patient_record.json")))
+    TreatmentRank().rank(patientRecord)
 }
--- report/src/main/kotlin/com/hartwig/actin/evidence/TreatmentScorer.kt
+++ report/src/main/kotlin/com/hartwig/actin/evidence/TreatmentScorer.kt
@@ -1,15 +1,34 @@
 package com.hartwig.actin.evidence
 
+import com.hartwig.actin.datamodel.molecular.evidence.EvidenceLevelDetails
 import com.hartwig.actin.datamodel.molecular.evidence.TreatmentEvidence
+
+data class Score(
+    val scoringMatch: ScoringMatch,
+    val evidenceLevelDetails: EvidenceLevelDetails,
+    val factor: Int,
+    val score: Int,
+) : Comparable<Score> {
+
+    fun score() = factor * score
+
+    override fun compareTo(other: Score): Int {
+        return score().compareTo(other.score)
+    }
+
+    override fun toString() = "Score [${score()}] from [$scoringMatch, $evidenceLevelDetails]"
+}
 
 class TreatmentScorer {
 
-    fun score(treatment: TreatmentEvidence): Int {
+    fun score(treatment: TreatmentEvidence): Score {
         val onLabel = if (treatment.isOnLabel) TumorMatch.ON_LABEL else TumorMatch.OFF_LABEL
-        val exactVariant = if (treatment.molecularMatch.isCategoryEvent) VariantMatch.CATEGORY else VariantMatch.CATEGORY
+        val exactVariant = if (treatment.molecularMatch.isCategoryEvent) VariantMatch.CATEGORY else VariantMatch.EXACT
         val config = create()
-        val factor = config.levels[ScoringMatch(onLabel, exactVariant)] ?: 0
+        val scoringMatch = ScoringMatch(onLabel, exactVariant)
+        val direction = if (treatment.evidenceDirection.hasBenefit) 1 else -1
+        val factor = (config.levels[scoringMatch] ?: 0) * direction
         val score = config.level.scoring[treatment.evidenceLevelDetails] ?: 0
-        return factor * score
+        return Score(scoringMatch = scoringMatch, evidenceLevelDetails = treatment.evidenceLevelDetails, factor = factor, score = score)
     }
 }
