a8009d8d40ef23918b8ee629d7aa4bb5065a18d1
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2025-01-29 14:31:45 +0100

    ACTIN-1683: Consider chemoradiation to contain platinum doublet when tumor type is NSCLC

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasReceivedPlatinumBasedDoublet.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasReceivedPlatinumBasedDoublet.kt
@@ -1,17 +1,25 @@
 package com.hartwig.actin.algo.evaluation.treatment
 
+import com.hartwig.actin.algo.doid.DoidConstants.LUNG_NON_SMALL_CELL_CARCINOMA_DOID
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.algo.evaluation.tumor.DoidEvaluationFunctions.createFullExpandedDoidTree
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
+import com.hartwig.actin.datamodel.clinical.treatment.Treatment
+import com.hartwig.actin.doid.DoidModel
 
-class HasReceivedPlatinumBasedDoublet : EvaluationFunction {
+class HasReceivedPlatinumBasedDoublet(private val doidModel: DoidModel) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
         val message = "received platinum based doublet chemotherapy"
+        val isNsclc = LUNG_NON_SMALL_CELL_CARCINOMA_DOID in createFullExpandedDoidTree(doidModel, record.tumor.doids)
+        val hasReceivedUndefinedChemoradiation = record.oncologicalHistory.any {
+            it.treatments.map(Treatment::name).containsAll(listOf("CHEMOTHERAPY", "RADIOTHERAPY"))
+        }
 
         return when {
-            TreatmentFunctions.receivedPlatinumDoublet(record) -> {
+            TreatmentFunctions.receivedPlatinumDoublet(record) || (isNsclc && hasReceivedUndefinedChemoradiation) -> {
                 EvaluationFactory.pass("Has $message ")
             }
 
