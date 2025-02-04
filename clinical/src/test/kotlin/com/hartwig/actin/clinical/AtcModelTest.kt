12fd76099cf9e706747f4a6be353ab7be95cb45c
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-02-04 15:42:22 +0100

    ACTIN-1700: Use a string literal name for test names

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/AtcModelTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/AtcModelTest.kt
@@ -15,20 +15,20 @@ import org.junit.Test
 class AtcModelTest {
 
     @Test
-    fun shouldReturnNullForTrialMedication() {
+    fun `Should return null for trial medication`() {
         assertThat(WhoAtcModel(emptyMap(), emptyMap()).resolveByCode("123", CHEMICAL_SUBSTANCE)).isNull()
     }
 
     @Test
-    fun shouldThrowWhenAtcCodeNotFound() {
+    fun `Should throw when ATC code not found`() {
         assertThatThrownBy {
             val victim = WhoAtcModel(mapOf("A" to ANATOMICAL), emptyMap())
-            victim.resolveByCode("notacod", "notalevelname")
+            victim.resolveByCode("notacode", "notalevelname")
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
 
     @Test
-    fun shouldResolveFiveLevelsOfAtcClassification() {
+    fun `Should resolve five levels of ATC classification`() {
         val victim = createAtcModel()
         val result = victim.resolveByCode(FULL_ATC_CODE, CHEMICAL_SUBSTANCE)!!
         assertThat(result.anatomicalMainGroup).isEqualTo(AtcLevel(code = "N", name = ANATOMICAL))
@@ -39,7 +39,7 @@ class AtcModelTest {
     }
 
     @Test
-    fun shouldReturnClassificationForThreeLevelsAtcClassification() {
+    fun `Should return classification for three levels ATC classification`() {
         val victim = createAtcModel()
         val result = victim.resolveByCode("N02B", CHEMICAL)!!
         assertThat(result.chemicalSubGroup).isNull()
@@ -47,14 +47,14 @@ class AtcModelTest {
     }
 
     @Test
-    fun shouldReturnNullForLessThanThreeLevelsAtcClassification() {
+    fun `Should return null for less than three levels ATC classification`() {
         val victim = createAtcModel()
         val result = victim.resolveByCode("N02", PHARMACOLOGICAL)
         assertThat(result).isNull()
     }
 
     @Test
-    fun shouldReturnNewClassificationForAtcClassificationWithPreviousAtcCode() {
+    fun `Should return new classification for ATC classification with previous ATC code`() {
         val victim = createAtcModel()
         val result = victim.resolveByCode("N03XZ91", CHEMICAL_SUBSTANCE)!!
         assertThat(result.anatomicalMainGroup).isEqualTo(AtcLevel(code = "N", name = ANATOMICAL))
@@ -65,14 +65,14 @@ class AtcModelTest {
     }
 
     @Test
-    fun shouldReturnSetOfAtcCodesForCorrectMedicationName() {
+    fun `Should return set of ATC codes for correct medication name`() {
         val victim = createAtcModel()
         val result = victim.resolveByName("paracetamol")
         assertThat(result).isEqualTo(setOf("N02BE01"))
     }
 
     @Test
-    fun shouldReturnEmptySetWhenMedicationNameNotFound() {
+    fun `Should return empty set when medication name not found`() {
         val victim = createAtcModel()
         val result = victim.resolveByName("not a medication name")
         assertThat(result).isNullOrEmpty()
