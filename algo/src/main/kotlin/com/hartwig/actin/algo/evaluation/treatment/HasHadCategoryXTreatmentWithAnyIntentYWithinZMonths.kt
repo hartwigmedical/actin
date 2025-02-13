ac1ce6735ffcecd38fa84d0093f1ba3a311c9cdd
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-13 13:40:17 -0500

    ACTIN-1672: Redid class to inherit evaluate and have consistent parameter names with HAS_HAD_CATEGORY_X_TREATMENT_WITH_ANY_INTENT_Y

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadCategoryXTreatmentWithAnyIntentYWithinZMonths.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadCategoryXTreatmentWithAnyIntentYWithinZMonths.kt
@@ -1,31 +1,34 @@
 package com.hartwig.actin.algo.evaluation.treatment
 
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
 import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
 import com.hartwig.actin.datamodel.clinical.treatment.history.Intent
 
-class HasHadCategoryXTreatmentWithAnyIntentYWithinZMonths {
-    fun evaluate(record: PatientRecord, treatmentCategory: TreatmentCategory, intents: Set<Intent>, months: Int): Evaluation {
+class HasHadCategoryXTreatmentWithAnyIntentYWithinZMonths (private val category: TreatmentCategory, private val intentsToFind: Set<Intent>, private val monthsCutoff: Int) :
+    EvaluationFunction {
+
+    override fun evaluate(record: PatientRecord): Evaluation {
         val treatmentHistory = record.oncologicalHistory
         if (treatmentHistory.isEmpty()) {
             return EvaluationFactory.fail("Patient has no treatment history")
         }
 
         val matchingTreatments = treatmentHistory.filter { entry ->
-            entry.categories().any { it == treatmentCategory }
+            entry.categories().any { it == category }
         }
         if (matchingTreatments.isEmpty()) {
-            return EvaluationFactory.fail("Patient has not had ${treatmentCategory.display()} treatment")
+            return EvaluationFactory.fail("Patient has not had ${category.display()} treatment")
         }
 
         val locoregionalTherapyIntents = matchingTreatments.flatMap { it.intents.orEmpty() }
-        if (locoregionalTherapyIntents.intersect(intents).isEmpty()) {
-            return EvaluationFactory.pass("Patient has had ${treatmentCategory.display()} treatment without the listed intents")
+        if (locoregionalTherapyIntents.intersect(intentsToFind).isEmpty()) {
+            return EvaluationFactory.pass("Patient has had ${category.display()} treatment without the listed intents")
         }
 
 
-        return EvaluationFactory.pass("Patient has received ${treatmentCategory.display()} therapy for the listed intents in the last $months months")
+        return EvaluationFactory.pass("Patient has received ${category.display()} therapy for the listed intents in the last $monthsCutoff months")
     }
 }
