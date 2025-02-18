f32b800d11d6205d39f0e229cca53c46207ef243
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-18 15:13:39 -0500

    ACTIN-1725: Fixed all test cases

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
@@ -17,7 +17,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ECG config from valid inputs`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf(INPUT, "interpretation",  "0",  "",  "",  "0",  "", "", "node 1"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf(INPUT, "interpretation", "node 1",  "1",  "0",  "1",  "ms",  "", ""))
         assertThat(config.errors).isEmpty()
         assertThat(config.config.input).isEqualTo(INPUT)
         assertThat(config.config.curated).isNotNull
@@ -31,35 +31,35 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ignore config when input evaluates to false`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "nvt"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("nvt", "interpretation", "node 1",  "1",  "0",  "1",  "ms",  "", ""))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("nvt", ignore = true, curated = null))
     }
 
     @Test
     fun `Should return ignore config when input evaluates to null`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "possible"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("possible", "interpretation", "node 1",  "1",  "0",  "1",  "ms",  "", ""))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("possible", ignore = true, curated = null))
     }
 
     @Test
     fun `Should return ignore config when interpretation is NULL`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "NULL"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf(INPUT, "NULL", "node 1",  "1",  "0",  "1",  "ms",  "", ""))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig(INPUT, ignore = true, curated = null))
     }
 
     @Test
     fun `Should return empty config when input evaluates to true`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "Ja"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("Ja", "", "",  "0",  "0",  "",  "",  "", ""))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("Ja", ignore = false, curated = Ecg(null, null, null)))
     }
 
     @Test
     fun `Should return validation error when QTCF is not a number`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("isQTCF", "not a number"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf(INPUT, "interpretation",  "node 1", "1",  "0",  "invalid",  "ms",  "", ""))
         assertThat(config.errors).containsExactly(
             CurationConfigValidationError(
                 CurationCategory.ECG.categoryName,
@@ -73,7 +73,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return validation error when JTC is not a number`() {
-        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("isJTC", "not a number"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf(INPUT, "interpretation",  "node 1", "0",  "1",  "",  "",  "invalid", "ms"))
         assertThat(config.errors).containsExactly(CurationConfigValidationError(
             CurationCategory.ECG.categoryName,
             INPUT,
