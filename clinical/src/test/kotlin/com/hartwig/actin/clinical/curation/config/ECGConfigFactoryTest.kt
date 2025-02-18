a4e65e2f5b0895e27fb7c037df1bc2dfb2e7bb24
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-18 13:25:24 -0500

    ACTIN-1725: Update CurationDatabaseContext.kt parameter. Made changes to test (still not functional)

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
@@ -5,6 +5,7 @@ import com.hartwig.actin.clinical.curation.CurationDatabaseReader
 import com.hartwig.actin.clinical.curation.TestCurationFactory
 import com.hartwig.actin.datamodel.clinical.Ecg
 import com.hartwig.actin.datamodel.clinical.EcgMeasure
+import com.hartwig.actin.icd.TestIcdFactory
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -16,7 +17,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ECG config from valid inputs`() {
-        val config = EcgConfigFactory().create(fields, arrayOf(INPUT, "interpretation", "1", "1", "ms", "0", "", ""))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "interpretation"))
         assertThat(config.errors).isEmpty()
         assertThat(config.config.input).isEqualTo(INPUT)
         assertThat(config.config.curated).isNotNull
@@ -30,35 +31,35 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ignore config when input evaluates to false`() {
-        val config = EcgConfigFactory().create(fields, arrayOf("nvt", "", "", "", "", "", "", ""))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "nvt"))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("nvt", ignore = true, curated = null))
     }
 
     @Test
     fun `Should return ignore config when input evaluates to null`() {
-        val config = EcgConfigFactory().create(fields, arrayOf("possible", "", "", "", "", "", "", ""))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "possible"))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("possible", ignore = true, curated = null))
     }
 
     @Test
     fun `Should return ignore config when interpretation is NULL`() {
-        val config = EcgConfigFactory().create(fields, arrayOf(INPUT, "NULL", "", "", "", "", "", ""))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "NULL"))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig(INPUT, ignore = true, curated = null))
     }
 
     @Test
     fun `Should return empty config when input evaluates to true`() {
-        val config = EcgConfigFactory().create(fields, arrayOf("Ja", "", "", "", "", "", "", ""))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("input", "Ja"))
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig("Ja", ignore = false, curated = Ecg(null, null, null)))
     }
 
     @Test
     fun `Should return validation error when QTCF is not a number`() {
-        val config = EcgConfigFactory().create(fields, arrayOf(INPUT, "interpretation", "1", "invalid", "ms", "1", "1", "ms"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("isQTCF", "not a number"))
         assertThat(config.errors).containsExactly(
             CurationConfigValidationError(
                 CurationCategory.ECG.categoryName,
@@ -72,7 +73,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return validation error when JTC is not a number`() {
-        val config = EcgConfigFactory().create(fields, arrayOf(INPUT, "interpretation", "1", "1", "ms", "1", "invalid", "ms"))
+        val config = EcgConfigFactory(TestIcdFactory.createTestModel()).create(fields, arrayOf("isJTC", "not a number"))
         assertThat(config.errors).containsExactly(CurationConfigValidationError(
             CurationCategory.ECG.categoryName,
             INPUT,
