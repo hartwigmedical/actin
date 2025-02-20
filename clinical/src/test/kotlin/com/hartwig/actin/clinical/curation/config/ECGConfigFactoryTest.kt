486c7093c72ebb72899525dca276c2fa5a4e9d61
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-20 16:12:54 -0500

    ACTIN-1718: Move createConfig to bottom. Delete extraneous parameters. Make default units "".

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/curation/config/ECGConfigFactoryTest.kt
@@ -13,20 +13,6 @@ import org.junit.Test
 
 private const val INPUT = "input"
 private val fields: Map<String, Int> = TestCurationFactory.curationHeaders(CurationDatabaseReader.ECG_TSV)
-private fun createConfig(
-    input: String = INPUT,
-    interpretation: String = "interpretation",
-    icd: String = "node 1",
-    isJTC: String = "0",
-    isQTCF: String = "0",
-    qtcfValue: String = "",
-    qtcfUnit: String = "ms",
-    jtcValue: String = "",
-    jtcUnit: String = "ms"
-): ValidatedCurationConfig<ComorbidityConfig>{
-    return EcgConfigFactory(TestIcdFactory.createTestModel()).create(
-        fields, arrayOf(input, interpretation, icd, isQTCF, isJTC, qtcfValue, qtcfUnit, jtcValue, jtcUnit))
-}
 
 class ECGConfigFactoryTest {
     private val icdModel = TestIcdFactory.createTestModel()
@@ -36,7 +22,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ECG config from valid inputs`() {
-        val config = createConfig(isQTCF = "1", qtcfValue = "1")
+        val config = createConfig(isQTCF = "1", qtcfValue = "1", qtcfUnit = "ms")
         assertThat(config.errors).isEmpty()
         assertThat(config.config.input).isEqualTo(INPUT)
         assertThat(config.config.curated).isNotNull
@@ -65,7 +51,7 @@ class ECGConfigFactoryTest {
 
     @Test
     fun `Should return ignore config when interpretation is NULL`() {
-        val config = createConfig(interpretation = "NULL", isQTCF = "1", qtcfValue = "1")
+        val config = createConfig(interpretation = "NULL")
         assertThat(config.errors).isEmpty()
         assertThat(config.config).isEqualTo(ComorbidityConfig(INPUT, ignore = true, curated = null))
     }
@@ -117,6 +103,22 @@ class ECGConfigFactoryTest {
                 "icd",
                 "ICD title \"invalid\" is not known - check for existence in ICD model"
             )
+        )
+    }
+
+    private fun createConfig(
+        input: String = INPUT,
+        interpretation: String = "interpretation",
+        icd: String = "node 1",
+        isJTC: String = "0",
+        isQTCF: String = "0",
+        qtcfValue: String = "",
+        qtcfUnit: String = "",
+        jtcValue: String = "",
+        jtcUnit: String = ""
+    ): ValidatedCurationConfig<ComorbidityConfig> {
+        return EcgConfigFactory(TestIcdFactory.createTestModel()).create(
+            fields, arrayOf(input, interpretation, icd, isQTCF, isJTC, qtcfValue, qtcfUnit, jtcValue, jtcUnit)
         )
     }
 }
