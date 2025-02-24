a3fba204fd782ba87da958ce8b4fe37139adc379
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-24 15:02:31 -0500

    ACTIN-1672:  Reformat HasHadSomeTreatmentsWithCategoryWithIntents.kt with more helper functions and less nested if statements

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSomeTreatmentsWithCategoryWithIntents.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSomeTreatmentsWithCategoryWithIntents.kt
@@ -7,6 +7,7 @@ import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
 import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
 import com.hartwig.actin.datamodel.clinical.treatment.history.Intent
+import com.hartwig.actin.datamodel.clinical.treatment.history.TreatmentHistoryEntry
 import java.time.LocalDate
 
 class HasHadSomeTreatmentsWithCategoryWithIntents(
@@ -15,15 +16,14 @@ class HasHadSomeTreatmentsWithCategoryWithIntents(
     private val minDate: LocalDate? = null
 ) : EvaluationFunction {
 
-    override fun evaluate(record: PatientRecord): Evaluation {
-        val oncologicalHistory = if (minDate == null) {
-            record.oncologicalHistory
-        } else {
-            record.oncologicalHistory.filter { TreatmentSinceDateFunctions.treatmentSinceMinDate(it, minDate, false) }
-        }
+    private fun hasAnyMatchingIntent(entry: TreatmentHistoryEntry) = entry.intents?.intersect(intentsToFind)?.isNotEmpty()
+    private fun historyAfterDate(record: PatientRecord, includeUnknown: Boolean): List<TreatmentHistoryEntry> {
+        return record.oncologicalHistory.filter { TreatmentSinceDateFunctions.treatmentSinceMinDate(it, minDate!!, includeUnknown) }
+    }
 
-        val treatmentSummary = TreatmentSummaryForCategory.createForTreatmentHistory(
-            oncologicalHistory, category, { it.intents?.intersect(intentsToFind)?.isNotEmpty() })
+    override fun evaluate(record: PatientRecord): Evaluation {
+        val oncologicalHistory = if (minDate == null) record.oncologicalHistory else historyAfterDate(record, false)
+        val treatmentSummary = TreatmentSummaryForCategory.createForTreatmentHistory(oncologicalHistory, category, ::hasAnyMatchingIntent)
 
         val intentsList = Format.concatItemsWithOr(intentsToFind)
 
@@ -41,21 +41,19 @@ class HasHadSomeTreatmentsWithCategoryWithIntents(
                 EvaluationFactory.undetermined("Undetermined if treatment received in previous trial included $intentsList ${category.display()}")
             }
 
+            minDate == null -> {
+                EvaluationFactory.fail("Has not received $intentsList ${category.display()}")
+            }
+
             else -> {
-                if (minDate != null) {
-                    val treatmentSummaryWithNull = TreatmentSummaryForCategory.createForTreatmentHistory(
-                        record.oncologicalHistory.filter { TreatmentSinceDateFunctions.treatmentSinceMinDate(it, minDate, true) },
-                        category,
-                        { it.intents?.intersect(intentsToFind)?.isNotEmpty() })
-                    if (treatmentSummaryWithNull.hasSpecificMatch()) {
-                        val treatmentDisplay = treatmentSummaryWithNull.specificMatches.joinToString(", ") { it.treatmentDisplay() }
-                        EvaluationFactory.undetermined("Has received $intentsList ${category.display()} ($treatmentDisplay) with unknown date")
-                    } else {
-                        EvaluationFactory.fail("Has not received $intentsList ${category.display()}")
-                    }
+                val treatmentSummaryWithNull = TreatmentSummaryForCategory.createForTreatmentHistory(
+                    historyAfterDate(record, true), category, ::hasAnyMatchingIntent
+                )
+                if (treatmentSummaryWithNull.hasSpecificMatch()) {
+                    val treatmentDisplay = treatmentSummaryWithNull.specificMatches.joinToString(", ") { it.treatmentDisplay() }
+                    EvaluationFactory.undetermined("Has received $intentsList ${category.display()} ($treatmentDisplay) with unknown date")
                 } else {
                     EvaluationFactory.fail("Has not received $intentsList ${category.display()}")
-
                 }
             }
         }
