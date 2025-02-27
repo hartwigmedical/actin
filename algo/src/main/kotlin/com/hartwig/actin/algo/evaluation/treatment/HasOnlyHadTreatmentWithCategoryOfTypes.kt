03d2ac2a7af67ea76cde3ce4a9cc9e9a0f2fcfcd
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-02-27 12:54:56 -0500

    ACTIN-1745: Implement first draft of HasOnlyHadTreatmentWithCategoryOfTypes.kt

---
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasOnlyHadTreatmentWithCategoryOfTypes.kt
@@ -0,0 +1,37 @@
+package com.hartwig.actin.algo.evaluation.treatment
+
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.algo.Evaluation
+import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
+import com.hartwig.actin.datamodel.clinical.treatment.TreatmentType
+
+class HasOnlyHadTreatmentWithCategoryOfTypes(
+    private val category: TreatmentCategory,
+    private val types: Set<TreatmentType>
+) : EvaluationFunction {
+    override fun evaluate(record: PatientRecord): Evaluation {
+        val treatments = record.oncologicalHistory.flatMap { it.allTreatments() }
+        val (matchingTreatments, diffTreatments) = treatments.partition {
+            it.categories().contains(category) && it.types().intersect(types).isNotEmpty()
+        }
+        val (nullType, wrongTreatments) = diffTreatments.partition {
+            it.categories().contains(category) && it.types().isEmpty()
+        }
+        return when {
+            wrongTreatments.isNotEmpty() -> {
+                EvaluationFactory.fail("There are treatments of the wrong category or type")
+            }
+            nullType.isNotEmpty() -> {
+                EvaluationFactory.warn("There are treatments with null type")
+            }
+            matchingTreatments.isNotEmpty() -> {
+                EvaluationFactory.pass("There are only treatments with the correct category and types")
+            }
+            else -> {
+                EvaluationFactory.fail("No treatments found")
+            }
+        }
+    }
+}
