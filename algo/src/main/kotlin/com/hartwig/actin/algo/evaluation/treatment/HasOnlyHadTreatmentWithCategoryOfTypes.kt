91def45d2d0cb88ef7f39f1fdb9ebdb0b159b20a
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-04 10:22:08 -0500

    ACTIN-1745: Reformat file

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasOnlyHadTreatmentWithCategoryOfTypes.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasOnlyHadTreatmentWithCategoryOfTypes.kt
@@ -13,21 +13,26 @@ class HasOnlyHadTreatmentWithCategoryOfTypes(
 ) : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
         val treatments = record.oncologicalHistory.flatMap { it.allTreatments() }
-        val treatmentMatch = treatments.groupBy { when{
-            it.categories().contains(category) && it.types().intersect(types).isNotEmpty() -> true
-            it.categories().contains(category) && it.types().isEmpty() -> null
-            else -> false
-        } }
+        val treatmentMatch = treatments.groupBy {
+            when {
+                it.categories().contains(category) && it.types().intersect(types).isNotEmpty() -> true
+                it.categories().contains(category) && it.types().isEmpty() -> null
+                else -> false
+            }
+        }
         return when {
-            !treatmentMatch[false].isNullOrEmpty()-> {
+            !treatmentMatch[false].isNullOrEmpty() -> {
                 EvaluationFactory.fail("There are treatments of the wrong category or type")
             }
+
             !treatmentMatch[null].isNullOrEmpty() -> {
                 EvaluationFactory.warn("There are treatments with unknown type")
             }
+
             !treatmentMatch[true].isNullOrEmpty() -> {
                 EvaluationFactory.pass("There are only treatments with the correct category and types")
             }
+
             else -> {
                 EvaluationFactory.fail("No treatments found")
             }
