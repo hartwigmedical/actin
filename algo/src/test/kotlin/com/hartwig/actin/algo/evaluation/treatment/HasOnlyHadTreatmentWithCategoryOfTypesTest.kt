f6bd39da14889ee82bdd5b4c5ee558ab51f27bec
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-05 09:30:21 -0500

    ACTIN-1745: Implement HasOnlyHadTreatmentWithCategoryOfTypes (#1005)
    * Implement HasOnlyHadTreatmentWithCategoryOfTypes.kt and test class
    * ACTIN-1745: Add rule to README, EligibilityRule and treatmentRuleMapper
    
    
    Proposed logics:
    
    PASS: The patient has had one or more treatments that all are of category X and have at least any type Y
    FAIL: No treatments, or 1 or more treatments that don’t contain category X and/or type Y
    WARN: The patient has one or more treatments that match category X but no types are configured (e.g. “Chemotherapy”) (so in theory it could be a PASS if types were known).
    
    This is bug fix for LKO151 I-2.
    
    Note that radiotherapies and surgeries can be ignored (this rule is about systemic treatment only)

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasOnlyHadTreatmentWithCategoryOfTypesTest.kt
@@ -0,0 +1,50 @@
+package com.hartwig.actin.algo.evaluation.treatment
+
+import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.algo.EvaluationResult
+import com.hartwig.actin.datamodel.clinical.TreatmentTestFactory.drugTreatment
+import com.hartwig.actin.datamodel.clinical.TreatmentTestFactory.treatmentHistoryEntry
+import com.hartwig.actin.datamodel.clinical.TreatmentTestFactory.withTreatmentHistory
+import com.hartwig.actin.datamodel.clinical.treatment.DrugType
+import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
+import org.junit.Test
+
+class HasOnlyHadTreatmentWithCategoryOfTypesTest {
+    private val matchingCategory = TreatmentCategory.CHEMOTHERAPY
+    private val matchingTypes = setOf(DrugType.PLATINUM_COMPOUND)
+
+    @Test
+    fun `Should fail if there are no treatments`() {
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(withTreatmentHistory(emptyList())))
+    }
+
+    @Test
+    fun `Should fail if there are treatments of the wrong category`() {
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(makeRecord(category = TreatmentCategory.SURGERY)))
+    }
+
+    @Test
+    fun `Should fail if there are treatments of the wrong type`() {
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(makeRecord(types = setOf(DrugType.HER2_ANTIBODY))))
+    }
+
+    @Test
+    fun `Should warn if there are treatments of the unknown type`() {
+        assertEvaluation(EvaluationResult.WARN, function.evaluate(makeRecord(types = emptySet())))
+    }
+
+    @Test
+    fun `Should pass if all treatments are of the right category and type`() {
+        assertEvaluation(EvaluationResult.PASS, function.evaluate(makeRecord()))
+    }
+
+    private val function = HasOnlyHadTreatmentWithCategoryOfTypes(matchingCategory, matchingTypes)
+
+    private fun makeRecord(category: TreatmentCategory = matchingCategory, types: Set<DrugType> = matchingTypes): PatientRecord {
+        val treatmentHistoryEntry = treatmentHistoryEntry(setOf(drugTreatment("test", category, types)))
+        val matchingTreatmentHistoryEntry = treatmentHistoryEntry(setOf(drugTreatment("test", matchingCategory, matchingTypes)))
+        return withTreatmentHistory(listOf(treatmentHistoryEntry, matchingTreatmentHistoryEntry))
+    }
+}
+
