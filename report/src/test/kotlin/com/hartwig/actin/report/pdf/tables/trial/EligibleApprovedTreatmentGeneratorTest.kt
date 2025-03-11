6a50220bf39969a68a88eb8b3de5aa2cfc359ca8
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-11 01:49:07 -0400

    ACTIN-1678: Generate fake report

--- report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/trial/EligibleApprovedTreatmentGeneratorTest.kt
+++ report/src/test/kotlin/com/hartwig/actin/report/pdf/tables/trial/EligibleApprovedTreatmentGeneratorTest.kt
@@ -1,5 +1,12 @@
 package com.hartwig.actin.report.pdf.tables.trial
 
+import com.hartwig.actin.PatientRecordFactory
+import com.hartwig.actin.configuration.EnvironmentConfiguration
+import com.hartwig.actin.datamodel.algo.TestTreatmentMatchFactory
+import com.hartwig.actin.datamodel.clinical.TestClinicalFactory
+import com.hartwig.actin.datamodel.molecular.MolecularHistory
+import com.hartwig.actin.report.datamodel.Report
+import com.hartwig.actin.report.datamodel.ReportFactory
 import com.hartwig.actin.report.datamodel.TestReportFactory
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -9,9 +16,13 @@ private val width: Float = 1F
 class EligibleApprovedTreatmentGeneratorTest {
     @Test
     fun `Should return approved treatments if available`() {
-        val report = TestReportFactory.createMinimalTestReport()
+        val report = ReportFactory.fromInputs(
+            PatientRecordFactory.fromInputs(TestClinicalFactory.createMinimalTestClinicalRecord(), MolecularHistory.empty()),
+            TestTreatmentMatchFactory.createProperTreatmentMatch(),
+            EnvironmentConfiguration.create(null)
+        )
         val contents = EligibleApprovedTreatmentGenerator(report, width).contents()
-        assertThat(contents != null)
+        assertThat(contents.numberOfColumns == 1)
     }
 }
 
