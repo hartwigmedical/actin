f5ab8e5a54b8a53d5b26733780a137ecc70cf2c4
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-13 14:28:13 -0400

    ACTIN-1673: Make test class

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/CurrentlyGetsChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCyclesTest.kt
@@ -0,0 +1,38 @@
+package com.hartwig.actin.algo.evaluation.treatment
+
+import com.hartwig.actin.algo.evaluation.EvaluationAssert
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.algo.EvaluationResult
+import com.hartwig.actin.datamodel.clinical.TreatmentTestFactory
+import com.hartwig.actin.datamodel.clinical.treatment.DrugType
+import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
+import com.hartwig.actin.datamodel.clinical.treatment.TreatmentType
+import com.hartwig.actin.datamodel.clinical.treatment.history.TreatmentHistoryDetails
+import com.hartwig.actin.datamodel.clinical.treatment.history.TreatmentHistoryEntry
+import org.junit.Test
+
+class CurrentlyGetsChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCyclesTest {
+    @Test
+    fun `Fail if there is no treatments`() {
+        val record = TreatmentTestFactory.withTreatmentHistory(emptyList())
+        assert(EvaluationResult.FAIL, DrugType.ALK_INHIBITOR, 10, record)
+    }
+
+    @Test
+    fun `Pass if there is a match` () {
+        val matchingTreatment = TreatmentHistoryEntry(
+            treatments = setOf(TreatmentTestFactory.drugTreatment("Alk Inhibitor",
+                TreatmentCategory.CHEMOTHERAPY,
+                setOf(DrugType.ALK_INHIBITOR))
+            ),
+            treatmentHistoryDetails = TreatmentHistoryDetails(cycles = 10)
+        )
+        val record = TreatmentTestFactory.withTreatmentHistory(listOf(matchingTreatment))
+        assert(EvaluationResult.PASS, DrugType.ALK_INHIBITOR, 1, record)
+    }
+
+    private fun assert(evaluationResult: EvaluationResult, type: TreatmentType, minCycles: Int, record: PatientRecord) {
+        val evaluation = CurrentlyGetsChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(type, minCycles).evaluate(record)
+        return EvaluationAssert.assertEvaluation(evaluationResult, evaluation)
+    }
+}
