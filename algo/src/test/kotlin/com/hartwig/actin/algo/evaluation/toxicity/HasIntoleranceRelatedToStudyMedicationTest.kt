56f17c420b7888dff965dbeecca8e01daa739cdc
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-03-14 16:23:00 +0100

    ACTIN-1794: Correct test

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasIntoleranceRelatedToStudyMedicationTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasIntoleranceRelatedToStudyMedicationTest.kt
@@ -5,12 +5,22 @@ import com.hartwig.actin.algo.evaluation.othercondition.ComorbidityTestFactory
 import com.hartwig.actin.algo.icd.IcdConstants
 import com.hartwig.actin.datamodel.algo.EvaluationResult
 import com.hartwig.actin.datamodel.clinical.ToxicitySource
-import com.hartwig.actin.icd.TestIcdFactory
+import com.hartwig.actin.icd.IcdModel
+import com.hartwig.actin.icd.datamodel.IcdNode
 import org.junit.Test
 
 class HasIntoleranceRelatedToStudyMedicationTest {
 
-    private val function = HasIntoleranceRelatedToStudyMedication(TestIcdFactory.createTestModel())
+    private val icdModel = IcdModel.create(
+        listOf(
+            IcdNode(
+                IcdConstants.NIVOLUMAB_CODE,
+                listOf(IcdConstants.ANTI_NEOPLASTIC_AGENTS, IcdConstants.DRUG_ALLERGY_CODE),
+                "Nivolumab"
+            )
+        )
+    )
+    private val function = HasIntoleranceRelatedToStudyMedication(icdModel)
     private val matchingIcdCodes = IcdConstants.DRUG_ALLERGY_SET
 
     @Test
@@ -45,6 +55,7 @@ class HasIntoleranceRelatedToStudyMedicationTest {
     fun `Should evaluate to undetermined when active intolerance has matching ICD code`() {
         val intolerance = ComorbidityTestFactory.intolerance(
             icdMainCode = matchingIcdCodes.first(),
+            icdExtensionCode = IcdConstants.NIVOLUMAB_CODE,
             clinicalStatus = HasIntoleranceRelatedToStudyMedication.CLINICAL_STATUS_ACTIVE
         )
         assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(ComorbidityTestFactory.withComorbidity(intolerance)))
@@ -54,9 +65,9 @@ class HasIntoleranceRelatedToStudyMedicationTest {
     fun `Should evaluate to undetermined when any other comorbidity has matching ICD code`() {
         val icdMainCode = matchingIcdCodes.first()
         listOf(
-            ComorbidityTestFactory.toxicity("tox", ToxicitySource.EHR, 2, icdMainCode),
-            ComorbidityTestFactory.otherCondition("condition", icdMainCode = icdMainCode),
-            ComorbidityTestFactory.complication("complication", icdMainCode = icdMainCode)
+            ComorbidityTestFactory.toxicity("tox", ToxicitySource.EHR, 2, icdMainCode, IcdConstants.NIVOLUMAB_CODE),
+            ComorbidityTestFactory.otherCondition("condition", icdMainCode = icdMainCode, icdExtensionCode = IcdConstants.NIVOLUMAB_CODE),
+            ComorbidityTestFactory.complication("complication", icdMainCode = icdMainCode, icdExtensionCode = IcdConstants.NIVOLUMAB_CODE)
         ).forEach { match ->
             assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(ComorbidityTestFactory.withComorbidity(match)))
         }
