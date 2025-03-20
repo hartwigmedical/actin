f1c89f4f5a4bb175376b13270302d84c22ef2511
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2025-03-20 10:34:50 +0100

    ACTIN-1775: Clarify insufficient molecular data message (#1034)
    * ACTIN-1775: Clarify insufficient molecular data message
    
    * ACTIN-1775: Move functions to the top

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1HaplotypeTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/HasUGT1A1HaplotypeTest.kt
@@ -1,11 +1,13 @@
 package com.hartwig.actin.algo.evaluation.molecular
 
 import com.hartwig.actin.algo.evaluation.EvaluationAssert
+import com.hartwig.actin.datamodel.TestPatientFactory
 import com.hartwig.actin.datamodel.algo.EvaluationResult
 import com.hartwig.actin.datamodel.molecular.pharmaco.Haplotype
 import com.hartwig.actin.datamodel.molecular.pharmaco.HaplotypeFunction
 import com.hartwig.actin.datamodel.molecular.pharmaco.PharmacoEntry
 import com.hartwig.actin.datamodel.molecular.pharmaco.PharmacoGene
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class HasUGT1A1HaplotypeTest {
@@ -61,5 +63,12 @@ class HasUGT1A1HaplotypeTest {
                 )
             )
         )
+    }
+
+    @Test
+    fun `Should evaluate to undetermined when molecular record not available`() {
+        val evaluation = function.evaluate(TestPatientFactory.createEmptyMolecularTestPatientRecord())
+        assertThat(evaluation.result).isEqualTo(EvaluationResult.UNDETERMINED)
+        assertThat(evaluation.undeterminedMessages).containsExactly("No molecular data to determine UGT1A1 haplotype")
     }
 }
