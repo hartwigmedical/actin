561361bbc4ef5fad5108ae73cc5d9d5cba393eff
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-03-20 17:24:24 +0100

    ACTIN-1798: Fix conflict

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/GeneHasSufficientCopyNumber.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/molecular/GeneHasSufficientCopyNumber.kt
@@ -8,14 +8,14 @@ import com.hartwig.actin.datamodel.molecular.driver.ProteinEffect
 import com.hartwig.actin.datamodel.molecular.driver.CopyNumber
 import java.time.LocalDate
 
-private enum class CopyNumberEvaluation2 {
+private enum class CopyNumberEvaluation {
     REPORTABLE_SUFFICIENT_COPY_NUMBER,
     SUFFICIENT_COPY_NUMBER_WITH_LOSS_OF_FUNCTION,
     SUFFICIENT_COPY_NUMBER_ON_NON_ONCOGENE,
     UNREPORTABLE_SUFFICIENT_COPY_NUMBER;
 
     companion object {
-        fun fromCopyNumber(copyNumber: CopyNumber): CopyNumberEvaluation2 {
+        fun fromCopyNumber(copyNumber: CopyNumber): CopyNumberEvaluation {
             return when {
                     copyNumber.geneRole == GeneRole.TSG -> {
                         SUFFICIENT_COPY_NUMBER_ON_NON_ONCOGENE
@@ -44,11 +44,11 @@ class GeneHasSufficientCopyNumber(private val gene: String, private val requeste
     override fun genes() = listOf(gene)
 
     override fun evaluate(test: MolecularTest): Evaluation {
-        val evaluatedCopyNumbers: Map<CopyNumberEvaluation2, Set<String>> = test.drivers.copyNumbers.filter { copyNumber ->
+        val evaluatedCopyNumbers: Map<CopyNumberEvaluation, Set<String>> = test.drivers.copyNumbers.filter { copyNumber ->
             copyNumber.gene == gene && (copyNumber.canonicalImpact.minCopies >= requestedMinCopyNumber)
         }
             .groupBy({ copyNumber ->
-                CopyNumberEvaluation2.fromCopyNumber(copyNumber)
+                CopyNumberEvaluation.fromCopyNumber(copyNumber)
             }, valueTransform = CopyNumber::event)
             .mapValues { (_, copyNumberEvents) -> copyNumberEvents.toSet() }
 
@@ -56,10 +56,10 @@ class GeneHasSufficientCopyNumber(private val gene: String, private val requeste
             copyNumber.gene == gene && copyNumber.otherImpacts.any { it.minCopies >= requestedMinCopyNumber }
         }.map { it.event }.toSet()
 
-        return evaluatedCopyNumbers[CopyNumberEvaluation2.REPORTABLE_SUFFICIENT_COPY_NUMBER]?.let { reportableSufficientCN ->
+        return evaluatedCopyNumbers[CopyNumberEvaluation.REPORTABLE_SUFFICIENT_COPY_NUMBER]?.let { reportableSufficientCN ->
             EvaluationFactory.pass("$gene copy number is above requested $requestedMinCopyNumber", inclusionEvents = reportableSufficientCN)
         }
-            ?: evaluatedCopyNumbers[CopyNumberEvaluation2.UNREPORTABLE_SUFFICIENT_COPY_NUMBER]?.let { unreportableSufficientCN ->
+            ?: evaluatedCopyNumbers[CopyNumberEvaluation.UNREPORTABLE_SUFFICIENT_COPY_NUMBER]?.let { unreportableSufficientCN ->
                 EvaluationFactory.pass(
                     "$gene has a copy number >$requestedMinCopyNumber copies but is considered not reportable",
                     inclusionEvents = unreportableSufficientCN
@@ -70,21 +70,21 @@ class GeneHasSufficientCopyNumber(private val gene: String, private val requeste
     }
 
     private fun evaluatePotentialWarns(
-        evaluatedCopyNumbers: Map<CopyNumberEvaluation2, Set<String>>,
+        evaluatedCopyNumbers: Map<CopyNumberEvaluation, Set<String>>,
         eventsOnNonCanonical: Set<String>,
         evidenceSource: String
     ): Evaluation? {
         val eventGroupsWithMessages = listOf(
             EventsWithMessages(
-                evaluatedCopyNumbers[CopyNumberEvaluation2.SUFFICIENT_COPY_NUMBER_WITH_LOSS_OF_FUNCTION],
+                evaluatedCopyNumbers[CopyNumberEvaluation.SUFFICIENT_COPY_NUMBER_WITH_LOSS_OF_FUNCTION],
                 "$gene has sufficient copies but gene associated with loss-of-function protein impact in $evidenceSource"
             ),
             EventsWithMessages(
-                evaluatedCopyNumbers[CopyNumberEvaluation2.SUFFICIENT_COPY_NUMBER_ON_NON_ONCOGENE],
+                evaluatedCopyNumbers[CopyNumberEvaluation.SUFFICIENT_COPY_NUMBER_ON_NON_ONCOGENE],
                 "$gene has sufficient copies but gene known as TSG in $evidenceSource"
             ),
             EventsWithMessages(
-                evaluatedCopyNumbers[CopyNumberEvaluation2.UNREPORTABLE_SUFFICIENT_COPY_NUMBER],
+                evaluatedCopyNumbers[CopyNumberEvaluation.UNREPORTABLE_SUFFICIENT_COPY_NUMBER],
                 "$gene has sufficient copies but not considered reportable"
             ),
             EventsWithMessages(
