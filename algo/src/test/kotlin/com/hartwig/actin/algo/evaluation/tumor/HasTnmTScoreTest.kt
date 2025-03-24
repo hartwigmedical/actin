e7988948a0d0bfbdef3352ccb4988459067aa6d9
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-24 15:52:58 -0400

    ACTIN-1673: Save work

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasTnmTScoreTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasTnmTScoreTest.kt
@@ -1,11 +1,28 @@
 package com.hartwig.actin.algo.evaluation.tumor
 
-import org.junit.Assert.*
+import com.hartwig.actin.algo.doid.DoidConstants
+import com.hartwig.actin.algo.doid.DoidConstants.SMALL_CELL_LUNG_CANCER_DOIDS
+import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.algo.evaluation.molecular.MolecularTestFactory
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.TestPatientFactory
+import com.hartwig.actin.datamodel.algo.EvaluationResult
+import com.hartwig.actin.datamodel.molecular.driver.GeneRole
+import com.hartwig.actin.datamodel.molecular.driver.ProteinEffect
+import com.hartwig.actin.datamodel.molecular.driver.TestCopyNumberFactory
+import com.hartwig.actin.datamodel.molecular.driver.TestTranscriptCopyNumberImpactFactory
+import com.hartwig.actin.datamodel.molecular.driver.CopyNumberType
+import com.hartwig.actin.doid.TestDoidModelFactory
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class HasTnmTScoreTest {
     @Test
     fun `Should PASS if the tumor and request are both TNM M`(){
-
+        assertEvaluation(
+            //EvaluationResult.FAIL, function("", )
+        )
     }
 }
+
+fun function(score: String, record: PatientRecord) = HasTnmTScore(score).evaluate(record)
