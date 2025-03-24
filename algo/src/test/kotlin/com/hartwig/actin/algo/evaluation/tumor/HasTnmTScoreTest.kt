e9738bae44bcdfaac4c4f38bab85515135838505
Zoe He <z.he@hartwigmedicalfoundation.nl>
2025-03-24 14:49:13 -0400

    ACTIN-1673: Create test class

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasTnmTScoreTest.kt
@@ -0,0 +1,11 @@
+package com.hartwig.actin.algo.evaluation.tumor
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class HasTnmTScoreTest {
+    @Test
+    fun `Should PASS if the tumor and request are both TNM M`(){
+
+    }
+}
