a40a44e31c0f460179ba0c1b0485e3139ea40fbe
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2025-03-26 10:17:47 +0100

    ACTIN-1820: Readd medication scrubbing to data quality mask

--- clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/DataQualityMask.kt
+++ clinical/src/main/kotlin/com/hartwig/actin/clinical/feed/standard/DataQualityMask.kt
@@ -9,6 +9,9 @@ val GENERIC_PANEL_ALWAYS_TESTED_GENES = setOf("EGFR", "BRAF", "KRAS")
 private fun ProvidedPatientRecord.scrubModifications() =
     this.copy(treatmentHistory = this.treatmentHistory.map { it.copy(modifications = emptyList()) })
 
+private fun ProvidedPatientRecord.scrubMedications() =
+    this.copy(medications = null)
+
 private fun ProvidedPatientRecord.addAlwaysTestedGenes() =
     this.copy(molecularTests = this.molecularTests.map {
         it.copy(testedGenes = knownGenes(it))
@@ -27,6 +30,6 @@ fun knownGenes(test: ProvidedMolecularTest): Set<String>? {
 
 class DataQualityMask {
     fun apply(ehrPatientRecord: ProvidedPatientRecord): ProvidedPatientRecord {
-        return ehrPatientRecord.scrubModifications().addAlwaysTestedGenes()
+        return ehrPatientRecord.scrubMedications().scrubModifications().addAlwaysTestedGenes()
     }
 } 
