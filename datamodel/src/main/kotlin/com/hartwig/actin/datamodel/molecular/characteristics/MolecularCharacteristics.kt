aec83d63150d16bf45484a14372c607e375682b5
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-04-03 09:52:26 -0400

    ACTIN-1846: Fix test factory and remove defaults for characteristics and drivers

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/characteristics/MolecularCharacteristics.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/characteristics/MolecularCharacteristics.kt
@@ -1,11 +1,11 @@
 package com.hartwig.actin.datamodel.molecular.characteristics
 
 data class MolecularCharacteristics(
-    val purity: Double? = null,
-    val ploidy: Double? = null,
-    val predictedTumorOrigin: PredictedTumorOrigin? = null,
-    val microsatelliteStability: MicrosatelliteStability? = null,
-    val homologousRecombination: HomologousRecombination? = null,
-    val tumorMutationalBurden: TumorMutationalBurden? = null,
-    val tumorMutationalLoad: TumorMutationalLoad? = null
+    val purity: Double?,
+    val ploidy: Double?,
+    val predictedTumorOrigin: PredictedTumorOrigin?,
+    val microsatelliteStability: MicrosatelliteStability?,
+    val homologousRecombination: HomologousRecombination?,
+    val tumorMutationalBurden: TumorMutationalBurden?,
+    val tumorMutationalLoad: TumorMutationalLoad?
 )
