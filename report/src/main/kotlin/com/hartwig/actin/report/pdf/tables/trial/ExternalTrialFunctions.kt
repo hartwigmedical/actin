a95912c5c4df99ddc34ed7343cff529eec9bd5bf
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2025-04-09 11:37:19 +0200

    ACTIN-1821: introduce helper for formatting

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/trial/ExternalTrialFunctions.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/trial/ExternalTrialFunctions.kt
@@ -9,29 +9,32 @@ const val MANY_PLEASE_CHECK_LINK = ">3 locations - please check link"
 
 object ExternalTrialFunctions {
 
+    private val format: (Collection<String>) -> String = { items ->
+        if (items.size > MAX_TO_DISPLAY) MANY_PLEASE_CHECK_LINK else items.joinToString()
+    }
+
+    private fun formatHospitalsAndCities(hospitalsPerCity: Map<String, Set<Hospital>>): Pair<String, String> {
+        val cities = hospitalsPerCity.keys
+        val hospitals = hospitalsPerCity.values.flatten().map(Hospital::name)
+        return format(hospitals) to format(cities)
+    }
+
     fun hospitalsAndCitiesInCountry(trial: ExternalTrialSummary, country: Country): Pair<String, String> {
         val homeCountries = trial.countries.filter { it.country == country }
-        
-        return if (homeCountries.size > 1 || homeCountries.isEmpty()) {
+
+        if (homeCountries.size != 1) {
             throw IllegalStateException(
                 "Country ${country.display()} not configured or configured multiple times for trial ${trial.nctId}. " +
                         "This should not be possible and indicates an issue in the SERVE data export"
             )
-        } else {
-            val (cities, hospitals) = homeCountries.first().hospitalsPerCity
-                .let { listOf(it.keys, it.values.flatten().map(Hospital::name)) }
-                .map { if (it.size > MAX_TO_DISPLAY) MANY_PLEASE_CHECK_LINK else it.joinToString() }
-            hospitals to cities
         }
+
+        return formatHospitalsAndCities(homeCountries.first().hospitalsPerCity)
     }
 
     fun countryNamesWithCities(externalTrial: ExternalTrialSummary): String {
         return externalTrial.countries.joinToString { country ->
-            val cities = if (country.hospitalsPerCity.keys.size > MAX_TO_DISPLAY) {
-                MANY_PLEASE_CHECK_LINK
-            } else {
-                country.hospitalsPerCity.keys.joinToString(", ")
-            }
+            val (_, cities) = formatHospitalsAndCities(country.hospitalsPerCity)
             "${country.country.display()} ($cities)"
         }
     }
