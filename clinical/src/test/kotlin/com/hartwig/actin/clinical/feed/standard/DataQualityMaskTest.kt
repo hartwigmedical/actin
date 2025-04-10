fe7d9a6bbc3beb8e86ffaff3fb25a6b92262e472
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-04-10 09:50:29 +0200

    ACTIN-1860: Remove other superfluous test

--- clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/DataQualityMaskTest.kt
+++ clinical/src/test/kotlin/com/hartwig/actin/clinical/feed/standard/DataQualityMaskTest.kt
@@ -68,7 +68,5 @@ class DataQualityMaskTest {
         assertThat(result.priorPrimaries).isEmpty()
         assertThat(result.tumorDetails.diagnosisDate).isNull()
         assertThat(result.tumorDetails.lesionSite).isNull()
-        assertThat(result.tumorDetails.lesions).isEmpty()
-
     }
 }
