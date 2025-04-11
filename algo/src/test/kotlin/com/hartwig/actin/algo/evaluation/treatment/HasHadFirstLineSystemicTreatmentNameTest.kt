ef69157406d97f018eb80347fb7e9a3ec7dfa59c
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-04-11 13:11:53 +0200

    ACTIN-1877: Fix tests

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadFirstLineSystemicTreatmentNameTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadFirstLineSystemicTreatmentNameTest.kt
@@ -9,14 +9,10 @@ import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
 import org.junit.Test
 
 class HasHadFirstLineSystemicTreatmentNameTest {
-    private val matchingTreatmentName = "treatment 1"
-    private val function = HasHadFirstLineSystemicTreatmentName(
-        treatment(
-            matchingTreatmentName,
-            categories = setOf(TreatmentCategory.CHEMOTHERAPY),
-            isSystemic = true
-        )
-    )
+    private val matchingTreatment =
+        treatmentHistoryEntry(setOf(treatment("matching treatment", categories = setOf(TreatmentCategory.CHEMOTHERAPY), isSystemic = true)))
+    private val otherTreatment = treatmentHistoryEntry(setOf(treatment("other treatment", true)))
+    private val function = HasHadFirstLineSystemicTreatmentName(matchingTreatment.treatments.first())
 
     @Test
     fun `Should fail for empty treatments`() {
@@ -25,69 +21,65 @@ class HasHadFirstLineSystemicTreatmentNameTest {
 
     @Test
     fun `Should fail when patient has not received correct treatment`() {
-        val treatmentHistoryEntry = treatmentHistoryEntry(setOf(treatment("wrong", true)))
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry))))
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(withTreatmentHistory(listOf(otherTreatment))))
     }
 
     @Test
     fun `Should fail when patient has not received correct treatment as first line treatment`() {
-        val treatmentHistoryEntry1 = treatmentHistoryEntry(setOf(treatment("other treatment", true)), startYear = 2024)
-        val treatmentHistoryEntry2 = treatmentHistoryEntry(setOf(treatment(matchingTreatmentName, true)), startYear = 2025)
         assertEvaluation(
             EvaluationResult.FAIL,
-            function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry1, treatmentHistoryEntry2)))
+            function.evaluate(withTreatmentHistory(listOf(otherTreatment.copy(startYear = 2024), matchingTreatment.copy(startYear = 2025))))
         )
     }
 
     @Test
     fun `Should fail when trial treatment received in first line but with incorrect category`() {
-        val treatmentHistoryEntry = treatmentHistoryEntry(
+        val firstLineTrialWithOtherTreatmentCategory = treatmentHistoryEntry(
             setOf(treatment("trial", categories = setOf(TreatmentCategory.IMMUNOTHERAPY), isSystemic = true)),
             isTrial = true,
-            startYear = 2025
+            startYear = 2024
         )
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry))))
+        assertEvaluation(
+            EvaluationResult.FAIL,
+            function.evaluate(withTreatmentHistory(listOf(firstLineTrialWithOtherTreatmentCategory, otherTreatment.copy(startYear = 2025))))
+        )
     }
 
     @Test
     fun `Should pass when patient has received correct treatment in first line`() {
-        val treatmentHistoryEntry1 = treatmentHistoryEntry(setOf(treatment(matchingTreatmentName, true)), startYear = 2024)
-        val treatmentHistoryEntry2 = treatmentHistoryEntry(setOf(treatment("other treatment", true)), startYear = 2025)
         assertEvaluation(
             EvaluationResult.PASS,
-            function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry1, treatmentHistoryEntry2)))
+            function.evaluate(withTreatmentHistory(listOf(matchingTreatment.copy(startYear = 2024), otherTreatment.copy(startYear = 2025))))
         )
     }
 
     @Test
     fun `Should pass when patient has only received correct treatment but with unknown date`() {
-        val treatmentHistoryEntry = treatmentHistoryEntry(setOf(treatment(matchingTreatmentName, true)))
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry))))
+        assertEvaluation(EvaluationResult.PASS, function.evaluate(withTreatmentHistory(listOf(matchingTreatment))))
     }
 
     @Test
     fun `Should evaluate to undetermined when trial treatment received in first line`() {
-        val treatmentHistoryEntry = treatmentHistoryEntry(setOf(treatment("trial", true)), isTrial = true, startYear = 2025)
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry))))
+        val firstLineTrial = treatmentHistoryEntry(setOf(treatment("trial", true)), isTrial = true, startYear = 2024)
+        assertEvaluation(
+            EvaluationResult.UNDETERMINED,
+            function.evaluate(withTreatmentHistory(listOf(firstLineTrial, otherTreatment.copy(startYear = 2025))))
+        )
     }
 
     @Test
     fun `Should evaluate to undetermined when correct treatment received as first treatment but other treatments with unknown date`() {
-        val treatmentHistoryEntry1 = treatmentHistoryEntry(setOf(treatment(matchingTreatmentName, true)), startYear = 2025)
-        val treatmentHistoryEntry2 = treatmentHistoryEntry(setOf(treatment("other treatment", true)))
         assertEvaluation(
             EvaluationResult.UNDETERMINED,
-            function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry1, treatmentHistoryEntry2)))
+            function.evaluate(withTreatmentHistory(listOf(matchingTreatment.copy(startYear = 2025), otherTreatment)))
         )
     }
 
     @Test
     fun `Should evaluate to undetermined when correct treatment received and another treatment but both with unknown start date`() {
-        val treatmentHistoryEntry1 = treatmentHistoryEntry(setOf(treatment(matchingTreatmentName, true)))
-        val treatmentHistoryEntry2 = treatmentHistoryEntry(setOf(treatment("other treatment", true)))
         assertEvaluation(
             EvaluationResult.UNDETERMINED,
-            function.evaluate(withTreatmentHistory(listOf(treatmentHistoryEntry1, treatmentHistoryEntry2)))
+            function.evaluate(withTreatmentHistory(listOf(matchingTreatment, otherTreatment)))
         )
     }
 }
