3c239db022f053546a89ad24d57c4da0fc87fbca
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-04-11 13:31:47 +0200

    ACTIN-1862: WIP

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/TableGeneratorFunctions.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/TableGeneratorFunctions.kt
@@ -1,6 +1,7 @@
 package com.hartwig.actin.report.pdf.tables
 
 import com.hartwig.actin.report.pdf.util.Cells
+import com.hartwig.actin.report.pdf.util.Formats
 import com.hartwig.actin.report.pdf.util.Tables
 import com.itextpdf.layout.element.Table
 
@@ -8,7 +9,7 @@ object TableGeneratorFunctions {
 
     fun addGenerators(generators: List<TableGenerator>, table: Table, overrideTitleFormatToSubtitle: Boolean) {
         generators.map { generator ->
-            val generatedTable = Table(1).setWidth(table.width.value - 5)
+            val generatedTable = Table(1).setWidth(table.width.value - Formats.STANDARD_INNER_TABLE_WIDTH_DECREASE)
             if (overrideTitleFormatToSubtitle) {
                 generatedTable.addCell(Cells.createSubTitle(generator.title()))
             } else {
@@ -16,11 +17,12 @@ object TableGeneratorFunctions {
             }
             val contentTable = generator.contents()
 
-            generatedTable.addCell(Cells.create(Tables.makeWrapping(contentTable, generator.forceKeepTogether())))
+            generatedTable.addCell(Tables.makeWrapping(contentTable, generator.forceKeepTogether()))
             if (contentTable.numberOfRows < 3) {
                 generatedTable.setKeepTogether(true)
             }
-            Cells.create(generatedTable)
+//            Cells.create(generatedTable)
+            generatedTable
         }.forEach(table::addCell)
     }
 }
