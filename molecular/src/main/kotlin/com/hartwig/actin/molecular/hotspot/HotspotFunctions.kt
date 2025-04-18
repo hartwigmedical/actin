003fa661c5c2ae9ce5f125318512049ed696146e
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-04-18 13:58:05 +0200

    ACTIN-1056: Add HotspotFunctions

---
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/hotspot/HotspotFunctions.kt
@@ -0,0 +1,21 @@
+package com.hartwig.actin.molecular.hotspot
+
+import com.hartwig.serve.datamodel.molecular.common.GeneAlteration
+import com.hartwig.serve.datamodel.molecular.common.ProteinEffect
+import com.hartwig.serve.datamodel.molecular.hotspot.KnownHotspot
+import com.hartwig.serve.datamodel.molecular.range.KnownCodon
+
+object HotspotFunctions {
+
+    private val SERVE_HOTSPOT_PROTEIN_EFFECTS = setOf(
+        ProteinEffect.LOSS_OF_FUNCTION,
+        ProteinEffect.LOSS_OF_FUNCTION_PREDICTED,
+        ProteinEffect.GAIN_OF_FUNCTION,
+        ProteinEffect.GAIN_OF_FUNCTION_PREDICTED
+    )
+
+    fun isHotspot(geneAlteration: GeneAlteration?): Boolean {
+        return (geneAlteration is KnownHotspot || geneAlteration is KnownCodon) &&
+                geneAlteration.proteinEffect() in SERVE_HOTSPOT_PROTEIN_EFFECTS
+    }
+}
