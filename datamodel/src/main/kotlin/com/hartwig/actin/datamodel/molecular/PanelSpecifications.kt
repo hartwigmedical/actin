6bbbb2e016215deecb6e0d1a5cb1ecaf45ae86e0
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-04-24 20:58:48 -0400

    ACTIN-1867 Integration testing

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/PanelSpecifications.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/PanelSpecifications.kt
@@ -1,13 +1,21 @@
 package com.hartwig.actin.datamodel.molecular
 
 import java.util.function.Predicate
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 data class PanelGeneSpecification(val geneName: String, val targets: List<MolecularTestTarget>)
 
 data class PanelSpecification(private val geneTargetMap: Map<String, List<MolecularTestTarget>>) {
 
-    fun testsGene(gene: String, molecularTestTargets: Predicate<List<MolecularTestTarget>>) =
-        geneTargetMap[gene]?.let { molecularTestTargets.test(it) } ?: false
+    fun testsGene(gene: String, molecularTestTargets: Predicate<List<MolecularTestTarget>>): Boolean {
+        LOGGER.info("Searching spec map [$geneTargetMap]")
+        return geneTargetMap[gene]?.let { molecularTestTargets.test(it) } ?: false
+    }
+
+    companion object {
+        val LOGGER: Logger = LogManager.getLogger(PanelSpecifications::class.java)
+    }
 }
 
 class PanelSpecifications(panelGeneSpecifications: Map<String, List<PanelGeneSpecification>>) {
