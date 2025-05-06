b382f606a17d61591e6fb86b635ef47b67afe7cd
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-05-06 13:21:49 +0200

    ACTIN-1939: Add new rule

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasRapidProgressiveDisease.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasRapidProgressiveDisease.kt
@@ -5,9 +5,9 @@ import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
 
-class HasSpecificProgressiveDiseaseCriteria : EvaluationFunction {
+class HasRapidProgressiveDisease : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        return EvaluationFactory.undetermined("Tumor progression according to specific criteria undetermined")
+        return EvaluationFactory.undetermined("Undetermined if patient may have rapid progressive disease")
     }
 }
