346f9c0d3722ed976c12852dced9820df4379460
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-05-19 14:59:59 +0200

    ACTIN-1985: Refactoring

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSystemicTherapyWithAnyIntentTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadSystemicTherapyWithAnyIntentTest.kt
@@ -9,19 +9,22 @@ import java.time.LocalDate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
+private val REQUESTED_INTENT = setOf(Intent.ADJUVANT)
+private val WRONG_INTENT = setOf(Intent.PALLIATIVE)
+private val SYSTEMIC_TREATMENT = TreatmentTestFactory.treatment("systemic treatment", true)
+private val NON_SYSTEMIC_TREATMENT = TreatmentTestFactory.treatment("non systemic treatment", false)
+
 class HasHadSystemicTherapyWithAnyIntentTest {
 
     private val referenceDate = LocalDate.of(2024, 1, 25)
     private val refDate = referenceDate.minusMonths(6)
     private val recentDate = referenceDate.minusMonths(3)
     private val olderDate = referenceDate.minusMonths(14)
-    private val requestedIntent = setOf(Intent.ADJUVANT)
-    private val wrongIntent = setOf(Intent.PALLIATIVE)
     private val functionEvaluatingWithinWeeks =
-        HasHadSystemicTherapyWithAnyIntent(requestedIntent, refDate, 6, true)
+        HasHadSystemicTherapyWithAnyIntent(REQUESTED_INTENT, refDate, 6, true)
     private val functionEvaluatingBeforeWeeks =
-        HasHadSystemicTherapyWithAnyIntent(requestedIntent, refDate, 6, false)
-    private val functionWithoutDate = HasHadSystemicTherapyWithAnyIntent(requestedIntent, null, null, null)
+        HasHadSystemicTherapyWithAnyIntent(REQUESTED_INTENT, refDate, 6, false)
+    private val functionWithoutDate = HasHadSystemicTherapyWithAnyIntent(REQUESTED_INTENT, null, null, null)
     private val functionWithoutIntentsAndWithinWeeks = HasHadSystemicTherapyWithAnyIntent(null, refDate, 6, true)
 
     @Test
@@ -37,14 +40,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with systemic treatment with wrong intent (and correct date) only if intent is evaluated`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = wrongIntent
+                    intents = WRONG_INTENT
                 )
             )
         )
@@ -55,14 +57,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with systemic treatment with wrong intent (and correct date) only if intent is evaluated also for not within weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
-                    intents = wrongIntent
+                    intents = WRONG_INTENT
                 )
             )
         )
@@ -71,14 +72,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with non-systemic treatment with correct intent (and correct date)`() {
-        val treatment = TreatmentTestFactory.treatment("non systemic treatment", false)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(NON_SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -89,14 +89,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with non-systemic treatment with correct intent (and correct date) also for not within weeks`() {
-        val treatment = TreatmentTestFactory.treatment("non systemic treatment", false)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(NON_SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -105,14 +104,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with systemic treatment with correct intent but too long ago if evaluating within weeks and pass if evaluating before weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -122,14 +120,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should fail with systemic treatment with correct intent but too long ago according to start date if evaluating within weeks and undetermined if evaluating before weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     startYear = olderDate.year,
                     startMonth = olderDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -139,14 +136,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should pass with systemic treatment with correct intent with recent stop date if evaluating before weeks and fail if evaluating not before weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -156,14 +152,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should pass with systemic treatment with correct intent with recent start date if evaluating before weeks and undetermined if evaluating not before weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     startYear = recentDate.year,
                     startMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -173,14 +168,13 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should pass with recent systemic treatment with correct intent and within weeks unless not evaluating within weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -192,22 +186,21 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should pass with systemic treatment with correct intent and unknown or older dates if evaluating without date`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecordOldDate = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
         val patientRecordUnknownDate = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = requestedIntent
+                    setOf(SYSTEMIC_TREATMENT),
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -217,20 +210,19 @@ class HasHadSystemicTherapyWithAnyIntentTest {
 
     @Test
     fun `Should pass with one recent systemic treatment and one non-recent systemic treatment, both with correct intent`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 ),
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 )
             )
         )
@@ -239,21 +231,20 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with systemic treatment with correct intent but without (stop)date`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
+    fun `Should evaluate to undetermined with systemic treatment with correct intent but without (stop)date`() {
         val therapyWithoutDate = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = requestedIntent
+                    setOf(SYSTEMIC_TREATMENT),
+                    intents = REQUESTED_INTENT
                 )
             )
         )
         val therapyWithoutStopDate = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
-                    intents = requestedIntent,
+                    setOf(SYSTEMIC_TREATMENT),
+                    intents = REQUESTED_INTENT,
                     startYear = 2023
                 )
             )
@@ -270,7 +261,7 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with treatment with missing intent if intent is evaluated and evaluating within weeks`() {
+    fun `Should evaluate to undetermined with treatment with missing intent if intent is evaluated and evaluating within weeks`() {
         val treatmentX = TreatmentTestFactory.treatmentHistoryEntry(
             setOf(TreatmentTestFactory.treatment("TREATMENT X", true)),
             stopYear = recentDate.year,
@@ -294,32 +285,31 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with treatment with missing intent if intent is evaluated also if not evaluating within weeks`() {
+    fun `Should evaluate to undetermined with treatment with missing intent if intent is evaluated also if not evaluating within weeks`() {
         val treatmentX = TreatmentTestFactory.treatmentHistoryEntry(
-            setOf(TreatmentTestFactory.treatment("TREATMENT X", true)),
+            setOf(TreatmentTestFactory.treatment("Treatment X", true)),
             stopYear = olderDate.year,
             stopMonth = olderDate.monthValue,
             intents = null
         )
-        val treatmentY = treatmentX.copy(treatments = setOf(TreatmentTestFactory.treatment("treatment y", true)))
+        val treatmentY = treatmentX.copy(treatments = setOf(TreatmentTestFactory.treatment("Treatment y", true)))
         val patientRecord = withTreatmentHistory(listOf(treatmentX, treatmentY))
 
         EvaluationAssert.assertEvaluation(EvaluationResult.UNDETERMINED, functionEvaluatingBeforeWeeks.evaluate(patientRecord))
     }
 
     @Test
-    fun `Should be undetermined with one too-old systemic treatment with correct intent and one recent systemic treatment with unknown intent and within weeks evaluated`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
+    fun `Should evaluate to undetermined with one too-old systemic treatment with correct intent and one recent systemic treatment with unknown intent and within weeks evaluated`() {
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
                     intents = setOf(Intent.ADJUVANT)
                 ),
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
                     intents = null
@@ -330,18 +320,17 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with one too-old systemic treatment with correct intent and one recent systemic treatment with unknown intent and not within weeks evaluated`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
+    fun `Should evaluate to undetermined with one too-old systemic treatment with correct intent and one recent systemic treatment with unknown intent and not within weeks evaluated`() {
         val patientRecord = withTreatmentHistory(
             listOf(
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = recentDate.year,
                     stopMonth = recentDate.monthValue,
-                    intents = requestedIntent
+                    intents = REQUESTED_INTENT
                 ),
                 TreatmentTestFactory.treatmentHistoryEntry(
-                    setOf(treatment),
+                    setOf(SYSTEMIC_TREATMENT),
                     stopYear = olderDate.year,
                     stopMonth = olderDate.monthValue,
                     intents = null
@@ -352,21 +341,20 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with one recent systemic treatment with incorrect intent and one recent systemic treatment with unknown intent and evaluating within weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
+    fun `Should evaluate to undetermined with one recent systemic treatment with incorrect intent and one recent systemic treatment with unknown intent and evaluating within weeks`() {
         EvaluationAssert.assertEvaluation(
             EvaluationResult.UNDETERMINED,
             functionEvaluatingWithinWeeks.evaluate(
                 withTreatmentHistory(
                     listOf(
                         TreatmentTestFactory.treatmentHistoryEntry(
-                            setOf(treatment),
+                            setOf(SYSTEMIC_TREATMENT),
                             stopYear = recentDate.year,
                             stopMonth = recentDate.monthValue,
-                            intents = wrongIntent
+                            intents = WRONG_INTENT
                         ),
                         TreatmentTestFactory.treatmentHistoryEntry(
-                            setOf(treatment),
+                            setOf(SYSTEMIC_TREATMENT),
                             stopYear = recentDate.year,
                             stopMonth = recentDate.monthValue,
                             intents = null
@@ -378,21 +366,20 @@ class HasHadSystemicTherapyWithAnyIntentTest {
     }
 
     @Test
-    fun `Should be undetermined with one non-recent systemic treatment with incorrect intent and one recent systemic treatment with unknown intent and not evaluating within weeks`() {
-        val treatment = TreatmentTestFactory.treatment("systemic treatment", true)
+    fun `Should evaluate to undetermined with one non-recent systemic treatment with incorrect intent and one recent systemic treatment with unknown intent and not evaluating within weeks`() {
         EvaluationAssert.assertEvaluation(
             EvaluationResult.UNDETERMINED,
             functionEvaluatingBeforeWeeks.evaluate(
                 withTreatmentHistory(
                     listOf(
                         TreatmentTestFactory.treatmentHistoryEntry(
-                            setOf(treatment),
+                            setOf(SYSTEMIC_TREATMENT),
                             stopYear = olderDate.year,
                             stopMonth = olderDate.monthValue,
                             intents = setOf(Intent.PALLIATIVE)
                         ),
                         TreatmentTestFactory.treatmentHistoryEntry(
-                            setOf(treatment),
+                            setOf(SYSTEMIC_TREATMENT),
                             stopYear = olderDate.year,
                             stopMonth = olderDate.monthValue,
                             intents = null
