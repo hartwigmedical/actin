b7052f97993098674fe00f6b3ed160eed1e46974
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-05-27 13:27:45 +0200

    ACTIN-1982: Merge master into branch

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelCopyNumberAnnotator.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelCopyNumberAnnotator.kt
@@ -8,14 +8,9 @@ import com.hartwig.actin.datamodel.molecular.driver.ProteinEffect
 import com.hartwig.actin.datamodel.molecular.driver.CopyNumber
 import com.hartwig.actin.datamodel.molecular.driver.CopyNumberType
 import com.hartwig.actin.datamodel.molecular.driver.TranscriptCopyNumberImpact
-import com.hartwig.actin.molecular.evidence.EvidenceDatabase
 import com.hartwig.actin.molecular.util.ExtractionUtil
 import com.hartwig.actin.tools.ensemblcache.EnsemblDataCache
 import org.apache.logging.log4j.LogManager
-
-private const val MIN_COPY_NUMBER = 6
-private const val MAX_COPY_NUMBER = 6
-private const val PLOIDY = 2
 
 class PanelCopyNumberAnnotator(private val ensembleDataCache: EnsemblDataCache) {
 
@@ -41,15 +36,15 @@ class PanelCopyNumberAnnotator(private val ensembleDataCache: EnsemblDataCache) 
         val canonicalImpact = TranscriptCopyNumberImpact(
             transcriptId = canonicalTranscript,
             type = if (isCanonicalTranscript) CopyNumberType.FULL_GAIN else CopyNumberType.NONE,
-            minCopies = if (isCanonicalTranscript) MIN_COPY_NUMBER else PLOIDY,
-            maxCopies = if (isCanonicalTranscript) MAX_COPY_NUMBER else PLOIDY
+            minCopies = if (isCanonicalTranscript) sequencedAmplifiedGene.copies else null,
+            maxCopies = if (isCanonicalTranscript) sequencedAmplifiedGene.copies else null
         )
         val otherImpacts = if (isCanonicalTranscript) emptySet() else setOf(
             TranscriptCopyNumberImpact(
                 transcriptId = transcriptId,
                 type = CopyNumberType.FULL_GAIN,
-                minCopies = MIN_COPY_NUMBER,
-                maxCopies = MAX_COPY_NUMBER
+                minCopies = sequencedAmplifiedGene.copies,
+                maxCopies = sequencedAmplifiedGene.copies
             )
         )
 
@@ -77,8 +72,8 @@ class PanelCopyNumberAnnotator(private val ensembleDataCache: EnsemblDataCache) 
         val canonicalImpact = TranscriptCopyNumberImpact(
             transcriptId = canonicalTranscript,
             type = if (isCanonicalTranscript) CopyNumberType.DEL else CopyNumberType.NONE,
-            minCopies = if (isCanonicalTranscript) 0 else PLOIDY,
-            maxCopies = if (isCanonicalTranscript) 0 else PLOIDY
+            minCopies = if (isCanonicalTranscript) 0 else null,
+            maxCopies = if (isCanonicalTranscript) 0 else null
         )
         val otherImpacts = if (isCanonicalTranscript) emptySet() else setOf(
             TranscriptCopyNumberImpact(
