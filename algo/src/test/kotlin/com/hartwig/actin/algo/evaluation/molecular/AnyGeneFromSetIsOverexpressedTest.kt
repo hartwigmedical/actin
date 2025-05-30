0e06a60ecb21648b8b3b81ce9dd3a1604ce4eff4
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2025-05-30 15:24:19 +0200

    ACTIN-2020: Add warn for amplifications (#1148)
    * ACTIN-2020: Add warn for amplification
    
    * ACTIN-2020: refactor test and simplify message
    
    * ACTIN-2020: refactor test and simplify message
    
    * ACTIN-2020: Fix broken test
    
    * ACTIN-2020: PR feedback
    
    * ACTIN-2020: PR feedback

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/AnyGeneFromSetIsOverexpressedTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/molecular/AnyGeneFromSetIsOverexpressedTest.kt
@@ -1,18 +1,47 @@
 package com.hartwig.actin.algo.evaluation.molecular
 
 import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
 import com.hartwig.actin.datamodel.TestPatientFactory
 import com.hartwig.actin.datamodel.algo.EvaluationResult
+import com.hartwig.actin.datamodel.molecular.MolecularRecord
+import io.mockk.every
+import io.mockk.mockk
 import java.time.LocalDate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class AnyGeneFromSetIsOverexpressedTest {
-    val function = AnyGeneFromSetIsOverexpressed(LocalDate.of(2024, 11, 6), setOf("gene a", "gene b", "gene c"))
+    private val alwaysPassGeneAmplificationEvaluation = mockk<GeneIsAmplified> {
+        every { evaluate(any<MolecularRecord>()) } returns EvaluationFactory.pass("amplification")
+    }
+    private val alwaysWarnGeneAmplificationEvaluation = mockk<GeneIsAmplified> {
+        every { evaluate(any<MolecularRecord>()) } returns EvaluationFactory.warn("possible amplification")
+    }
+    private val alwaysFailGeneAmplificationEvaluation = mockk<GeneIsAmplified> {
+        every { evaluate(any<MolecularRecord>()) } returns EvaluationFactory.fail("no amplification")
+    }
 
     @Test
-    fun `Should evaluate to undetermined with correct message`() {
-        val evaluation = function.evaluate(TestPatientFactory.createMinimalTestWGSPatientRecord())
+    fun `Should warn when amplification`() {
+        val geneIsAmplifiedCreator: (String, LocalDate?) -> GeneIsAmplified = { gene, _ ->
+            when (gene) {
+                "gene a" -> alwaysPassGeneAmplificationEvaluation
+                "gene b" -> alwaysFailGeneAmplificationEvaluation
+                else -> alwaysWarnGeneAmplificationEvaluation
+            }
+        }
+        val evaluation =
+            createFunctionWithEvaluations(geneIsAmplifiedCreator).evaluate(TestPatientFactory.createMinimalTestWGSPatientRecord())
+        assertEvaluation(EvaluationResult.WARN, evaluation)
+        assertThat(evaluation.warnMessages).contains("gene a and gene c is amplified therefore possible overexpression in RNA")
+    }
+
+    @Test
+    fun `Should evaluate to undetermined when no amplification`() {
+        val geneIsAmplifiedCreator: (String, LocalDate?) -> GeneIsAmplified = { _, _ -> alwaysFailGeneAmplificationEvaluation }
+        val evaluation =
+            createFunctionWithEvaluations(geneIsAmplifiedCreator).evaluate(TestPatientFactory.createMinimalTestWGSPatientRecord())
         assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
         assertThat(evaluation.undeterminedMessages)
             .contains("Overexpression of gene a, gene b and gene c in RNA undetermined")
@@ -20,8 +49,18 @@ class AnyGeneFromSetIsOverexpressedTest {
 
     @Test
     fun `Should evaluate to undetermined when molecular record not available`() {
-        val evaluation = function.evaluate(TestPatientFactory.createEmptyMolecularTestPatientRecord())
-        assertThat(evaluation.result).isEqualTo(EvaluationResult.UNDETERMINED)
+        val geneIsAmplifiedCreator: (String, LocalDate?) -> GeneIsAmplified = { _, _ -> alwaysFailGeneAmplificationEvaluation }
+        val evaluation =
+            createFunctionWithEvaluations(geneIsAmplifiedCreator).evaluate(TestPatientFactory.createEmptyMolecularTestPatientRecord())
+        assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
         assertThat(evaluation.undeterminedMessages).containsExactly("No molecular data to determine overexpression of gene a, gene b and gene c in RNA")
+    }
+
+    private fun createFunctionWithEvaluations(geneIsAmplified: (String, LocalDate?) -> GeneIsAmplified): AnyGeneFromSetIsOverexpressed {
+        return AnyGeneFromSetIsOverexpressed(
+            LocalDate.of(2024, 11, 6),
+            setOf("gene a", "gene b", "gene c"),
+            geneIsAmplified
+        )
     }
 }
