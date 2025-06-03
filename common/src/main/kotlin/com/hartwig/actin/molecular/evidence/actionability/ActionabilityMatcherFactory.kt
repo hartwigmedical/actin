9e21a4fbfec970bd195f573c4546cfe97e9630b6
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2025-06-03 12:25:21 -0400

    ACTIN-1811: drop filtered criteria from trials

--- common/src/main/kotlin/com/hartwig/actin/molecular/evidence/actionability/ActionabilityMatcherFactory.kt
+++ common/src/main/kotlin/com/hartwig/actin/molecular/evidence/actionability/ActionabilityMatcherFactory.kt
@@ -5,6 +5,7 @@ import com.hartwig.serve.datamodel.ServeRecord
 import com.hartwig.serve.datamodel.efficacy.EfficacyEvidence
 import com.hartwig.serve.datamodel.molecular.MolecularCriterium
 import com.hartwig.serve.datamodel.trial.ActionableTrial
+import com.hartwig.serve.datamodel.trial.ImmutableActionableTrial
 
 object ActionabilityMatcherFactory {
 
@@ -17,13 +18,18 @@ object ActionabilityMatcherFactory {
             .filter { it.source() == ActionabilityConstants.EVIDENCE_SOURCE }
             .filter { isMolecularCriteriumApplicable(it.molecularCriterium()) }
     }
-
+    
     private fun filterTrials(trials: List<ActionableTrial>): List<ActionableTrial> {
         return trials
             .filter { it.source() == ActionabilityConstants.EXTERNAL_TRIAL_SOURCE }
-            .flatMap { it.anyMolecularCriteria().map { c -> it to c } }
-            .filter { isMolecularCriteriumApplicable(it.second) }
-            .map { it.first }
+            .mapNotNull { trial ->
+                val applicableCriteria = trial.anyMolecularCriteria().filter { isMolecularCriteriumApplicable(it) }
+                if (applicableCriteria.isNotEmpty()) {
+                    ImmutableActionableTrial.builder().from(trial).anyMolecularCriteria(applicableCriteria).build()
+                } else {
+                    null
+                }
+            }
     }
 
     private fun isMolecularCriteriumApplicable(molecularCriterium: MolecularCriterium): Boolean {
