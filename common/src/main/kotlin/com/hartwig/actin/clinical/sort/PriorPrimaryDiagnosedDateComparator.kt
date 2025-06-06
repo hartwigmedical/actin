da3f40404e6c11c6e3951d19aa8b29ad28a8bf14
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-06-06 14:32:39 +0200

    ACTIN-2058: Remove tumor (sub)location and tumor (sub)type from prior primary data model

--- common/src/main/kotlin/com/hartwig/actin/clinical/sort/PriorPrimaryDiagnosedDateComparator.kt
+++ common/src/main/kotlin/com/hartwig/actin/clinical/sort/PriorPrimaryDiagnosedDateComparator.kt
@@ -7,7 +7,7 @@ class PriorPrimaryDiagnosedDateComparator : Comparator<PriorPrimary> {
     private val nullSafeComparator = Comparator.nullsLast(Comparator.naturalOrder<Int?>())
     private val comparator = Comparator.comparing(PriorPrimary::diagnosedYear, nullSafeComparator)
         .thenComparing(PriorPrimary::diagnosedMonth, nullSafeComparator)
-        .thenComparing(PriorPrimary::tumorLocation)
+        .thenComparing(PriorPrimary::name)
 
     override fun compare(priorPrimary1: PriorPrimary, priorPrimary2: PriorPrimary): Int {
         return comparator.compare(priorPrimary1, priorPrimary2)
--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/PriorPrimary.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/PriorPrimary.kt
@@ -1,10 +1,7 @@
 package com.hartwig.actin.datamodel.clinical
 
 data class PriorPrimary(
-    val tumorLocation: String,
-    val tumorSubLocation: String,
-    val tumorType: String,
-    val tumorSubType: String,
+    val name: String,
     val doids: Set<String> = emptySet(),
     val diagnosedYear: Int? = null,
     val diagnosedMonth: Int? = null,
