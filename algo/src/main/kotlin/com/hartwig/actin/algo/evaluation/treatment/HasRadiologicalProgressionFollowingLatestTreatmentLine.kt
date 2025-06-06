710847da7c6e6dbe517b2b76624437c1e2f87db5
Beracira Chen <g.beracira@gmail.com>
2025-06-06 13:31:24 -0400

    add eval rules

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasRadiologicalProgressionFollowingLatestTreatmentLine.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasRadiologicalProgressionFollowingLatestTreatmentLine.kt
@@ -6,9 +6,34 @@ import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
 
 //TODO: Implement according to README
-class HasRadiologicalProgressionFollowingLatestTreatmentLine : EvaluationFunction {
+class HasRadiologicalProgressionFollowingLatestTreatmentLine(
+    private val mustBeRadiological: Boolean
+) :
+    EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        return EvaluationFactory.recoverableUndetermined("Radiological progression following latest treatment line undetermined")
+        val undeterminedMessage = "Radiological progression following latest treatment line undetermined"
+
+        val treatmentHistory = record.oncologicalHistory
+        val systemicTreatments =
+            treatmentHistory.filter { SystemicTreatmentAnalyser.treatmentHistoryEntryIsSystemic(it) }
+        val (systemicTreatmentsWithStartDate, systemicTreatmentsWithoutStartDate) = systemicTreatments.partition { it.startYear != null }
+        val lastTreatment = SystemicTreatmentAnalyser.lastSystemicTreatment(systemicTreatmentsWithStartDate)
+        val lastTreatmentResultedInPD =
+            lastTreatment?.let { ProgressiveDiseaseFunctions.treatmentResultedInPD(it) } == true
+
+        return when {
+            lastTreatmentResultedInPD -> {
+                EvaluationFactory.pass("Last systemic treatment resulted in radiological progression.")
+            }
+            // or stop year is provided, and PD assumed to be radiological
+            lastTreatment?.treatmentHistoryDetails?.stopYear != null && mustBeRadiological -> {
+                EvaluationFactory.pass("Last systemic treatment stopped and radiological progression is assumed.")
+            }
+
+            else -> {
+                EvaluationFactory.undetermined(undeterminedMessage);
+            }
+        }
     }
 }
