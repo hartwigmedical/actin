32a0c7f7d75e69af2007060ad3e7641bff61c034
beracira-hartwig <b.chen@hartwigmedicalfoundation.nl>
2025-06-10 00:47:22 -0400

    update rule mapper

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/TreatmentRuleMapper.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/TreatmentRuleMapper.kt
@@ -5,11 +5,13 @@ import com.hartwig.actin.algo.evaluation.RuleMapper
 import com.hartwig.actin.algo.evaluation.RuleMappingResources
 import com.hartwig.actin.algo.evaluation.tumor.HasAcquiredResistanceToAnyDrug
 import com.hartwig.actin.algo.evaluation.tumor.HasMetastaticCancer
+import com.hartwig.actin.algo.evaluation.composite.And
 import com.hartwig.actin.algo.soc.StandardOfCareEvaluatorFactory
 import com.hartwig.actin.clinical.interpretation.MedicationStatusInterpreterOnEvaluationDate.Companion.createInterpreterForWashout
 import com.hartwig.actin.datamodel.trial.EligibilityFunction
 import com.hartwig.actin.datamodel.trial.EligibilityRule
 import com.hartwig.actin.medication.MedicationCategories
+
 
 class TreatmentRuleMapper(resources: RuleMappingResources) : RuleMapper(resources) {
 
@@ -553,7 +555,12 @@ class TreatmentRuleMapper(resources: RuleMappingResources) : RuleMapper(resource
     private fun hasProgressiveDiseaseFollowingSomeSystemicTreatmentsCreator(): FunctionCreator {
         return { function: EligibilityFunction ->
             val minSystemicTreatments = functionInputResolver().createOneIntegerInput(function)
-            HasHadPDFollowingSomeSystemicTreatments(minSystemicTreatments, false)
+            And(
+                listOf(
+                    HasHadSomeSystemicTreatments(minSystemicTreatments),
+                    HasRadiologicalProgressionFollowingLatestTreatmentLine(canAssumePDIsRadiological=false)
+                )
+            )
         }
     }
 
@@ -580,7 +587,12 @@ class TreatmentRuleMapper(resources: RuleMappingResources) : RuleMapper(resource
     private fun hasRadiologicalProgressionFollowingSomeTreatmentLinesCreator(): FunctionCreator {
         return { function: EligibilityFunction ->
             val minSystemicTreatments = functionInputResolver().createOneIntegerInput(function)
-            HasHadPDFollowingSomeSystemicTreatments(minSystemicTreatments, true)
+            And(
+                listOf(
+                    HasHadSomeSystemicTreatments(minSystemicTreatments),
+                    HasRadiologicalProgressionFollowingLatestTreatmentLine(canAssumePDIsRadiological=true)
+                )
+            )
         }
     }
 
