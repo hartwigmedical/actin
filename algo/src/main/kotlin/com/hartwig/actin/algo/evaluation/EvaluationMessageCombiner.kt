749019fa5e33f91280a98dd995373977c6cac5aa
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-06-10 13:36:24 -0400

    ACTIN-2087 PR feedback

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/EvaluationMessageCombiner.kt
+++
@@ -1,9 +0,0 @@
-package com.hartwig.actin.algo.evaluation
-
-import com.hartwig.actin.datamodel.algo.Evaluation
-import com.hartwig.actin.datamodel.algo.EvaluationMessage
-
-object EvaluationMessageCombiner {
-
-  
-}
