56c4f9af3e1309b4d8c910966e2f444f91c31a99
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-06-11 12:46:46 +0200

    ACTIN-2067: Make internal

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/TumorTermConstants.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/TumorTermConstants.kt
@@ -1,6 +1,6 @@
 package com.hartwig.actin.algo.evaluation.tumor
 
-object TumorTermConstants {
+internal object TumorTermConstants {
 
     val NEUROENDOCRINE_TERMS = setOf("neuroendocrine")
     val NON_SMALL_CELL_TERMS = setOf("non-small", "non small")
