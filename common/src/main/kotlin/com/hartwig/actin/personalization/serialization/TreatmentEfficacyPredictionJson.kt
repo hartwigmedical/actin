49b2d87b32042c09c7be705a1f0a9fe80638d8f2
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-06-20 09:00:09 +0200

    ACTIN-2138: Make helper class private

--- common/src/main/kotlin/com/hartwig/actin/personalization/serialization/TreatmentEfficacyPredictionJson.kt
+++ common/src/main/kotlin/com/hartwig/actin/personalization/serialization/TreatmentEfficacyPredictionJson.kt
@@ -7,12 +7,16 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 import java.nio.file.Files
 
-data class TreatmentEfficacyPrediction(@JsonProperty("survival_probs") val survivalProbs: List<Double>)
+private data class TreatmentEfficacyPrediction(@JsonProperty("survival_probs") val survivalProbs: List<Double>)
 
 object TreatmentEfficacyPredictionJson {
+
     private val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
 
-    fun read(treatmentEfficacyPredictionJson: String): Map<String, TreatmentEfficacyPrediction> {
-        return mapper.readValue(Files.readString(File(treatmentEfficacyPredictionJson).toPath()))
+    fun read(treatmentEfficacyPredictionJson: String): Map<String, List<Double>> {
+        val results: Map<String, TreatmentEfficacyPrediction> =
+            mapper.readValue(Files.readString(File(treatmentEfficacyPredictionJson).toPath()))
+        
+        return results.mapValues { it.value.survivalProbs }
     }
 }
