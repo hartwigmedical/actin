57be1ad933dbb1dda6a637b3d5bdba4976ea92fa
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-06-20 16:42:18 +0200

    ACTIN-2138: Add logging

--- common/src/main/kotlin/com/hartwig/actin/personalization/serialization/TreatmentEfficacyPredictionJson.kt
+++ common/src/main/kotlin/com/hartwig/actin/personalization/serialization/TreatmentEfficacyPredictionJson.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
+import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.nio.file.Files
 
@@ -11,11 +12,15 @@ private data class TreatmentEfficacyPrediction(@JsonProperty("survival_probs") v
 
 object TreatmentEfficacyPredictionJson {
 
+    private val logger = LogManager.getLogger(TreatmentEfficacyPredictionJson::class.java)
     private val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
 
     fun read(treatmentEfficacyPredictionJson: String): Map<String, List<Double>> {
+        logger.info("Loading treatment efficacy predictions from $treatmentEfficacyPredictionJson")
         val results: Map<String, TreatmentEfficacyPrediction> =
             mapper.readValue(Files.readString(File(treatmentEfficacyPredictionJson).toPath()))
+
+        logger.info(" Loaded treatment efficacy predictions for ${results.keys.size} treatments")
         
         return results.mapValues { it.value.survivalProbs }
     }
