22fec1336eb49b9694fe855e83f8098fdb29a6e8
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2025-06-23 11:02:31 +0200

    ACTIN-2147: Add standard gene maps for Archer and NGS when no panel specification is found

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/PanelSpecifications.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/PanelSpecifications.kt
@@ -3,6 +3,28 @@ package com.hartwig.actin.datamodel.molecular
 import com.hartwig.actin.datamodel.clinical.SequencingTest
 import java.util.function.Predicate
 
+private val ARCHER_ALWAYS_TESTED_GENE_MAP = listOf(
+    "ALK",
+    "BRAF",
+    "EGFR",
+    "ERBB2",
+    "FGFR1",
+    "FGFR2",
+    "FGFR3",
+    "KRAS",
+    "NTRK1",
+    "NTRK2",
+    "NTRK3",
+    "RET",
+    "ROS1"
+).associateWith { listOf(MolecularTestTarget.MUTATION, MolecularTestTarget.FUSION) } + listOf(
+    "MET",
+    "NRG1"
+).associateWith { listOf(MolecularTestTarget.FUSION) }
+
+private val NGS_ALWAYS_TESTED_GENE_MAP =
+    listOf("BRAF", "EGFR", "MET", "ERBB2", "KRAS").associateWith { listOf(MolecularTestTarget.MUTATION) }
+
 data class PanelGeneSpecification(val geneName: String, val targets: List<MolecularTestTarget>)
 
 data class PanelSpecification(private val geneTargetMap: Map<String, List<MolecularTestTarget>>) {
@@ -10,8 +32,16 @@ data class PanelSpecification(private val geneTargetMap: Map<String, List<Molecu
         geneTargetMap[gene]?.let { molecularTestTargets.test(it) } ?: false
 }
 
-fun derivedGeneTargetMap(testResults: SequencingTest) =
-    testResults.variants.associate { it.gene to listOf(MolecularTestTarget.MUTATION) } +
+fun derivedGeneTargetMap(testResults: SequencingTest): Map<String, List<MolecularTestTarget>> {
+    val standardGeneMap = when {
+        testResults.test.lowercase().contains("archer") -> ARCHER_ALWAYS_TESTED_GENE_MAP
+        setOf("ngs", "panel", "next-generation sequencing", "next generation sequencing").any {
+            testResults.test.lowercase().contains(it)
+        } -> NGS_ALWAYS_TESTED_GENE_MAP
+        else -> emptyMap()
+    }
+
+    return standardGeneMap + testResults.variants.associate { it.gene to listOf(MolecularTestTarget.MUTATION) } +
             testResults.fusions.flatMap { listOfNotNull(it.geneUp, it.geneDown) }.associateWith { listOf(MolecularTestTarget.FUSION) } +
             testResults.amplifications.map { it.gene to listOf(MolecularTestTarget.MUTATION, MolecularTestTarget.AMPLIFICATION) } +
             testResults.deletions.map { it.gene to listOf(MolecularTestTarget.MUTATION, MolecularTestTarget.DELETION) } +
@@ -22,6 +52,7 @@ fun derivedGeneTargetMap(testResults: SequencingTest) =
                 )
             } +
             testResults.negativeResults.map { it.gene to listOf(MolecularTestTarget.MUTATION) }
+}
 
 class PanelSpecifications(panelGeneSpecifications: Map<String, List<PanelGeneSpecification>>) {
 
