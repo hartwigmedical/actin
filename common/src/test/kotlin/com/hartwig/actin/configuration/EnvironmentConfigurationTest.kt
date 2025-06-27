ff87a29ea315e7dc2c66120ac3d3a00c835f9d49
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-06-27 14:36:23 +0200

    ACTIN-1494: Remove "profile" from environment config

--- common/src/test/kotlin/com/hartwig/actin/configuration/EnvironmentConfigurationTest.kt
+++ common/src/test/kotlin/com/hartwig/actin/configuration/EnvironmentConfigurationTest.kt
@@ -10,16 +10,11 @@ class EnvironmentConfigurationTest {
     private val minimalConfigFile = resourceOnClasspath("environment/minimal_config.yaml")
     private val basicConfigFile = resourceOnClasspath("environment/basic_config.yaml")
     private val properConfigFile = resourceOnClasspath("environment/proper_config.yaml")
-
-    @Test
-    fun `Should create with default values`() {
-        assertThat(defaultConfig.algo.warnIfToxicitiesNotFromQuestionnaire).isTrue
-    }
-
+    
     @Test
     fun `Should load proper config from file`() {
         val config = EnvironmentConfiguration.create(properConfigFile)
-        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isFalse
+        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isFalse()
         assertThat(config.requestingHospital).isEqualTo("Erasmus MC")
     }
 
@@ -27,44 +22,17 @@ class EnvironmentConfigurationTest {
     fun `Should load config from file with requesting hospital only`() {
         val config = EnvironmentConfiguration.create(basicConfigFile)
         assertThat(config.requestingHospital).isEqualTo("Erasmus MC")
-        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isTrue
+        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isTrue()
     }
 
     @Test
     fun `Should use defaults for fields not provided in file`() {
         val config = EnvironmentConfiguration.create(minimalConfigFile)
-        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isTrue
+        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isTrue()
     }
 
     @Test
     fun `Should create default configuration when provided file is null`() {
         assertThat(EnvironmentConfiguration.create(null)).isEqualTo(defaultConfig)
-    }
-
-    @Test
-    fun `Should override configuration from file with profile`() {
-        val config = EnvironmentConfiguration.create(properConfigFile, "CRC")
-        assertThat(config.algo.warnIfToxicitiesNotFromQuestionnaire).isFalse
-        assertThat(config.report.includeOverviewWithClinicalHistorySummary).isTrue
-        assertThat(config.report.includeMolecularDetailsChapter).isFalse
-    }
-
-    @Test
-    fun `Should override default configuration with profile when file is null`() {
-        assertThat(EnvironmentConfiguration.create(null, "CRC")).isEqualTo(
-            defaultConfig.copy(
-                report = defaultConfig.report.copy(
-                    includeOverviewWithClinicalHistorySummary = true,
-                    includeMolecularDetailsChapter = false,
-                    includeIneligibleTrialsInSummary = true,
-                    includeApprovedTreatmentsInSummary = false,
-                    includeSOCLiteratureEfficacyEvidence = true,
-                    includeEligibleSOCTreatmentSummary = true,
-                    molecularSummaryType = MolecularSummaryType.NONE,
-                    includePatientHeader = false,
-                    filterOnSOCExhaustionAndTumorType = true
-                )
-            )
-        )
     }
 }
