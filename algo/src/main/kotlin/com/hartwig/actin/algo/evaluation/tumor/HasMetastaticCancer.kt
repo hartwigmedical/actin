a6d123deff8b3c67f5f537a2775dfd4548b2a795
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2025-07-04 13:24:00 +0200

    ACTIN-2201: Add HAS_OLIGOMETASTATIC_CANCER plus some refactoring

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMetastaticCancer.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMetastaticCancer.kt
@@ -1,38 +1,23 @@
 package com.hartwig.actin.algo.evaluation.tumor
 
-import com.hartwig.actin.algo.doid.DoidConstants
 import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFactory.undetermined
 import com.hartwig.actin.algo.evaluation.EvaluationFunction
-import com.hartwig.actin.algo.evaluation.tumor.TumorEvaluationFunctions.isStageMatch
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
-import com.hartwig.actin.datamodel.clinical.TumorStage
 import com.hartwig.actin.doid.DoidModel
 
 class HasMetastaticCancer(private val doidModel: DoidModel) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
-        val stage = record.tumor.stage ?: return EvaluationFactory.undetermined("Undetermined if metastatic cancer (tumor stage missing)")
-        val stageMessage = stage.display()
-        val hasPotentiallyMetastaticStageIICancerType =
-            DoidEvaluationFunctions.isOfAtLeastOneDoidType(doidModel, record.tumor.doids, STAGE_II_POTENTIALLY_METASTATIC_CANCER_DOIDS)
+        val metastaticCancerEvaluation = MetastaticCancerEvaluator.isMetastatic(record, doidModel)
+        val stageDisplay = record.tumor.stage?.display()
 
-        return when {
-            isStageMatch(stage, setOf(TumorStage.III, TumorStage.IV)) -> {
-                EvaluationFactory.pass("Stage $stageMessage is considered metastatic")
-            }
-
-            isStageMatch(stage, setOf(TumorStage.II)) && hasPotentiallyMetastaticStageIICancerType -> {
-                EvaluationFactory.undetermined("Undetermined if stage $stageMessage is considered metastatic")
-            }
-
-            else -> {
-                EvaluationFactory.fail("Stage $stageMessage is not considered metastatic")
-            }
+        return when (metastaticCancerEvaluation) {
+            MetastaticCancerEvaluation.DATA_MISSING -> undetermined("Undetermined if metastatic cancer (tumor stage missing)")
+            MetastaticCancerEvaluation.METASTATIC -> EvaluationFactory.pass("Stage $stageDisplay is considered metastatic")
+            MetastaticCancerEvaluation.UNDETERMINED -> undetermined("Undetermined if stage $stageDisplay is considered metastatic")
+            MetastaticCancerEvaluation.NON_METASTATIC -> EvaluationFactory.fail("Stage $stageDisplay is not considered metastatic")
         }
-    }
-
-    companion object {
-        val STAGE_II_POTENTIALLY_METASTATIC_CANCER_DOIDS = setOf(DoidConstants.BRAIN_CANCER_DOID, DoidConstants.HEAD_AND_NECK_CANCER_DOID)
     }
 }
---
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasOligometastaticCancer.kt
@@ -0,0 +1,27 @@
+package com.hartwig.actin.algo.evaluation.tumor
+
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFactory.undetermined
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.algo.Evaluation
+import com.hartwig.actin.doid.DoidModel
+
+class HasOligometastaticCancer(private val doidModel: DoidModel) : EvaluationFunction {
+
+    override fun evaluate(record: PatientRecord): Evaluation {
+        val metastaticCancerEvaluation = MetastaticCancerEvaluator.isMetastatic(record, doidModel)
+
+        return when (metastaticCancerEvaluation) {
+            MetastaticCancerEvaluation.DATA_MISSING -> undetermined("Undetermined if oligometastatic cancer (tumor stage missing)")
+
+            MetastaticCancerEvaluation.METASTATIC, MetastaticCancerEvaluation.UNDETERMINED -> {
+                undetermined("Undetermined if oligometastatic cancer")
+            }
+
+            MetastaticCancerEvaluation.NON_METASTATIC -> {
+                EvaluationFactory.fail("No oligometastatic cancer (stage ${record.tumor.stage?.display()})")
+            }
+        }
+    }
+}
