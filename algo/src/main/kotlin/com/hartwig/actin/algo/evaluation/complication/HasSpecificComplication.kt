55cac202ef072e3789181bd650f354d44eb7029f
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-07-10 11:11:43 +0200

    ACTIN-2246: Fix bug in ComplicationRuleMapper

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/HasSpecificComplication.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/HasSpecificComplication.kt
@@ -5,17 +5,18 @@ import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.algo.evaluation.util.Format
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
+import com.hartwig.actin.datamodel.clinical.IcdCode
 import com.hartwig.actin.icd.IcdModel
 
-class HasSpecificComplication(private val icdModel: IcdModel, private val targetIcdTitles: List<String>) : EvaluationFunction {
+class HasSpecificComplication(private val icdModel: IcdModel, private val targetIcdCodes: Set<IcdCode>) : EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
+        val targetIcdTitles = targetIcdCodes.map { icdModel.resolveTitleForCode(it) }.toSet()
         val icdTitleText = if (targetIcdTitles.size > 1) {
             "belonging to type ${Format.concatLowercaseWithCommaAndOr(targetIcdTitles)}"
         } else targetIcdTitles.takeIf { it.isNotEmpty() }?.first()
 
-        val targetCodes = targetIcdTitles.map { icdModel.resolveCodeForTitle(it)!! }.toSet()
-        val icdMatches = icdModel.findInstancesMatchingAnyIcdCode(record.complications, targetCodes)
+        val icdMatches = icdModel.findInstancesMatchingAnyIcdCode(record.complications, targetIcdCodes)
 
         return when {
             icdMatches.fullMatches.isNotEmpty() -> EvaluationFactory.pass(
