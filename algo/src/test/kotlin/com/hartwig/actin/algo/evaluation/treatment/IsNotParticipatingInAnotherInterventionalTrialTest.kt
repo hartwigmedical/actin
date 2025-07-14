3bba0ad5da1589e760a358e4fa43dbc51372edda
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-07-14 09:21:16 +0200

    ACTIN-1872: Revise IS_NOT_PARTICIPATING_IN_ANOTHER_TRIAL

---
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/IsNotParticipatingInAnotherInterventionalTrialTest.kt
@@ -0,0 +1,31 @@
+package com.hartwig.actin.algo.evaluation.treatment
+
+import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.algo.evaluation.medication.MedicationTestFactory
+import com.hartwig.actin.algo.evaluation.washout.WashoutTestFactory.medication
+import com.hartwig.actin.datamodel.TestPatientFactory
+import com.hartwig.actin.datamodel.algo.EvaluationResult
+import org.junit.Test
+import java.time.LocalDate
+
+class IsNotParticipatingInAnotherInterventionalTrialTest {
+
+    private val function = IsNotParticipatingInAnotherInterventionalTrial(MedicationTestFactory.alwaysActive(), LocalDate.of(2025, 2, 2))
+
+    @Test
+    fun `Should warn when patient recently received trial medication`() {
+        val medications = listOf(medication(isTrialMedication = true))
+        assertEvaluation(
+            EvaluationResult.WARN,
+            function.evaluate(MedicationTestFactory.withMedications(medications))
+        )
+    }
+
+    @Test
+    fun `Should return not evaluated when patient had no trial medication`() {
+        assertEvaluation(
+            EvaluationResult.NOT_EVALUATED,
+            function.evaluate(TestPatientFactory.createMinimalTestWGSPatientRecord())
+        )
+    }
+}
