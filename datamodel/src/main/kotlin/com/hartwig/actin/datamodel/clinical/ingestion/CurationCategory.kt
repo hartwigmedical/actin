e24c7024e7d1b65241c3196de4b7ceaa7bc3039f
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-07-14 15:56:50 -0400

    ACTIN-2286: Remove unrelated change

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/ingestion/CurationCategory.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/ingestion/CurationCategory.kt
@@ -5,7 +5,11 @@ enum class CurationCategory(val categoryName: String) {
     ADMINISTRATION_ROUTE_TRANSLATION("Administration Route Translation"),
     BLOOD_TRANSFUSION_TRANSLATION("Blood Transfusion Translation"),
     COMORBIDITY("Comorbidity"),
+    COMPLICATION("Complication"),
     DOSAGE_UNIT_TRANSLATION("Dosage Unit Translation"),
+    ECG("ECG"),
+    INFECTION("Infection"),
+    INTOLERANCE("Intolerance"),
     LAB_MEASUREMENT("Lab Measurement"),
     LESION_LOCATION("Lesion Location"),
     MEDICATION_DOSAGE("Medication Dosage"),
@@ -18,6 +22,7 @@ enum class CurationCategory(val categoryName: String) {
     PERIOD_BETWEEN_UNIT_INTERPRETATION("Period Between Unit Interpretation"),
     QUESTIONNAIRE_MAPPING("Questionnaire mapping"),
     PRIOR_PRIMARY("Prior Primary"),
+    TOXICITY("Toxicity"),
     TOXICITY_TRANSLATION("Toxicity Translation"),
     SURGERY("Surgery")
 }
