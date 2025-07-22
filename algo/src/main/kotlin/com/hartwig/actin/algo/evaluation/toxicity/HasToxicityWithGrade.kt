f26f52f4187ee71c00f5f0d6908837d3999f248a
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-07-22 14:36:19 +0200

    ACTIN-2322: Change "" to null

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasToxicityWithGrade.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasToxicityWithGrade.kt
@@ -64,5 +64,5 @@ class HasToxicityWithGrade(
     }
 
     private fun formatToxicities(toxicities: List<Toxicity>) =
-        if (toxicities.any { it.name != "" }) " (${toxicities.joinToString(", ") { it.display() }})" else ""
+        if (toxicities.any { !it.name.isNullOrEmpty() }) " (${toxicities.joinToString(", ") { it.display() }})" else ""
 }
