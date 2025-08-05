d345a01c40773c9cb053c415f2c3ae9d441fb5e3
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-08-05 13:33:38 -0400

    ACTIN-2351: PR feedback

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/CohortMetadata.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/CohortMetadata.kt
@@ -1,14 +1,16 @@
 package com.hartwig.actin.datamodel.trial
 
+typealias Location = String
+
 data class CohortAvailability(val open: Boolean, val slotsAvailable: Boolean)
 
 data class CohortMetadata(
     val cohortId: String,
-    val evaluable: Boolean,
+    val description: String,
     val cohortAvailability: CohortAvailability,
-    val availabilityByLocation: Map<String, CohortAvailability>? = null,
+    val availabilityByLocation: Map<Location, CohortAvailability>? = null,
+    val evaluable: Boolean,
     val ignore: Boolean,
-    val description: String
 ) {
     val open = cohortAvailability.open
     val slotsAvailable = cohortAvailability.slotsAvailable
