35ba03286704ad3141b0b6eed7c65fdf429f3a76
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2025-08-05 14:46:09 +0200

    ACTIN-2357: Add new rule HAS_OLIGOPROGRESSIVE_DISEASE

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasOligoprogressiveDisease.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasOligoprogressiveDisease.kt
@@ -5,8 +5,8 @@ import com.hartwig.actin.algo.evaluation.EvaluationFunction
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.algo.Evaluation
 
-class HasSynchronousMetastaticDisease: EvaluationFunction {
+class HasOligoprogressiveDisease : EvaluationFunction {
     override fun evaluate(record: PatientRecord): Evaluation {
-        return EvaluationFactory.undetermined("Undetermined if patient has oligoprogressive disease")
+        return EvaluationFactory.undetermined("Undetermined if patient has synchronous metastatic disease")
     }
 }
