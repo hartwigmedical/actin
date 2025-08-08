c1b041e85acc4a6c514f7118d26348918db60244
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2025-08-08 08:44:04 +0200

    ACTIN-2356: Add new rule HAS_HAD_RESPONSE_X_FOLLOWING_CATEGORY_Y_TREATMENT_OF_TYPES_Z (#1281)
    * ACTIN-2356: Add new rule HAS_HAD_BEST_RESPONSE_X_FOLLOWING_CATEGORY_Y_TREATMENT_OF_TYPES_Z
    
    * ACTIN-2356: Improve testing
    
    * ACTIN-2356: PR feedback
    
    * ACTIN-2356: Final PR feedback

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/treatment/history/TreatmentResponse.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/treatment/history/TreatmentResponse.kt
@@ -1,6 +1,8 @@
 package com.hartwig.actin.datamodel.clinical.treatment.history
 
-enum class TreatmentResponse {
+import com.hartwig.actin.datamodel.Displayable
+
+enum class TreatmentResponse : Displayable {
     PROGRESSIVE_DISEASE,
     STABLE_DISEASE,
     MIXED,
@@ -9,18 +11,17 @@ enum class TreatmentResponse {
     COMPLETE_RESPONSE,
     REMISSION;
 
+    override fun display(): String {
+        return this.toString().replace("_", " ").lowercase()
+    }
+
     companion object {
-        fun createFromString(input: String): TreatmentResponse? {
-            return when (input.uppercase()) {
-                "PD" -> PROGRESSIVE_DISEASE
-                "SD" -> STABLE_DISEASE
-                "MIXED" -> MIXED
-                "PR" -> PARTIAL_RESPONSE
-                "NEAR CR" -> NEAR_COMPLETE_RESPONSE
-                "CR" -> COMPLETE_RESPONSE
-                "REMISSION" -> REMISSION
-                else -> null
-            }
+        val BENEFIT_RESPONSES = setOf(PARTIAL_RESPONSE, NEAR_COMPLETE_RESPONSE, COMPLETE_RESPONSE, REMISSION)
+
+        fun fromString(string: String): TreatmentResponse {
+            return TreatmentResponse.valueOf(
+                string.trim { it <= ' ' }.replace(" ".toRegex(), "_").uppercase()
+            )
         }
     }
 }
