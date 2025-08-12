9524a7875bcd03476ff2ad461219aa78c3553d52
Matthew Froggatt <m.froggatt@hartwigmedicalfoundation.nl>
2025-08-12 16:36:52 -0400

    Remove HAS_ANY_COMPLICATION rule

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/ComplicationRuleMapper.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/complication/ComplicationRuleMapper.kt
@@ -15,7 +15,6 @@ class ComplicationRuleMapper(resources: RuleMappingResources) : RuleMapper(resou
 
     override fun createMappings(): Map<EligibilityRule, FunctionCreator> {
         return mapOf(
-            EligibilityRule.HAS_ANY_COMPLICATION to hasAnyComplicationCreator(),
             EligibilityRule.HAS_COMPLICATION_WITH_ANY_ICD_TITLE_X to hasSpecificComplicationCreator(),
             EligibilityRule.HAS_POTENTIAL_UNCONTROLLED_TUMOR_RELATED_PAIN to hasPotentialUncontrolledTumorRelatedPainCreator(),
             EligibilityRule.HAS_LEPTOMENINGEAL_DISEASE to hasLeptomeningealDiseaseCreator(),
@@ -38,10 +37,6 @@ class ComplicationRuleMapper(resources: RuleMappingResources) : RuleMapper(resou
                 )
             }
         )
-    }
-
-    private fun hasAnyComplicationCreator(): FunctionCreator {
-        return { HasAnyComplication() }
     }
 
     private fun hasSpecificComplicationCreator(): FunctionCreator {
--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/EligibilityRule.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/EligibilityRule.kt
@@ -498,7 +498,6 @@ enum class EligibilityRule(val input: FunctionInput?, val ihcProtein: String? = 
     ADHERES_TO_SPERM_OR_EGG_DONATION_PRESCRIPTIONS(FunctionInput.NONE),
 
     // Rules related to complications
-    HAS_ANY_COMPLICATION(FunctionInput.NONE),
     HAS_COMPLICATION_WITH_ANY_ICD_TITLE_X(FunctionInput.MANY_ICD_TITLES),
     HAS_POTENTIAL_UNCONTROLLED_TUMOR_RELATED_PAIN(FunctionInput.NONE),
     HAS_LEPTOMENINGEAL_DISEASE(FunctionInput.NONE),
