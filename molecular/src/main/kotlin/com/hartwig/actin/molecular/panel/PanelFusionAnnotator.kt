4662547d8d21860ffff24fe3cf219f2ba3046040
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2025-08-18 10:54:33 +0200

    ACTIN-2407: Evaluate to LOW DL and provide logging instead of exception when exon not on canonical transcript (#1289)
    * ACTIN-2407: Evaluate to LOW DL and provide logging instead of exception when exon not on canonical transcript
    
    * ACTIN-2407: PR feedback

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelFusionAnnotator.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelFusionAnnotator.kt
@@ -98,38 +98,35 @@ class PanelFusionAnnotator(
 
     fun isPromiscuousWithMatchingExons(driverType: FusionDriverType, sequencedFusion: SequencedFusion): Boolean? {
         return when (driverType) {
-            FusionDriverType.PROMISCUOUS_3 -> {
-                sequencedFusion.exonDown?.let {
-                    knownFusionCache.withinPromiscuousExonRange(
-                        KnownFusionType.PROMISCUOUS_3,
-                        sequencedFusion.geneDown?.let { gene ->
-                            val canonicalTranscript = canonicalTranscriptForGene(gene)
-                            checkCanonicalTranscript(canonicalTranscript, gene, it)
-                            canonicalTranscript.transcriptName()
-                        } ?: "",
-                        it,
-                        it
-                    )
-                }
-            }
+            FusionDriverType.PROMISCUOUS_3 -> checkPromiscuousExonRange(
+                sequencedFusion.geneDown,
+                sequencedFusion.exonDown,
+                KnownFusionType.PROMISCUOUS_3
+            )
 
-            FusionDriverType.PROMISCUOUS_5 -> {
-                sequencedFusion.exonUp?.let {
-                    knownFusionCache.withinPromiscuousExonRange(
-                        KnownFusionType.PROMISCUOUS_5,
-                        sequencedFusion.geneUp?.let { gene ->
-                            val canonicalTranscript = canonicalTranscriptForGene(gene)
-                            checkCanonicalTranscript(canonicalTranscript, gene, it)
-                            canonicalTranscript.transcriptName()
-                        } ?: "",
-                        it,
-                        it
-                    )
-                }
-            }
-
+            FusionDriverType.PROMISCUOUS_5 -> checkPromiscuousExonRange(
+                sequencedFusion.geneUp,
+                sequencedFusion.exonUp,
+                KnownFusionType.PROMISCUOUS_5
+            )
             else -> false
         }
+    }
+
+    private fun checkPromiscuousExonRange(
+        gene: String?,
+        exon: Int?,
+        fusionType: KnownFusionType
+    ): Boolean? {
+        if (gene == null || exon == null) return null
+        val canonicalTranscript = canonicalTranscriptForGene(gene)
+        if (!exonOnCanonicalTranscript(canonicalTranscript, gene, exon)) return false
+        return knownFusionCache.withinPromiscuousExonRange(
+            fusionType,
+            canonicalTranscript.transcriptName(),
+            exon,
+            exon
+        )
     }
 
     private fun createFusionFromExonSkip(sequencedSkippedExons: SequencedSkippedExons): Fusion {
@@ -164,10 +161,12 @@ class PanelFusionAnnotator(
             ?: throw IllegalStateException("No canonical transcript found for gene $gene")
     }
 
-    private fun checkCanonicalTranscript(transcript: TranscriptData, gene: String, exonEnd: Int) {
+    private fun exonOnCanonicalTranscript(transcript: TranscriptData, gene: String, exonEnd: Int?): Boolean {
         val numberOfExons = transcript.exons().size
         if (exonEnd !in 1..numberOfExons) {
-            throw IllegalStateException("Exon $exonEnd is out of canonical transcript range 1-$numberOfExons for gene $gene")
+            logger.warn("Exon $exonEnd is out of canonical transcript range 1-$numberOfExons for gene $gene")
+            return false
         }
+        return true
     }
 }
