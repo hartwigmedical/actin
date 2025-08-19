52562f5dc0c8ce7aaf0b397d8c31dd6d4bf21708
Andreia Esteves <a.esteves@hartwigmedicalfoundation.nl>
2025-08-19 14:30:01 +0200

    ACTIN-2427 Derive latestWho and latestAsa from whoStatuses and asaScores

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/othercondition/ComorbidityTestFactory.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/othercondition/ComorbidityTestFactory.kt
@@ -11,6 +11,7 @@ import com.hartwig.actin.datamodel.clinical.OtherCondition
 import com.hartwig.actin.datamodel.clinical.PerformanceStatus
 import com.hartwig.actin.datamodel.clinical.Toxicity
 import com.hartwig.actin.datamodel.clinical.ToxicitySource
+import com.hartwig.actin.datamodel.clinical.WhoStatus
 import java.time.LocalDate
 
 internal object ComorbidityTestFactory {
@@ -91,6 +92,10 @@ internal object ComorbidityTestFactory {
     }
 
     fun withWHO(who: Int?): PatientRecord {
-        return base.copy(performanceStatus = PerformanceStatus(latestWho = who))
+        return base.copy(
+            performanceStatus = PerformanceStatus(
+                whoStatuses = who?.let { listOf(WhoStatus(LocalDate.now(), it)) } ?: emptyList()
+            )
+        )
     }
 }
--- datamodel/src/test/kotlin/com/hartwig/actin/datamodel/clinical/TestClinicalFactory.kt
+++ datamodel/src/test/kotlin/com/hartwig/actin/datamodel/clinical/TestClinicalFactory.kt
@@ -133,7 +133,7 @@ object TestClinicalFactory {
         )
     }
 
-    private fun createTestPerformanceStatus() = PerformanceStatus(latestWho = 1)
+    private fun createTestPerformanceStatus() = PerformanceStatus(whoStatuses = listOf(WhoStatus(FIXED_DATE, 1)))
 
     private fun drug(name: String, drugType: DrugType, category: TreatmentCategory): Drug {
         return Drug(name = name, drugTypes = setOf(drugType), category = category)
