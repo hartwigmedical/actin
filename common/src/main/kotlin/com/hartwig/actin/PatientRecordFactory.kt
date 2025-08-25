e7103c60cfbefe08d6f1e1c99e459b18c8103e3c
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2025-08-25 12:48:23 +0200

    ACTIN-2436: Retain warning for missing molecular data upon molecular interpretation

--- common/src/main/kotlin/com/hartwig/actin/PatientRecordFactory.kt
+++ common/src/main/kotlin/com/hartwig/actin/PatientRecordFactory.kt
@@ -3,10 +3,18 @@ package com.hartwig.actin
 import com.hartwig.actin.datamodel.PatientRecord
 import com.hartwig.actin.datamodel.clinical.ClinicalRecord
 import com.hartwig.actin.datamodel.molecular.MolecularTest
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 object PatientRecordFactory {
 
+    private val LOGGER: Logger = LogManager.getLogger(PatientRecordFactory::class.java)
+
     fun fromInputs(clinical: ClinicalRecord, molecularTests: List<MolecularTest>): PatientRecord {
+        if (molecularTests.isEmpty()) {
+            LOGGER.warn("No molecular data for patient '{}'", clinical.patientId)
+        }
+    
         return PatientRecord(
             patientId = clinical.patientId,
             patient = clinical.patient,
