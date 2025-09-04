34c84010435e517bd8b4fade008ebde885ced9dc
Matthew Froggatt <m.froggatt@hartwigmedicalfoundation.nl>
2025-09-04 15:32:23 -0400

    Reorder trial phases, make comparable for sorting

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/TrialPhase.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/trial/TrialPhase.kt
@@ -2,14 +2,14 @@ package com.hartwig.actin.datamodel.trial
 
 import com.hartwig.actin.datamodel.Displayable
 
-enum class TrialPhase(private val text: String, private val includePrefix: Boolean = true) : Displayable {
-    PHASE_4("4"),
-    PHASE_3("3"),
-    PHASE_2_3("2/3"),
-    PHASE_2("2"),
-    PHASE_1_2("1/2"),
+enum class TrialPhase(private val text: String, private val includePrefix: Boolean = true) : Displayable, Comparable<TrialPhase> {
     PHASE_1("1"),
+    PHASE_2("2"),
+    PHASE_3("3"),
+    PHASE_4("4"),
+    PHASE_1_2("1/2"),
     PHASE_1_OR_MORE("1, ≥2"),
+    PHASE_2_3("2/3"),
     PHASE_2_OR_MORE("≥2"),
     COMPASSIONATE_USE("Compassionate Use", includePrefix = false);
 
