6dd624a42e859c6ed9fc9e06bb7c485858464d03
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2025-09-26 13:12:37 -0400

    ACTIN-2392: Update toxicity evaluated date; seal Comorbidity interface

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/ComorbidityClass.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/clinical/ComorbidityClass.kt
@@ -2,9 +2,9 @@ package com.hartwig.actin.datamodel.clinical
 
 import java.lang.reflect.Type
 
-enum class ComorbidityClass(val treatmentClass: Type) {
+enum class ComorbidityClass(val comorbidityClass: Type) {
     OTHER_CONDITION(OtherCondition::class.java),
-    COMPLICATION(Complication::class.java),
+    COMPLICATION(OtherCondition::class.java),
     TOXICITY(Toxicity::class.java),
     INTOLERANCE(Intolerance::class.java),
     ECG(Ecg::class.java),
