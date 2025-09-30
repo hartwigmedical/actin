293c32e1308010bd53c5852c749532bb2a21f8d7
vr-ramnarine <r.ramnarine@hartwigmedicalfoundation.nl>
2025-09-30 11:50:59 -0400

    - Relocated constants from `TestPanel' - Removed unused function from PanelTestConstants.kt

--- molecular/src/test/kotlin/com/hartwig/actin/molecular/panel/PanelTestConstants.kt
+++ molecular/src/test/kotlin/com/hartwig/actin/molecular/panel/PanelTestConstants.kt
@@ -2,25 +2,12 @@ package com.hartwig.actin.molecular.panel
 
 import com.hartwig.actin.datamodel.molecular.TestMolecularFactory
 import com.hartwig.actin.datamodel.molecular.driver.VariantType
-import com.hartwig.actin.datamodel.molecular.evidence.CancerTypeMatchApplicability
-import com.hartwig.actin.datamodel.molecular.evidence.EvidenceLevel
-import com.hartwig.actin.datamodel.molecular.evidence.EvidenceLevelDetails
 import com.hartwig.actin.datamodel.molecular.evidence.TestClinicalEvidenceFactory
-import com.hartwig.actin.datamodel.molecular.evidence.TestEvidenceDirectionFactory
-import com.hartwig.actin.datamodel.molecular.evidence.TestTreatmentEvidenceFactory
-
-val ON_LABEL_MATCH = TestClinicalEvidenceFactory.withEvidence(
-    TestTreatmentEvidenceFactory.create(
-        treatment = "treatment",
-        evidenceLevel = EvidenceLevel.A,
-        evidenceLevelDetails = EvidenceLevelDetails.GUIDELINE,
-        evidenceDirection = TestEvidenceDirectionFactory.certainPositiveResponse(),
-        cancerTypeMatchApplicability = CancerTypeMatchApplicability.SPECIFIC_TYPE
-    )
-)
 
 val EMPTY_MATCH = TestClinicalEvidenceFactory.createEmpty()
 
+const val GENE = "gene"
+const val HGVS_CODING = "c.1A>T"
 const val ALT = "T"
 const val REF = "G"
 const val CHROMOSOME = "1"
--- molecular/src/test/kotlin/com/hartwig/actin/molecular/panel/TestPanelConstants.kt
+++
@@ -1,4 +0,0 @@
-package com.hartwig.actin.molecular.panel
-
-const val GENE = "gene"
-const val HGVS_CODING = "c.1A>T"
