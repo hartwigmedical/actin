161bf7fe1dc883819cf6adb4c75052c5e36952a8
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-10-22 15:13:58 +0200

    ACTIN-2678: Delete now unused file

--- common/src/main/kotlin/com/hartwig/actin/trial/input/single/OneTreatmentCategoryOneInteger.kt
+++
@@ -1,8 +0,0 @@
-package com.hartwig.actin.trial.input.single
-
-import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
-
-data class OneTreatmentCategoryOneInteger(
-    val category: TreatmentCategory,
-    val integer: Int
-)
