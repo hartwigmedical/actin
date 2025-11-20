54f3468bae56c75907debb1b968c4b4dc09892dd
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2025-11-20 15:27:26 +0100

    ACTIN-2786 make classes used by OncoACTIN nio compatible (#1380)
    * ACTIN-2786 make classes used by OncoACTIN nio compatible
    
    * ACTIN-2786 use Path instead of string for directory
    
    * ACTIN-2786 Removed duplication
    
    * ACTIN-2786 Refactored to nio

--- common/src/main/kotlin/com/hartwig/actin/clinical/serialization/ClinicalRecordJson.kt
+++ common/src/main/kotlin/com/hartwig/actin/clinical/serialization/ClinicalRecordJson.kt
@@ -2,37 +2,40 @@ package com.hartwig.actin.clinical.serialization
 
 import com.hartwig.actin.clinical.sort.ClinicalRecordComparator
 import com.hartwig.actin.datamodel.clinical.ClinicalRecord
-import com.hartwig.actin.util.Paths.forceTrailingFileSeparator
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
 import java.nio.file.Files
+import java.nio.file.Path
 
 object ClinicalRecordJson {
 
     private const val CLINICAL_JSON_EXTENSION = ".clinical.json"
 
-    fun write(records: List<ClinicalRecord>, directory: String) {
-        val path = forceTrailingFileSeparator(directory)
+    fun write(records: List<ClinicalRecord>, directory: Path) {
         for (record in records) {
-            val jsonFile = path + record.patientId + CLINICAL_JSON_EXTENSION
-            val writer = BufferedWriter(FileWriter(jsonFile))
+            val jsonFile = directory.resolve(record.patientId + CLINICAL_JSON_EXTENSION)
+            val writer = Files.newBufferedWriter(jsonFile)
             writer.write(toJson(record))
             writer.close()
         }
     }
 
     fun readFromDir(directory: String): List<ClinicalRecord> {
-        val files = File(directory).listFiles()
-            ?: throw IllegalArgumentException("Could not retrieve clinical json files from $directory")
+        return readFromDir(Path.of(directory))
+    }
 
-        return files.filter { it.getName().endsWith(CLINICAL_JSON_EXTENSION) }
-            .map { fromJson(Files.readString(it.toPath())) }
+    fun readFromDir(directory: Path): List<ClinicalRecord> {
+        Files.isDirectory(directory) || throw IllegalArgumentException("Not a directory: $directory")
+        return Files.list(directory).filter { it.fileName.toString().endsWith(CLINICAL_JSON_EXTENSION) }
+            .map { read(it) }
+            .toList()
             .sortedWith(ClinicalRecordComparator())
     }
 
     fun read(clinicalJson: String): ClinicalRecord {
-        return fromJson(Files.readString(File(clinicalJson).toPath()))
+        return read(Path.of(clinicalJson))
+    }
+
+    fun read(clinicalJson: Path): ClinicalRecord {
+        return fromJson(Files.readString(clinicalJson))
     }
 
     fun toJson(record: ClinicalRecord): String {
