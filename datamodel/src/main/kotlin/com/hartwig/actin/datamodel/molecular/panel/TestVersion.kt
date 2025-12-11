ef67aa40a68720531f31af7d88fc74930f258ed3
Paul Wolfe <paul.d.wolfe@gmail.com>
2025-12-11 15:23:11 -0500

    ACTIN-2891: Remove the "testIsOlder..." from the test version equals (#1401)
    Otherwise we will never match older tests as the specs are built up with this flag always false.

--- datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/panel/TestVersion.kt
+++ datamodel/src/main/kotlin/com/hartwig/actin/datamodel/molecular/panel/TestVersion.kt
@@ -5,4 +5,17 @@ import java.time.LocalDate
 data class TestVersion(
     val versionDate: LocalDate? = null,
     val testDateIsBeforeOldestTestVersion: Boolean = false
-)
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TestVersion
+
+        return versionDate == other.versionDate
+    }
+
+    override fun hashCode(): Int {
+        return versionDate?.hashCode() ?: 0
+    }
+}
