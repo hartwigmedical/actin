37a1bc1ed4f817d6ba1ebc00aa037f43be62d65a
Nina Jacobs <67696681+ninajacobs@users.noreply.github.com>
2025-12-12 16:33:47 +0100

    ACTIN-2888/2899: Add HAS_HAD_DOSE_REDUCTION_DURING_TREATMENT_NAME_X + drop ihcProtein (#1400)
    * ACTIN-2888: Add HAS_HAD_DOSE_REDUCTION_DURING_TREATMENT_NAME_X
    
    * ACTIN-2888: Cleanup unused property
    
    * ACTIN-2888: test

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/TreatmentRuleMapper.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/TreatmentRuleMapper.kt
@@ -55,6 +55,7 @@ class TreatmentRuleMapper(resources: RuleMappingResources) : RuleMapper(resource
             EligibilityRule.HAS_HAD_ANY_SYSTEMIC_CANCER_TREATMENT_WITHIN_X_MONTHS to hasHadAnyCancerTreatmentWithinMonthsCreator(true),
             EligibilityRule.HAS_HAD_TREATMENT_NAME_X to hasHadSpecificTreatmentCreator(),
             EligibilityRule.HAS_HAD_TREATMENT_NAME_X_WITHIN_Y_WEEKS to hasHadSpecificTreatmentWithinWeeksCreator(),
+            EligibilityRule.HAS_HAD_DOSE_REDUCTION_DURING_TREATMENT_NAME_X to hasHadSpecificTreatmentAndDoseReductionCreator(),
             EligibilityRule.HAS_HAD_FIRST_LINE_SYSTEMIC_TREATMENT_NAME_X to hasHadFirstLineSystemicTreatmentNameCreator(),
             EligibilityRule.HAS_HAD_FIRST_LINE_SYSTEMIC_TREATMENT_NAME_X_WITHOUT_PROGRESSION_AND_AT_LEAST_Y_CYCLES to hasHadFirstLineTreatmentNameWithoutPdAndWithCyclesCreator(),
             EligibilityRule.HAS_HAD_DRUG_X_COMBINED_WITH_CATEGORY_Y_TREATMENT_OF_TYPES_Z to hasHadSpecificDrugCombinedWithCategoryAndTypesCreator(),
@@ -281,6 +282,13 @@ class TreatmentRuleMapper(resources: RuleMappingResources) : RuleMapper(resource
             val input = functionInputResolver().createOneSpecificTreatmentOneIntegerInput(function)
             val minDate = referenceDate.minusWeeks(input.integer.toLong())
             HasHadSpecificTreatmentSinceDate(input.treatment, minDate)
+        }
+    }
+
+    private fun hasHadSpecificTreatmentAndDoseReductionCreator(): FunctionCreator {
+        return { function: EligibilityFunction ->
+            val treatment = functionInputResolver().createOneSpecificTreatmentInput(function)
+            HasHadSomeSpecificTreatmentsWithDoseReduction(listOf(treatment))
         }
     }
 
