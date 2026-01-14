ca808ba9ca0adf93641a5f94ebb36feeda1d940f
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2026-01-14 17:02:50 -0500

    ACTIN-2947

--- common/src/main/kotlin/com/hartwig/actin/util/json/EligibilityFunctionDeserializer.kt
+++ common/src/main/kotlin/com/hartwig/actin/util/json/EligibilityFunctionDeserializer.kt
@@ -5,14 +5,20 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
 import com.hartwig.actin.datamodel.clinical.treatment.Drug
 import com.hartwig.actin.datamodel.clinical.treatment.Treatment
+import com.hartwig.actin.datamodel.trial.DrugParameter
 import com.hartwig.actin.datamodel.trial.EligibilityFunction
 import com.hartwig.actin.datamodel.trial.FunctionParameter
+import com.hartwig.actin.datamodel.trial.ManyDrugsParameter
+import com.hartwig.actin.datamodel.trial.ManyTreatmentsParameter
 import com.hartwig.actin.datamodel.trial.Parameter
+import com.hartwig.actin.datamodel.trial.SystemicTreatmentParameter
 import com.hartwig.actin.datamodel.trial.TreatmentCategoryOrType
+import com.hartwig.actin.datamodel.trial.TreatmentParameter
 import com.hartwig.actin.trial.input.EligibilityRule
 import java.lang.reflect.Type
 
@@ -30,25 +36,25 @@ class EligibilityFunctionDeserializer : JsonDeserializer<EligibilityFunction>, J
     }
 
     override fun deserialize(jsonElement: JsonElement, type: Type, context: JsonDeserializationContext): EligibilityFunction {
-        return toEligibilityFunction(jsonElement.asJsonObject)
+        return toEligibilityFunction(jsonElement.asJsonObject, context)
     }
 
-    private fun toEligibilityFunction(function: JsonObject): EligibilityFunction {
+    private fun toEligibilityFunction(function: JsonObject, context: JsonDeserializationContext): EligibilityFunction {
         val rule = function.get("rule").asString
         return EligibilityFunction(
             rule = rule,
-            parameters = toParameters(function.getAsJsonArray("parameters"), rule)
+            parameters = toParameters(function.getAsJsonArray("parameters"), rule, context)
         )
     }
 
-    private fun toParameters(parameterArray: JsonArray, rule: String): List<Parameter<*>> {
+    private fun toParameters(parameterArray: JsonArray, rule: String, context: JsonDeserializationContext): List<Parameter<*>> {
         val expectedTypes = runCatching { EligibilityRule.valueOf(rule).input }.getOrNull()
         return parameterArray.mapIndexedNotNull { index, element ->
             when {
-                element.isJsonObject -> parseParameterObject(element.asJsonObject)
+                element.isJsonObject -> parseParameterObject(element.asJsonObject, context)
                 element.isJsonPrimitive -> {
                     val type = expectedTypes?.getOrNull(index) ?: Parameter.Type.STRING
-                    type.create(element.asJsonPrimitive.asString)
+                    parseParameterValue(type, element.asJsonPrimitive.asString)
                 }
 
                 else -> null
@@ -56,10 +62,10 @@ class EligibilityFunctionDeserializer : JsonDeserializer<EligibilityFunction>, J
         }
     }
 
-    private fun parseParameterObject(element: JsonObject): Parameter<*>? {
+    private fun parseParameterObject(element: JsonObject, context: JsonDeserializationContext): Parameter<*>? {
         if (!element.has("type")) {
             return if (element.has("rule") || element.has("ruleName")) {
-                FunctionParameter(toEligibilityFunction(element))
+                FunctionParameter(toEligibilityFunction(element, context))
             } else {
                 null
             }
@@ -68,16 +74,48 @@ class EligibilityFunctionDeserializer : JsonDeserializer<EligibilityFunction>, J
         val type = Parameter.Type.valueOf(element.get("type").asString)
         if (type == Parameter.Type.FUNCTION) {
             val function = element.getAsJsonObject("value")
-            return FunctionParameter(toEligibilityFunction(function))
+            return FunctionParameter(toEligibilityFunction(function, context))
         }
 
-        val value = element.get("value") ?: return type.create("")
-        val valueString = when {
-            value.isJsonPrimitive -> value.asJsonPrimitive.asString
-            value.isJsonArray -> value.asJsonArray.joinToString(";") { it.asString }
-            else -> value.toString()
+        val value = element.get("value")
+        return when (type) {
+            Parameter.Type.TREATMENT -> {
+                if (value == null || value.isJsonNull || value.isJsonPrimitive) {
+                    return parseParameterValue(type, parameterValueAsString(value))
+                }
+                TreatmentParameter(context.deserialize(value, Treatment::class.java))
+            }
+
+            Parameter.Type.SYSTEMIC_TREATMENT -> {
+                if (value == null || value.isJsonNull || value.isJsonPrimitive) {
+                    return parseParameterValue(type, parameterValueAsString(value))
+                }
+                SystemicTreatmentParameter(context.deserialize(value, Treatment::class.java))
+            }
+
+            Parameter.Type.DRUG -> {
+                if (value == null || value.isJsonNull || value.isJsonPrimitive) {
+                    return parseParameterValue(type, parameterValueAsString(value))
+                }
+                DrugParameter(context.deserialize(value, Drug::class.java))
+            }
+
+            Parameter.Type.MANY_TREATMENTS -> {
+                if (value == null || value.isJsonNull || value.isJsonPrimitive) {
+                    return parseParameterValue(type, parameterValueAsString(value))
+                }
+                ManyTreatmentsParameter(parseTreatments(value, context))
+            }
+
+            Parameter.Type.MANY_DRUGS -> {
+                if (value == null || value.isJsonNull || value.isJsonPrimitive) {
+                    return parseParameterValue(type, parameterValueAsString(value))
+                }
+                ManyDrugsParameter(parseDrugs(value, context).toSet())
+            }
+
+            else -> parseParameterValue(type, parameterValueAsString(value))
         }
-        return type.create(valueString)
     }
 
     private fun serializeParameters(parameters: List<Parameter<*>>, context: JsonSerializationContext): JsonArray {
@@ -88,7 +126,16 @@ class EligibilityFunctionDeserializer : JsonDeserializer<EligibilityFunction>, J
             if (param is FunctionParameter) {
                 parameter.add("value", context.serialize(param.value))
             } else {
-                parameter.addProperty("value", parameterValueToString(param))
+                val valueElement = when (param.type) {
+                    Parameter.Type.TREATMENT,
+                    Parameter.Type.SYSTEMIC_TREATMENT,
+                    Parameter.Type.DRUG,
+                    Parameter.Type.MANY_TREATMENTS,
+                    Parameter.Type.MANY_DRUGS -> context.serialize(param.value)
+
+                    else -> JsonPrimitive(parameterValueToString(param))
+                }
+                parameter.add("value", valueElement)
             }
             array.add(parameter)
         }
@@ -112,5 +159,42 @@ class EligibilityFunctionDeserializer : JsonDeserializer<EligibilityFunction>, J
             is Enum<*> -> value.name
             else -> value.toString()
         }
+    }
+
+    private fun parameterValueAsString(value: JsonElement?): String {
+        if (value == null || value.isJsonNull) {
+            return ""
+        }
+        return when {
+            value.isJsonPrimitive -> value.asJsonPrimitive.asString
+            value.isJsonArray -> value.asJsonArray.joinToString(";") { it.asString }
+            else -> value.toString()
+        }
+    }
+
+    private fun parseTreatments(value: JsonElement?, context: JsonDeserializationContext): List<Treatment> {
+        if (value == null || value.isJsonNull) {
+            return emptyList()
+        }
+        return if (value.isJsonArray) {
+            value.asJsonArray.mapNotNull { context.deserialize<Treatment>(it, Treatment::class.java) }
+        } else {
+            listOf(context.deserialize(value, Treatment::class.java))
+        }
+    }
+
+    private fun parseDrugs(value: JsonElement?, context: JsonDeserializationContext): List<Drug> {
+        if (value == null || value.isJsonNull) {
+            return emptyList()
+        }
+        return if (value.isJsonArray) {
+            value.asJsonArray.mapNotNull { context.deserialize<Drug>(it, Drug::class.java) }
+        } else {
+            listOf(context.deserialize(value, Drug::class.java))
+        }
+    }
+
+    private fun parseParameterValue(type: Parameter.Type, value: String): Parameter<*> {
+        return type.create(value)
     }
 }
--- common/src/test/kotlin/com/hartwig/actin/util/json/EligibilityFunctionDeserializerTest.kt
+++ common/src/test/kotlin/com/hartwig/actin/util/json/EligibilityFunctionDeserializerTest.kt
@@ -1,8 +1,16 @@
 package com.hartwig.actin.util.json
 
 import com.google.gson.GsonBuilder
+import com.hartwig.actin.TestTreatmentDatabaseFactory
+import com.hartwig.actin.clinical.serialization.TreatmentAdapter
+import com.hartwig.actin.datamodel.clinical.treatment.Treatment
+import com.hartwig.actin.datamodel.trial.DrugParameter
 import com.hartwig.actin.datamodel.trial.EligibilityFunction
 import com.hartwig.actin.datamodel.trial.FunctionParameter
+import com.hartwig.actin.datamodel.trial.ManyDrugsParameter
+import com.hartwig.actin.datamodel.trial.ManyTreatmentsParameter
+import com.hartwig.actin.datamodel.trial.SystemicTreatmentParameter
+import com.hartwig.actin.datamodel.trial.TreatmentParameter
 import com.hartwig.actin.trial.input.EligibilityRule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -19,6 +27,31 @@ class EligibilityFunctionDeserializerTest {
             )
         )
         val gson = GsonBuilder().registerTypeAdapter(EligibilityFunction::class.java, EligibilityFunctionDeserializer()).create()
+
+        assertThat(gson.fromJson(gson.toJson(function), EligibilityFunction::class.java)).isEqualTo(function)
+    }
+
+    @Test
+    fun `Should deserialize treatment and drug parameters with full objects`() {
+        val treatmentDb = TestTreatmentDatabaseFactory.createProper()
+        val treatment = requireNotNull(treatmentDb.findTreatmentByName(TestTreatmentDatabaseFactory.CISPLATIN))
+        val drug = requireNotNull(treatmentDb.findDrugByName(TestTreatmentDatabaseFactory.CISPLATIN))
+
+        val function = EligibilityFunction(
+            rule = "CUSTOM_RULE",
+            parameters = listOf(
+                TreatmentParameter(treatment),
+                SystemicTreatmentParameter(treatment),
+                DrugParameter(drug),
+                ManyTreatmentsParameter(listOf(treatment)),
+                ManyDrugsParameter(setOf(drug))
+            )
+        )
+
+        val gson = GsonBuilder()
+            .registerTypeAdapter(Treatment::class.java, TreatmentAdapter())
+            .registerTypeAdapter(EligibilityFunction::class.java, EligibilityFunctionDeserializer())
+            .create()
 
         assertThat(gson.fromJson(gson.toJson(function), EligibilityFunction::class.java)).isEqualTo(function)
     }
