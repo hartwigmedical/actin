f92762c3833cbedf3ac3f44317efc8b364ed74b8
Andreia Esteves <a.esteves@hartwigmedicalfoundation.nl>
2026-01-16 14:13:07 +0100

    ACTIN-2867 add test

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/general/HasMaximumWHOStatusTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/general/HasMaximumWHOStatusTest.kt
@@ -1,6 +1,7 @@
 package com.hartwig.actin.algo.evaluation.general
 
 import com.hartwig.actin.algo.evaluation.EvaluationAssert.assertEvaluation
+import com.hartwig.actin.algo.evaluation.general.GeneralTestFactory.withWHO
 import com.hartwig.actin.datamodel.algo.EvaluationResult
 import com.hartwig.actin.datamodel.clinical.WhoStatusPrecision
 import org.assertj.core.api.Assertions.assertThat
@@ -12,14 +13,14 @@ class HasMaximumWHOStatusTest {
 
     @Test
     fun `Should return undetermined when WHO is null`() {
-        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(GeneralTestFactory.withWHO(null)))
+        assertEvaluation(EvaluationResult.UNDETERMINED, function.evaluate(withWHO(null)))
     }
 
     @Test
     fun `Should pass when WHO is less than or equal to maximum`() {
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(GeneralTestFactory.withWHO(0)))
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(GeneralTestFactory.withWHO(1)))
-        assertEvaluation(EvaluationResult.PASS, function.evaluate(GeneralTestFactory.withWHO(2)))
+        assertEvaluation(EvaluationResult.PASS, function.evaluate(withWHO(0)))
+        assertEvaluation(EvaluationResult.PASS, function.evaluate(withWHO(1)))
+        assertEvaluation(EvaluationResult.PASS, function.evaluate(withWHO(2)))
     }
 
     @Test
@@ -31,7 +32,7 @@ class HasMaximumWHOStatusTest {
 
     @Test
     fun `Should return recoverable fail when WHO difference is exactly one with at most range`() {
-        val evaluation = function.evaluate(GeneralTestFactory.withWHO(3, WhoStatusPrecision.AT_MOST))
+        val evaluation = function.evaluate(withWHO(3, WhoStatusPrecision.AT_MOST))
         assertEvaluation(EvaluationResult.FAIL, evaluation)
         assertThat(evaluation.recoverable).isTrue()
         assertThat(evaluation.failMessages.first().toString()).isEqualTo("WHO <=3 exceeds WHO 2")
@@ -39,31 +40,37 @@ class HasMaximumWHOStatusTest {
 
     @Test
     fun `Should fail when WHO difference is greater than one`() {
-        assertEvaluation(EvaluationResult.FAIL, function.evaluate(GeneralTestFactory.withWHO(4)))
+        assertEvaluation(EvaluationResult.FAIL, function.evaluate(withWHO(4)))
     }
 
     @Test
     fun `Should return undetermined when precision is at least`() {
         assertEvaluation(
-            EvaluationResult.UNDETERMINED,
-            function.evaluate(GeneralTestFactory.withWHO(4, WhoStatusPrecision.AT_LEAST))
+            EvaluationResult.UNDETERMINED, function.evaluate(withWHO(4, WhoStatusPrecision.AT_LEAST))
         )
     }
 
     @Test
     fun `Should pass when WHO at most value is less than or equal to maximum`() {
         assertEvaluation(
-            EvaluationResult.PASS,
-            function.evaluate(GeneralTestFactory.withWHO(2, WhoStatusPrecision.AT_MOST))
+            EvaluationResult.PASS, function.evaluate(withWHO(2, WhoStatusPrecision.AT_MOST))
         )
     }
 
     @Test
     fun `Should fail when WHO at most value is more than the maximum`() {
         assertEvaluation(
-            EvaluationResult.FAIL,
-            function.evaluate(GeneralTestFactory.withWHO(3, WhoStatusPrecision.AT_MOST))
+            EvaluationResult.FAIL, function.evaluate(withWHO(3, WhoStatusPrecision.AT_MOST))
         )
+    }
+
+    @Test
+    fun `Should result in recoverable Fail when WHO with at most range may be recoverable`() {
+        val function: HasMaximumWHOStatus = HasMaximumWHOStatus(0)
+        val evaluation = function.evaluate(withWHO(1, WhoStatusPrecision.AT_MOST))
+        assertEvaluation(EvaluationResult.FAIL, evaluation)
+        assertThat(evaluation.recoverable).isTrue()
+        assertThat(evaluation.failMessages.first().toString()).isEqualTo("WHO <=1 exceeds WHO 0")
     }
 
 }
