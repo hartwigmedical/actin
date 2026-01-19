593c6b8efabf31b8c881dac2202b2320253667ff
Eljo Dorrestijn <e.dorrestijn@hartwigmedicalfoundation.nl>
2026-01-19 16:24:44 +0100

    ACTIN-3005: Reformat test

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadTreatmentResponseFollowingDrugTreatmentTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadTreatmentResponseFollowingDrugTreatmentTest.kt
@@ -23,7 +23,13 @@ private val TREATMENT_CATEGORY = TreatmentCategory.TARGETED_THERAPY
 class HasHadTreatmentResponseFollowingDrugTreatmentTest {
 
     private val functionWithDrug =
-        HasHadTreatmentResponseFollowingDrugTreatment(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+        HasHadTreatmentResponseFollowingDrugTreatment(
+            Drug(
+                name = MATCHING_DRUG_NAME,
+                category = TREATMENT_CATEGORY,
+                drugTypes = emptySet()
+            )
+        )
 
 
     @Test
@@ -33,107 +39,174 @@ class HasHadTreatmentResponseFollowingDrugTreatmentTest {
 
     @Test
     fun `Should fail if no matching drugs found`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = "other_drug", category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.COMPLETE_RESPONSE
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = "other_drug", category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.COMPLETE_RESPONSE
+            )
+        )
         assertEvaluation(EvaluationResult.FAIL, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Should fail if matching drugs found and response is progressive disease`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.PROGRESSIVE_DISEASE
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.PROGRESSIVE_DISEASE
+            )
+        )
         assertEvaluation(EvaluationResult.FAIL, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Should fail if matching drugs found and response is stable disease`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.STABLE_DISEASE
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.STABLE_DISEASE
+            )
+        )
         assertEvaluation(EvaluationResult.FAIL, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Undetermined if matching drugs found and response is mixed`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.MIXED
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.MIXED
+            )
+        )
         assertEvaluation(EvaluationResult.UNDETERMINED, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Undetermined if matching drugs found and no response available`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))))
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                )
+            )
+        )
         assertEvaluation(EvaluationResult.UNDETERMINED, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Undetermined if treatments of matching drugs found and both positive an negative response available`() {
         val treatmentHistory = listOf(
-            treatmentHistoryEntry(treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-                bestResponse = TreatmentResponse.COMPLETE_RESPONSE),
-            treatmentHistoryEntry(treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-                bestResponse = TreatmentResponse.PROGRESSIVE_DISEASE)
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.COMPLETE_RESPONSE
+            ),
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.PROGRESSIVE_DISEASE
             )
+        )
         assertEvaluation(EvaluationResult.UNDETERMINED, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Pass if matching drugs found and response is partial`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.PARTIAL_RESPONSE
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.PARTIAL_RESPONSE
 
-        ))
+            )
+        )
         assertEvaluation(EvaluationResult.PASS, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
+
     @Test
     fun `Pass if matching drugs found and response is near complete`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.NEAR_COMPLETE_RESPONSE
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.NEAR_COMPLETE_RESPONSE
 
-        ))
+            )
+        )
         assertEvaluation(EvaluationResult.PASS, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
 
     @Test
     fun `Pass if matching drugs found and response is complete`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.COMPLETE_RESPONSE
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.COMPLETE_RESPONSE
 
-        ))
+            )
+        )
         assertEvaluation(EvaluationResult.PASS, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
+
     @Test
     fun `Pass if matching drugs found and response is remission`() {
-        val treatmentHistory = listOf(treatmentHistoryEntry(
-            treatments = setOf(DrugTreatment("treatment",
-                setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet())))),
-            bestResponse = TreatmentResponse.REMISSION
-        ))
+        val treatmentHistory = listOf(
+            treatmentHistoryEntry(
+                treatments = setOf(
+                    DrugTreatment(
+                        "treatment",
+                        setOf(Drug(name = MATCHING_DRUG_NAME, category = TREATMENT_CATEGORY, drugTypes = emptySet()))
+                    )
+                ),
+                bestResponse = TreatmentResponse.REMISSION
+            )
+        )
         assertEvaluation(EvaluationResult.PASS, functionWithDrug.evaluate(withTreatmentHistory(treatmentHistory)))
     }
-
 
 
 }
