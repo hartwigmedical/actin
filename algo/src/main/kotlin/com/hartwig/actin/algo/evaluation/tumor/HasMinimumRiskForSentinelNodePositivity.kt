51cbd1efe40741b2d681aa491167c0f9cc019256
Charlotte Bruel <131683629+cbruel@users.noreply.github.com>
2026-02-06 09:58:01 +0100

    ACTIN-3127: Add new rule HAS_RISK_OF_AT_LEAST_X_PERCENTAGE_FOR_SENTINEL_NODE_POSITIVITY (#1446)
    * ACTIN-3127: Add new rule HAS_RISK_OF_AT_LEAST_X_PERCENTAGE_FOR_SENTINEL_NODE_POSITIVITY
    
    * ACTIN-3127: Formatting

---
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/tumor/HasMinimumRiskForSentinelNodePositivity.kt
@@ -0,0 +1,13 @@
+package com.hartwig.actin.algo.evaluation.tumor
+
+import com.hartwig.actin.algo.evaluation.EvaluationFactory
+import com.hartwig.actin.algo.evaluation.EvaluationFunction
+import com.hartwig.actin.datamodel.PatientRecord
+import com.hartwig.actin.datamodel.algo.Evaluation
+
+class HasMinimumRiskForSentinelNodePositivity(private val minimumRisk: Int) : EvaluationFunction {
+
+    override fun evaluate(record: PatientRecord): Evaluation {
+        return EvaluationFactory.undetermined("Undetermined if patient has risk of sentinel node positivity above $minimumRisk%")
+    }
+}
