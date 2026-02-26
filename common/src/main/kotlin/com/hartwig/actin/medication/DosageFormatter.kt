5e0364ca7ba0158caf01706b66ed63d5ce401cea
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2026-02-26 16:30:28 -0500

    ACTIN-3024: Refactor dosage formatting to support feed review

---
+++ common/src/main/kotlin/com/hartwig/actin/medication/DosageFormatter.kt
@@ -0,0 +1,47 @@
+package com.hartwig.actin.medication
+
+import com.hartwig.actin.datamodel.clinical.Dosage
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
+
+object DosageFormatter {
+    private val DECIMAL_FORMAT_SYMBOLS = DecimalFormatSymbols.getInstance(Locale.ENGLISH)
+    private val TWO_DECIMAL_PLACES_FORMAT = DecimalFormat("#.##", DECIMAL_FORMAT_SYMBOLS)
+    private val NO_DECIMAL_PLACES_FORMAT = DecimalFormat("#", DECIMAL_FORMAT_SYMBOLS)
+    private val SPECIFIC_OR_UNKNOWN = setOf("specific prescription", "unknown prescription")
+
+    fun formatDosage(dosage: Dosage): String {
+        val dosageMin = formatDosageLimit(dosage.dosageMin)
+        val dosageMax = formatDosageLimit(dosage.dosageMax)
+        val dosageString = if (dosageMin == dosageMax) dosageMin else "$dosageMin - $dosageMax"
+
+        return when (val dosageUnit = dosage.dosageUnit) {
+            null -> "unknown prescription"
+            in SPECIFIC_OR_UNKNOWN -> dosageUnit
+            else -> listOfNotNull("if needed".takeIf { dosage.ifNeeded == true }, dosageString, dosageUnit).joinToString(" ")
+        }
+    }
+
+    private fun formatDosageLimit(dosageLimit: Double?) =
+        if (dosageLimit != null && dosageLimit != 0.0) TWO_DECIMAL_PLACES_FORMAT.format(dosageLimit) else "?"
+
+    fun formatFrequency(dosage: Dosage): String {
+        val frequency = if (dosage.frequency != null && dosage.frequency != 0.0) {
+            TWO_DECIMAL_PLACES_FORMAT.format(dosage.frequency!!)
+        } else "?"
+
+        val frequencyUnit = dosage.frequencyUnit
+        return when {
+            frequencyUnit == null -> "unknown prescription"
+
+            frequencyUnit in SPECIFIC_OR_UNKNOWN + "once" -> frequencyUnit
+
+            dosage.periodBetweenUnit != null -> {
+                "$frequency / ${NO_DECIMAL_PLACES_FORMAT.format(dosage.periodBetweenValue!! + 1)} ${dosage.periodBetweenUnit}"
+            }
+
+            else -> "$frequency / $frequencyUnit"
+        }
+    }
+}
--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/MedicationGenerator.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/MedicationGenerator.kt
@@ -2,26 +2,20 @@ package com.hartwig.actin.report.pdf.tables.clinical
 
 import com.hartwig.actin.clinical.interpretation.MedicationStatusInterpretation
 import com.hartwig.actin.clinical.interpretation.MedicationStatusInterpreter
-import com.hartwig.actin.datamodel.clinical.Dosage
 import com.hartwig.actin.datamodel.clinical.Medication
+import com.hartwig.actin.medication.DosageFormatter
 import com.hartwig.actin.report.pdf.tables.TableGenerator
 import com.hartwig.actin.report.pdf.util.Cells
 import com.hartwig.actin.report.pdf.util.Formats
 import com.hartwig.actin.report.pdf.util.Tables
 import com.itextpdf.layout.element.Table
 
-private const val SPECIFIC_OR_UNKNOWN = "specific prescription|unknown prescription"
-
 class MedicationGenerator(private val medications: List<Medication>, private val interpreter: MedicationStatusInterpreter) :
     TableGenerator {
 
-    override fun title(): String {
-        return "Active medication details"
-    }
+    override fun title(): String = "Active medication details"
 
-    override fun forceKeepTogether(): Boolean {
-        return false
-    }
+    override fun forceKeepTogether(): Boolean = false
 
     override fun contents(): Table {
         val table = Tables.createRelativeWidthCols(1f, 1f, 1f, 1f, 1f, 1f)
@@ -36,70 +30,15 @@ class MedicationGenerator(private val medications: List<Medication>, private val
         medications.distinct()
             .filter { interpreter.interpret(it) == MedicationStatusInterpretation.ACTIVE }
             .forEach { medication: Medication ->
+                val hideDosage = medication.administrationRoute in setOf("Cutaneous", "Intravenous") && medication.dosage.dosageMin == 0.0
                 table.addCell(Cells.createContent(medication.name))
-                table.addCell(Cells.createContent(administrationRoute(medication)))
+                table.addCell(Cells.createContent(medication.administrationRoute ?: ""))
                 table.addCell(Cells.createContent(Formats.date(medication.startDate, "")))
                 table.addCell(Cells.createContent(Formats.date(medication.stopDate, "")))
-                table.addCell(Cells.createContent(dosage(medication)))
-                table.addCell(Cells.createContent(frequency(medication.dosage)))
+                table.addCell(Cells.createContent(if (hideDosage) "" else DosageFormatter.formatDosage(medication.dosage)))
+                table.addCell(Cells.createContent(DosageFormatter.formatFrequency(medication.dosage)))
             }
 
         return table
-    }
-
-    private fun administrationRoute(medication: Medication): String {
-        return if (medication.administrationRoute != null) medication.administrationRoute!! else ""
-    }
-
-    private fun dosage(medication: Medication): String {
-        val dosage = medication.dosage
-        if (medication.administrationRoute in setOf("Cutaneous", "Intravenous") && dosage.dosageMin == 0.0) {
-            return ""
-        }
-        val dosageMin = formatDosageLimit(dosage.dosageMin)
-        val dosageMax = formatDosageLimit(dosage.dosageMax)
-        val dosageString = if (dosageMin == dosageMax) dosageMin else "$dosageMin - $dosageMax"
-        val result = if (dosage.ifNeeded == true) "if needed $dosageString" else dosageString
-
-        val dosageUnit = dosage.dosageUnit
-        return when {
-            dosageUnit == null -> {
-                "unknown prescription"
-            }
-
-            dosageUnit.matches(SPECIFIC_OR_UNKNOWN.toRegex()) -> {
-                dosageUnit
-            }
-
-            else -> {
-                "$result $dosageUnit"
-            }
-        }
-    }
-
-    private fun formatDosageLimit(dosageLimit: Double?) =
-        if (dosageLimit != null && dosageLimit != 0.0) Formats.twoDigitNumber(dosageLimit) else "?"
-
-    private fun frequency(dosage: Dosage): String {
-        val frequency =
-            if (dosage.frequency != null && dosage.frequency != 0.0) Formats.twoDigitNumber(dosage.frequency!!) else "?"
-        val frequencyUnit = dosage.frequencyUnit
-        return when {
-            frequencyUnit == null -> {
-                "unknown prescription"
-            }
-
-            frequencyUnit.matches(("$SPECIFIC_OR_UNKNOWN|once").toRegex()) -> {
-                frequencyUnit
-            }
-
-            dosage.periodBetweenUnit != null -> {
-                "$frequency / ${Formats.noDigitNumber(dosage.periodBetweenValue!! + 1)} ${dosage.periodBetweenUnit}"
-            }
-
-            else -> {
-                "$frequency / $frequencyUnit"
-            }
-        }
     }
 }
