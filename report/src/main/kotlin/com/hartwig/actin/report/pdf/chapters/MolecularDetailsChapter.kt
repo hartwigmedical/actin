77d67cb81a647d2aabce27da3b580032fba9076c
Thomas Visser <t.visser@hartwigmedicalfoundation.nl>
2026-02-27 13:30:22 +0100

    ACTIN-2977: Update order of pathology reports and tests in molecular sections (#1459)
    * ACTIN-2977: Fix typo in ReportRegressionTest
    
    * ACTIN-2977: Update order of pathology reports and tests in molecular sections
    
    Items with only IHC tests are now shown last.
    
    * Add missing type adapter for CopyNumberType to ClinicalRecordJsonMapper
    
    * ACTIN-2977: Improve visual hierarchy of molecular sections through indentation
    
    * ACTIN-2977: Improve visual hierarchy of molecular sections through borders
    
    * Revert "ACTIN-2977: Improve visual hierarchy of molecular sections through borders"
    
    36b1d0175866504f0b071cb18e0eeb625020c5b4
    
    * Fix old/incorrect copy number type values
    
    These caused the tests to fail
    
    * Update reference pdfs for ReportRegressionTest
    
    * Process PR feedback

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/chapters/MolecularDetailsChapter.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/chapters/MolecularDetailsChapter.kt
@@ -15,8 +15,6 @@ import com.hartwig.actin.report.interpretation.InterpretedCohortFactory
 import com.hartwig.actin.report.pdf.SummaryType
 import com.hartwig.actin.report.pdf.tables.TableGeneratorFunctions
 import com.hartwig.actin.report.pdf.tables.molecular.IhcResultGenerator
-import com.hartwig.actin.report.pdf.tables.molecular.ImmunologyDisplayMode
-import com.hartwig.actin.report.pdf.tables.molecular.ImmunologyGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.LongitudinalMolecularHistoryGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.OrangeMolecularRecordGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.PathologyReportFunctions
@@ -75,10 +73,7 @@ class MolecularDetailsChapter(
 
     private fun addMolecularDetails(document: Document) {
         val cohorts =
-            InterpretedCohortFactory.createEvaluableCohorts(
-                report.treatmentMatch.trialMatches,
-                configuration.filterOnSOCExhaustionAndTumorType
-            )
+            InterpretedCohortFactory.createEvaluableCohorts(report.treatmentMatch.trialMatches, configuration.filterOnSOCExhaustionAndTumorType)
 
         val orangeMolecularTest = MolecularHistory(report.patientRecord.molecularTests).latestOrangeMolecularRecord()
         val externalPanelResults = report.patientRecord.molecularTests.filter { it.experimentType == ExperimentType.PANEL }
@@ -133,18 +128,13 @@ class MolecularDetailsChapter(
                 valueWidth
             )
         }
-        val immunologyGenerators = orangeMolecularRecord.mapNotNull { molecularTest ->
-            if (molecularTest.immunology != null) {
-                ImmunologyGenerator(molecularTest, ImmunologyDisplayMode.DETAILED, "Immunology", keyWidth, valueWidth)
-            } else null
-        }
 
         val ihcGenerator = if (ihcTests.isNotEmpty()) {
             IhcResultGenerator(ihcTests, keyWidth, valueWidth - 10, IhcTestInterpreter())
         } else null
 
         TableGeneratorFunctions.addGenerators(
-            orangeGenerators + wgsSummaryGenerators + immunologyGenerators + listOfNotNull(ihcGenerator),
+            orangeGenerators + wgsSummaryGenerators + listOfNotNull(ihcGenerator),
             topTable,
             overrideTitleFormatToSubtitle = (pathologyReport != null)
         )
