68b9e8ba53c1959ce240e0f6ab5ee2a258083862
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2026-02-27 13:55:19 -0500

    Revert "ACTIN-2977: Update order of pathology reports and tests in molecular sections (#1459)"
    This reverts commit 26ca31e2a68c77ce5a9ce90cafd426751133c5d9.

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/chapters/MolecularDetailsChapter.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/chapters/MolecularDetailsChapter.kt
@@ -15,6 +15,8 @@ import com.hartwig.actin.report.interpretation.InterpretedCohortFactory
 import com.hartwig.actin.report.pdf.SummaryType
 import com.hartwig.actin.report.pdf.tables.TableGeneratorFunctions
 import com.hartwig.actin.report.pdf.tables.molecular.IhcResultGenerator
+import com.hartwig.actin.report.pdf.tables.molecular.ImmunologyDisplayMode
+import com.hartwig.actin.report.pdf.tables.molecular.ImmunologyGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.LongitudinalMolecularHistoryGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.OrangeMolecularRecordGenerator
 import com.hartwig.actin.report.pdf.tables.molecular.PathologyReportFunctions
@@ -73,7 +75,10 @@ class MolecularDetailsChapter(
 
     private fun addMolecularDetails(document: Document) {
         val cohorts =
-            InterpretedCohortFactory.createEvaluableCohorts(report.treatmentMatch.trialMatches, configuration.filterOnSOCExhaustionAndTumorType)
+            InterpretedCohortFactory.createEvaluableCohorts(
+                report.treatmentMatch.trialMatches,
+                configuration.filterOnSOCExhaustionAndTumorType
+            )
 
         val orangeMolecularTest = MolecularHistory(report.patientRecord.molecularTests).latestOrangeMolecularRecord()
         val externalPanelResults = report.patientRecord.molecularTests.filter { it.experimentType == ExperimentType.PANEL }
@@ -130,6 +135,11 @@ class MolecularDetailsChapter(
                 keyWidth,
                 valueWidth
             )
+        }
+        val immunologyGenerators = orangeMolecularRecord.mapNotNull { molecularTest ->
+            if (molecularTest.immunology != null) {
+                ImmunologyGenerator(molecularTest, ImmunologyDisplayMode.DETAILED, "Immunology", keyWidth, valueWidth)
+            } else null
         }
 
         val ihcGenerator = if (ihcTests.isNotEmpty()) {
