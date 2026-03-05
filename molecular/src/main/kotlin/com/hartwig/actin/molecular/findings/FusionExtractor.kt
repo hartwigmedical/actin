2889201efc3ac1afd69217d763cf4808eef72ee4
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-03-05 15:20:26 +1100

    Migrated to findings model enums

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/findings/FusionExtractor.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/findings/FusionExtractor.kt
@@ -6,7 +6,6 @@ import com.hartwig.actin.datamodel.molecular.driver.ProteinEffect
 import com.hartwig.actin.molecular.filter.GeneFilter
 import com.hartwig.actin.molecular.util.ExtractionUtil
 import com.hartwig.hmftools.finding.datamodel.DriverFindingList
-import com.hartwig.hmftools.datamodel.linx.LinxFusionType
 
 fun domainsAsList(domains: String): List<String> {
     return domains.split(";").filter { it.isNotEmpty() }
@@ -40,39 +39,39 @@ class FusionExtractor(private val geneFilter: GeneFilter) {
 
     internal fun determineDriverType(fusion: com.hartwig.hmftools.finding.datamodel.Fusion): FusionDriverType {
         return when (fusion.reportedType()) {
-            LinxFusionType.PROMISCUOUS_3 -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.PROMISCUOUS_3 -> {
                 FusionDriverType.PROMISCUOUS_3
             }
 
-            LinxFusionType.PROMISCUOUS_5 -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.PROMISCUOUS_5 -> {
                 FusionDriverType.PROMISCUOUS_5
             }
 
-            LinxFusionType.PROMISCUOUS_BOTH -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.PROMISCUOUS_BOTH -> {
                 FusionDriverType.PROMISCUOUS_BOTH
             }
 
-            LinxFusionType.IG_PROMISCUOUS -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.IG_PROMISCUOUS -> {
                 FusionDriverType.PROMISCUOUS_IG
             }
 
-            LinxFusionType.KNOWN_PAIR -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.KNOWN_PAIR -> {
                 FusionDriverType.KNOWN_PAIR
             }
 
-            LinxFusionType.IG_KNOWN_PAIR -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.IG_KNOWN_PAIR -> {
                 FusionDriverType.KNOWN_PAIR_IG
             }
 
-            LinxFusionType.EXON_DEL_DUP -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.EXON_DEL_DUP -> {
                 FusionDriverType.KNOWN_PAIR_DEL_DUP
             }
 
-            LinxFusionType.PROMISCUOUS_ENHANCER_TARGET -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.PROMISCUOUS_ENHANCER_TARGET -> {
                 FusionDriverType.PROMISCUOUS_ENHANCER_TARGET
             }
 
-            LinxFusionType.NONE -> {
+            com.hartwig.hmftools.finding.datamodel.Fusion.FusionType.NONE -> {
                 FusionDriverType.NONE
             }
 
