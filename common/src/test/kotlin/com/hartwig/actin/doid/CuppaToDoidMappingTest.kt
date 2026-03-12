b39e03a04265c891d0d8b9c68402a99ce347c74c
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2026-03-12 15:50:08 +0100

    ACTIN-3303: Add HAS_HAD_TREATMENT_NAME_X_FOR_AT_LEAST_Y_WEEKS

--- common/src/test/kotlin/com/hartwig/actin/doid/CuppaToDoidMappingTest.kt
+++ common/src/test/kotlin/com/hartwig/actin/doid/CuppaToDoidMappingTest.kt
@@ -2,7 +2,7 @@ package com.hartwig.actin.doid
 
 import com.hartwig.actin.testutil.ResourceLocator.resourceOnClasspath
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class CuppaToDoidMappingTest {
 
