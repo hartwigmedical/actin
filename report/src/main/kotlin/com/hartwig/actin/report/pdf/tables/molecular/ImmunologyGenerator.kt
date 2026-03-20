7c1602405c428c7e4b1afab6ed3e4c9ed265ae35
Eljo Dorrestijn <e.dorrestijn@hartwigmedicalfoundation.nl>
2026-03-20 10:46:53 +0100

    Revert "ACTIN-3284: Fix report generation"
    This reverts commit 536772dd0ab4594839d93b7aa1de64e8107d8d26.

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/ImmunologyGenerator.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/molecular/ImmunologyGenerator.kt
@@ -37,7 +37,6 @@ class ImmunologyGenerator(
         val table = Tables.createRelativeWidthCols(2f, 3f, 2f, 2f, 5f)
 
         table.addHeaderCell(Cells.createHeaderWithBorder("HLA gene"))
-        table.addHeaderCell(Cells.createHeaderWithBorder(""))
         table.addHeaderCell(Cells.createHeaderWithBorder("Type"))
         table.addHeaderCell(Cells.createHeaderWithBorder("Tumor copy number"))
         table.addHeaderCell(Cells.createHeaderWithBorder("Mutated in tumor"))
@@ -89,8 +88,6 @@ class ImmunologyGenerator(
                 alleles.forEachIndexed { index, hlaAllele ->
                     if (index == 0) {
                         table.addCell(Cells.createContentNoBorder("HLA-A"))
-                        table.addCell(Cells.createContentNoBorder(""))
-
                     } else {
                         table.addCell(Cells.createContentNoBorder(""))
                         table.addCell(Cells.createContentNoBorder(""))
@@ -115,12 +112,10 @@ class ImmunologyGenerator(
                 table.addCell(Cells.createContentNoBorder("No HLA-A alleles detected"))
                 table.addCell(Cells.createContentNoBorder(""))
                 table.addCell(Cells.createContentNoBorder(""))
-                table.addCell(Cells.createContentNoBorder(""))
             }
         } ?: run {
             table.addCell(Cells.createKey("HLA-A"))
             table.addCell(Cells.createContentNoBorder("HLA typing not available"))
-            table.addCell(Cells.createContentNoBorder(""))
             table.addCell(Cells.createContentNoBorder(""))
             table.addCell(Cells.createContentNoBorder(""))
         }
