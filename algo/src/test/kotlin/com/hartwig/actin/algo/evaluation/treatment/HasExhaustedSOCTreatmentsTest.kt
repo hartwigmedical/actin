d5450d732d07077b6872fe620a39b35c3648eed2
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2026-03-27 15:01:35 +0100

    ACTIN-2671: Add tests

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasExhaustedSOCTreatmentsTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasExhaustedSOCTreatmentsTest.kt
@@ -256,7 +256,7 @@ class HasExhaustedSOCTreatmentsTest {
         val treatments = listOf(
             EvaluatedTreatment(
                 TreatmentCandidate(
-                    TreatmentTestFactory.drugTreatment("OXALIPLATIN", TreatmentCategory.CHEMOTHERAPY),
+                    TreatmentTestFactory.drugTreatment("CAPECITABINE+OXALIPLATIN", TreatmentCategory.CHEMOTHERAPY),
                     optional = false,
                     potentialIntolerance = true,
                     eligibilityFunctions = emptySet()
@@ -267,7 +267,7 @@ class HasExhaustedSOCTreatmentsTest {
 
         val evaluation = function.evaluate(TreatmentTestFactory.withTreatmentHistory(emptyList()))
         assertEvaluation(EvaluationResult.WARN, evaluation)
-        assertThat(evaluation.warnMessagesStrings()).containsExactly("Has potentially not exhausted SOC (Oxaliplatin) due to drug intolerance")
+        assertThat(evaluation.warnMessagesStrings()).containsExactly("Has potentially not exhausted SOC (Capecitabine+Oxaliplatin) due to drug intolerance")
     }
 
     private fun setStandardOfCareCanBeEvaluatedForPatient(canBeEvaluated: Boolean) {
