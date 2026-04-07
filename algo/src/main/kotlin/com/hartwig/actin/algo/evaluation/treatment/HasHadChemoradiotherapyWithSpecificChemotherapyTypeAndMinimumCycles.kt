b07b8ee0012252f3f7ab3a1dc4b07423a43329d2
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2026-04-07 11:54:39 +0200

    ACTIN-3414: Reformat

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles.kt
@@ -29,8 +29,15 @@ class HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(privat
 
         val typeString = type.display()
         return when {
-            true in treatmentMatches -> EvaluationFactory.pass("Had received chemoradiotherapy with $typeString chemotherapy and at least $minCycles cycles")
-            null in treatmentMatches -> EvaluationFactory.undetermined("Undetermined if patient received chemoradiotherapy with $typeString chemotherapy and at least $minCycles cycles")
+            true in treatmentMatches -> EvaluationFactory.pass(
+                "Had received chemoradiotherapy with $typeString chemotherapy " +
+                        "and at least $minCycles cycles"
+            )
+
+            null in treatmentMatches -> EvaluationFactory.undetermined(
+                "Undetermined if patient received chemoradiotherapy " +
+                        "with $typeString chemotherapy and at least $minCycles cycles"
+            )
             else -> EvaluationFactory.fail("Has not received chemoradiotherapy with $typeString chemotherapy")
         }
     }
