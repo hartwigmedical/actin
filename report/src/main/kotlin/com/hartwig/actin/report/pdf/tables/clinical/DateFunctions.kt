456b10ce5e84dddea8a314b6157820f3ea7656d4
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2026-06-05 14:15:11 +0200

    ACTIN-3611: remove 'maybe' prefixes

--- report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/DateFunctions.kt
+++ report/src/main/kotlin/com/hartwig/actin/report/pdf/tables/clinical/DateFunctions.kt
@@ -2,11 +2,11 @@ package com.hartwig.actin.report.pdf.tables.clinical
 
 object DateFunctions {
 
-    fun toDateString(maybeYear: Int?, maybeMonth: Int?, maybeDay: Int? = null): String? =
+    fun toDateString(year: Int?, month: Int?, day: Int? = null): String? =
         when {
-            maybeYear == null -> null
-            maybeMonth == null -> "%04d".format(maybeYear)
-            maybeDay == null -> "%04d-%02d".format(maybeYear, maybeMonth)
-            else -> "%04d-%02d-%02d".format(maybeYear, maybeMonth, maybeDay)
+            year == null -> null
+            month == null -> "%04d".format(year)
+            day == null -> "%04d-%02d".format(year, month)
+            else -> "%04d-%02d-%02d".format(year, month, day)
         }
 }
