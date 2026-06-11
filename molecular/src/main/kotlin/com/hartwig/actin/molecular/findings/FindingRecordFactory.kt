b3e53da41f61211c0bbc8befd005019d74ee444e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2026-06-11 16:52:02 +0200

    ACTIN-3667: Adjust packages and modules

--- molecular/src/main/kotlin/com/hartwig/actin/molecular/findings/FindingRecordFactory.kt
+++ molecular/src/main/kotlin/com/hartwig/actin/molecular/findings/FindingRecordFactory.kt
@@ -1,12 +1,12 @@
 package com.hartwig.actin.molecular.findings
 
 import com.hartwig.hmftools.datamodel.orange.OrangeRecord
-import com.hartwig.hmftools.finding.ConversionUtil
-import com.hartwig.hmftools.finding.datamodel.FindingRecord
+import com.hartwig.hmftools.findings.finding.ConversionUtil
+import com.hartwig.hmftools.findings.finding.datamodel.FindingRecord
 
 object FindingRecordFactory {
 
     fun create(orangeRecord: OrangeRecord): FindingRecord {
-        return ConversionUtil.orangeRecordToFindingRecord(orangeRecord, null, null, null)
+        return ConversionUtil. and morangeRecordToFindingRecord(orangeRecord, null, null, null)
     }
 }
