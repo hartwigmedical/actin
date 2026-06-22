55285bc8b15187da5bdb03b4642ddf3966e6bc07
Ned Leitch <48131072+nedleitch@users.noreply.github.com>
2026-06-22 16:58:54 -0400

    ACTIN-3751 Load gene specs from string not file (#1540)
    Move the opening of the file containing panel data out to
    `MolecularInterpreterInputData` to allow the methods in
    `PanelGeneSpecificationsFile` to continue to be used by clinical.
    
    Also drop the `File` suffix from `PanelGeneSpecificationsFile`.
    
    ---------
    
    Co-authored-by: Ned Leitch <ned.leitch@gmail.com>

--- common/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelGeneSpecifications.kt
+++ common/src/main/kotlin/com/hartwig/actin/molecular/panel/PanelGeneSpecifications.kt
@@ -8,24 +8,23 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.hartwig.actin.datamodel.molecular.panel.PanelTestSpecification
 import com.hartwig.actin.datamodel.molecular.panel.TestVersion
 import com.hartwig.actin.molecular.filter.GeneFilter
-import java.io.File
 
-object PanelGeneSpecificationsFile {
+object PanelGeneSpecifications {
 
-    fun parseEntries(panelGeneListTsvPath: String): List<PanelGeneEntry> {
+    fun parseEntries(panelGeneListTsvData: String): List<PanelGeneEntry> {
         return CsvMapper().apply {
             registerModule(KotlinModule.Builder().build())
             registerModule(JavaTimeModule())
             disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         }.readerFor(PanelGeneEntry::class.java)
-            .with(CsvSchema.emptySchema().withHeader().withColumnSeparator('\t')).readValues<PanelGeneEntry>(File(panelGeneListTsvPath))
+            .with(CsvSchema.emptySchema().withHeader().withColumnSeparator('\t')).readValues<PanelGeneEntry>(panelGeneListTsvData)
             .readAll()
     }
 
-    fun create(panelGeneListTsvPath: String, geneFilter: GeneFilter): PanelSpecifications {
+    fun create(panelGeneListTsvData: String, geneFilter: GeneFilter): PanelSpecifications {
         return PanelSpecifications(
             geneFilter,
-            parseEntries(panelGeneListTsvPath).groupBy(
+            parseEntries(panelGeneListTsvData).groupBy(
                 { PanelTestSpecification(it.testName, TestVersion(it.versionDate)) },
                 { it.toPanelGeneSpecification() })
         )
