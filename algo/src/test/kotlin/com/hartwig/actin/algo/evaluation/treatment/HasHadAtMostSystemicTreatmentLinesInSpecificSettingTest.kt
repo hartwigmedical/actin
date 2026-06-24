2a3177eb4eb102140930fd4988b2d2dc2da6eaa2
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2026-06-24 12:56:26 +0200

    ACTIN-3765: Add extra tests

--- algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest.kt
+++ algo/src/test/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest.kt
@@ -43,7 +43,8 @@ class HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest {
         )
         val evaluation = function.evaluate(record)
         assertEvaluation(EvaluationResult.FAIL, evaluation)
-        assertThat(evaluation.failMessagesStrings()).containsExactly("Has had more than 2 systemic treatment line(s) with palliative intent in metastatic setting (Treatment)")
+        assertThat(evaluation.failMessagesStrings()).containsExactly("Has had more than 2 systemic treatment line(s) with palliative " +
+                "intent in metastatic setting (Treatment)")
     }
 
     @Test
@@ -58,7 +59,8 @@ class HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest {
         )
         val evaluation = function.evaluate(record)
         assertEvaluation(EvaluationResult.FAIL, evaluation)
-        assertThat(evaluation.failMessagesStrings()).containsExactly("Likely exceeded maximum of 2 systemic treatment line(s) in metastatic setting (4 lines likely in metastatic setting) (Treatment)")
+        assertThat(evaluation.failMessagesStrings()).containsExactly("Likely exceeded maximum of 2 systemic treatment line(s) in " +
+                "metastatic setting (4 lines likely in metastatic setting) (Treatment)")
     }
 
     @Test
@@ -72,7 +74,43 @@ class HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest {
         )
         val evaluation = function.evaluate(record)
         assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
-        assertThat(evaluation.undeterminedMessagesStrings()).containsExactly("Uncertain whether maximum of 2 systemic treatment line(s) in metastatic setting is exceeded (3 lines likely in metastatic setting, setting unclear for some) (Treatment)")
+        assertThat(evaluation.undeterminedMessagesStrings()).containsExactly("Uncertain whether maximum of 2 systemic treatment line(s) " +
+                "in metastatic setting is exceeded (3 lines likely in metastatic setting, setting unclear for some) (Treatment)")
+    }
+
+    @Test
+    fun `Should be undetermined when non-palliative treatment with unknown stop date could push total over maximum`() {
+        val record = withTreatmentHistory(
+            listOf(
+                createTreatment(Intent.PALLIATIVE),
+                createTreatment(Intent.MAINTENANCE),
+                createTreatment(null)
+            )
+        )
+        val evaluation = function.evaluate(record)
+        assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
+        assertThat(evaluation.undeterminedMessagesStrings()).containsExactly(
+            "Uncertain whether maximum of 2 systemic treatment line(s) in metastatic setting is exceeded" +
+                    " (3 lines likely in metastatic setting, setting unclear for some) (Treatment)"
+        )
+    }
+
+    @Test
+    fun `Should fail when non-palliative treatments with unknown stop date push probable count above maximum plus one buffer`() {
+        val record = withTreatmentHistory(
+            listOf(
+                createTreatment(Intent.PALLIATIVE),
+                createTreatment(Intent.MAINTENANCE),
+                createTreatment(Intent.INDUCTION),
+                createTreatment(null)
+            )
+        )
+        val evaluation = function.evaluate(record)
+        assertEvaluation(EvaluationResult.FAIL, evaluation)
+        assertThat(evaluation.failMessagesStrings()).containsExactly(
+            "Likely exceeded maximum of 2 systemic treatment line(s) in metastatic setting" +
+                    " (4 lines likely in metastatic setting) (Treatment)"
+        )
     }
 
     @Test
@@ -86,7 +124,8 @@ class HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest {
         )
         val evaluation = function.evaluate(record)
         assertEvaluation(EvaluationResult.UNDETERMINED, evaluation)
-        assertThat(evaluation.undeterminedMessagesStrings()).containsExactly("Uncertain whether maximum of 2 systemic treatment line(s) in metastatic setting is exceeded (3 lines with non-excluded intent, setting unclear for older lines) (Treatment)")
+        assertThat(evaluation.undeterminedMessagesStrings()).containsExactly("Uncertain whether maximum of 2 systemic treatment line(s) " +
+                "in metastatic setting is exceeded (3 lines with non-excluded intent, setting unclear for older lines) (Treatment)")
     }
 
     @Test
@@ -112,8 +151,10 @@ class HasHadAtMostSystemicTreatmentLinesInSpecificSettingTest {
     }
 
     @Test
-    fun `Should pass when included lines are below the maximum`() {
-        val record = withTreatmentHistory(listOf(createTreatment(Intent.PALLIATIVE)))
+    fun `Should pass when included lines are equal to the maximum`() {
+        val record = withTreatmentHistory(
+            listOf(Intent.PALLIATIVE, Intent.PALLIATIVE).map { createTreatment(it) }
+        )
         assertEvaluation(EvaluationResult.PASS, function.evaluate(record))
     }
 
