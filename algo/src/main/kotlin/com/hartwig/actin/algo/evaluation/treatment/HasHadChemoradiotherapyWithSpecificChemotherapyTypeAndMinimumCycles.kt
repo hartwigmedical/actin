e0a881bf2f9294ff40dbc7a89b524d5a32a9acb7
Simon de Leeuw <149689385+spdeleeuw@users.noreply.github.com>
2026-08-25 09:03:16 +0200

    ACTIN-4047: Add rule HAS_HAD_CHEMORADIOTHERAPY_OF_TYPE_X_CHEMOTHERAPY

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/treatment/HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles.kt
@@ -7,7 +7,7 @@ import com.hartwig.actin.datamodel.algo.Evaluation
 import com.hartwig.actin.datamodel.clinical.treatment.TreatmentCategory
 import com.hartwig.actin.datamodel.clinical.treatment.TreatmentType
 
-class HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(private val type: TreatmentType, private val minCycles: Int) :
+class HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(private val type: TreatmentType, private val minCycles: Int?) :
     EvaluationFunction {
 
     override fun evaluate(record: PatientRecord): Evaluation {
@@ -16,7 +16,7 @@ class HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(privat
             val chemotherapies = it.allTreatments().filter { treatment -> treatment.categories().contains(TreatmentCategory.CHEMOTHERAPY) }
             val matchingChemoType = chemotherapies.any { t -> type in t.types() }
             val allChemoTypesAreKnown = chemotherapies.all { t -> t.types().isNotEmpty() }
-            val matchingCycles = it.treatmentHistoryDetails?.cycles?.let { cycles -> cycles >= minCycles }
+            val matchingCycles = if (minCycles == null) true else it.treatmentHistoryDetails?.cycles?.let { cycles -> cycles >= minCycles }
 
             when {
                 matchingCategories && matchingChemoType -> true to matchingCycles
@@ -30,18 +30,21 @@ class HasHadChemoradiotherapyWithSpecificChemotherapyTypeAndMinimumCycles(privat
         val typeString = type.display()
         return when {
             treatmentMatches.any { (match, cycles) -> match == true && cycles == true } -> EvaluationFactory.pass(
-                "Had received chemoradiotherapy with $typeString chemotherapy and at least $minCycles cycles"
+                "Had received chemoradiotherapy with $typeString chemotherapy${minCyclesString("and at least $minCycles cycles")}"
             )
 
             treatmentMatches.any { (match, cycles) -> match == true && cycles == false } -> EvaluationFactory.warn(
-                "Had received chemoradiotherapy with $typeString chemotherapy but with less than $minCycles cycles"
+                "Had received chemoradiotherapy with $typeString chemotherapy${minCyclesString("but with less than $minCycles cycles")}"
             )
 
             treatmentMatches.any { (match, cycles) -> match == null || (match && cycles == null) } -> EvaluationFactory.undetermined(
-                "Undetermined if patient received chemoradiotherapy with $typeString chemotherapy and at least $minCycles cycles"
+                "Undetermined if patient received chemoradiotherapy with $typeString chemotherapy" +
+                        minCyclesString("and at least $minCycles cycles")
             )
 
             else -> EvaluationFactory.fail("Has not received chemoradiotherapy with $typeString chemotherapy")
         }
     }
+
+    private fun minCyclesString(message: String) = minCycles?.let { " $message" } ?: ""
 }
