a3280bcf6a3a8f03d0c7a1f0ccf6d2f314164b3b
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2026-09-15 09:32:02 +0200

    ACTIN-4091: Take into account child/parent codes

--- algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasToxicityWithGrade.kt
+++ algo/src/main/kotlin/com/hartwig/actin/algo/evaluation/toxicity/HasToxicityWithGrade.kt
@@ -23,7 +23,12 @@ class HasToxicityWithGrade(
 
     override fun evaluate(record: PatientRecord): Evaluation {
         val ignoredIcdMainCodes = icdTitlesToIgnore.mapNotNull(icdModel::resolveCodeForTitle).map { it.mainCode }.toSet()
-        val relevantToxicities = ToxicityFunctions.selectRelevantToxicities(record, referenceDate, ignoredIcdMainCodes)
+        val relevantToxicities = ToxicityFunctions.selectRelevantToxicities(record, referenceDate)
+            .filterNot { toxicity ->
+                toxicity.icdCodes.any { code ->
+                    icdModel.codeWithAllParents(code.mainCode).any(ignoredIcdMainCodes::contains)
+                }
+            }
         val icdMatches = targetIcdTitles?.mapNotNull(icdModel::resolveCodeForTitle)?.toSet()?.let { targetCodes ->
             icdModel.findInstancesMatchingAnyIcdCode(relevantToxicities, targetCodes).fullMatches.toSet()
         }
